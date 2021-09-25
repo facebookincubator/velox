21a7654e17e5ad017c9532daad963f8a11677f0b
Darren Fu <kotime42@gmail.com>
2021-09-24 20:20:38 -0700

    sync with main

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -159,4 +159,7 @@ function install_velox_deps {
   else
     install_velox_deps
   fi
-)
+
+echo "All deps for Velox installed! Now try \"make\""
+echo 'To add cmake-format bin to your $PATH, consider adding this to your ~/.profile:'
+echo 'export PATH=$HOME/bin:$HOME/Library/Python/3.7/bin:$PATH')
