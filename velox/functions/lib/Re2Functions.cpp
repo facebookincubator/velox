71f2553e80f8ec1114abaa80ae57da51d7466e07
Aditi Pandit <aditi@ahana.io>
2021-10-05 18:22:34 -0700

    Minor fix for review comment

--- velox/functions/lib/Re2Functions.cpp
+++ velox/functions/lib/Re2Functions.cpp
@@ -344,7 +344,7 @@ class Re2SearchAndExtract final : public VectorFunction {
 
 class LikeConstantPattern final : public VectorFunction {
  public:
-  explicit LikeConstantPattern(StringView pattern, char escapeChar)
+   LikeConstantPattern(StringView pattern, char escapeChar)
       : re_(toStringPiece(likePatternToRe2(pattern, escapeChar)), RE2::Quiet) {}
 
   void apply(
