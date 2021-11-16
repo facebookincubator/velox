b1c7de20c5a782f86ad4ffbff92039c4bab760b4
Michael Shang <mikesh@fb.com>
2021-11-16 03:16:15 -0800

    Fine tune the regex

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -102,11 +102,11 @@ function update_brew {
 function install_build_prerequisites {
   for pkg in ${MACOS_DEPS}
   do
-    if [[ "${pkg}" =~ ([0-9a-z-]*):([0-9](\.[0-9\])*) ]];
+    if [[ "${pkg}" =~ ^([0-9a-z-]*):([0-9](\.[0-9\])*)$ ]];
     then
       pkg=${BASH_REMATCH[1]}
       ver=${BASH_REMATCH[2]}
-      echo "Installing ${pkg} at ${ver}"
+      echo "Installing '${pkg}' at '${ver}'"
       tap="velox/local-${pkg}"
       brew tap-new "${tap}"
       brew extract "--version=${ver}" "${pkg}" "${tap}"
