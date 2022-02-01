c6ead28ea64b5690be43dea8b1d1e86cce0a8c74
Michael Shang <mikesh@fb.com>
2022-01-31 23:39:14 -0800

    Set FOLLY_HAVE_INT128_T=ON for macos

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -137,7 +137,7 @@ function install_double_conversion {
 function install_folly {
   github_checkout facebook/folly "${FB_OS_VERSION}"
   OPENSSL_ROOT_DIR=$(brew --prefix openssl@1.1) \
-    cmake_install -DBUILD_TESTS=OFF
+    cmake_install -DBUILD_TESTS=OFF -DFOLLY_HAVE_INT128_T=ON
 }
 
 function install_ranges_v3 {
