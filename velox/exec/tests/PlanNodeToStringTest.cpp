bec4a38fa22069d5fa4b5f36224f94b646de60f2
Deepak Majeti <deepak@ahana.io>
2022-03-23 12:12:07 -0400

    add test

--- velox/exec/tests/PlanNodeToStringTest.cpp
+++ velox/exec/tests/PlanNodeToStringTest.cpp
@@ -14,12 +14,18 @@
  * limitations under the License.
  */
 #include <gtest/gtest.h>
+#include "velox/exec/tests/utils/HiveConnectorTestBase.h"
 #include "velox/exec/tests/utils/PlanBuilder.h"
 #include "velox/functions/prestosql/registration/RegistrationFunctions.h"
 #include "velox/parse/TypeResolver.h"
+#include "velox/type/tests/FilterBuilder.h"
+#include "velox/type/tests/SubfieldFiltersBuilder.h"
 #include "velox/vector/tests/VectorTestBase.h"
 
 using namespace facebook::velox;
+using namespace facebook::velox::common::test;
+using namespace facebook::velox::exec::test;
+
 using facebook::velox::exec::test::PlanBuilder;
 
 class PlanNodeToStringTest : public testing::Test, public test::VectorTestBase {
@@ -407,4 +413,30 @@ TEST_F(PlanNodeToStringTest, exchange) {
 
   ASSERT_EQ("-> Exchange\n", plan->toString());
   ASSERT_EQ("-> Exchange[]\n", plan->toString(true, false));
+}
+
+TEST_F(PlanNodeToStringTest, tableScan) {
+  const auto shipDate = "shipdate";
+  const auto lineitem = "lineitem";
+  RowTypePtr rowType{
+      ROW({"discount", "tax", "shipdate"}, {DOUBLE(), DOUBLE(), VARCHAR()})};
+  ColumnHandleMap assignments = {
+      {"discount", HiveConnectorTestBase::regularColumn("discount", DOUBLE())},
+      {"tax", HiveConnectorTestBase::regularColumn("tax", DOUBLE())},
+      {"shipdate",
+       HiveConnectorTestBase::regularColumn("shipdate", VARCHAR())}};
+  auto filters = SubfieldFiltersBuilder()
+                     .add(shipDate, between("1994-01-01", "1994-12-31"))
+                     .add("l_discount", betweenDouble(0.05, 0.07))
+                     .add("l_quantity", lessThanDouble(24.0))
+                     .build();
+  auto tableHandle = std::make_shared<connector::hive::HiveTableHandle>(
+      lineitem, true, std::move(filters), nullptr);
+  auto plan =
+      PlanBuilder().tableScan(rowType, tableHandle, assignments).planNode();
+
+  ASSERT_EQ("-> TableScan\n", plan->toString());
+  ASSERT_EQ(
+      "-> TableScan[Name: lineitem, Filters: [(l_quantity, DoubleRange: (-inf, 24.000000) no nulls), (l_discount, DoubleRange: [0.050000, 0.070000] no nulls), (shipdate, BytesRange: [1994-01-01, 1994-12-31] no nulls)]]\n",
+      plan->toString(true, false));
 }
