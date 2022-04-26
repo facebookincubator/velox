86661002192fad6cf34523a20704d7f7eab6f265
Orri Erling <oerling@fb.com>
2022-04-25 21:08:41 -0700

    Fix lazy load of children of lazy struct
    A top level struct will make a lazy vector. A struct inside this will
    nmake a lazy vector if the top level struct did not have any nulls.
    
    When a lazy struct is loaded its children will be lazy if they are
    aligned with top level rows, e.g. intervening structs do not add
    nulls. When loading these children, the immediately containing struct
    must have its 'outputRows_' set. These must be the row set for which
    the lazy struct was loaded. Added
    SelectiveStructColumnReader::loadableRows.
    
    In E2EFiltertest, we add a test with multiple levels of nested structs and change the makeNotNull method to be recursive, ensure no nulls throughout a tree of structs. We then make the lazy branch of the test to make vectors recursively lazy. lazy structs will result in lazy struct members as long as the structs do not add nulls, i.e. all row numbers are aligned with top level rows.
    
    In SelectiveStructColumnReader::seekTo, after advancing field readers,
    set the current row recursively and not just for the immediate child.
    Deeper nested column readers may advance less in their data stream due
    to parents being null. Notwithstanding this, they must be set after
    advance to the same top level row number as the top struct
    reader. Failure to do this will advance them extra if the next batch
    starts at anything except its row 0.

--- velox/dwio/dwrf/reader/SelectiveStructColumnReader.h
+++ velox/dwio/dwrf/reader/SelectiveStructColumnReader.h
@@ -29,6 +29,10 @@ class SelectiveStructColumnReader : public SelectiveColumnReader {
       common::ScanSpec* scanSpec,
       FlatMapContext flatMapContext);
 
+  ~SelectiveStructColumnReader() {
+    numReads_ = 0xf7eedf7eed;
+  }
+
   void resetFilterCaches() override {
     for (auto& child : children_) {
       child->resetFilterCaches();
@@ -112,6 +116,16 @@ class SelectiveStructColumnReader : public SelectiveColumnReader {
         child->setIsTopLevel();
       }
     }
+  }
+
+  // Sets 'rows' as the set of rows for which 'this' or its children
+  // may be loaded as LazyVectors. When a struct is loaded as lazy,
+  // its children will be lazy if the struct does not add nulls. The
+  // children will reference the struct reader, whih must have a live
+  // and up-to-date set of rows for which children can be loaded.
+  void setLoadableRows(RowSet rows) {
+    setOutputRows(rows);
+    inputRows_ = outputRows_;
   }
 
  private:
