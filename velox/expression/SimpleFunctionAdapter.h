051901d819abb010e76abe4f6359e8bc5318cbbb
Laith Sakka <lsakka@fb.com>
2022-04-27 16:46:21 -0700

    split
    Differential Revision: D35908921
    
    fbshipit-source-id: 5eb99dac1c0fdba65b1c23d33fb3e446dc6caea2

--- velox/expression/SimpleFunctionAdapter.h
+++ velox/expression/SimpleFunctionAdapter.h
@@ -22,8 +22,8 @@
 #include "velox/expression/ComplexWriterTypes.h"
 #include "velox/expression/Expr.h"
 #include "velox/expression/VectorFunction.h"
-#include "velox/expression/VectorUdfTypeSystem.h"
-#include "velox/vector/VectorTypeUtils.h"
+#include "velox/expression/VectorReaders.h"
+#include "velox/expression/VectorWriters.h"
 
 namespace facebook::velox::exec {
 
