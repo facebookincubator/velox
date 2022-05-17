5f8f0b5deeed8d42e4907855aac75951a847e003
usurai <crowdwei@gmail.com>
2022-05-17 22:50:52 +0800

    1. Seperate setup and execution logic 2. Change test vector / bucket size

--- velox/connectors/hive/benchmarks/HivePartitionFunctionBenchmark.cpp
+++ velox/connectors/hive/benchmarks/HivePartitionFunctionBenchmark.cpp
@@ -20,125 +20,291 @@
 #include "velox/type/Type.h"
 #include "velox/vector/ComplexVector.h"
 #include "velox/vector/fuzzer/VectorFuzzer.h"
+#include "velox/vector/tests/VectorMaker.h"
 
-namespace facebook::velox::test {
+DEFINE_int64(fuzzer_seed, 99887766, "Seed for random input dataset generator");
+
+using namespace facebook::velox;
+using namespace facebook::velox::test;
+using connector::hive::HivePartitionFunction;
+
 namespace {
 
-void BM_HivePartFunc(
-    size_t,
-    size_t vectorSize,
-    size_t bucketCount,
-    const TypePtr& type,
-    size_t stringLength = 0) {
-  functions::test::FunctionBenchmarkBase benchmarkBase;
-  folly::BenchmarkSuspender suspender;
+constexpr std::array<TypeKind, 10> supportedTypes {
+  TypeKind::BOOLEAN,
+  TypeKind::TINYINT,
+  TypeKind::SMALLINT,
+  TypeKind::INTEGER,
+  TypeKind::BIGINT,
+  TypeKind::REAL,
+  TypeKind::DOUBLE,
+  TypeKind::VARCHAR,
+  TypeKind::TIMESTAMP,
+  TypeKind::DATE
+};
 
-  VectorFuzzer::Options opts;
-  opts.vectorSize = vectorSize;
-  opts.stringLength = stringLength;
-  VectorFuzzer fuzzer(opts, benchmarkBase.pool());
-  auto vector = fuzzer.fuzzRow(ROW({type}));
+constexpr size_t smallVectorSize = 1'000;
+constexpr size_t largeVectorSize = 10'000;
+constexpr size_t smallBucketCount = 20;
+constexpr size_t largeBucketCount = 100;
 
-  std::vector<int> bucketToPartition(bucketCount);
-  std::vector<ChannelIndex> keyChannels;
-  std::vector<uint32_t> partitions(vectorSize);
+class HivePartitionFunctionBenchmark
+  : public functions::test::FunctionBenchmarkBase {
+public:
+  explicit HivePartitionFunctionBenchmark(
+      size_t vectorSize,
+      size_t smallBucketCount,
+      size_t largeBucketCount,
+      size_t stringLength = 20)
+    : FunctionBenchmarkBase() {
+    // Prepare input data
+    VectorFuzzer::Options opts;
+    opts.vectorSize = vectorSize;
+    opts.stringLength = stringLength;
+    VectorFuzzer fuzzer(opts, pool(), FLAGS_fuzzer_seed);
+    VectorMaker vm{pool_.get()};
+    for (auto typeKind : supportedTypes) {
+      auto flatVector = fuzzer.fuzzFlat(createScalarType(typeKind));
+      auto rowVecotr = vm.rowVector({flatVector});
+      rowVectors_[typeKind] = std::move(rowVecotr);
+    }
 
-  std::iota(bucketToPartition.begin(), bucketToPartition.end(), 0);
-  keyChannels.emplace_back(0);
-  connector::hive::HivePartitionFunction partitionFunction(
-      bucketCount, bucketToPartition, keyChannels);
+    // Prepare HivePartitionFunction
+    smallBucketFunction_ = createHivePartitionFunction(smallBucketCount);
+    largeBucketFunction_ = createHivePartitionFunction(largeBucketCount);
 
-  suspender.dismiss();
+    partitions_.resize(vectorSize);
+  }
 
-  partitionFunction.partition(
-      *reinterpret_cast<RowVector*>(vector.get()), partitions);
+  template<TypeKind KIND>
+  void run(HivePartitionFunction* function) {
+    function->partition(*rowVectors_[KIND], partitions_);
+  }
+
+  template<TypeKind KIND>
+  void runSmall() {
+    run<KIND>(smallBucketFunction_.get());
+  }
+
+  template<TypeKind KIND>
+  void runLarge() {
+    run<KIND>(largeBucketFunction_.get());
+  }
+
+private:
+  std::unique_ptr<HivePartitionFunction> createHivePartitionFunction(
+      size_t bucketCount) {
+    std::vector<int> bucketToPartition(bucketCount);
+    std::iota(bucketToPartition.begin(), bucketToPartition.end(), 0);
+    std::vector<ChannelIndex> keyChannels;
+    keyChannels.emplace_back(0);
+    return std::make_unique<HivePartitionFunction>(
+        bucketCount,
+        bucketToPartition,
+        keyChannels);
+  }
+
+  std::unordered_map<TypeKind, RowVectorPtr> rowVectors_;
+  std::unique_ptr<HivePartitionFunction> smallBucketFunction_;
+  std::unique_ptr<HivePartitionFunction> largeBucketFunction_;
+  std::vector<uint32_t> partitions_;
+};
+
+std::unique_ptr<HivePartitionFunctionBenchmark> benchmarkSmall;
+std::unique_ptr<HivePartitionFunctionBenchmark> benchmarkLarge;
+
+BENCHMARK(booleanSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::BOOLEAN>();
+}
+
+BENCHMARK_RELATIVE(booleanSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::BOOLEAN>();
+}
+
+BENCHMARK(booleanLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::BOOLEAN>();
+}
+
+BENCHMARK_RELATIVE(booleanLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::BOOLEAN>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(tinyintSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::TINYINT>();
+}
+
+BENCHMARK_RELATIVE(tinyintSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::TINYINT>();
+}
+
+BENCHMARK(tinyintLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::TINYINT>();
+}
+
+BENCHMARK_RELATIVE(tinyintLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::TINYINT>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(smallintSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::SMALLINT>();
+}
+
+BENCHMARK_RELATIVE(smallintSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::SMALLINT>();
+}
+
+BENCHMARK(smallintLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::SMALLINT>();
+}
+
+BENCHMARK_RELATIVE(smallintLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::SMALLINT>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(integerSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::INTEGER>();
+}
+
+BENCHMARK_RELATIVE(integerSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::INTEGER>();
+}
+
+BENCHMARK(integerLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::INTEGER>();
+}
+
+BENCHMARK_RELATIVE(integerLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::INTEGER>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(bigintSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::BIGINT>();
+}
+
+BENCHMARK_RELATIVE(bigintSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::BIGINT>();
+}
+
+BENCHMARK(bigintLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::BIGINT>();
+}
+
+BENCHMARK_RELATIVE(bigintLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::BIGINT>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(realSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::REAL>();
+}
+
+BENCHMARK_RELATIVE(realSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::REAL>();
+}
+
+BENCHMARK(realLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::REAL>();
+}
+
+BENCHMARK_RELATIVE(realLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::REAL>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(doubleSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::DOUBLE>();
+}
+
+BENCHMARK_RELATIVE(doubleSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::DOUBLE>();
+}
+
+BENCHMARK(doubleLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::DOUBLE>();
+}
+
+BENCHMARK_RELATIVE(doubleLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::DOUBLE>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(varcharSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::VARCHAR>();
+}
+
+BENCHMARK_RELATIVE(varcharSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::VARCHAR>();
+}
+
+BENCHMARK(varcharLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::VARCHAR>();
+}
+
+BENCHMARK_RELATIVE(varcharLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::VARCHAR>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(timestampSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::TIMESTAMP>();
+}
+
+BENCHMARK_RELATIVE(timestampSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::TIMESTAMP>();
+}
+
+BENCHMARK(timestampLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::TIMESTAMP>();
+}
+
+BENCHMARK_RELATIVE(timestampLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::TIMESTAMP>();
+}
+
+BENCHMARK_DRAW_LINE();
+
+BENCHMARK(dateSmallRowsSmallBuckets) {
+  benchmarkSmall->runSmall<TypeKind::DATE>();
+}
+
+BENCHMARK_RELATIVE(dateSmallRowsLargeBuckets) {
+  benchmarkSmall->runLarge<TypeKind::DATE>();
+}
+
+BENCHMARK(dateLargeRowsSmallBuckets) {
+  benchmarkLarge->runSmall<TypeKind::DATE>();
+}
+
+BENCHMARK_RELATIVE(dateLargeRowsLargeBuckets) {
+  benchmarkLarge->runLarge<TypeKind::DATE>();
 }
 
 } // namespace
 
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    BOOLEAN_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    BOOLEAN());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    TINYINT_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    TINYINT());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    SMALLINT_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    SMALLINT());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    INTEGER_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    INTEGER());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    BIGINT_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    BIGINT());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    REAL_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    REAL());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    DOUBLE_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    DOUBLE());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    TIMESTAMP_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    TIMESTAMP());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    DATE_10M_ROWS_1K_BUCKETS,
-    10'000'000,
-    1'000,
-    DATE());
-
-BENCHMARK_NAMED_PARAM(
-    BM_HivePartFunc,
-    VARCHAR_10M_ROWS_1K_BUCKETS_20,
-    10'000'000,
-    1'000,
-    VARCHAR(),
-    20);
-
-BENCHMARK_RELATIVE_NAMED_PARAM(
-    BM_HivePartFunc,
-    VARCHAR_10M_ROWS_1K_BUCKETS_40,
-    10'000'000,
-    1'000,
-    VARCHAR(),
-    40);
-
-} // namespace facebook::velox::test
-
 int main(int argc, char** argv) {
-  folly::init(&argc, &argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  benchmarkSmall = std::make_unique<HivePartitionFunctionBenchmark>(
+      smallVectorSize, smallBucketCount, largeBucketCount);
+  benchmarkLarge = std::make_unique<HivePartitionFunctionBenchmark>(
+      largeVectorSize, smallBucketCount, largeBucketCount);
+
   folly::runBenchmarks();
+
+  benchmarkSmall.reset();
+  benchmarkLarge.reset();
+
   return 0;
 }
