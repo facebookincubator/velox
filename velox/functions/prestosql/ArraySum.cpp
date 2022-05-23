fa2435a7bad8fbde4cdc966fbb38ed6d309465ca
James Wyles <jwyles@ahana.io>
2022-05-23 15:44:27 -0600

    Responding to reviewers

--- velox/functions/prestosql/ArraySum.cpp
+++ velox/functions/prestosql/ArraySum.cpp
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) Facebook, Inc. and its affiliates.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "velox/expression/EvalCtx.h"
 #include "velox/expression/Expr.h"
@@ -29,14 +29,14 @@ namespace {
 ///
 template <typename IT, typename OT>
 class ArraySumFunction : public exec::VectorFunction {
-public:
- // Execute function.
- void apply(
-     const SelectivityVector& rows,
-     std::vector<VectorPtr>& args, // Not using const ref so we can reuse args
-     const TypePtr& outputType,
-     exec::EvalCtx* context,
-     VectorPtr* result) const override;
+ public:
+  // Execute function.
+  void apply(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args, // Not using const ref so we can reuse args
+      const TypePtr& outputType,
+      exec::EvalCtx* context,
+      VectorPtr* result) const override;
 };
 
 template <typename IT, typename OT>
@@ -50,28 +50,19 @@ void ArraySumFunction<IT, OT>::apply(
   auto arrayVector = args[0]->as<ArrayVector>();
   VELOX_CHECK(arrayVector);
   auto elementsVector = arrayVector->elements();
-
-  auto elementsRows =
-      toElementRows(elementsVector->size(), rows, arrayVector);
+  auto elementsRows = toElementRows(elementsVector->size(), rows, arrayVector);
   exec::LocalDecodedVector elements(context, *elementsVector, elementsRows);
-
   vector_size_t numRows = arrayVector->size();
 
   // Allocate new vector for the result
   memory::MemoryPool* pool = context->pool();
   auto resultVector = BaseVector::create(outputType, numRows, pool);
+  OT* resultValues = (OT*)resultVector->valuesAsVoid();
 
-
-  // Get access to raw values for the result
-  OT* resultValues = (OT*) resultVector->valuesAsVoid();
-
-  // Iterate over the input vector and find the sum of each array's values
   for (int i = 0; i < numRows; i++) {
-    // If the whole array is null then set the row null in the output
     if (arrayVector->isNullAt(i)) {
       resultVector->setNull(i, true);
     }
-    // If the array is not null then sum the elements and set the result to the sum
     else {
       int start = arrayVector->offsetAt(i);
       int end = start + arrayVector->sizeAt(i);
@@ -82,8 +73,6 @@ void ArraySumFunction<IT, OT>::apply(
           sum += elements->template valueAt<IT>(start);
         }
       }
-
-      // Set the value at i equal to the sum
       resultValues[i] = sum;
     }
   }
@@ -91,6 +80,9 @@ void ArraySumFunction<IT, OT>::apply(
   context->moveOrCopyResult(resultVector, rows, result);
 }
 
+// The following specializations are required so things will compile, they
+// are never used, the compiler thinks they will be because of the way we
+// do dynamic dispatching.
 template <>
 void ArraySumFunction<Timestamp, int64_t>::apply(
     const SelectivityVector& rows,
@@ -141,82 +133,85 @@ void ArraySumFunction<Date, double>::apply(
 
 // Validate number of parameters and types.
 void validateType(const std::vector<exec::VectorFunctionArg>& inputArgs) {
- VELOX_USER_CHECK_EQ(
-     inputArgs.size(), 1, "array_sum requires exactly one parameter");
+  VELOX_USER_CHECK_EQ(
+      inputArgs.size(), 1, "array_sum requires exactly one parameter");
 
- auto arrayType = inputArgs.front().type;
- VELOX_USER_CHECK_EQ(
-     arrayType->kind(),
-     TypeKind::ARRAY,
-     "array_sum requires argument of type ARRAY");
+  auto arrayType = inputArgs.front().type;
+  VELOX_USER_CHECK_EQ(
+      arrayType->kind(),
+      TypeKind::ARRAY,
+      "array_sum requires argument of type ARRAY");
+
+  auto valueTypeKind = inputArgs.front().type->childAt(0)->kind();
+  bool isCoercibleToDouble = valueTypeKind == TypeKind::TINYINT
+      || valueTypeKind == TypeKind::SMALLINT || valueTypeKind == TypeKind::INTEGER
+      || valueTypeKind == TypeKind::BIGINT || valueTypeKind == TypeKind::REAL
+      || valueTypeKind == TypeKind::DOUBLE;
+  VELOX_USER_CHECK_EQ(isCoercibleToDouble, true, "Invalid value type");
 }
 
 // Create function template based on type.
 template <TypeKind kind>
 std::shared_ptr<exec::VectorFunction> createTyped(
-   const std::vector<exec::VectorFunctionArg>& inputArgs) {
- VELOX_CHECK_EQ(inputArgs.size(), 1);
+    const std::vector<exec::VectorFunctionArg>& inputArgs) {
+  VELOX_CHECK_EQ(inputArgs.size(), 1);
 
- using IT = typename TypeTraits<kind>::NativeType;
- if (kind == TypeKind::TINYINT || kind == TypeKind::SMALLINT
-     || kind == TypeKind::INTEGER || kind == TypeKind::BIGINT) {
-   return std::make_shared<ArraySumFunction<IT, int64_t>>();
- }
- if (kind == TypeKind::REAL || kind == TypeKind::DOUBLE) {
-   return std::make_shared<ArraySumFunction<IT, double>>();
- }
- VELOX_FAIL()
+  using IT = typename TypeTraits<kind>::NativeType;
+  if (kind == TypeKind::TINYINT || kind == TypeKind::SMALLINT ||
+      kind == TypeKind::INTEGER || kind == TypeKind::BIGINT) {
+    return std::make_shared<ArraySumFunction<IT, int64_t>>();
+  }
+  if (kind == TypeKind::REAL || kind == TypeKind::DOUBLE) {
+    return std::make_shared<ArraySumFunction<IT, double>>();
+  }
+  VELOX_FAIL("Unsupported type")
 }
 
 // Create function.
 std::shared_ptr<exec::VectorFunction> create(
-   const std::string& /* name */,
-   const std::vector<exec::VectorFunctionArg>& inputArgs) {
- validateType(inputArgs);
- auto elementType = inputArgs.front().type->childAt(0);
+    const std::string& /* name */,
+    const std::vector<exec::VectorFunctionArg>& inputArgs) {
+  validateType(inputArgs);
+  auto elementType = inputArgs.front().type->childAt(0);
 
- return VELOX_DYNAMIC_SCALAR_TYPE_DISPATCH(
-     createTyped, elementType->kind(), inputArgs);
+  return VELOX_DYNAMIC_SCALAR_TYPE_DISPATCH(
+      createTyped, elementType->kind(), inputArgs);
 }
 
 // Define function signature.
-// array(T1) -> T2 where T must be coercible to bigint or double, and
+// array(T1) -> T2 where T1 must be coercible to bigint or double, and
 // T2 is bigint or double
 std::vector<std::shared_ptr<exec::FunctionSignature>> signatures() {
- return {
-     exec::FunctionSignatureBuilder()
-         .returnType("bigint")
-         .argumentType("array(tinyint)")
-         .build(),
-     exec::FunctionSignatureBuilder()
-         .returnType("bigint")
-         .argumentType("array(smallint)")
-         .build(),
-     exec::FunctionSignatureBuilder()
-         .returnType("bigint")
-         .argumentType("array(integer)")
-         .build(),
-     exec::FunctionSignatureBuilder()
-         .returnType("bigint")
-         .argumentType("array(bigint)")
-         .build(),
-     exec::FunctionSignatureBuilder()
-         .returnType("double")
-         .argumentType("array(real)")
-         .build(),
-     exec::FunctionSignatureBuilder()
-         .returnType("double")
-         .argumentType("array(double)")
-         .build()};
+  return {
+      exec::FunctionSignatureBuilder()
+          .returnType("bigint")
+          .argumentType("array(tinyint)")
+          .build(),
+      exec::FunctionSignatureBuilder()
+          .returnType("bigint")
+          .argumentType("array(smallint)")
+          .build(),
+      exec::FunctionSignatureBuilder()
+          .returnType("bigint")
+          .argumentType("array(integer)")
+          .build(),
+      exec::FunctionSignatureBuilder()
+          .returnType("bigint")
+          .argumentType("array(bigint)")
+          .build(),
+      exec::FunctionSignatureBuilder()
+          .returnType("double")
+          .argumentType("array(real)")
+          .build(),
+      exec::FunctionSignatureBuilder()
+          .returnType("double")
+          .argumentType("array(double)")
+          .build()};
 }
 
 } // namespace
 
 // Register function.
-VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(
-   udf_array_sum,
-   signatures(),
-   create);
+VELOX_DECLARE_STATEFUL_VECTOR_FUNCTION(udf_array_sum, signatures(), create);
 
 } // namespace facebook::velox::functions
-
--- velox/functions/prestosql/benchmarks/ArraySumBenchmark.cpp
+++ velox/functions/prestosql/benchmarks/ArraySumBenchmark.cpp
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) Facebook, Inc. and its affiliates.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <folly/Benchmark.h>
 #include <folly/init/Init.h>
 #include "velox/functions/Macros.h"
@@ -27,9 +27,7 @@ namespace {
 
 template <typename IT, typename OT>
 VELOX_UDF_BEGIN(array_sum)
-FOLLY_ALWAYS_INLINE bool call(
-    OT& out,
-    const arg_type<Array<IT>>& array) {
+FOLLY_ALWAYS_INLINE bool call(OT& out, const arg_type<Array<IT>>& array) {
   if (array.mayHaveNulls()) {
     bool allNulls = true;
     OT sum = 0;
@@ -65,8 +63,7 @@ class ArrayContainsBenchmark : public functions::test::FunctionBenchmarkBase {
     registerFunction<
         udf_array_sum<int32_t, int64_t>,
         int64_t,
-        facebook::velox::Array<int32_t>
-        >({"array_sum_alt"});
+        facebook::velox::Array<int32_t>>({"array_sum_alt"});
   }
 
   void runInteger(const std::string& functionName) {
