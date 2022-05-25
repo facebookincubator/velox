d68b7b39dbb9aba35c5c5e881a056ec254bf660e
Karteek Murthy Samba Murthy <karteek.murthy06@gmail.com>
2022-05-25 12:32:12 -0700

    Add FlatVector support for Decimal Type (#1572)
    Summary:
    Add support for Long and Short Decimal FlatVector.
    1. Wrapped int128_t in a new value type LongDecimal.
    2. Extended ByteStream::read function for LongDecimal for de-serialization.
    
    **Testing:**
    Added VectorTest::createDecimal tests that creates FlatVectors of Short and Long decimal.
    Serialization and dictionary encoding are tested as part of this test.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/1572
    
    Reviewed By: amitkdutta
    
    Differential Revision: D36557565
    
    Pulled By: Yuhta
    
    fbshipit-source-id: d8a3a93a6ca7fead182e0d7c97241ffe9fc1f03d

--- velox/type/LongDecimal.h
+++ velox/type/LongDecimal.h
@@ -16,57 +16,58 @@
 #include <folly/dynamic.h>
 #include <sstream>
 #include <string>
+#include "velox/common/base/BitUtil.h"
 #include "velox/common/base/Exceptions.h"
 #include "velox/type/StringView.h"
 
 #pragma once
 
+#define BUILD_INT128(X, Y) ((int128_t)(X) << 64 | Y)
+
 namespace facebook::velox {
 
-struct ShortDecimal {
+using int128_t = __int128_t;
+
+struct LongDecimal {
  public:
   // Default required for creating vector with NULL values.
-  ShortDecimal() = default;
-  constexpr explicit ShortDecimal(int64_t value) : unscaledValue_(value) {}
+  LongDecimal() = default;
+  constexpr explicit LongDecimal(int128_t value) : unscaledValue_(value) {}
 
-  int64_t unscaledValue() const {
+  int128_t unscaledValue() const {
     return unscaledValue_;
   }
 
-  bool operator==(const ShortDecimal& other) const {
+  bool operator==(const LongDecimal& other) const {
     return unscaledValue_ == other.unscaledValue_;
   }
 
-  bool operator!=(const ShortDecimal& other) const {
+  bool operator!=(const LongDecimal& other) const {
     return unscaledValue_ != other.unscaledValue_;
   }
 
-  bool operator<(const ShortDecimal& other) const {
+  bool operator<(const LongDecimal& other) const {
     return unscaledValue_ < other.unscaledValue_;
   }
 
-  bool operator<=(const ShortDecimal& other) const {
+  bool operator<=(const LongDecimal& other) const {
     return unscaledValue_ <= other.unscaledValue_;
   }
 
-  bool operator>(const ShortDecimal& other) const {
-    return unscaledValue_ > other.unscaledValue_;
-  }
-
-  bool operator>=(const ShortDecimal& other) const {
-    return unscaledValue_ >= other.unscaledValue_;
-  }
-
  private:
-  int64_t unscaledValue_;
-};
+  int128_t unscaledValue_;
+}; // struct LongDecimal
 } // namespace facebook::velox
 
 namespace folly {
 template <>
-struct hasher<::facebook::velox::ShortDecimal> {
-  size_t operator()(const ::facebook::velox::ShortDecimal& value) const {
-    return std::hash<int64_t>{}(value.unscaledValue());
+struct hasher<::facebook::velox::LongDecimal> {
+  size_t operator()(const ::facebook::velox::LongDecimal& value) const {
+    auto upperHash = folly::hasher<uint64_t>{}(
+        static_cast<uint64_t>(value.unscaledValue() >> 64));
+    auto lowerHash =
+        folly::hasher<uint64_t>{}(static_cast<uint64_t>(value.unscaledValue()));
+    return facebook::velox::bits::hashMix(upperHash, lowerHash);
   }
 };
 } // namespace folly
