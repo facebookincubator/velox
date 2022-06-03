35cb1e23f25d326c46dee9d291733c03cb726d05
xuedongluan <luanxuedong2009@gmail.com>
2022-06-03 14:46:55 +0800

    fix format check error

--- velox/functions/prestosql/registration/DateTimeFunctionsRegistration.cpp
+++ velox/functions/prestosql/registration/DateTimeFunctionsRegistration.cpp
@@ -39,7 +39,8 @@ void registerSimpleFunctions() {
   registerFunction<MonthFunction, int64_t, TimestampWithTimezone>({"month"});
   registerFunction<WeekFunction, int64_t, Timestamp>({"week", "week_of_year"});
   registerFunction<WeekFunction, int64_t, Date>({"week", "week_of_year"});
-  registerFunction<WeekFunction, int64_t, TimestampWithTimezone>({"week", "week_of_year"});
+  registerFunction<WeekFunction, int64_t, TimestampWithTimezone>(
+      {"week", "week_of_year"});
   registerFunction<DayFunction, int64_t, Timestamp>({"day", "day_of_month"});
   registerFunction<DayFunction, int64_t, Date>({"day", "day_of_month"});
   registerFunction<DateMinusIntervalDayTime, Date, Date, IntervalDayTime>(
