6fb2f1d184fd2f7e7a26988d666367ab5cd211b0
yingsu00 <yingsu00@outlook.com>
2022-06-04 18:58:09 -0700

    Put bitwise_cast() to duckdb_apache::thrift namespace

--- velox/external/duckdb/parquet-amalgamation.hpp
+++ velox/external/duckdb/parquet-amalgamation.hpp
@@ -1261,6 +1261,7 @@ protected:
 #include <vector>
 #include <climits>
 
+namespace duckdb_apache::thrift::protocol {
 // Use this to get around strict aliasing rules.
 // For example, uint64_t i = bitwise_cast<uint64_t>(returns_double());
 // The most obvious implementation is to just cast a pointer,
@@ -1299,6 +1300,7 @@ static inline To bitwise_cast(From from) {
   return u.t;
 }
 
+}
 
 #ifdef HAVE_SYS_PARAM_H
 #include <sys/param.h>
@@ -5681,7 +5683,7 @@ uint32_t TCompactProtocolT<Transport_>::writeDouble(const double dub) {
   //BOOST_STATIC_ASSERT(sizeof(double) == sizeof(uint64_t));
   //BOOST_STATIC_ASSERT(std::numeric_limits<double>::is_iec559);
 
-  uint64_t bits = bitwise_cast<uint64_t>(dub);
+  uint64_t bits = duckdb_apache::thrift::protocol::bitwise_cast<uint64_t>(dub);
   bits = THRIFT_htolell(bits);
   trans_->write((uint8_t*)&bits, 8);
   return 8;
@@ -6087,7 +6089,7 @@ uint32_t TCompactProtocolT<Transport_>::readDouble(double& dub) {
   } u;
   trans_->readAll(u.b, 8);
   u.bits = THRIFT_letohll(u.bits);
-  dub = bitwise_cast<double>(u.bits);
+  dub = duckdb_apache::thrift::protocol::bitwise_cast<double>(u.bits);
   return 8;
 }
 
