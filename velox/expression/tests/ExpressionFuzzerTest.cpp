b94a4db33c82e4e3d70056d86ecc732934b352ec
Pedro Eugenio Rocha Pedreira <pedroerp@fb.com>
2022-07-07 08:15:27 -0700

    Use random seed by default
    Summary:
    Use random seed by default to increase entropy, instead of hard coded
    one.
    
    Differential Revision: D37677792
    
    fbshipit-source-id: 593b17acfea1188f0c04ae85c85c79175b758645

--- velox/expression/tests/ExpressionFuzzerTest.cpp
+++ velox/expression/tests/ExpressionFuzzerTest.cpp
@@ -26,9 +26,9 @@
 
 DEFINE_int64(
     seed,
-    123456,
-    "Initial seed for random number generator "
-    "(use it to reproduce previous results).");
+    0,
+    "Initial seed for random number generator used to reproduce previous "
+    "results (0 means start with random seed).");
 
 DEFINE_string(
     only,
@@ -80,5 +80,6 @@ int main(int argc, char** argv) {
       // TODO: T117753276
       "date_parse",
   };
-  return FuzzerRunner::run(FLAGS_only, FLAGS_steps, FLAGS_seed, skipFunctions);
+  size_t initialSeed = FLAGS_seed == 0 ? std::time(nullptr) : FLAGS_seed;
+  return FuzzerRunner::run(FLAGS_only, FLAGS_steps, initialSeed, skipFunctions);
 }
