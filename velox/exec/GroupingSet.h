4360988ea6df4352e637f395dca0043a94827a63
erdembilegt.j <erdembilegt.j@bytedance.com>
2022-07-20 13:40:01 -0700

    Global aggregation does not destructor of aggregates
    When accumulator of global aggregator uses external memory, we need to use destructor of the accumulator through aggregate's destroy method. This can cause memory leak.
    
    This happens for non global aggregates, but not global aggregates. Fixing the issue.

--- velox/exec/GroupingSet.h
+++ velox/exec/GroupingSet.h
@@ -91,6 +91,8 @@ class GroupingSet {
 
   void initializeGlobalAggregation();
 
+  void destroyGlobalAggregations();
+
   void addGlobalAggregationInput(const RowVectorPtr& input, bool mayPushdown);
 
   bool getGlobalAggregationOutput(
