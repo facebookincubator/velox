46cec2de57d435ee7f0ee302d0fc00e9a29385cd
Orri Erling <oerling@fb.com>
2022-08-17 10:27:37 -0700

    hash Join Optimization Collection
    - Simdize Vectorhasher int64 range path.
    
    - Simdize retrieving hash join result rows.
    
    - Special inline path for normalized key hash join probe.
    
    - faster mixing of bits for normalized key to hash number conversion.
    
    Drops single thread time for velox_tpch_benchmark from 40s to 36s with
    10G uncompressed Parquet dataset.

--- velox/common/base/SimdUtil-inl.h
+++ velox/common/base/SimdUtil-inl.h
@@ -294,7 +294,8 @@ inline bool copyNextWord(void*& to, const void*& from, int32_t& bytes) {
 } // namespace detail
 
 template <typename A>
-void memcpy(void* to, const void* from, int32_t bytes, const A& arch) {
+FOLLY_ALWAYS_INLINE void
+memcpy(void* to, const void* from, int32_t bytes, const A& arch) {
   while (bytes >= batchByteSize(arch)) {
     if (!detail::copyNextWord<xsimd::batch<int8_t, A>, A>(to, from, bytes)) {
       return;
