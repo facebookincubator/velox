b3132bc7160b2f8e3fac898968a5632e95970572
Raúl Cumplido <raulcumplido@gmail.com>
2022-09-07 12:07:28 +0200

    Fix linting and add ThirdpartyToolchain.cmake to linting scripts

--- ThirdpartyToolchain.cmake
+++ ThirdpartyToolchain.cmake
@@ -14,40 +14,35 @@
 
 # MODULE:   ThirdpartyToolchain
 #
-# PROVIDES:
-#   resolve_dependency( DEPENDENCY_NAME dependencyName
-#                    [REQUIRED_VERSION required version]
-#                    ...
-#   )
+# PROVIDES: resolve_dependency( DEPENDENCY_NAME dependencyName [REQUIRED_VERSION
+# required version] ... )
 #
-#       Provides the ability to resolve third party dependencies. If the dependency is
-#       already available in the system it will be used.
+# Provides the ability to resolve third party dependencies. If the dependency is
+# already available in the system it will be used.
 #
-#       The DEPENDENCY_NAME argument is required. The dependencyName value will be used to search for
-#       the installed dependencies Config file and thus this name should match find_package()
-#       standards.
+# The DEPENDENCY_NAME argument is required. The dependencyName value will be
+# used to search for the installed dependencies Config file and thus this name
+# should match find_package() standards.
 #
-# EXAMPLE USAGE:
-#   # Download and setup or use already installed dependency.
-#   include(ThirdpartyToolchain)
-#   resolve_dependency(folly)
+# EXAMPLE USAGE: # Download and setup or use already installed dependency.
+# include(ThirdpartyToolchain) resolve_dependency(folly)
 #
-#
-#========================================================================================
-
+# ========================================================================================
 
 add_custom_target(toolchain)
 include(ExternalProject)
 
-#=====================================FOLLY==============================================
+# =====================================FOLLY==============================================
 
 if(DEFINED ENV{VELOX_FOLLY_URL})
   set(FOLLY_SOURCE_URL "$ENV{VELOX_FOLLY_URL}")
 else()
   set(VELOX_FOLLY_BUILD_VERSION v2022.07.11.00)
   set(FOLLY_SOURCE_URL
-      "https://github.com/facebook/folly/archive/${VELOX_FOLLY_BUILD_VERSION}.tar.gz")
-  set(VELOX_FOLLY_BUILD_SHA256_CHECKSUM b6cc4082afd1530fdb8d759bc3878c1ea8588f6d5bc9eddf8e1e8abe63f41735)
+      "https://github.com/facebook/folly/archive/${VELOX_FOLLY_BUILD_VERSION}.tar.gz"
+  )
+  set(VELOX_FOLLY_BUILD_SHA256_CHECKSUM
+      b6cc4082afd1530fdb8d759bc3878c1ea8588f6d5bc9eddf8e1e8abe63f41735)
 endif()
 
 macro(build_folly)
@@ -63,19 +58,19 @@ macro(build_folly)
       "${FOLLY_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}follybenchmark${CMAKE_STATIC_LIBRARY_SUFFIX}"
   )
   if(NOT SCRIPT_CXX_FLAGS)
-    GET_SCRIPT_CXX_FLAGS()
+    get_script_cxx_flags()
   endif()
   set(FOLLY_CMAKE_ARGS
-      "-DCMAKE_CXX_FLAGS=${SCRIPT_CXX_FLAGS}"
-      -DCMAKE_CXX_STANDARD=17
+      "-DCMAKE_CXX_FLAGS=${SCRIPT_CXX_FLAGS}" -DCMAKE_CXX_STANDARD=17
       -DCMAKE_POSITION_INDEPENDENT_CODE=ON
       "-DCMAKE_INSTALL_PREFIX=${FOLLY_PREFIX}")
 
-  externalproject_add(folly
-                      URL ${FOLLY_SOURCE_URL}
-                      URL_HASH "SHA256=${VELOX_FOLLY_BUILD_SHA256_CHECKSUM}"
-                      CMAKE_ARGS ${FOLLY_CMAKE_ARGS}
-                      BUILD_BYPRODUCTS "${FOLLY_STATIC_LIB}" "${FOLLY_BENCHMARK_STATIC_LIB}")
+  ExternalProject_Add(
+    folly
+    URL ${FOLLY_SOURCE_URL}
+    URL_HASH "SHA256=${VELOX_FOLLY_BUILD_SHA256_CHECKSUM}"
+    CMAKE_ARGS ${FOLLY_CMAKE_ARGS}
+    BUILD_BYPRODUCTS "${FOLLY_STATIC_LIB}" "${FOLLY_BENCHMARK_STATIC_LIB}")
 
   # TODO: FIX HOW TO LINK FOLLY REQUIRED LIBRARIES
 
@@ -110,16 +105,10 @@ macro(build_folly)
   endif()
 
   # BOOST INTERFACE_LINK_LIBRARIES REQUIRED
-  find_package(Boost 1.51.0 MODULE
-  COMPONENTS
-    context
-    filesystem
-    program_options
-    regex
-    system
-    thread
-  REQUIRED
-  )
+  find_package(
+    Boost 1.51.0 MODULE
+    COMPONENTS context filesystem program_options regex system thread
+    REQUIRED)
   list(APPEND FOLLY_LINK_LIBRARIES ${Boost_LIBRARIES})
 
   # ZSTD INTERFACE_LINK_LIBRARIES REQUIRED
@@ -130,20 +119,20 @@ macro(build_folly)
 
   # LZ4 INTERFACE_LINK_LIBRARIES REQUIRED
   find_library(LZ4_LIBRARY_RELEASE NAMES lz4)
-  if (LZ4_LIBRARY_RELEASE)
+  if(LZ4_LIBRARY_RELEASE)
     list(APPEND FOLLY_LINK_LIBRARIES ${LZ4_LIBRARY_RELEASE})
   endif()
 
   # LIBLZMA INTERFACE_LINK_LIBRARIES REQUIRED
   find_package(LibLZMA MODULE)
   set(FOLLY_HAVE_LIBLZMA ${LIBLZMA_FOUND})
-  if (LIBLZMA_FOUND)
+  if(LIBLZMA_FOUND)
     list(APPEND FOLLY_LINK_LIBRARIES ${LIBLZMA_LIBRARIES})
   endif()
 
-  #ZLIB INTERFACE_LINK_LIBRARIES REQUIRED
+  # ZLIB INTERFACE_LINK_LIBRARIES REQUIRED
   find_package(ZLIB MODULE)
-  if (ZLIB_FOUND)
+  if(ZLIB_FOUND)
     list(APPEND FOLLY_LINK_LIBRARIES ${ZLIB_LIBRARIES})
   endif()
 
@@ -151,27 +140,30 @@ macro(build_folly)
   add_library(Folly::follybenchmark STATIC IMPORTED)
   add_library(Folly::folly STATIC IMPORTED)
 
-  set_target_properties(Folly::follybenchmark
-                        PROPERTIES IMPORTED_LOCATION "${FOLLY_BENCHMARK_STATIC_LIB}"
-                        INTERFACE_INCLUDE_DIRECTORIES "${FOLLY_INCLUDE_DIR}"
-                        INTERFACE_LINK_LIBRARIES "${FOLLY_LINK_LIBRARIES}")
-  set_target_properties(Folly::folly
-                        PROPERTIES IMPORTED_LOCATION "${FOLLY_STATIC_LIB}"
-                        INTERFACE_INCLUDE_DIRECTORIES "${FOLLY_INCLUDE_DIR}"
-                        INTERFACE_LINK_LIBRARIES "${FOLLY_LINK_LIBRARIES}")
+  set_target_properties(
+    Folly::follybenchmark
+    PROPERTIES IMPORTED_LOCATION "${FOLLY_BENCHMARK_STATIC_LIB}"
+               INTERFACE_INCLUDE_DIRECTORIES "${FOLLY_INCLUDE_DIR}"
+               INTERFACE_LINK_LIBRARIES "${FOLLY_LINK_LIBRARIES}")
+  set_target_properties(
+    Folly::folly
+    PROPERTIES IMPORTED_LOCATION "${FOLLY_STATIC_LIB}"
+               INTERFACE_INCLUDE_DIRECTORIES "${FOLLY_INCLUDE_DIR}"
+               INTERFACE_LINK_LIBRARIES "${FOLLY_LINK_LIBRARIES}")
 
   add_dependencies(Folly::folly folly)
 
   set(FOLLY_LIBRARIES Folly::folly)
 endmacro()
 
-#===================================END FOLLY============================================
+# ===================================END FOLLY===============================
 
 macro(build_dependency DEPENDENCY_NAME)
   if("${DEPENDENCY_NAME}" STREQUAL "folly")
     build_folly()
   else()
-    message(FATAL_ERROR "Unknown thirdparty dependency to build: ${DEPENDENCY_NAME}")
+    message(
+      FATAL_ERROR "Unknown thirdparty dependency to build: ${DEPENDENCY_NAME}")
   endif()
 endmacro()
 
@@ -179,13 +171,11 @@ macro(resolve_dependency DEPENDENCY_NAME)
   set(options)
   set(one_value_args REQUIRED_VERSION)
   set(multi_value_args)
-  cmake_parse_arguments(ARG
-                        "${options}"
-                        "${one_value_args}"
-                        "${multi_value_args}"
-                        ${ARGN})
+  cmake_parse_arguments(ARG "${options}" "${one_value_args}"
+                        "${multi_value_args}" ${ARGN})
   if(ARG_UNPARSED_ARGUMENTS)
-    message(SEND_ERROR "Error: unrecognized arguments: ${ARG_UNPARSED_ARGUMENTS}")
+    message(
+      SEND_ERROR "Error: unrecognized arguments: ${ARG_UNPARSED_ARGUMENTS}")
   endif()
   set(PACKAGE_NAME ${DEPENDENCY_NAME})
   set(FIND_PACKAGE_ARGUMENTS ${PACKAGE_NAME})
