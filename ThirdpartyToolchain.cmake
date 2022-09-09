9a3c47442cfab13a9f542865a3e67e857f13ffe7
Raúl Cumplido <raulcumplido@gmail.com>
2022-09-09 12:24:20 +0200

    Add ThirpartyToolchain.cmake to linter and lint file

--- ThirdpartyToolchain.cmake
+++ ThirdpartyToolchain.cmake
@@ -14,64 +14,59 @@
 
 # MODULE:   ThirdpartyToolchain
 #
-# PROVIDES:
-#   resolve_dependency( DEPENDENCY_NAME dependencyName
-#                    [REQUIRED_VERSION required version]
-#                    ...
-#   )
+# PROVIDES: resolve_dependency( DEPENDENCY_NAME dependencyName [REQUIRED_VERSION
+# required version] ... )
 #
-#       Provides the ability to resolve third party dependencies. If the dependency is
-#       already available in the system it will be used.
+# Provides the ability to resolve third party dependencies. If the dependency is
+# already available in the system it will be used.
 #
-#       The DEPENDENCY_NAME argument is required. The dependencyName value will be used to search for
-#       the installed dependencies Config file and thus this name should match find_package()
-#       standards.
+# The DEPENDENCY_NAME argument is required. The dependencyName value will be
+# used to search for the installed dependencies Config file and thus this name
+# should match find_package() standards.
 #
-# EXAMPLE USAGE:
-#   # Download and setup or use already installed dependency.
-#   include(ThirdpartyToolchain)
-#   resolve_dependency(folly)
+# EXAMPLE USAGE: # Download and setup or use already installed dependency.
+# include(ThirdpartyToolchain) resolve_dependency(folly)
 #
-#
-#========================================================================================
-
+# ========================================================================================
 
 include(FetchContent)
 
-#=====================================FOLLY==============================================
+# =====================================FOLLY==============================================
 
 if(DEFINED ENV{VELOX_FOLLY_URL})
   set(FOLLY_SOURCE_URL "$ENV{VELOX_FOLLY_URL}")
 else()
   set(VELOX_FOLLY_BUILD_VERSION v2022.07.11.00)
   set(FOLLY_SOURCE_URL
-           "https://github.com/facebook/folly/archive/${VELOX_FOLLY_BUILD_VERSION}.tar.gz")
-  set(VELOX_FOLLY_BUILD_SHA256_CHECKSUM b6cc4082afd1530fdb8d759bc3878c1ea8588f6d5bc9eddf8e1e8abe63f41735)
+      "https://github.com/facebook/folly/archive/${VELOX_FOLLY_BUILD_VERSION}.tar.gz"
+  )
+  set(VELOX_FOLLY_BUILD_SHA256_CHECKSUM
+      b6cc4082afd1530fdb8d759bc3878c1ea8588f6d5bc9eddf8e1e8abe63f41735)
 endif()
 
 macro(build_folly)
   message(STATUS "Building Folly from source")
   FetchContent_Declare(
-      folly
-      URL ${FOLLY_SOURCE_URL}
-      URL_HASH SHA256=${VELOX_FOLLY_BUILD_SHA256_CHECKSUM}
-    )
+    folly
+    URL ${FOLLY_SOURCE_URL}
+    URL_HASH SHA256=${VELOX_FOLLY_BUILD_SHA256_CHECKSUM})
   if(NOT folly_POPULATED)
     # Fetch the content using previously declared details
     FetchContent_Populate(folly)
     add_subdirectory(${folly_SOURCE_DIR} ${folly_BINARY_DIR})
   endif()
   set(FOLLY_BENCHMARK_STATIC_LIB
-    ${folly_BINARY_DIR}/folly/libfollybenchmark${CMAKE_STATIC_LIBRARY_SUFFIX})
+      ${folly_BINARY_DIR}/folly/libfollybenchmark${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(FOLLY_LIBRARIES folly)
 endmacro()
-#===================================END FOLLY============================================
+# ===============================END FOLLY================================
 
 macro(build_dependency DEPENDENCY_NAME)
   if("${DEPENDENCY_NAME}" STREQUAL "folly")
     build_folly()
   else()
-    message(FATAL_ERROR "Unknown thirdparty dependency to build: ${DEPENDENCY_NAME}")
+    message(
+      FATAL_ERROR "Unknown thirdparty dependency to build: ${DEPENDENCY_NAME}")
   endif()
 endmacro()
 
@@ -79,13 +74,11 @@ macro(resolve_dependency DEPENDENCY_NAME)
   set(options)
   set(one_value_args REQUIRED_VERSION)
   set(multi_value_args)
-  cmake_parse_arguments(ARG
-                        "${options}"
-                        "${one_value_args}"
-                        "${multi_value_args}"
-                        ${ARGN})
+  cmake_parse_arguments(ARG "${options}" "${one_value_args}"
+                        "${multi_value_args}" ${ARGN})
   if(ARG_UNPARSED_ARGUMENTS)
-    message(SEND_ERROR "Error: unrecognized arguments: ${ARG_UNPARSED_ARGUMENTS}")
+    message(
+      SEND_ERROR "Error: unrecognized arguments: ${ARG_UNPARSED_ARGUMENTS}")
   endif()
   set(PACKAGE_NAME ${DEPENDENCY_NAME})
   set(FIND_PACKAGE_ARGUMENTS ${PACKAGE_NAME})
