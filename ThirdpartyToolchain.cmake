602ed586218156676bd595ce160d710e5afab773
Raúl Cumplido <raulcumplido@gmail.com>
2022-09-13 17:59:42 +0200

    Remove test code

--- ThirdpartyToolchain.cmake
+++ ThirdpartyToolchain.cmake
@@ -46,6 +46,7 @@ endif()
 
 macro(build_folly)
   message(STATUS "Building Folly from source")
+  # Avoid possible errors for some known warnings on folly
   set(FOLLY_CXX_FLAGS
       -Wno-deprecated-declarations -Wno-nullability-completeness -Wno-unused
       -Wno-unused-parameter -Wno-overloaded-virtual)
@@ -57,8 +58,6 @@ macro(build_folly)
     # Fetch the content using previously declared details
     FetchContent_Populate(folly)
     add_subdirectory(${folly_SOURCE_DIR} ${folly_BINARY_DIR})
-    # Avoid possible errors for some known warnings on folly
-    target_compile_options(folly PRIVATE -Wno-deprecated-declarations)
   endif()
   set(FOLLY_BENCHMARK_STATIC_LIB
       ${folly_BINARY_DIR}/folly/libfollybenchmark${CMAKE_STATIC_LIBRARY_SUFFIX})
