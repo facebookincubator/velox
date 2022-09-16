2a542f9898a1362114ee47b00c5b0ddbd20fdd49
Zhang, Chaojun <chaojun.zhang@intel.com>
2022-09-16 14:13:50 +0000

    Add comment for substrait type signature

--- velox/substrait/VeloxSubstraitSignature.h
+++ velox/substrait/VeloxSubstraitSignature.h
@@ -23,7 +23,12 @@ namespace facebook::velox::substrait {
 class VeloxSubstraitSignature {
  public:
   /// Given a velox type kind, return the Substrait type signature, throw if no
-  /// match found.
+  /// match found, Substrait signature used in the function extension
+  /// declaration is a combination of the name of the function along with a list
+  /// of input argument types.The format is as follows : <function
+  /// name>:<short_arg_type0>_<short_arg_type1>_..._<short_arg_typeN> for more
+  /// detail information about the argument type please refer to link
+  /// https://substrait.io/extensions/#function-signature-compound-names.
   static std::string toSubstraitSignature(const TypeKind typeKind);
 
   /// Given a velox scalar function name and argument types, return the
