db8d292ae6a5dcdf28a3b4ab7868d52d18889a4c
zhixingheyi-tian <xiangxiang.shen@intel.com>
2022-11-01 16:25:18 +0800

    Refine

--- scripts/setup-centos8.sh
+++ scripts/setup-centos8.sh
@@ -46,7 +46,7 @@ pip3 install sphinx sphinx-tabs breathe sphinx_rtd_theme
 source /opt/rh/gcc-toolset-9/enable || exit 1
 set -u
 
-function cmake_install {
+function cmake_install_deps {
   cmake -B "$1-build" -GNinja -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_CXX_FLAGS="${CFLAGS}" -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -Wno-dev "$@"
   ninja -C "$1-build" install
@@ -69,13 +69,9 @@ wget_and_untar http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz 
 wget_and_untar https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz boost &
 wget_and_untar https://github.com/google/snappy/archive/1.1.8.tar.gz snappy &
 wget_and_untar https://github.com/fmtlib/fmt/archive/8.0.0.tar.gz fmt &
-#  wget_and_untar https://github.com/ericniebler/range-v3/archive/0.11.0.tar.gz ranges-v3 &
-wget_and_untar https://archive.apache.org/dist/hadoop/common/hadoop-2.10.1/hadoop-2.10.1.tar.gz hadoop
 wget_and_untar https://github.com/protocolbuffers/protobuf/releases/download/v21.4/protobuf-all-21.4.tar.gz protobuf &
 
 wait  # For cmake and source downloads to complete.
-
-cp -a hadoop /usr/local/
 
 # Build & install.
 (
@@ -99,11 +95,10 @@ cp -a hadoop /usr/local/
   ldconfig
 )
 
-cmake_install gflags -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DBUILD_gflags_LIB=ON -DLIB_SUFFIX=64 -DCMAKE_INSTALL_PREFIX:PATH=/usr
-cmake_install glog -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr
-cmake_install snappy -DSNAPPY_BUILD_TESTS=OFF
-cmake_install fmt -DFMT_TEST=OFF
-# cmake_install ranges-v3
+cmake_install_deps gflags -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DBUILD_gflags_LIB=ON -DLIB_SUFFIX=64 -DCMAKE_INSTALL_PREFIX:PATH=/usr
+cmake_install_deps glog -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr
+cmake_install_deps snappy -DSNAPPY_BUILD_TESTS=OFF
+cmake_install_deps fmt -DFMT_TEST=OFF
 
 dnf clean all
 
