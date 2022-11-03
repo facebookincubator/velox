56a9256ef367af9f416dd89c6959bb91419cae62
xiaoxmeng <xiaoxmeng@fb.com>
2022-11-02 23:19:44 -0700

    Fix batch size when get output from spilling plus code cleanup

--- velox/exec/GroupingSet.cpp
+++ velox/exec/GroupingSet.cpp
@@ -408,7 +408,7 @@ bool GroupingSet::getOutput(
     return getGlobalAggregationOutput(batchSize, isPartial_, iterator, result);
   }
   if (spiller_) {
-    return getOutputWithSpill(result);
+    return getOutputWithSpill(batchSize, result);
   }
 
   // @lint-ignore CLANGTIDY
@@ -583,7 +583,9 @@ void GroupingSet::spill(int64_t targetRows, int64_t targetBytes) {
   spiller_->spill(targetRows, targetBytes);
 }
 
-bool GroupingSet::getOutputWithSpill(const RowVectorPtr& result) {
+bool GroupingSet::getOutputWithSpill(
+    int32_t batchSize,
+    const RowVectorPtr& result) {
   if (outputPartition_ == -1) {
     mergeArgs_.resize(1);
     std::vector<TypePtr> keyTypes;
@@ -610,19 +612,8 @@ bool GroupingSet::getOutputWithSpill(const RowVectorPtr& result) {
   }
 
   if (nonSpilledIndex_ < nonSpilledRows_.value().size()) {
-    uint64_t bytes = 0;
-    vector_size_t numGroups = 0;
-    // Produce non-spilled content at max 1000 rows at a time.
-    auto limit = std::min<size_t>(
-        1000, nonSpilledRows_.value().size() - nonSpilledIndex_);
-    for (; numGroups < limit; ++numGroups) {
-      bytes += rowsWhileReadingSpill_->rowSize(
-          nonSpilledRows_.value()[nonSpilledIndex_ + numGroups]);
-      if (bytes > maxBatchBytes_) {
-        ++numGroups;
-        break;
-      }
-    }
+    const size_t numGroups = std::min<vector_size_t>(
+        batchSize, nonSpilledRows_.value().size() - nonSpilledIndex_);
     extractGroups(
         folly::Range<char**>(
             nonSpilledRows_.value().data() + nonSpilledIndex_, numGroups),
@@ -635,7 +626,7 @@ bool GroupingSet::getOutputWithSpill(const RowVectorPtr& result) {
       merge_ = spiller_->startMerge(outputPartition_);
     }
     // NOTE: 'merge_' might be nullptr if 'outputPartition_' is empty.
-    if (merge_ == nullptr || !mergeNext(result)) {
+    if (merge_ == nullptr || !mergeNext(batchSize, result)) {
       ++outputPartition_;
       merge_ = nullptr;
       continue;
@@ -645,8 +636,7 @@ bool GroupingSet::getOutputWithSpill(const RowVectorPtr& result) {
   return false;
 }
 
-bool GroupingSet::mergeNext(const RowVectorPtr& result) {
-  constexpr int32_t kBatchBytes = 1 << 20; // 1MB
+bool GroupingSet::mergeNext(int32_t batchSize, const RowVectorPtr& result) {
   for (;;) {
     auto next = merge_->nextWithEquals();
     if (!next.first) {
@@ -660,7 +650,7 @@ bool GroupingSet::mergeNext(const RowVectorPtr& result) {
     updateRow(*next.first, mergeState_);
     nextKeyIsEqual_ = next.second;
     next.first->pop();
-    if (!nextKeyIsEqual_ && mergeRows_->allocatedBytes() > kBatchBytes) {
+    if (!nextKeyIsEqual_ && mergeRows_->numRows() >= batchSize) {
       extractSpillResult(result);
       return true;
     }
