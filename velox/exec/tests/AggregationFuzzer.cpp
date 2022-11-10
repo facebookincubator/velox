449ff00a4bbe2e4701322a3507d57524ae35e6d2
tanjialiang <jacob.jialiang.tan@gmail.com>
2022-11-10 12:36:43 -0800

    Dump fuzzer repro info to CircleCI Artifacts (#3012)
    Summary:
    Allow CircleCI to dump repro info upon fuzzer failure to Artifacts.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/3012
    
    Test Plan:
    Manually created failure in fuzzer and observed repro files dumped to Artifacts.
    ![Screen Shot 2022-10-28 at 2 24 09 PM](https://user-images.githubusercontent.com/9097144/198751107-c475e050-049e-4e40-abad-2d52fd54065b.png)
    
    Fixes
    
    Reviewed By: mbasmanova
    
    Differential Revision: D40823396
    
    Pulled By: tanjialiang
    
    fbshipit-source-id: 5ba5403a794e0c27cbd3fb1829587f87a0c2c3a9

--- velox/exec/tests/AggregationFuzzer.cpp
+++ velox/exec/tests/AggregationFuzzer.cpp
@@ -363,7 +363,7 @@ void persistReproInfo(
   }
 
   // Save input vector.
-  auto inputPathOpt = generateFilePath(basePath.c_str(), "vector");
+  auto inputPathOpt = common::generateTempFilePath(basePath.c_str(), "vector");
   if (!inputPathOpt.has_value()) {
     inputPath = "Failed to create file for saving input vector.";
   } else {
@@ -378,7 +378,7 @@ void persistReproInfo(
 
   // Save plan.
   std::string planPath;
-  auto planPathOpt = generateFilePath(basePath.c_str(), "plan");
+  auto planPathOpt = common::generateTempFilePath(basePath.c_str(), "plan");
   if (!planPathOpt.has_value()) {
     planPath = "Failed to create file for saving SQL.";
   } else {
