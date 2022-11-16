dafdbb146fda38c0442676619f6a68f0861ee478
Jacob Wujciak-Jens <jacob@wujciak.de>
2022-11-16 14:53:13 +0100

    add conda env info, copy to clipboard

--- scripts/info.sh
+++ scripts/info.sh
@@ -15,27 +15,60 @@
 set -e
 
 version='0.0.1'
-info=$(mktemp)
-cmake --system-information >> $info 
+cb='```'
 
-e() { echo "$*"
-}
+if [ ! -x "$(command -v cmake)" ]; then
+  echo "Please install cmake to use this script."
+  exit 1
+fi
+
+info=$(cmake --system-information)
 
 ext() {
   grep -oP '\".+\"$' $1 | tr -d '\"' 
 }
 
-e '```'
-e "Velox System Info v${version}"
-e Commit: $(git rev-parse HEAD 2> /dev/null || echo "Not in a git repo.")
-e CMake Version: $(cmake --version | grep -oP '\d+\.\d+\.\d+')
-e System: $(grep -aP 'CMAKE_SYSTEM \"' $info | ext )
-e Arch: $(grep -aP 'CMAKE_SYSTEM_PROCESSOR' $info | ext)
-e C++ Compiler: $(grep -aP 'CMAKE_CXX_COMPILER ==' $info | ext)
-e C++ Compiler Version: $(grep -aP 'CMAKE_CXX_COMPILER_VERSION' $info | ext)
-e C Compiler: $(grep -aP 'CMAKE_C_COMPILER ==' $info | ext)
-e C Compiler Version: $(grep -aP 'CMAKE_C_COMPILER_VERSION' $info | ext)
-e CMake Prefix Path: $(grep -aP '_PREFIX_PATH ' $info | ext)
-echo '```'
+print_info() {
+echo "$info" | grep -P "$1" | ext 
+}
 
-rm $info
+result="
+Velox System Info v${version}
+Commit: $(git rev-parse HEAD 2> /dev/null || echo "Not in a git repo.")
+CMake Version: $(cmake --version | grep -oP '\d+\.\d+\.\d+')
+System: $(print_info 'CMAKE_SYSTEM \"')
+Arch: $(print_info 'CMAKE_SYSTEM_PROCESSOR')
+C++ Compiler: $(print_info 'CMAKE_CXX_COMPILER ==')
+C++ Compiler Version: $(print_info 'CMAKE_CXX_COMPILER_VERSION')
+C Compiler: $(print_info 'CMAKE_C_COMPILER ==')
+C Compiler Version: $(print_info 'CMAKE_C_COMPILER_VERSION')
+CMake Prefix Path: $(print_info '_PREFIX_PATH ')
+"
+if [ "$CONDA_SHLVL" == 1 ]; then
+  conda="
+Conda Env
+<details>
+
+$cb
+$(conda list)
+$cb
+
+</details>
+"
+fi
+
+all="$result  $conda"
+echo "$all"
+
+if [ -x "$(command -v xclip)" ]; then
+ clip="xclip -selection c"
+elif [ -x "$(command -v pbcopy)"]; then
+  clip="pbcopy"
+else
+  echo "\nThe results will be copied to your clipboard if xclip is installed."
+fi
+
+if [ ! -z "$clip" ]; then
+  echo "$all" | $clip
+  echo "Result copied to clipboard!"
+fi
