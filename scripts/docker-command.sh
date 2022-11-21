5ad83b3077a5f2c319ade3d930dc9ac4271c2646
Raúl Cumplido <raulcumplido@gmail.com>
2022-11-21 11:56:46 +0100

    Minor fix when env vars are not present

--- scripts/docker-command.sh
+++ scripts/docker-command.sh
@@ -15,5 +15,5 @@
 
 set -eu
 # Compilation and testing
-make EXTRA_CMAKE_FLAGS="${EXTRA_CMAKE_FLAGS}" AWSSDK_ROOT_DIR="${AWSSDK_ROOT_DIR}"
+make EXTRA_CMAKE_FLAGS="${EXTRA_CMAKE_FLAGS:-}" AWSSDK_ROOT_DIR="${AWSSDK_ROOT_DIR:-}"
 cd _build/release && ctest -j${NUM_THREADS} -VV --output-on-failure
