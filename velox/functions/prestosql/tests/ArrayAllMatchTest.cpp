11d32003d405472bcf22c5c6bf6c16b7793e8046
macduan <macduan@tencent.com>
2023-02-07 21:20:17 +0800

    resolve comments

--- velox/functions/prestosql/tests/ArrayAllMatchTest.cpp
+++ velox/functions/prestosql/tests/ArrayAllMatchTest.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "velox/functions/prestosql/tests/utils/FunctionBaseTest.h"
 
 using namespace facebook::velox;
