2a0ef68ae2dcb4d750af9ea2e7fc06c5fab35b5b
张政豪 <103476031+zhenghao-bot@users.noreply.github.com>
2023-02-13 08:57:01 +0000

    fix typo

--- velox/dwio/parquet/tests/reader/E2EFilterTest.cpp
+++ velox/dwio/parquet/tests/reader/E2EFilterTest.cpp
@@ -424,21 +424,6 @@ TEST_F(E2EFilterTest, stringDictionary) {
       20);
 }
 
-TEST_F(E2EFilterTest, varbinaeyDictionary) {
-  testWithTypes(
-      "varbinary_val:varbinary,"
-      "varbinary_val_2:varbinary,"
-      "varbinary_const:varbinary",
-      [&]() {
-        makeStringDistribution("varbinary_val", 100, true, false);
-        makeStringDistribution("varbinary_val_2", 170, false, true);
-        makeStringDistribution("varbinary_const", 1, true, false);
-      },
-      true,
-      {"varbinary_val", "varbinary_val_2"},
-      20);
-}
-
 TEST_F(E2EFilterTest, dedictionarize) {
   writerProperties_ = ::parquet::WriterProperties::Builder()
                           .max_row_group_length(10000000)
@@ -510,6 +495,21 @@ TEST_F(E2EFilterTest, map) {
       false,
       {"long_val", "map_val"},
       10);
+}
+
+TEST_F(E2EFilterTest, varbinaryDictionary) {
+  testWithTypes(
+      "varbinary_val:varbinary,"
+      "varbinary_val_2:varbinary,"
+      "varbinary_const:varbinary",
+      [&]() {
+        makeStringDistribution("varbinary_val", 100, true, false);
+        makeStringDistribution("varbinary_val_2", 170, false, true);
+        makeStringDistribution("varbinary_const", 1, true, false);
+      },
+      true,
+      {"varbinary_val", "varbinary_val_2"},
+      20);
 }
 
 TEST_F(E2EFilterTest, largeMetadata) {
