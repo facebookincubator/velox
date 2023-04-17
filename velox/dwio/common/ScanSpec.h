6cc8d399690e4cee7a954414ea659bd3c90cc513
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-04-17 11:35:19 +0800

    add reorder

--- velox/dwio/common/ScanSpec.h
+++ velox/dwio/common/ScanSpec.h
@@ -286,19 +286,13 @@ class ScanSpec {
 
   // Resets cached values after this or children were updated, e.g. a new filter
   // was added or existing filter was modified.
-  void resetCachedValues() {
+  void resetCachedValues(bool reorder) {
     hasFilter_.reset();
     for (auto& child : children_) {
       child->resetCachedValues();
     }
-
-    reorder();
-  }
-
-  void clearHasFilter() {
-    hasFilter_.reset();
-    for (auto& child : children_) {
-      child->clearHasFilter();
+    if (reorder) {
+      reorder();
     }
   }
 
