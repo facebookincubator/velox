12ee18d6e21289cf2d18ac3469735232beeaaf11
Jacob Wujciak-Jens <jacob@wujciak.de>
2023-04-19 20:17:31 +0200

    add find modules for lzo, lz4, snappy and zstd

--- CMake/resolve_dependency_modules/gflags.cmake
+++ CMake/resolve_dependency_modules/gflags.cmake
@@ -29,7 +29,7 @@ FetchContent_Declare(
   URL_HASH ${VELOX_GFLAGS_BUILD_SHA256_CHECKSUM}
   PATCH_COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/gflags/gflags-config.patch)
 
-#set(GFLAGS_BUILD_SHARED_LIBS ON)
+# set(GFLAGS_BUILD_SHARED_LIBS ON)
 set(GFLAGS_BUILD_STATIC_LIBS ON)
 set(GFLAGS_BUILD_gflags_LIB ON)
 set(GFLAGS_BUILD_gflags_nothreads_LIB ON)
@@ -41,7 +41,6 @@ FetchContent_MakeAvailable(gflags)
 
 # the flag has to be added to each target we build so adjust to settings choosen
 # above
-#target_compile_options(gflags_shared PRIVATE -Wno-cast-function-type)
 target_compile_options(gflags_static PRIVATE -Wno-cast-function-type)
 target_compile_options(gflags_nothreads_static PRIVATE -Wno-cast-function-type)
 
