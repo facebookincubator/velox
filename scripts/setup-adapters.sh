6c00a22885058554d4c0322b4fc4598c649d65e9
Sandino Flores <sandino.flores.moreno@intel.com>
2023-04-24 01:29:06 +0000

    Install dependencies of GCS in setup-adapters.sh
    This change is needed because currently the scripts
    setup-ubuntu.sh and setup-centos.sh are only invoked
    when the docker image is created, not during the CI/CD builds.

--- scripts/setup-adapters.sh
+++ scripts/setup-adapters.sh
@@ -128,8 +128,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
    LINUX_DISTRIBUTION=$(. /etc/os-release && echo ${ID})
    if [[ "$LINUX_DISTRIBUTION" == "ubuntu" ]]; then
       apt install -y --no-install-recommends libxml2-dev libgsasl7-dev uuid-dev
+      # Dependencies of GCS, probably a workaround until the docker image is rebuilt
+      apt install -y --no-install-recommends libc-ares-dev libcurl4-openssl-dev python3-dev
    else # Assume Fedora/CentOS
       yum -y install libxml2-devel libgsasl-devel libuuid-devel
+      # Dependencies of GCS, probably a workaround until the docker image is rebuilt
+      yum -y install curl-devel c-ares-devel python3-devel
    fi
 fi
 
