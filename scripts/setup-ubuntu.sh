1e347592560fdbc8c245f0cfd2051fd816e19249
Jacob Wujciak-Jens <jacob@wujciak.de>
2023-04-25 01:43:59 +0200

    fix glog install issue

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -28,8 +28,11 @@ NPROC=$(getconf _NPROCESSORS_ONLN)
 DEPENDENCY_DIR=${DEPENDENCY_DIR:-$(pwd)}
 export CMAKE_BUILD_TYPE=Release
 
-# Install all velox and folly dependencies.
-sudo --preserve-env apt update && sudo apt install -y \
+# Install all velox and folly dependencies. 
+# The is an issue on 22.04 where a version conflict prevents glog install,
+# installing libunwind first fixes this.
+sudo --preserve-env apt update && sudo apt install -y libunwind-dev && \
+  sudo apt install -y \
   g++ \
   cmake \
   ccache \
