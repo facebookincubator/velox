7cf097d05679e6516c1a0249821a871f9546f89e
vibhatha <vibhatha@gmail.com>
2023-05-16 21:08:06 +0530

    fix: addressed reviews

--- pyvelox/serde.cpp
+++ pyvelox/serde.cpp
@@ -38,11 +38,52 @@ void addSerdeBindings(py::module& m, bool asModuleLocalDefinitions) {
   m.def(
       "save_vector",
       &saveVectorToFile,
-      "Serializes the vector into binary format and writes it to a new file.");
+      R"delimiter(
+        Serializes the vector into binary format and writes it to a new file.
+
+        Parameters
+        ----------
+        vector : Union[FlatVector, ConstantVector, DictionaryVector]
+              The vector to be saved.
+        file_path: str
+              The path to which the vector will be saved.
+
+        Returns
+        -------
+        None
+
+        Examples
+        --------
+
+        >>> import pyvelox.pyvelox as pv
+        >>> vec = pv.from_list([1, 2, 3])
+        >>> pv.save_vector(vec, '/tmp/flatvector.bin')
+      )delimiter",
+      py::arg("vector"),
+      py::arg("file_path"));
   m.def(
       "load_vector",
       &pyRestoreVectorFromFileHelper,
-      "Reads and deserializes a vector from a file stored by save_vector.");
+      R"delimiter(
+        Reads and deserializes a vector from a file stored by save_vector.
+
+        Parameters
+        ----------
+        file_path: str
+              The path from which the vector will be loaded.
+
+        Returns
+        -------
+        Union[FlatVector, ConstantVector, DictionaryVector]
+
+        Examples
+        --------
+
+        >>> import pyvelox.pyvelox as pv
+        >>> pv.load_vector('/tmp/flatvector.bin')
+        <pyvelox.pyvelox.FlatVector_BIGINT object at 0x7f8f6f818bb0>
+      )delimiter",
+      py::arg("file_path"));
 }
 
 } // namespace facebook::velox::py
