cbb30d3b07d43c3b4d355939ba50773c21841d9b
Nima <74507096+Nimausfi@users.noreply.github.com>
2023-06-12 12:35:52 -0700

    Remove default CPU_TARGET avx from cmake_install function in setup-helper-functions.sh

--- scripts/setup-helper-functions.sh
+++ scripts/setup-helper-functions.sh
@@ -15,6 +15,43 @@
 
 # github_checkout $REPO $VERSION $GIT_CLONE_PARAMS clones or re-uses an existing clone of the
 # specified repo, checking out the requested version.
+function github_checkout {
+  local REPO=$1
+  shift
+  local VERSION=$1
+  shift
+  local GIT_CLONE_PARAMS=$@
+  local DIRNAME=$(basename "$REPO")
+  cd "$DEPENDENCY_DIR"
+  if [ -z "$DIRNAME" ]; then
+    echo "Failed to get repo name from $REPO"
+    exit 1
+  fi
+  if [ -d "$DIRNAME" ] && prompt "$DIRNAME already exists. Delete?"; then
+    rm -rf "$DIRNAME"
+  fi
+  if [ ! -d "$DIRNAME" ]; then
+    git clone -q -b "$VERSION" $GIT_CLONE_PARAMS "https://github.com/$REPO.git"
+  fi
+  cd "$DIRNAME"
+}
+
+
+# get_cxx_flags [$CPU_ARCH]
+# Sets and exports the variable VELOX_CXX_FLAGS with appropriate compiler flags.
+# If $CPU_ARCH is set then we use that else we determine best possible set of flags
+# to use based on current cpu architecture.
+# The goal of this function is to consolidate all architecture specific flags to one
+# location.
+# The values that CPU_ARCH can take are as follows:
+#   arm64  : Target Apple silicon.
+#   aarch64: Target general 64 bit arm cpus.
+#   avx:     Target Intel CPUs with AVX.
+#   sse:     Target Intel CPUs with sse.
+# Echo's the appropriate compiler flags which can be captured as so
+# CXX_FLAGS=$(get_cxx_flags) or
+# CXX_FLAGS=$(get_cxx_flags "avx")
+
 function get_cxx_flags {
   local OS
   OS=$(uname)
