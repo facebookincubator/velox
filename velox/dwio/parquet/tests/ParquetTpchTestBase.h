bcffc261685042884376fa2cb560c084d4d29344
Paul Amonson <paul.d.amonson@intel.com>
2023-06-22 18:04:31 +0000

    [Fix] More misses not caught in local build.
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- velox/dwio/parquet/tests/ParquetTpchTestBase.h
+++ velox/dwio/parquet/tests/ParquetTpchTestBase.h
@@ -117,10 +117,7 @@ class ParquetTpchTestBase : public testing::Test {
         for (const auto& entry : tpchPlan.dataFiles) {
           for (const auto& path : entry.second) {
             auto const splits = HiveConnectorTestBase::makeHiveConnectorSplits(
-                path,
-                kNumSplits,
-                tpchPlan.dataFileFormat,
-                dwio::common::ReaderOptions(nullptr));
+                path, kNumSplits, tpchPlan.dataFileFormat);
             for (const auto& split : splits) {
               task->addSplit(entry.first, Split(split));
             }
