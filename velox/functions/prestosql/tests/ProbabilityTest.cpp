bcdc0198acd3595f9c39565dbfe59e8015a94fe1
Krishna-Prasad-P-V <Krishna.Prasad.P.V@ibm.com>
2023-06-28 20:53:39 +0530

    Fix testing issue weidbull_cdf

--- velox/functions/prestosql/tests/ProbabilityTest.cpp
+++ velox/functions/prestosql/tests/ProbabilityTest.cpp
@@ -175,9 +175,9 @@ TEST_F(ProbabilityTest, weibullCDF) {
   EXPECT_EQ(weibullCDF(1.0, kDoubleMax, 3.0), 1.668805393880401e-308);
   EXPECT_EQ(weibullCDF(kInf, 1.0, 3.0), 0.0);
   EXPECT_EQ(weibullCDF(1.0, kInf, 20.0), 0.0);
-  EXPECT_EQ(weibullCDF(kInf, kInf, kInf), IsNan());
-  EXPECT_EQ(weibullCDF(kDoubleMin, kDoubleMin, kInf), IsNan());
-  EXPECT_EQ(weibullCDF(kDoubleMax, kDoubleMax, kInf), IsNan());
+  EXPECT_EQ(weibullCDF(kDoubleMin, kDoubleMin, 1.0), 0.63212055882855767);
+  EXPECT_EQ(weibullCDF(kDoubleMax, kDoubleMax, 4.0), 0.0);
+  VELOX_ASSERT_THROW(weibullCDF(kNan, kNan, kDoubleMin), "a must be greater than 0");
   VELOX_ASSERT_THROW(weibullCDF(0, 3, 0.5), "a must be greater than 0");
   VELOX_ASSERT_THROW(weibullCDF(3, 0, 0.5), "b must be greater than 0");
   VELOX_ASSERT_THROW(weibullCDF(kNan, 3.0, 0.5), "a must be greater than 0");
