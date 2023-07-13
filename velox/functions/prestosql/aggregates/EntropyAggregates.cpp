ffc69f987f41e956aa1df9d9b5205045a1720896
joey.ljy <joey.ljy@alibaba-inc.com>
2023-07-13 09:27:20 +0800

    add query config in registerAggregateFunction

--- velox/functions/prestosql/aggregates/EntropyAggregates.cpp
+++ velox/functions/prestosql/aggregates/EntropyAggregates.cpp
@@ -361,7 +361,8 @@ exec::AggregateRegistrationResult registerEntropy(const std::string& name) {
       [name](
           core::AggregationNode::Step step,
           const std::vector<TypePtr>& argTypes,
-          const TypePtr& resultType) -> std::unique_ptr<exec::Aggregate> {
+          const TypePtr& resultType,
+          const core::QueryConfig& /*config*/) -> std::unique_ptr<exec::Aggregate> {
         VELOX_CHECK_LE(
             argTypes.size(), 1, "{} takes at most one argument", name);
         const auto& inputType = argTypes[0];
