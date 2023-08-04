9ffb5dee93193ea93df37b80c5688036e4376416
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-08-04 09:26:29 +0800

    implement round  spark function

--- velox/functions/sparksql/Arithmetic.h
+++ velox/functions/sparksql/Arithmetic.h
@@ -26,6 +26,43 @@
 namespace facebook::velox::functions::sparksql {
 
 template <typename T>
+struct RoundFunction {
+  template <typename TNum, typename TDecimals, bool alwaysRoundNegDec = false>
+  FOLLY_ALWAYS_INLINE TNum
+  round(const TNum& number, const TDecimals& decimals = 0) {
+    static_assert(
+        !std::is_same_v<TNum, bool> && "round not supported for bool");
+
+    if constexpr (std::is_integral_v<TNum>) {
+      if constexpr (alwaysRoundNegDec) {
+        if (decimals >= 0)
+          return number;
+      } else {
+        return number;
+      }
+    }
+    if (!std::isfinite(number)) {
+      return number;
+    }
+
+    double factor = std::pow(10, decimals);
+    static const TNum kInf = std::numeric_limits<TNum>::infinity();
+    if (number < 0) {
+      return (std::round(std::nextafter(number, -kInf) * factor * -1) /
+              factor) *
+          -1;
+    }
+    return std::round(std::nextafter(number, kInf) * factor) / factor;
+  }
+
+  template <typename TInput>
+  FOLLY_ALWAYS_INLINE void
+  call(TInput& result, const TInput& a, const int32_t b = 0) {
+    result = round(a, b);
+  }
+};
+
+template <typename T>
 struct RemainderFunction {
   template <typename TInput>
   FOLLY_ALWAYS_INLINE bool
--- velox/functions/sparksql/tests/ArithmeticTest.cpp
+++ velox/functions/sparksql/tests/ArithmeticTest.cpp
@@ -21,6 +21,100 @@
 namespace facebook::velox::functions::sparksql::test {
 namespace {
 
+class RoundTest : public SparkFunctionBaseTest {
+ protected:
+  template <typename T>
+  void runRoundTest(const std::vector<std::tuple<T, T>>& data) {
+    auto result = evaluate<SimpleVector<T>>(
+        "round(c0)", makeRowVector({makeFlatVector<T, 0>(data)}));
+    for (int32_t i = 0; i < data.size(); ++i) {
+      ASSERT_EQ(result->valueAt(i), std::get<1>(data[i]));
+    }
+  }
+
+  template <typename T>
+  void runRoundWithDecimalTest(
+      const std::vector<std::tuple<T, int32_t, T>>& data) {
+    auto result = evaluate<SimpleVector<T>>(
+        "round(c0, c1)",
+        makeRowVector(
+            {makeFlatVector<T, 0>(data), makeFlatVector<int32_t, 1>(data)}));
+    for (int32_t i = 0; i < data.size(); ++i) {
+      ASSERT_EQ(result->valueAt(i), std::get<2>(data[i]));
+    }
+  }
+
+  template <typename T>
+  std::vector<std::tuple<T, T>> testRoundFloatData() {
+    return {
+        {1.0, 1.0},
+        {1.9, 2.0},
+        {1.3, 1.0},
+        {0.0, 0.0},
+        {0.9999, 1.0},
+        {-0.9999, -1.0},
+        {1.0 / 9999999, 0},
+        {123123123.0 / 9999999, 12.0}};
+  }
+
+  template <typename T>
+  std::vector<std::tuple<T, T>> testRoundIntegralData() {
+    return {{1, 1}, {0, 0}, {-1, -1}};
+  }
+
+  template <typename T>
+  std::vector<std::tuple<T, int32_t, T>> testRoundWithDecFloatAndDoubleData() {
+    return {{1.122112, 0, 1},         {1.129, 1, 1.1},
+            {1.129, 2, 1.13},         {1.0 / 3, 0, 0.0},
+            {1.0 / 3, 1, 0.3},        {1.0 / 3, 2, 0.33},
+            {1.0 / 3, 6, 0.333333},   {-1.122112, 0, -1},
+            {-1.129, 1, -1.1},        {-1.129, 2, -1.13},
+            {-1.129, 2, -1.13},       {-1.0 / 3, 0, 0.0},
+            {-1.0 / 3, 1, -0.3},      {-1.0 / 3, 2, -0.33},
+            {-1.0 / 3, 6, -0.333333}, {1.0, -1, 0.0},
+            {0.0, -2, 0.0},           {-1.0, -3, 0.0},
+            {11111.0, -1, 11110.0},   {11111.0, -2, 11100.0},
+            {11111.0, -3, 11000.0},   {11111.0, -4, 10000.0},
+            {0.575, 2, 0.58},         {0.574, 2, 0.57},
+            {-0.575, 2, -0.58},       {-0.574, 2, -0.57}};
+  }
+
+  template <typename T>
+  std::vector<std::tuple<T, int32_t, T>> testRoundWithDecIntegralData() {
+    return {
+        {1, 0, 1},
+        {0, 0, 0},
+        {-1, 0, -1},
+        {1, 1, 1},
+        {0, 1, 0},
+        {-1, 1, -1},
+        {1, 10, 1},
+        {0, 10, 0},
+        {-1, 10, -1},
+        {1, -1, 1},
+        {0, -2, 0},
+        {-1, -3, -1}};
+  }
+};
+
+TEST_F(RoundTest, round) {
+  runRoundTest<float>(testRoundFloatData<float>());
+  runRoundTest<double>(testRoundFloatData<double>());
+  runRoundTest<int64_t>(testRoundIntegralData<int64_t>());
+  runRoundTest<int32_t>(testRoundIntegralData<int32_t>());
+  runRoundTest<int16_t>(testRoundIntegralData<int16_t>());
+  runRoundTest<int8_t>(testRoundIntegralData<int8_t>());
+}
+
+TEST_F(RoundTest, roundWithDecimal) {
+  runRoundWithDecimalTest<float>(testRoundWithDecFloatAndDoubleData<float>());
+  runRoundWithDecimalTest<double>(testRoundWithDecFloatAndDoubleData<double>());
+  runRoundWithDecimalTest<int64_t>(testRoundWithDecIntegralData<int64_t>());
+  runRoundWithDecimalTest<int32_t>(testRoundWithDecIntegralData<int32_t>());
+  runRoundWithDecimalTest<int16_t>(testRoundWithDecIntegralData<int16_t>());
+  runRoundWithDecimalTest<int8_t>(testRoundWithDecIntegralData<int8_t>());
+}
+
 class PmodTest : public SparkFunctionBaseTest {
  protected:
   template <typename T>
