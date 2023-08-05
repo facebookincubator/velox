90d80cad9ddcd2a83a20427551f5e00eebb34d34
Zhe Wan <zhe.wan@intel.com>
2023-08-05 11:19:54 +0800

    Add date_sub Spark function

--- velox/functions/sparksql/DateTimeFunctions.h
+++ velox/functions/sparksql/DateTimeFunctions.h
@@ -213,10 +213,8 @@ template <typename T>
 struct DateSubFunction {
   VELOX_DEFINE_FUNCTION_TYPES(T);
 
-  FOLLY_ALWAYS_INLINE void call(
-      out_type<Date>& result,
-      const arg_type<Date>& date,
-      const int32_t value) 
+  FOLLY_ALWAYS_INLINE void
+  call(out_type<Date>& result, const arg_type<Date>& date, const int32_t value) 
 #if defined(__has_feature)
 #if __has_feature(__address_sanitizer__)
       __attribute__((__no_sanitize__("signed-integer-overflow")))
