b73d97fa001070789e9222405ea7f2eebc9dd82e
GOPU-Krishna-S <GOPU.Krishna.S@ibm.com>
2023-08-23 16:23:27 +0530

    Added huge int support in arbitrary() function

--- velox/functions/prestosql/aggregates/ArbitraryAggregate.cpp
+++ velox/functions/prestosql/aggregates/ArbitraryAggregate.cpp
@@ -289,6 +289,11 @@ exec::AggregateRegistrationResult registerArbitraryAggregate(
             return std::make_unique<ArbitraryAggregate<int32_t>>(inputType);
           case TypeKind::BIGINT:
             return std::make_unique<ArbitraryAggregate<int64_t>>(inputType);
+          case TypeKind::HUGEINT:
+            if (inputType->isLongDecimal()) {
+              return std::make_unique<ArbitraryAggregate<int128_t>>(inputType);
+            }
+            VELOX_NYI();
           case TypeKind::REAL:
             return std::make_unique<ArbitraryAggregate<float>>(inputType);
           case TypeKind::DOUBLE:
