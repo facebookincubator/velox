a279966d94fdd8f8b2d5511999f7c07be8e1acc8
Orri Erling <oerling@fb.com>
2023-08-25 11:34:38 -0700

    Add Wave Projection

--- velox/experimental/wave/exec/WaveCore.cuh
+++ velox/experimental/wave/exec/WaveCore.cuh
@@ -21,7 +21,7 @@
 #include "velox/experimental/wave/vector/Operand.h"
 
 namespace facebook::velox::wave {
-
+  
 template <typename T>
 __device__ inline T& flatValue(void* base, int32_t blockBase) {
   return reinterpret_cast<T*>(base)[blockBase + threadIdx.x];
@@ -32,22 +32,31 @@ __device__ inline bool isNull(Operand* op, int32_t blockBase) {
 }
 
 template <typename T>
-__device__ inline T value(Operand* op, int32_t blockBase, char* shared) {
-  int32_t index = (threadIdx.x + blockBase) & op->indexMask;
-  void* base = op->sharedOffset != Operand::kGlobal ? shared + op->sharedOffset
-                                                    : op->base;
+__device__ inline T getOperand(Operand** operands, OperandIndex opIdx, int32_t blockBase, char* shared) {
+  if (opIdx > kMinSharedMemIndex) {
+    return reinterpret_cast<T*>(shared + opIdx - kMinSharedMemIndex)[blockIdx.x];
+  }
+  auto op = operands[opIdx];
+int32_t index = (threadIdx.x + blockBase) & op->indexMask;
   if (auto indicesInOp = op->indices) {
     auto indices = indicesInOp[blockBase / kBlockSize];
     if (indices) {
       index = indices[index];
     }
   }
-  return reinterpret_cast<const T*>(base)[index];
+  return reinterpret_cast<const T*>(op->base)[index];
 }
 
 template <typename T>
-__device__ T& flatResult(Operand* op, int32_t blockBase) {
-  return flatValue<T>(op->base, blockBase);
+__device__ inline T& flatResult(Operand** operands, OperandIndex opIdx,  int32_t blockBase, char* shared) {
+  if (opIdx >= kMinSharedMemIndex) {
+    return reinterpret_cast<T*>(shared  + opIdx - kMinSharedMemIndex)[threadIdx.x];
+  }
+  auto* op = operands[opIdx];
+  if (op->nulls) {
+    op->nulls[blockBase + threadIdx.x] = kNotNull;
+}
+return reinterpret_cast<T*>(op->base)[blockBase + threadIdx.x];
 }
 
 } // namespace facebook::velox::wave
