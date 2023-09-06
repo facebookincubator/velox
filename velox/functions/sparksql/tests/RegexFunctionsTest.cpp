198f5bec163be48a72525ddf6ddecd6851185f30
Schierbeck, Cody <cody.schierbeck@intel.com>
2023-09-06 12:58:03 -0700

    Added extensive tests for RegexReplace

--- velox/functions/sparksql/tests/RegexFunctionsTest.cpp
+++ velox/functions/sparksql/tests/RegexFunctionsTest.cpp
@@ -300,12 +300,20 @@ TEST_F(RegexFunctionsTest, RegexReplaceRegistration) {
   regex_replace_constant({"abc"}, "a", "teehee", {{{"teeheebc"}}});
 }
 
-TEST_F(RegexFunctionsTest, RegexReplaceTwoParams) {
+TEST_F(RegexFunctionsTest, RegexReplaceEmptyString) {
   regex_replace_constant({"abc"}, "a", "", {{{"bc"}}});
+}
+
+TEST_F(RegexFunctionsTest, RegexBadJavaPattern){
+  EXPECT_THROW(regex_replace_constant({"[]"}, "[a[b]]", "", {{{""}}}), VeloxUserError);
+  EXPECT_THROW(regex_replace_constant({"[]"}, "[a&&[b]]", "", {{{""}}}), VeloxUserError);
+  EXPECT_THROW(regex_replace_constant({"[]"}, "[a&&[^b]]","", {{{""}}}), VeloxUserError);
 }
 
 TEST_F(RegexFunctionsTest, RegexReplacePosition) {
   regex_replace_constant({"abca"}, "a", "", {{{"abc"}}}, {2});
+  regex_replace_constant({"abca"}, "a", "", {{{"bc"}}}, {0});
+  regex_replace_constant({"abca"}, "bc", "aaa", {{{"aaaaa"}}}, {1});
 }
 
 TEST_F(RegexFunctionsTest, RegexReplaceNegativePosition) {
@@ -314,17 +322,141 @@ TEST_F(RegexFunctionsTest, RegexReplaceNegativePosition) {
       VeloxRuntimeError);
 }
 
-TEST_F(RegexFunctionsTest, RegexReplaceDataframe) {
-  regex_replace_rows_position(
-      {"abca", "this is the second"},
-      {"a", " is"}, // include a space to not take the "is" in "this"
-      {"", " is not"},
-      {{std::vector<std::string>{"abc"}},
-       {std::vector<std::string>{
-           "this is not the second"}}}, // Need two vectors two represent two
-                                        // rows of size 1 for conversion
-      {2, 0});
+TEST_F(RegexFunctionsTest, RegexReplacePositionTooLarge) {
+  EXPECT_THROW(
+    regex_replace_constant({"abc"},"a", "", {{{"abc"}}}, {1000}),
+    VeloxRuntimeError);
+  EXPECT_THROW(
+    regex_replace_constant({"abc"},"a", "", {{{"abc"}}}, {4}),
+    VeloxRuntimeError);
 }
+
+TEST_F(RegexFunctionsTest, RegexReplaceSpecialCharacters) {
+  regex_replace_constant({"a.b.c.a"}, "\\.", "", {{{"abca"}}});
+}
+
+TEST_F(RegexFunctionsTest, RegexReplaceNoReplacement) {
+  regex_replace_constant({"abcde"}, "f", "z", {{{"abcde"}}});
+}
+
+TEST_F(RegexFunctionsTest, RegexReplaceMultipleMatches) {
+  regex_replace_constant({"aa"}, "a", "b", {{{"bb"}}});
+}
+
+TEST_F(RegexFunctionsTest, RegexReplaceWord) {
+  regex_replace_constant({"I like pie"}, "pie", "cake", {{{"I like cake"}}});
+}
+
+TEST_F(RegexFunctionsTest, RegexReplaceEscapedCharacters) {
+  regex_replace_constant({"abc\\de"}, "\\\\", "", {{{"abcde"}}});
+}
+
+TEST_F(RegexFunctionsTest, RegexReplacePatternBeforePosition) {
+  regex_replace_constant({"abcdef"}, "d", "z", {{{"abcdef"}}}, 4);
+}
+
+TEST_F(RegexFunctionsTest, RegexReplaceDataframe) {
+    // Basic Replacement
+    regex_replace_rows_position(
+        {"hello world", "coding is fun"},
+        {"hello", " is"},
+        {"hi", " was"},
+        {{std::vector<std::string>{"hi world"}},
+        {std::vector<std::string>{"coding was fun"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeMultiple) {
+    // Multiple matches
+    regex_replace_rows_position(
+        {"apple apple apple", "banana banana banana"},
+        {"apple", "banana"},
+        {"fruit", "fruit"},
+        {{std::vector<std::string>{"fruit fruit fruit"}},
+        {std::vector<std::string>{"fruit fruit fruit"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeSpecial) {
+    // Special characters
+    regex_replace_rows_position(
+        {"a.b.c", "[coding]"},
+        {R"(\.)", R"(\[|\])"},
+        {"-", ""},
+        {{std::vector<std::string>{"a-b-c"}},
+        {std::vector<std::string>{"coding"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeSizes) {
+    // Replacement with different sizes
+    regex_replace_rows_position(
+        {"good day", "shorter"},
+        {"good", "shorter"},
+        {"fantastic", "short"},
+        {{std::vector<std::string>{"fantastic day"}},
+        {std::vector<std::string>{"short"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeNoMatches) {
+    // No matches
+    regex_replace_rows_position(
+        {"apple", "banana"},
+        {"orange", "grape"},
+        {"fruit", "fruit"},
+        {{std::vector<std::string>{"apple"}},
+        {std::vector<std::string>{"banana"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeOffsetPosition) {
+    // Offset position
+    regex_replace_rows_position(
+        {"apple pie apple", "grape banana grape"},
+        {"apple", "banana"},
+        {"fruit", "fruit"},
+        {{std::vector<std::string>{"apple pie fruit"}},
+        {std::vector<std::string>{"grape fruit grape"}}},
+        {9, 6});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceEmptyStringsAndPatterns) {
+    // Empty strings and patterns
+    regex_replace_rows_position(
+        {"", "hello"},
+        {"", "llo"},
+        {"prefix ", " world"},
+        {{std::vector<std::string>{"prefix "}},
+        {std::vector<std::string>{"he world"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeCharacterTypes) {
+    // Multiple character types
+    regex_replace_rows_position(
+        {"123ABC", "!@#"},
+        {R"(\d)", R"(\W)"},
+        {"X", "Y"},
+        {{std::vector<std::string>{"XXXABC"}},
+        {std::vector<std::string>{"YYY"}}},
+        {0, 0});
+}
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeBadPosition) {
+    // Larger offsets than string size
+    EXPECT_THROW(regex_replace_rows_position(
+        {"apple", "banana"},
+        {"apple", "banana"},
+        {"fruit", "fruit"},
+        {{std::vector<std::string>{"apple"}},
+        {std::vector<std::string>{"banana"}}},
+        {10, 15}),
+        VeloxRuntimeError);
+}
+
+TEST_F(RegexFunctionsTest, RegexReplaceDataframeLastCharacter) {
+  regex_replace_rows_position(
+    {"applez", "bananaz"},
+    {"z", "z"},
+    {"", ""},
+    {{std::vector<std::string>{"apple"}},
+    {std::vector<std::string>{"banana"}}},
+    {5,6});
+}
+
 // Test to match
 // https://github.com/apache/spark/blob/master/sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala#L180-L184
 // This test is the crux of why regex_replace needed to support non-constant
