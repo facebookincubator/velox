e64db65f546e879e38a6e9a9112f76a1c9d1d885
xiaodou <chenshuyu.csy@alibaba-inc.com>
2023-09-07 11:13:19 +0800

    Add comments and test cases of different errors

--- velox/functions/prestosql/tests/JsonFunctionsTest.cpp
+++ velox/functions/prestosql/tests/JsonFunctionsTest.cpp
@@ -369,6 +369,11 @@ false, false, false, false, false, false, true, false, false, false, false])",
 true, true, true, true, true, true, true, true, true, true, true])",
           false),
       false);
+
+  // Test errors of getting the specified type of json value.
+  // Error code is "INCORRECT_TYPE".
+  EXPECT_EQ(jsonArrayContains<bool>(R"([truet])", false), false);
+  EXPECT_EQ(jsonArrayContains<bool>(R"([truet, false])", false), true);
 }
 
 TEST_F(JsonFunctionsTest, jsonArrayContainsBigint) {
@@ -405,8 +410,15 @@ TEST_F(JsonFunctionsTest, jsonArrayContainsBigint) {
           23),
       false);
   EXPECT_EQ(jsonArrayContains<int64_t>(R"([92233720368547758071])", -9), false);
+
+  // Test errors of getting the specified type of json value.
+  // Error code is "INCORRECT_TYPE".
+  EXPECT_EQ(jsonArrayContains<int64_t>(R"([-9223372036854775809])", -9), false);
   EXPECT_EQ(
-      jsonArrayContains<int64_t>(R"([92233720368547758071,-9])", -9), true);
+      jsonArrayContains<int64_t>(R"([-9223372036854775809,-9])", -9), true);
+  // Error code is "NUMBER_ERROR".
+  EXPECT_EQ(jsonArrayContains<int64_t>(R"([01])", 4), false);
+  EXPECT_EQ(jsonArrayContains<int64_t>(R"([01, 4])", 4), true);
 }
 
 TEST_F(JsonFunctionsTest, jsonArrayContainsDouble) {
@@ -444,6 +456,9 @@ TEST_F(JsonFunctionsTest, jsonArrayContainsDouble) {
           R"([1.2, 2.3, 3.4, 4.5, 1.2, 2.3, 3.4, 4.5, 1.2, 2.3, 3.4, 4.5, 1.2, 2.3, 3.4, 4.5, 1.2, 2.3, 3.4, 4.5])",
           4.3),
       false);
+
+  // Test errors of getting the specified type of json value.
+  // Error code is "NUMBER_ERROR".
   EXPECT_EQ(jsonArrayContains<double>(R"([9.6E400])", 4.2), false);
   EXPECT_EQ(jsonArrayContains<double>(R"([9.6E400,4.2])", 4.2), true);
 }
