b88cd3d49d2541afdbb3cd57c2789d9f6549b672
gouyikui <gouyikui@baidu.com>
2023-09-13 20:39:09 +0800

    ut fix
    Change-Id: I08f7d6cf4392f393006cbc677c88e918d5ec9a75

--- velox/functions/sparksql/tests/StringTest.cpp
+++ velox/functions/sparksql/tests/StringTest.cpp
@@ -868,9 +868,7 @@ TEST_F(StringTest, concat_ws) {
     });
 
     auto result = evaluate<SimpleVector<StringView>>(
-        "concat_ws('----', c0)",
-        makeRowVector(
-            {makeConstant("----", arrayVector->size()), arrayVector}));
+        "concat_ws('----', c0)", makeRowVector({arrayVector}));
 
     auto expected = makeArrayVector<StringView>({
         {S("red----blue")},
