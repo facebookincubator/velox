34b298c9ede8860622744cc35603e0ee3756fe3f
Deepak Majeti <deepak.majeti@ibm.com>
2023-10-03 15:49:10 -0400

    Remove Parquet amalgamation

--- velox/dwio/common/tests/BitPackDecoderBenchmark.cpp
+++ velox/dwio/common/tests/BitPackDecoderBenchmark.cpp
@@ -23,8 +23,7 @@
 #endif
 
 #include "velox/external/duckdb/duckdb-fastpforlib.hpp"
-#include "velox/external/duckdb/parquet-amalgamation.hpp"
-#include "velox/vector/TypeAliases.h"
+#include "velox/external/duckdb/duckdb-parquetdecodeutils.hpp"
 
 #include <arrow/util/rle_encoding.h>
 #include <folly/Benchmark.h>
@@ -37,6 +36,77 @@ using namespace facebook::velox;
 using RowSet = folly::Range<const facebook::velox::vector_size_t*>;
 
 static const uint64_t kNumValues = 1024768 * 8;
+const uint64_t duckdb::ParquetDecodeUtils::BITPACK_MASKS[] = {
+    0,
+    1,
+    3,
+    7,
+    15,
+    31,
+    63,
+    127,
+    255,
+    511,
+    1023,
+    2047,
+    4095,
+    8191,
+    16383,
+    32767,
+    65535,
+    131071,
+    262143,
+    524287,
+    1048575,
+    2097151,
+    4194303,
+    8388607,
+    16777215,
+    33554431,
+    67108863,
+    134217727,
+    268435455,
+    536870911,
+    1073741823,
+    2147483647,
+    4294967295,
+    8589934591,
+    17179869183,
+    34359738367,
+    68719476735,
+    137438953471,
+    274877906943,
+    549755813887,
+    1099511627775,
+    2199023255551,
+    4398046511103,
+    8796093022207,
+    17592186044415,
+    35184372088831,
+    70368744177663,
+    140737488355327,
+    281474976710655,
+    562949953421311,
+    1125899906842623,
+    2251799813685247,
+    4503599627370495,
+    9007199254740991,
+    18014398509481983,
+    36028797018963967,
+    72057594037927935,
+    144115188075855871,
+    288230376151711743,
+    576460752303423487,
+    1152921504606846975,
+    2305843009213693951,
+    4611686018427387903,
+    9223372036854775807,
+    18446744073709551615ULL};
+
+const uint64_t duckdb::ParquetDecodeUtils::BITPACK_MASKS_SIZE =
+    sizeof(ParquetDecodeUtils::BITPACK_MASKS) / sizeof(uint64_t);
+
+const uint8_t duckdb::ParquetDecodeUtils::BITPACK_DLEN = 8;
 
 // Array of bit packed representations of randomInts_u32. The array at index i
 // is packed i bits wide and the values come from the low bits of
