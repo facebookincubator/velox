316d525cb869934d8232bf716ac6ac11a67636ac
Krishna Pai <kpai@fb.com>
2023-10-13 13:16:41 -0700

    Fix row_constructor to create trailing nulls.

--- velox/functions/prestosql/RowFunction.cpp
+++ velox/functions/prestosql/RowFunction.cpp
@@ -28,13 +28,18 @@ class RowFunction : public exec::VectorFunction {
       exec::EvalCtx& context,
       VectorPtr& result) const override {
     auto argsCopy = args;
+
+    for (auto& child : args) {
+      VELOX_CHECK_GE(child->size(), rows.end(), "Child size less than Row size")
+    }
+
     RowVectorPtr row = std::make_shared<RowVector>(
         context.pool(),
         outputType,
         BufferPtr(nullptr),
         rows.end(),
         std::move(argsCopy),
-        0 /*nullCount*/);
+        0);
     context.moveOrCopyResult(row, rows, result);
   }
 
