95802368653e61a8d4a9313cce1d3cfb9c036f42
mayan <yan.ma@intel.com>
2023-10-17 05:33:06 +0000

    switch to benchmark using RowContainer

--- velox/exec/benchmarks/RowContainerSortBenchmark.cpp
+++ velox/exec/benchmarks/RowContainerSortBenchmark.cpp
@@ -17,7 +17,9 @@
 #include <folly/Benchmark.h>
 #include <folly/init/Init.h>
 
+#include "velox/exec/RowContainer.h"
 #include "velox/external/timsort/TimSort.hpp"
+#include "velox/vector/DecodedVector.h"
 #include "velox/vector/SimpleVector.h"
 #include "velox/vector/tests/VectorTestUtils.h"
 #include "velox/vector/tests/utils/VectorMaker.h"
@@ -26,12 +28,12 @@ namespace facebook::velox::test {
 namespace {
 
 template <typename T>
-std::vector<std::optional<T>>
+VectorGeneratedData<T>
 testData(size_t length, size_t cardinality, bool sequences, size_t iteration) {
   int32_t seqCount =
       sequences ? std::max(2, (int32_t)(length / cardinality)) : 0;
   int32_t seqLength = sequences ? std::max(2, (int32_t)(length / seqCount)) : 0;
-  auto data = genTestDataWithSequences<T>(
+  return genTestDataWithSequences<T>(
       length,
       cardinality,
       false /* isSorted */,
@@ -40,42 +42,80 @@ testData(size_t length, size_t cardinality, bool sequences, size_t iteration) {
       seqLength,
       false /* useFullTypeRange */,
       length + iteration /* seed */);
-  // Moveing data to unconst for sort
-  std::vector<std::optional<T>> stdVector(data.data().size());
-  for (int i = 0; i < data.data().size(); i++) {
-    stdVector[i] = data.data()[i];
+}
+
+std::vector<char*> store(
+    velox::exec::RowContainer& rowContainer,
+    DecodedVector& decodedVector,
+    vector_size_t size) {
+  std::vector<char*> rows(size);
+  for (size_t row = 0; row < size; ++row) {
+    rows[row] = rowContainer.newRow();
+    rowContainer.store(decodedVector, row, rows[row], 0);
   }
-  return stdVector;
+  return rows;
 }
 
 template <typename T>
-void vectorStdSortBenchmark(
+void rowContainerStdSortBenchmark(
     uint32_t iterations,
     size_t length,
     size_t cardinality,
     bool sequences) {
   folly::BenchmarkSuspender suspender;
+  auto pool = memory::addDefaultLeafMemoryPool();
+  VectorMaker vectorMaker(pool.get());
 
   for (size_t k = 0; k < iterations; ++k) {
     auto data = testData<T>(length, cardinality, sequences, k);
+    auto vector =
+        vectorMaker.encodedVector<T>(VectorEncoding::Simple::FLAT, data.data());
+    DecodedVector decoded(*vector);
+    // Create row container.
+    std::vector<TypePtr> types{vector->type()};
+
+    // Store the vector in the rowContainer.
+    auto rowContainer =
+        std::make_unique<velox::exec::RowContainer>(types, pool.get());
+    int size = data.data().size();
+    auto rows = store(*rowContainer, decoded, size);
     suspender.dismiss();
-    std::sort(data.begin(), data.end());
+    std::sort(
+        rows.begin(), rows.end(), [&](const char* left, const char* right) {
+          return rowContainer->compareRows(left, right) < 0;
+        });
     suspender.rehire();
   }
 }
 
 template <typename T>
-void vectorTimSortBenchmark(
+void rowContainerTimSortBenchmark(
     uint32_t iterations,
     size_t length,
     size_t cardinality,
     bool sequences) {
   folly::BenchmarkSuspender suspender;
+  auto pool = memory::addDefaultLeafMemoryPool();
+  VectorMaker vectorMaker(pool.get());
 
   for (size_t k = 0; k < iterations; ++k) {
     auto data = testData<T>(length, cardinality, sequences, k);
+    auto vector =
+        vectorMaker.encodedVector<T>(VectorEncoding::Simple::FLAT, data.data());
+    DecodedVector decoded(*vector);
+    // Create row container.
+    std::vector<TypePtr> types{vector->type()};
+
+    // Store the vector in the rowContainer.
+    auto rowContainer =
+        std::make_unique<velox::exec::RowContainer>(types, pool.get());
+    int size = vector->size();
+    auto rows = store(*rowContainer, decoded, size);
     suspender.dismiss();
-    gfx::timsort(data.begin(), data.end());
+    gfx::timsort(
+        rows.begin(), rows.end(), [&](const char* left, const char* right) {
+          return rowContainer->compareRows(left, right) < 0;
+        });
     suspender.rehire();
   }
 }
@@ -85,7 +125,8 @@ void BM_Int64_stdSort(
     size_t numRows,
     size_t cardinality,
     bool sequences) {
-  vectorStdSortBenchmark<int64_t>(iterations, numRows, cardinality, sequences);
+  rowContainerStdSortBenchmark<int64_t>(
+      iterations, numRows, cardinality, sequences);
 }
 
 void BM_Int64_timSort(
@@ -93,7 +134,8 @@ void BM_Int64_timSort(
     size_t numRows,
     size_t cardinality,
     bool sequences) {
-  vectorTimSortBenchmark<int64_t>(iterations, numRows, cardinality, sequences);
+  rowContainerTimSortBenchmark<int64_t>(
+      iterations, numRows, cardinality, sequences);
 }
 } // namespace
 
