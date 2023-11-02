96f1d098d055174a8ada5f372c17c65478428b12
duanmeng <macduan@tencent.com>
2023-11-02 19:43:40 +0800

    add simple http client

---
+++ velox/exec/tests/CprHttpClientTest.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "folly/json.h"
+#include "velox/common/base/VeloxException.h"
+#include "velox/common/base/tests/GTestUtils.h"
+
+#include <cpr/cpr.h>
+#include <map>
+#include <string>
+#include <thread>
+
+class CprHttpClientTest : public testing::Test {};
+
+TEST_F(CprHttpClientTest, basic) {
+  auto response = cpr::Get(
+      cpr::Url{"https://facebookincubator.github.io/velox/"},
+      cpr::Timeout{std::chrono::seconds{3}});
+  ASSERT_EQ(response.status_code, 200);
+  ASSERT_FALSE(response.text.empty());
+
+  response = cpr::Get(cpr::Url{"null"});
+  ASSERT_NE(response.status_code, 200);
+  ASSERT_TRUE(response.text.empty());
+
+  response = cpr::Post(
+      cpr::Url{"https://facebookincubator.github.io/velox/"},
+      cpr::Body{"select * from nation limit 1"},
+      cpr::Header({{"Content-Type", "text/plain"}}));
+  ASSERT_EQ(response.status_code, 405);
+  ASSERT_FALSE(response.text.empty());
+
+  response = cpr::Post(
+      cpr::Url{"null"},
+      cpr::Body{"select * from nation limit 1"},
+      cpr::Header({{"Content-Type", "text/plain"}}));
+  ASSERT_NE(response.status_code, 200);
+  ASSERT_TRUE(response.text.empty());
+}
