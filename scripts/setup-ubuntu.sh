8e28190e460c8fcf8bb9b40f58e464b0e8a68d9f
Jacob Wujciak-Jens <jacob@wujciak.de>
2024-01-24 04:59:35 +0100

    fix ubuntu setup

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -102,6 +102,11 @@ function install_wangle {
   cmake_install -DBUILD_TESTS=OFF -S wangle
 }
 
+function install_mvfst {
+  github_checkout facebook/mvfst "${FB_OS_VERSION}"
+  cmake_install -DBUILD_TESTS=OFF
+}
+
 function install_fbthrift {
   github_checkout facebook/fbthrift "${FB_OS_VERSION}"
   cmake_install -DBUILD_TESTS=OFF
@@ -118,6 +123,7 @@ function install_velox_deps {
   run_and_time install_folly
   run_and_time install_fizz
   run_and_time install_wangle
+  run_and_time install_mvfst
   run_and_time install_fbthrift
   run_and_time install_conda
 }
