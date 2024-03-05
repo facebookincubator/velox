b2032d556043b5fc18f4f3c27f30027ad6f90859
duanmeng <macduan@tencent.com>
2024-03-05 19:37:31 +0800

    Remove testWithTableScan flag

--- velox/functions/prestosql/aggregates/tests/MapAggTest.cpp
+++ velox/functions/prestosql/aggregates/tests/MapAggTest.cpp
@@ -150,13 +150,7 @@ TEST_F(MapAggTest, groupByWithDuplicates) {
 
   // We don't test with TableScan because when there are duplicate keys in
   // different splits, the result is non-deterministic.
-  testAggregations(
-      vectors,
-      {"c0"},
-      {"map_agg(c1, c2)"},
-      {expectedResult},
-      /*config*/ {},
-      /*testWithTableScan*/ false);
+  testAggregations(vectors, {"c0"}, {"map_agg(c1, c2)"}, {expectedResult});
 }
 
 TEST_F(MapAggTest, groupByNoData) {
@@ -310,13 +304,7 @@ TEST_F(MapAggTest, globalDuplicateKeys) {
 
   // We don't test with TableScan because when there are duplicate keys in
   // different splits, the result is non-deterministic.
-  testAggregations(
-      vectors,
-      {},
-      {"map_agg(c0, c1)"},
-      {expectedResult},
-      /*config*/ {},
-      /*testWithTableScan*/ false);
+  testAggregations(vectors, {}, {"map_agg(c0, c1)"}, {expectedResult});
 }
 
 /// Reproduces the bug reported in
@@ -373,20 +361,9 @@ TEST_F(MapAggTest, unknownKey) {
   });
 
   testAggregations(
-      {data},
-      {"c0"},
-      {"map_agg(c1, c2)"},
-      "VALUES (1, NULL), (2, NULL)",
-      {},
-      false /*testWithTableScan*/);
+      {data}, {"c0"}, {"map_agg(c1, c2)"}, "VALUES (1, NULL), (2, NULL)");
 
-  testAggregations(
-      {data},
-      {},
-      {"map_agg(c1, c2)"},
-      "VALUES (NULL)",
-      {},
-      false /*testWithTableScan*/);
+  testAggregations({data}, {}, {"map_agg(c1, c2)"}, "VALUES (NULL)");
 }
 
 TEST_F(MapAggTest, stringLifeCycle) {
