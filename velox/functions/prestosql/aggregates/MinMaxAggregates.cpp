4ca50f477a517530916fc9c168769d23027be392
Minhan Cao <minhancao@Minhans-MBP.attlocal.net>
2024-03-07 14:55:11 -0800

    Adding decimal support for min() and max() functions

--- velox/functions/prestosql/aggregates/MinMaxAggregates.cpp
+++ velox/functions/prestosql/aggregates/MinMaxAggregates.cpp
@@ -916,6 +916,17 @@ exec::AggregateRegistrationResult registerMinMax(
             .build());
   }
 
+  // decimal(p,s), bigint -> row(array(decimal(p,s)), bigint) -> array(decimal(p,s))
+  signatures.push_back(
+      exec::AggregateFunctionSignatureBuilder()
+          .integerVariable("a_precision")
+          .integerVariable("a_scale")
+          .argumentType("DECIMAL(a_precision, a_scale)")
+          .argumentType("bigint")
+          .intermediateType("row(bigint, array(DECIMAL(a_precision, a_scale)))")
+          .returnType("array(DECIMAL(a_precision, a_scale))")
+          .build());
+
   return exec::registerAggregateFunction(
       name,
       std::move(signatures),
@@ -952,7 +963,10 @@ exec::AggregateRegistrationResult registerMinMax(
             case TypeKind::TIMESTAMP:
               return std::make_unique<TNumericN<Timestamp>>(resultType);
             case TypeKind::HUGEINT:
-              return std::make_unique<TNumericN<int128_t>>(resultType);
+              if (inputType->isLongDecimal()) {
+                return std::make_unique<TNumericN<int128_t>>(resultType);
+              }
+              VELOX_NYI();
             default:
               VELOX_CHECK(
                   false,
