ae213897db272870831687a41becbc3881d5c8e6
joey.ljy <joey.ljy@alibaba-inc.com>
2024-03-19 21:25:17 +0800

    Move additional types validation

--- velox/expression/FunctionSignature.cpp
+++ velox/expression/FunctionSignature.cpp
@@ -146,7 +146,12 @@ void validate(
     const TypeSignature& returnType,
     const std::vector<TypeSignature>& argumentTypes,
     const std::vector<bool>& constantArguments,
-    std::unordered_set<std::string> usedVariables) {
+    const std::vector<TypeSignature>& additionalTypes = {}) {
+  std::unordered_set<std::string> usedVariables;
+  // Validate the additional types, and collect the used variables.
+  for (const auto& type : additionalTypes) {
+    validateBaseTypeAndCollectTypeParams(variables, type, usedVariables, false);
+  }
   // Validate the argument types.
   for (const auto& arg : argumentTypes) {
     // Is base type a type parameter or a built in type ?
@@ -212,7 +217,7 @@ FunctionSignature::FunctionSignature(
       argumentTypes_{std::move(argumentTypes)},
       constantArguments_{std::move(constantArguments)},
       variableArity_{variableArity} {
-  validate(variables_, returnType_, argumentTypes_, constantArguments_, {});
+  validate(variables_, returnType_, argumentTypes_, constantArguments_);
 }
 
 FunctionSignature::FunctionSignature(
@@ -227,17 +232,12 @@ FunctionSignature::FunctionSignature(
       argumentTypes_{std::move(argumentTypes)},
       constantArguments_{std::move(constantArguments)},
       variableArity_{variableArity} {
-  std::unordered_set<std::string> usedVariables;
-  for (const auto& type : additionalTypes) {
-    validateBaseTypeAndCollectTypeParams(
-        variables_, type, usedVariables, false);
-  }
   validate(
       variables_,
       returnType_,
       argumentTypes_,
       constantArguments_,
-      usedVariables);
+      additionalTypes);
 }
 
 std::string AggregateFunctionSignature::toString() const {
