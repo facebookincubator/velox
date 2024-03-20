0cdba867016257e3e4e1c13cd306cc0709d5a52d
mwish <maplewish117@gmail.com>
2024-03-21 00:07:01 +0800

    remove codegen code

--- velox/core/QueryConfig.h
+++ velox/core/QueryConfig.h
@@ -48,16 +48,9 @@ class QueryConfig {
 
   explicit QueryConfig(std::unordered_map<std::string, std::string>&& values);
 
-  static constexpr const char* kCodegenEnabled = "codegen.enabled";
-
   /// Maximum memory that a query can use on a single host.
   static constexpr const char* kQueryMaxMemoryPerNode =
       "query_max_memory_per_node";
-
-  static constexpr const char* kCodegenConfigurationFilePath =
-      "codegen.configuration_file_path";
-
-  static constexpr const char* kCodegenLazyLoading = "codegen.lazy_loading";
 
   /// User provided session timezone. Stores a string with the actual timezone
   /// name, e.g: "America/Los_Angeles".
@@ -497,18 +490,6 @@ class QueryConfig {
 
   bool isMatchStructByName() const {
     return get<bool>(kCastMatchStructByName, false);
-  }
-
-  bool codegenEnabled() const {
-    return get<bool>(kCodegenEnabled, false);
-  }
-
-  std::string codegenConfigurationFilePath() const {
-    return get<std::string>(kCodegenConfigurationFilePath, "");
-  }
-
-  bool codegenLazyLoading() const {
-    return get<bool>(kCodegenLazyLoading, true);
   }
 
   bool adjustTimestampToTimezone() const {
