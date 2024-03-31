7062ef10ad8a326596e519c0ede4dfc3a094336c
wangguangxin.cn <wangguangxin.cn@bytedance.com>
2024-03-31 21:19:14 +0800

    recover more check

--- velox/functions/prestosql/aggregates/ApproxPercentileAggregate.cpp
+++ velox/functions/prestosql/aggregates/ApproxPercentileAggregate.cpp
@@ -647,7 +647,18 @@ class ApproxPercentileAggregate : public exec::Aggregate {
     VELOX_CHECK_EQ(args.size(), 1);
     DecodedVector decoded(*args[0], rows);
     auto rowVec = decoded.base()->as<RowVector>();
-    VELOX_CHECK(rowVec);
+    if constexpr (checkIntermediateInputs) {
+      VELOX_USER_CHECK(rowVec);
+      for (int i = kK; i <= kMaxValue; ++i) {
+        VELOX_USER_CHECK(rowVec->childAt(i)->isFlatEncoding());
+      }
+      for (int i = kItems; i <= kLevels; ++i) {
+        VELOX_USER_CHECK(
+            rowVec->childAt(i)->encoding() == VectorEncoding::Simple::ARRAY);
+      }
+    } else {
+      VELOX_CHECK(rowVec);
+    }
 
     const SelectivityVector* baseRows = &rows;
     SelectivityVector innerRows{rowVec->size(), false};
