1fdefed6349d892d30e477333aae4bc0c4544061
Bradley Dice <bdice@bradleydice.com>
2024-04-10 14:35:51 -0700

    Use pip to install the latest cmake.

--- install-cmake-latest.sh
+++ install-cmake-latest.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install ca-certificates gpg wget
-
-test -f /usr/share/doc/kitware-archive-keyring/copyright || wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
-
-echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ jammy main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
-sudo apt-get update
-
-test -f /usr/share/doc/kitware-archive-keyring/copyright || sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
-
-sudo apt-get install kitware-archive-keyring
-
-sudo apt-get install cmake
+pip install cmake
