630ed88bdc77a032e6ce195a8fbb1a76a20f738a
Bradley Dice <bdice@bradleydice.com>
2024-04-13 20:35:13 -0700

    Improve build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 set -euo pipefail
 
-# Run this inside the CUDA container:
+# Run this to launch the CUDA container:
 # docker-compose run -e NUM_THREADS=$(nproc) --rm ubuntu-cuda-cpp
+# Then invoke ./build.sh to build with GPU support and run tests.
 
 # Run a GPU build and test
 pushd "$(dirname ${0})"
 
-make cmake-gpu
+#make cmake-gpu
 make build
 
 cd _build/release
