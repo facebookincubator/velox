51ba2ba94c9b229bf736d8d07612eac8bd003748
yingsu00 <yingsu00@outlook.com>
2024-04-30 19:02:28 -0700

    feat(connector): Support reading Iceberg split with equality deletes
    This commit introduces EqualityDeleteFileReader, which is used to read
    Iceberg splits with equality delete files. The equality delete files
    are read to construct domain filters or filter functions, which then
    would be evaluated in the base file readers.
    
    When there is only one equality delete field, and when that field is
    an Iceberg identifier field, i.e. non-floating point primitive types,
    the values would be converted to a list as a NOT IN domain filter,
    with the NULL treated separately. This domain filter would then be
    pushed to the ColumnReaders to filter our unwanted rows before they
    are read into Velox vectors. When the equality delete column is a
    nested column, e.g. a sub-column in a struct, or the key in a map,
    such column may not be in the base file ScanSpec. We need to add/remove
    these subfields to/from the SchemaWithId and ScanSpec recursively if
    they were not in the ScanSpec already. A test is also added for such
    case.
    
    If there are more than one equality delete field, or the field is not
    an Iceberg identifier field, the values would be converted to a typed
    expression in the conjunct of disconjunts form. This expression would
    be evaluated as the remaining filter function after the rows are read
    into the Velox vectors. Note that this only works for Presto now as
    the "neq" function is not registered by Spark. See https://github.com/
    facebookincubator/velox/issues/12667
    
    Note that this commit only supports integral types. VARCHAR and
    VARBINARY need to be supported in future commits (see
    https://github.com/facebookincubator/velox/issues/12664).
    
    Co-authored-by: Naveen Kumar Mahadevuni <Naveen.Mahadevuni@ibm.com>

---
+++ velox/connectors/hive/iceberg/FilterUtil.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "velox/connectors/hive/iceberg/FilterUtil.h"
+
+namespace facebook::velox::connector::hive::iceberg {
+
+using namespace facebook::velox::exec;
+using namespace facebook::velox::core;
+
+template <typename T>
+std::unique_ptr<common::Filter> createNegatedBigintValuesFilter(
+    const VectorPtr& valuesVector,
+    vector_size_t offset,
+    vector_size_t size) {
+  auto valuesPair =
+      common::deDuplicateValues<int64_t, T>(valuesVector, offset, size);
+
+  const auto& values = valuesPair.first;
+  bool hasNull = valuesPair.second;
+
+  return common::createNegatedBigintValues(values, !hasNull);
+}
+
+std::unique_ptr<common::Filter> createNotInFilter(
+    const VectorPtr& elements,
+    vector_size_t offset,
+    vector_size_t size,
+    TypeKind type) {
+  std::unique_ptr<common::Filter> filter;
+  switch (type) {
+    case TypeKind::HUGEINT:
+      // TODO: createNegatedHugeintValuesFilter is not implemented yet.
+      VELOX_NYI("createNegatedHugeintValuesFilter is not implemented yet");
+    case TypeKind::BIGINT:
+      filter = createNegatedBigintValuesFilter<int64_t>(elements, offset, size);
+      break;
+    case TypeKind::INTEGER:
+      filter = createNegatedBigintValuesFilter<int32_t>(elements, offset, size);
+      break;
+    case TypeKind::SMALLINT:
+      filter = createNegatedBigintValuesFilter<int16_t>(elements, offset, size);
+      break;
+    case TypeKind::TINYINT:
+      filter = createNegatedBigintValuesFilter<int8_t>(elements, offset, size);
+      break;
+    case TypeKind::BOOLEAN:
+      // Hack: using BIGINT filter for bool, which is essentially "int1_t".
+      filter = createNegatedBigintValuesFilter<bool>(elements, offset, size);
+      break;
+    case TypeKind::TIMESTAMP:
+      filter =
+          createNegatedBigintValuesFilter<Timestamp>(elements, offset, size);
+      break;
+    case TypeKind::VARCHAR:
+    case TypeKind::VARBINARY:
+      // TODO: createNegatedBytesValuesFilter is not implemented yet.
+      VELOX_NYI("createNegatedBytesValuesFilter is not implemented yet");
+    case TypeKind::REAL:
+    case TypeKind::DOUBLE:
+    case TypeKind::UNKNOWN:
+      [[fallthrough]];
+    case TypeKind::ARRAY:
+      [[fallthrough]];
+    case TypeKind::MAP:
+      [[fallthrough]];
+    case TypeKind::ROW:
+      [[fallthrough]];
+    default:
+      VELOX_USER_FAIL(
+          "Iceberg equality delete column cannot be of type ", type);
+  }
+  return filter;
+}
+} // namespace facebook::velox::connector::hive::iceberg
