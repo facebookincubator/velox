ccec920f57da621f30fb703b1646f19e8c128df7
gaoyangxiaozhu <gyycoder@gmail.com>
2024-06-05 18:44:30 +0800

    raise_error spark function

---
+++ velox/functions/sparksql/tests/RaiseErrorTest.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/common/base/tests/GTestUtils.h"
+#include "velox/functions/sparksql/tests/SparkFunctionBaseTest.h"
+
+namespace facebook::velox::functions::sparksql::test {
+namespace {
+
+class RaiseErrorTest : public SparkFunctionBaseTest {
+ protected:
+  void raiseError(const std::optional<std::string>& errMsg) {
+    evaluateOnce<UnknownValue>("raise_error(c0)", errMsg);
+  }
+  void testRaiseError(
+      const std::optional<std::string>& errMsg,
+      std::string expectedMsg) {
+    VELOX_ASSERT_THROW(raiseError(errMsg), expectedMsg);
+  }
+};
+
+TEST_F(RaiseErrorTest, basic) {
+  testRaiseError("", "");
+  testRaiseError("0 > 1 is not true", "0 > 1 is not true");
+}
+} // namespace
+} // namespace facebook::velox::functions::sparksql::test
