f517b8ec87dd75cd89c555cc74d86ef2f6e450a5
Deepashree Gandhi <deegandh@in.ibm.com>
2024-06-09 21:54:33 +0530

    Getting user confirmation before replacing minio pkg

--- scripts/setup-adapters.sh
+++ scripts/setup-adapters.sh
@@ -35,7 +35,13 @@ function install_aws_deps {
   # We need this specific version of Minio for testing.
   if [[ "$OSTYPE" == linux-gnu* ]]; then
     wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio-20220526054841.0.0.x86_64.rpm
-    rpm -i --replacepkgs minio-20220526054841.0.0.x86_64.rpm
+    if yum list installed minio >/dev/null 2>&1;then
+      if  prompt "Do you want to replace minio package with compatible version minio-20220526054841.0.0.x86_64.rpm?"; then
+        rpm -i --replacepkgs minio-20220526054841.0.0.x86_64.rpm 
+      fi
+    else
+      rpm -i minio-20220526054841.0.0.x86_64.rpm
+    fi
     rm minio-20220526054841.0.0.x86_64.rpm
   fi
   # minio will have to approved under the Privacy & Security on MacOS on first use.
