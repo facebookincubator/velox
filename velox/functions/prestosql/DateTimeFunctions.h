77633b3a3f6bb3f0173f9cb95a156973cd9a328d
wypb <wyphao.2007@163.com>
2024-06-12 10:35:51 +0800

    Refactor DateTimeFormatter::parse to return velox::Expected

--- velox/functions/prestosql/DateTimeFunctions.h
+++ velox/functions/prestosql/DateTimeFunctions.h
@@ -1354,8 +1354,8 @@ struct DateParseFunction {
     }
 
     auto dateTimeResult = format_->parse((std::string_view)(input));
-    if (!dateTimeResult.has_value()) {
-      return Status::UserError("Invalid date format: '{}'", input);
+    if (dateTimeResult.hasError()) {
+      return dateTimeResult.error();
     }
 
     // Since MySql format has no timezone specifier, simply check if session
@@ -1471,9 +1471,9 @@ struct ParseDateTimeFunction {
           std::string_view(format.data(), format.size()));
     }
     auto dateTimeResult =
-        format_->parse(std::string_view(input.data(), input.size()), false);
-    if (!dateTimeResult.has_value()) {
-      return Status::UserError("Invalid date format: '{}'", input);
+        format_->parse(std::string_view(input.data(), input.size()));
+    if (dateTimeResult.hasError()) {
+      return dateTimeResult.error();
     }
 
     // If timezone was not parsed, fallback to the session timezone. If there's
