ca849ff34b603f974cee0ccd62d44214aaa22cc2
Bradley Dice <bdice@bradleydice.com>
2024-06-12 20:18:31 -0700

    Add CudfHashJoin.cpp and VeloxCudfInterop.cpp. Builds successfully.

---
+++ velox/experimental/cudf/exec/CudfHashJoin.cpp
@@ -0,0 +1,410 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// For custom hash join operator
+#include "velox/exec/JoinBridge.h"
+#include "velox/exec/tests/utils/OperatorTestBase.h"
+#include "velox/exec/tests/utils/PlanBuilder.h"
+
+#include <cudf/join.hpp>
+#include <cudf/copying.hpp>
+
+#include "VeloxCudfInterop.hpp"
+#include "CudfHashJoin.hpp"
+#include <nvtx3/nvtx3.hpp>
+
+using namespace facebook::velox;
+using namespace facebook::velox::test;
+using namespace facebook::velox::exec;
+using namespace facebook::velox::exec::test;
+
+// few utility functions
+std::vector<std::string> concat(
+    const std::vector<std::string>& a,
+    const std::vector<std::string>& b) {
+  std::vector<std::string> result;
+  result.insert(result.end(), a.begin(), a.end());
+  result.insert(result.end(), b.begin(), b.end());
+  return result;
+}
+
+// Custom hash join operator which uses libcudf
+// need to define a new PlanNode, JoinBridge, Operators (Build, Probe), and a PlanNodeTranslator
+// and Register the PlanNodeTranslator
+class CudfHashJoinNode : public core::PlanNode {
+public:
+    CudfHashJoinNode(const core::PlanNodeId& id,
+    core::PlanNodePtr left,
+    core::PlanNodePtr right)
+    : PlanNode(id), sources_{std::move(left), std::move(right)} {}
+    // 4 abstract functions to implement
+    const RowTypePtr& outputType() const override {
+        // TODO similar to PlanBuilder::hashJoin()
+        return sources_.front()->outputType();
+    }
+    const std::vector<core::PlanNodePtr>& sources() const override {
+        return sources_;
+    }
+    std::string_view name() const override {
+        return "cudf hash join";
+    }
+private:
+    void addDetails(std::stringstream& /* stream */) const override {}
+    std::vector<core::PlanNodePtr> sources_;
+};
+
+class CudfHashJoinBridge : public JoinBridge {
+    public:
+    using HashType = std::pair<std::unique_ptr<cudf::table>, std::shared_ptr<cudf::hash_join>>;
+    // using HashType = int;
+    void setHashTable(std::optional<HashType> hashObject) {
+      std::vector<ContinuePromise> promises;
+      {
+        std::lock_guard<std::mutex> l(mutex_);
+        VELOX_CHECK(!hashObject_.has_value(), "HashJoinBridge already has a hash table");
+        hashObject_ = std::move(hashObject);
+        promises = std::move(promises_);
+      }
+      notify(std::move(promises));
+    }
+
+    std::optional<HashType> HashOrFuture(ContinueFuture* future) {
+        std::lock_guard<std::mutex> l(mutex_);
+        if (hashObject_.has_value()) {
+            return std::move(hashObject_);
+        }
+        promises_.emplace_back("CudfHashJoinBridge::HashOrFuture");
+        *future = promises_.back().getSemiFuture();
+        return std::nullopt;
+    }
+
+private:
+  std::optional<HashType> hashObject_;
+};
+
+class CudfHashJoinBuild : public Operator {
+public:
+  CudfHashJoinBuild(
+    int32_t operatorId,
+    DriverCtx* driverCtx,
+    std::shared_ptr<const CudfHashJoinNode> joinNode)
+    // TODO check outputType should be set or not?
+    : Operator(driverCtx,  nullptr, // joinNode->sources(),
+    operatorId, joinNode->id(), "CudfHashJoinBuild") {}
+
+    void addInput(RowVectorPtr input) override {
+        // Queue inputs, process all at once.
+        // TODO distribute work equally.
+        auto inputSize = input->size();
+        if (inputSize > 0) {
+            inputs_.push_back(std::move(input));
+        }
+    }
+    bool needsInput() const override {
+    return !noMoreInput_;
+    }
+    RowVectorPtr getOutput() override {
+        return nullptr;
+    }
+    void noMoreInput() override {
+        NVTX3_FUNC_RANGE();
+        Operator::noMoreInput();
+        // TODO
+        std::vector<ContinuePromise> promises;
+        std::vector<std::shared_ptr<Driver>> peers;
+        // Only last driver collects all answers
+        if (!operatorCtx_->task()->allPeersFinished(
+            planNodeId(), operatorCtx_->driver(), &future_, promises, peers)) {
+            return;
+        }
+        // Collect results from peers
+        for (auto& peer : peers) {
+            auto op = peer->findOperator(planNodeId());
+            auto* build = dynamic_cast<CudfHashJoinBuild*>(op);
+            VELOX_CHECK(build);
+            // numRows_ += build->numRows_;
+            inputs_.insert(inputs_.end(), build->inputs_.begin(), build->inputs_.end());
+        }
+        // TODO build hash table
+        auto tbl = to_cudf_table(inputs_[0]); // TODO how to process multiple inputs?
+        // copy host to device table,
+        // CudfHashJoinBridge::HashType hashObject = 1;
+        // TODO create hash table in device.
+        // CudfHashJoinBridge::HashType
+        auto hashObject =
+         std::make_shared<cudf::hash_join>(tbl->view(), cudf::null_equality::EQUAL);
+
+        // Copied
+        peers.clear();
+        for (auto& promise : promises) {
+            promise.setValue();
+        }
+
+        // set hash table to CudfHashJoinBridge
+        auto joinBridge = operatorCtx_->task()->getCustomJoinBridge(
+            operatorCtx_->driverCtx()->splitGroupId, planNodeId());
+        auto cudf_HashJoinBridge =
+            std::dynamic_pointer_cast<CudfHashJoinBridge>(joinBridge);
+        cudf_HashJoinBridge->setHashTable(std::make_optional(std::make_pair(std::move(tbl), std::move(hashObject))));
+    }
+
+    BlockingReason isBlocked(ContinueFuture* future) override {
+        if (!future_.valid()) {
+        return BlockingReason::kNotBlocked;
+        }
+        *future = std::move(future_);
+        return BlockingReason::kWaitForJoinBuild;
+    }
+
+    bool isFinished() override {
+        return !future_.valid() && noMoreInput_;
+    }
+
+private:
+    std::vector<RowVectorPtr> inputs_;
+    ContinueFuture future_{ContinueFuture::makeEmpty()};
+};
+
+class CudfHashJoinProbe : public Operator {
+    public:
+    using HashType = CudfHashJoinBridge::HashType;
+    CudfHashJoinProbe(
+        int32_t operatorId,
+        DriverCtx* driverCtx,
+        std::shared_ptr<const CudfHashJoinNode> joinNode)
+        : Operator(driverCtx, nullptr, // joinNode->sources(),
+        operatorId, joinNode->id(), "CudfHashJoinProbe") {}
+
+    bool needsInput() const override {
+        return !finished_ && input_ == nullptr;
+    }
+    void addInput(RowVectorPtr input) override {
+        input_ = std::move(input);
+    }
+
+    RowVectorPtr getOutput() override {
+        NVTX3_FUNC_RANGE();
+        if (!input_) {
+            return nullptr;
+        }
+        const auto inputSize = input_->size();
+        if(!hashObject_.has_value()) {
+            return nullptr;
+        }
+        // std::cout<<"here\n\n";
+        // TODO convert input to cudf table
+        auto tbl = to_cudf_table(input_);
+        // TODO pass the input pool !!!
+        RowVectorPtr output;
+        // RowVectorPtr output;
+        auto const [left_join_indices, right_join_indices] = hashObject_.value().second->inner_join(tbl->view());
+        auto left_indices_span  = cudf::device_span<cudf::size_type const>{*left_join_indices};
+        auto right_indices_span = cudf::device_span<cudf::size_type const>{*right_join_indices};
+        auto left_input  = tbl->view();
+        auto right_input = hashObject_.value().first->view();
+
+        auto left_indices_col  = cudf::column_view{left_indices_span};
+        auto right_indices_col = cudf::column_view{right_indices_span};
+        auto constexpr oob_policy = cudf::out_of_bounds_policy::DONT_CHECK;
+        auto left_result  = cudf::gather(left_input, left_indices_col, oob_policy);
+        auto right_result = cudf::gather(right_input, right_indices_col, oob_policy);
+        auto joined_cols = left_result->release();
+        auto right_cols  = right_result->release();
+        joined_cols.insert(joined_cols.end(),
+                            std::make_move_iterator(right_cols.begin()),
+                            std::make_move_iterator(right_cols.end()));
+        auto cudf_output = std::make_unique<cudf::table>(std::move(joined_cols));
+        // TODO convert output to RowVector
+        if (cudf_output->num_columns() == 0 or cudf_output->num_rows() == 0) {
+            output = nullptr;
+        } else {
+            output = to_velox_column(cudf_output->view(), input_->pool());
+        }
+        // auto output = input_;
+        // auto output = std::make_shared<RowVector>(
+        //     input_->pool(),
+        //     input_->type(),
+        //     input_->nulls(),
+        //     std::min(20, inputSize-2),
+        //     input_->children());
+        // std::cout<<"there\n\n";
+        input_.reset();
+        finished_ = true;
+        // printResults(output, std::cout);
+        return output;
+    }
+
+    BlockingReason isBlocked(ContinueFuture* future) override {
+        if (hashObject_.has_value()) {
+        return BlockingReason::kNotBlocked;
+        }
+
+        auto joinBridge = operatorCtx_->task()->getCustomJoinBridge(
+            operatorCtx_->driverCtx()->splitGroupId, planNodeId());
+        auto hashObject = std::dynamic_pointer_cast<CudfHashJoinBridge>(joinBridge)
+                        ->HashOrFuture(future);
+
+        if (!hashObject.has_value()) {
+        return BlockingReason::kWaitForJoinBuild;
+        }
+        hashObject_ = std::move(hashObject);
+        // remainingLimit_ = hashObject.value();
+
+        return BlockingReason::kNotBlocked;
+    }
+
+    bool isFinished() override {
+        return finished_ || (noMoreInput_ && input_ == nullptr);
+    }
+
+    private:
+    std::optional<HashType> hashObject_;
+    bool finished_{false};
+};
+
+class CudfHashJoinBridgeTranslator : public Operator::PlanNodeTranslator {
+  std::unique_ptr<Operator>
+  toOperator(DriverCtx* ctx, int32_t id, const core::PlanNodePtr& node) {
+    if (auto joinNode = std::dynamic_pointer_cast<const CudfHashJoinNode>(node)) {
+      return std::make_unique<CudfHashJoinProbe>(id, ctx, joinNode);
+    }
+    return nullptr;
+  }
+
+  std::unique_ptr<JoinBridge> toJoinBridge(const core::PlanNodePtr& node) {
+    if (auto joinNode = std::dynamic_pointer_cast<const CudfHashJoinNode>(node)) {
+      auto joinBridge = std::make_unique<CudfHashJoinBridge>();
+      return joinBridge;
+    }
+    return nullptr;
+  }
+
+  OperatorSupplier toOperatorSupplier(const core::PlanNodePtr& node) {
+    if (auto joinNode = std::dynamic_pointer_cast<const CudfHashJoinNode>(node)) {
+      return [joinNode](int32_t operatorId, DriverCtx* ctx) {
+        return std::make_unique<CudfHashJoinBuild>(operatorId, ctx, joinNode);
+      };
+    }
+    return nullptr;
+  }
+};
+
+// CudfHashJoinDemo class methods implementation
+CudfHashJoinDemo::CudfHashJoinDemo() {
+    // // Register Presto scalar functions.
+    // functions::prestosql::registerAllScalarFunctions();
+
+    // // Register Presto aggregate functions.
+    // aggregate::prestosql::registerAllAggregateFunctions();
+
+    // // Register type resolver with DuckDB SQL parser.
+    // parse::registerTypeResolver();
+
+    // Register custom Operator
+    // Operator::registerOperator(std::make_unique<CustomJoinBridgeTranslator>());
+    Operator::registerOperator(std::make_unique<CudfHashJoinBridgeTranslator>());
+    }
+
+RowVectorPtr CudfHashJoinDemo::makeSimpleRowVector(vector_size_t size, vector_size_t init, std::string name_prefix) {
+    std::vector<VectorPtr> col_vec = {
+        makeFlatVector<int64_t>(size, [init](auto row) { return init+row; })
+        // ,makeFlatVector<int64_t>(size, [](auto row) { return row; })
+        };
+    std::vector<std::string> names;
+    for (int32_t i = 0; i < col_vec.size(); ++i) {
+        names.push_back(fmt::format("{}{}", name_prefix, i));
+    }
+    return makeRowVector(std::move(names), std::move(col_vec));
+    }
+
+CudfHashJoinDemo::result_type CudfHashJoinDemo::testVeloxHashJoin(
+      int32_t numThreads,
+      const std::vector<RowVectorPtr>& leftBatch, // probe input
+      const std::vector<RowVectorPtr>& rightBatch, // build input
+      const std::string& referenceQuery) {
+    NVTX3_FUNC_RANGE();
+    // createDuckDbTable("t", {leftBatch});
+    auto planNodeIdGenerator = std::make_shared<core::PlanNodeIdGenerator>();
+    CursorParameters params;
+    params.maxDrivers = numThreads;
+    params.planNode = PlanBuilder(planNodeIdGenerator)
+                            .values(leftBatch, true)
+                            .hashJoin(
+                                {"c0"},
+                                {"d0"},
+                                PlanBuilder(planNodeIdGenerator)
+                                    .values(rightBatch, true)
+                                    .planNode(),
+                                "",
+                                // {"c0"})
+                                concat({"c0"}, {"d0"}))
+                            // .project({"c0"}) // project only first column
+                            .planNode();
+        auto result = readCursor(params, [](Task*) {});
+        // std::cout<<"Velox Hash Join Result: \n";
+        // printResults(result.second.front(), std::cout);
+        return result;
+      }
+
+CudfHashJoinDemo::result_type CudfHashJoinDemo::testCudfHashJoin(
+      int32_t numThreads,
+      const std::vector<RowVectorPtr>& leftBatch, // probe input
+      const std::vector<RowVectorPtr>& rightBatch, // build input
+      const std::string& referenceQuery) {
+    NVTX3_FUNC_RANGE();
+    // createDuckDbTable("t", {leftBatch});
+
+    auto planNodeIdGenerator = std::make_shared<core::PlanNodeIdGenerator>();
+    auto leftNode =
+        PlanBuilder(planNodeIdGenerator).values({leftBatch}, true).planNode();
+    auto rightNode =
+        PlanBuilder(planNodeIdGenerator).values({rightBatch}, true).planNode();
+
+    CursorParameters params;
+    params.maxDrivers = numThreads;
+    params.planNode =
+        PlanBuilder(planNodeIdGenerator)
+            .values({leftBatch}, true)
+            .addNode([&leftNode, &rightNode](
+                         std::string id, core::PlanNodePtr /* input */) {
+              return std::make_shared<CudfHashJoinNode>(
+                  id, std::move(leftNode), std::move(rightNode));
+            })
+            // .project({"c0"}) // project only first column
+            .planNode();
+
+    // OperatorTestBase::assertQuery(params, referenceQuery);
+
+    // assertQuery(params, leftBatch);
+
+    auto result = readCursor(params, [](Task*) {});
+    // std::cout<<"cudf Hash Join Result: \n";
+    // printResults(result.second.front(), std::cout);
+    return result;
+
+    // Shared pointer of pool must be in scope. Otherwise pure virtual method called error.
+    // auto pool = memory::getDefaultMemoryPool();
+  }
+
+bool CudfHashJoinDemo::CompareResults(
+      int32_t numThreads,
+      const std::vector<RowVectorPtr>& leftBatch, // probe input
+      const std::vector<RowVectorPtr>& rightBatch, // build input
+      const std::string& referenceQuery) {
+    auto reference = testVeloxHashJoin(numThreads, leftBatch, rightBatch, referenceQuery);
+    auto result    = testCudfHashJoin (numThreads, leftBatch, rightBatch, referenceQuery);
+    return assertEqualResults(result.second, reference.second);
+  }
---
+++ velox/experimental/cudf/exec/VeloxCudfInterop.cpp
@@ -0,0 +1,236 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/vector/ComplexVector.h"
+#include "velox/vector/FlatVector.h"
+#include "velox/vector/BaseVector.h"
+#include "velox/common/memory/Memory.h"
+#include "velox/type/Type.h"
+
+#include "velox/vector/tests/utils/VectorMaker.h"
+
+#include <cudf/detail/utilities/vector_factories.hpp>
+#include <cudf/column/column.hpp>
+#include <cudf/table/table.hpp>
+#include <cudf/types.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
+#include <cudf/column/column_factories.hpp>
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/device_buffer.hpp>
+#include <rmm/mr/device/per_device_resource.hpp>
+
+#include <thrust/copy.h>
+#include <thrust/execution_policy.h>
+#include <nvtx3/nvtx3.hpp>
+
+#include "VeloxCudfInterop.hpp"
+
+using namespace facebook::velox;
+
+// Velox type to CUDF type
+/*
+template<TypeKind::KIND kind>
+struct VeloxToCudfType {
+  using type = typename TypeTraits<kind>::NativeType;
+  static constexpr cudf::type_id id = cudf::type_id::EMPTY;
+  //cudf::type_to_id<type>();
+};
+
+#define VELOX_TO_CUDF_TYPE(CUDF_KIND, VELOX_KIND)   \
+template <>                                         \
+struct TypeTraits<TypeKind::VELOX_KIND> {           \
+using type = typename TypeTraits<kind>::NativeType; \
+static constexpr cudf::type_id id = CUDF_KIND;      \
+};
+
+VELOX_TO_CUDF_TYPE(cudf::type_id::BOOL8, BOOLEAN)
+VELOX_TO_CUDF_TYPE(cudf::type_id::INT8, TINYINT)
+VELOX_TO_CUDF_TYPE(cudf::type_id::INT16, SMALLINT)
+VELOX_TO_CUDF_TYPE(cudf::type_id::INT32, INTEGER)
+VELOX_TO_CUDF_TYPE(cudf::type_id::INT64, BIGINT)
+VELOX_TO_CUDF_TYPE(cudf::type_id::FLOAT32, REAL)
+VELOX_TO_CUDF_TYPE(cudf::type_id::FLOAT64, DOUBLE)
+VELOX_TO_CUDF_TYPE(cudf::type_id::STRING, VARCHAR)
+VELOX_TO_CUDF_TYPE(cudf::type_id::STRING, VARBINARY)
+VELOX_TO_CUDF_TYPE(cudf::type_id::TIMESTAMP_NANOSECONDS, TIMESTAMP)
+VELOX_TO_CUDF_TYPE(cudf::type_id::DURATION_DAYS, DATE)
+// VELOX_TO_CUDF_TYPE(IntervalDayTime, INTERVAL_DAY_TIME)
+VELOX_TO_CUDF_TYPE(cudf::type_id::DECIMAL64, SHORT_DECIMAL)
+VELOX_TO_CUDF_TYPE(cudf::type_id::DECIMAL128, LONG_DECIMAL)
+// VELOX_TO_CUDF_TYPE(Array, ARRAY)
+// VELOX_TO_CUDF_TYPE(Map, MAP)
+// VELOX_TO_CUDF_TYPE(Row, ROW)
+// VELOX_TO_CUDF_TYPE(Opaque, OPAQUE)
+// VELOX_TO_CUDF_TYPE(UnKnown, UNKNOWN)
+*/
+
+cudf::type_id velox_to_cudf_type_id(TypeKind kind) {
+    switch(kind) {
+        case TypeKind::BOOLEAN: return cudf::type_id::BOOL8;
+        case TypeKind::TINYINT: return cudf::type_id::INT8;
+        case TypeKind::SMALLINT: return cudf::type_id::INT16;
+        case TypeKind::INTEGER: return cudf::type_id::INT32;
+        case TypeKind::BIGINT: return cudf::type_id::INT64;
+        case TypeKind::REAL: return cudf::type_id::FLOAT32;
+        case TypeKind::DOUBLE: return cudf::type_id::FLOAT64;
+        case TypeKind::VARCHAR: return cudf::type_id::STRING;
+        case TypeKind::VARBINARY: return cudf::type_id::STRING;
+        case TypeKind::TIMESTAMP: return cudf::type_id::TIMESTAMP_NANOSECONDS;
+        // case TypeKind::HUGEINT: return cudf::type_id::DURATION_DAYS;
+        // TODO: DATE was converted to a logical type: https://github.com/facebookincubator/velox/commit/e480f5c03a6c47897ef4488bd56918a89719f908
+        // case TypeKind::DATE: return cudf::type_id::DURATION_DAYS;
+        // case TypeKind::INTERVAL_DAY_TIME: return cudf::type_id::EMPTY;
+        // TODO: Decimals are now logical types: https://github.com/facebookincubator/velox/commit/73d2f935b55f084d30557c7be94b9768efb8e56f
+        // case TypeKind::SHORT_DECIMAL: return cudf::type_id::DECIMAL64;
+        // case TypeKind::LONG_DECIMAL: return cudf::type_id::DECIMAL128;
+        // case TypeKind::ARRAY: return cudf::type_id::EMPTY;
+        // case TypeKind::MAP: return cudf::type_id::EMPTY;
+        case TypeKind::ROW: return cudf::type_id::STRUCT;
+        // case TypeKind::UNKNOWN: return cudf::type_id::EMPTY;
+        // case TypeKind::FUNCTION: return cudf::type_id::EMPTY;
+        // case TypeKind::OPAQUE: return cudf::type_id::EMPTY;
+        // case TypeKind::INVALID: return cudf::type_id::EMPTY;
+        default: return cudf::type_id::EMPTY;
+    }
+}
+
+ TypeKind cudf_to_velox_type_id(cudf::type_id kind) {
+    switch(kind) {
+        case cudf::type_id::BOOL8: return TypeKind::BOOLEAN;
+        case cudf::type_id::INT8: return TypeKind::TINYINT;
+        case cudf::type_id::INT16: return TypeKind::SMALLINT;
+        case cudf::type_id::INT32: return TypeKind::INTEGER;
+        case cudf::type_id::INT64: return TypeKind::BIGINT;
+        case cudf::type_id::FLOAT32: return TypeKind::REAL;
+        case cudf::type_id::FLOAT64: return TypeKind::DOUBLE;
+        case cudf::type_id::STRING: return TypeKind::VARCHAR;
+        case cudf::type_id::TIMESTAMP_NANOSECONDS: return TypeKind::TIMESTAMP;
+        // TODO: DATE is now a logical type
+        // case cudf::type_id::DURATION_DAYS: return TypeKind::DATE;
+        // case cudf::type_id::EMPTY: return TypeKind::INTERVAL_DAY_TIME;
+        // TODO: DECIMAL is now a logical type
+        // case cudf::type_id::DECIMAL64: return TypeKind::SHORT_DECIMAL;
+        // case cudf::type_id::DECIMAL128: return TypeKind::LONG_DECIMAL;
+        // case cudf::type_id::EMPTY: return TypeKind::ARRAY;
+        // case cudf::type_id::EMPTY: return TypeKind::MAP;
+        case cudf::type_id::STRUCT: return TypeKind::ROW;
+        // case cudf::type_id::EMPTY: return TypeKind::OPAQUE;
+        // case cudf::type_id::EMPTY: return TypeKind::UNKNOWN;
+        default: return TypeKind::UNKNOWN;
+    }
+}
+
+
+// Convert a Velox vector to a CUDF column
+struct copy_to_device {
+    rmm::cuda_stream_view stream;
+    template <typename T>
+    static constexpr bool is_supported() {
+        return cudf::is_rep_layout_compatible<T>();
+    }
+    // Fixed width types
+    template<typename T,
+             std::enable_if_t<is_supported<T>() >* = nullptr>
+    std::unique_ptr<cudf::column> operator()(VectorPtr& h_vec) const
+    {
+        auto velox_data = h_vec->as<FlatVector<T>>();
+        auto velox_data_ptr = velox_data->rawValues();
+        cudf::host_span<const T> velox_host_span(velox_data_ptr, int{h_vec->size()});
+        auto d_v = cudf::detail::make_device_uvector_sync(velox_host_span, stream, rmm::mr::get_current_device_resource());
+        return std::make_unique<cudf::column>(std::move(d_v), rmm::device_buffer{}, 0);
+    }
+
+  template <typename T, typename... Args, std::enable_if_t<not is_supported<T>()>* = nullptr>
+  std::unique_ptr<cudf::column> operator()(Args... args) const
+    {
+        CUDF_FAIL("Unsupported type for to_cudf conversion");
+    }
+};
+
+// Row vector to table
+// Vector to column
+// template<bool is_table=true>
+std::unique_ptr<cudf::table> to_cudf_table(const RowVectorPtr& leftBatch) {
+    NVTX3_FUNC_RANGE();
+    // cudf type dispatcher to copy data from velox vector to cudf column
+    using cudf_col_ptr = std::unique_ptr<cudf::column>;
+    std::vector<cudf_col_ptr> cudf_columns;
+    auto copier = copy_to_device{cudf::get_default_stream()};
+    for(auto& h_vec : leftBatch->children()) {
+        auto cudf_kind = cudf::data_type{velox_to_cudf_type_id(h_vec->type()->kind())};
+        auto cudf_column = cudf::type_dispatcher(cudf_kind, copier, h_vec);
+        cudf_columns.push_back(std::move(cudf_column));
+    }
+    return std::make_unique<cudf::table>(std::move(cudf_columns));
+}
+
+// Convert a CUDF column to a Velox vector
+struct copy_to_host {
+    rmm::cuda_stream_view stream;
+    memory::MemoryPool* pool_;
+
+    template <typename T>
+    static constexpr bool is_supported() {
+        // return cudf::is_rep_layout_compatible<T>();
+        return cudf::is_numeric<T>() and not std::is_same<T, bool>::value;
+    }
+    // Fixed width types
+    template<typename T,
+             std::enable_if_t<is_supported<T>() >* = nullptr>
+    VectorPtr operator()(TypePtr velox_type, cudf::column_view const& col) const
+    {
+        // auto velox_col = BaseVector::create(velox_type, col.size(), pool_);
+        // auto velox_col = BaseVector::create<FlatVector<T> >(velox_type, col.size(), pool_);
+        auto velox_col = test::VectorMaker{pool_}.flatVector<T>(col.size());
+        // auto velox_data = velox_col->as<FlatVector<T>>();
+        auto velox_data_ptr = velox_col->mutableRawValues();
+        CUDF_CUDA_TRY(cudaMemcpyAsync(velox_data_ptr,
+    col.data<T>(), col.size() * sizeof(T), cudaMemcpyDefault, stream.value()));
+        stream.synchronize();
+        return velox_col;
+    }
+
+  template <typename T, typename... Args, std::enable_if_t<not is_supported<T>()>* = nullptr>
+  VectorPtr operator()(Args... args) const
+    {
+        CUDF_FAIL("Unsupported type for to_velox conversion");
+    }
+};
+
+
+VectorPtr to_velox_column(const cudf::column_view& col, memory::MemoryPool* pool) {
+    NVTX3_FUNC_RANGE();
+    auto velox_kind = cudf_to_velox_type_id(col.type().id());
+    auto velox_type = createScalarType(velox_kind);
+    // cudf type dispatcher to copy data from cudf column to velox vector
+    auto copier = copy_to_host{cudf::get_default_stream(), pool};
+    return cudf::type_dispatcher(col.type(), copier, velox_type, col);
+}
+
+RowVectorPtr to_velox_column(const cudf::table_view& table, memory::MemoryPool* pool,
+      std::string name_prefix) {
+    NVTX3_FUNC_RANGE();
+    std::vector<VectorPtr> children;
+    std::vector<std::string> names;
+    for(auto& col : table) {
+        auto velox_col = to_velox_column(col, pool);
+        children.push_back(std::move(velox_col));
+        names.push_back(name_prefix + std::to_string(names.size()));
+    }
+    auto vcol = test::VectorMaker{pool}.rowVector(std::move(names), std::move(children));
+    return vcol;
+}
