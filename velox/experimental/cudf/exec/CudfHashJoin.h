04130ba8da31963433bbddc09b79a388b1fc5982
Bradley Dice <bdice@bradleydice.com>
2024-06-12 21:12:42 -0700

    Forgot to commit some headers.

---
+++ velox/experimental/cudf/exec/CudfHashJoin.h
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "velox/exec/tests/utils/QueryAssertions.h"
+#include "velox/vector/ComplexVector.h"
+#include "velox/vector/tests/utils/VectorTestBase.h"
+
+#include <cudf/join.hpp>
+#include <cudf/table/table.hpp>
+
+#include <string>
+
+namespace facebook::velox::cudf_velox {
+
+// Custom hash join operator which uses libcudf
+// need to define a new PlanNode, JoinBridge, Operators (Build, Probe), and a PlanNodeTranslator
+// and Register the PlanNodeTranslator
+class CudfHashJoinNode : public core::PlanNode {
+public:
+    CudfHashJoinNode(
+        const core::PlanNodeId& id,
+        core::PlanNodePtr left,
+        core::PlanNodePtr right);
+
+    const RowTypePtr& outputType() const override;
+
+    const std::vector<core::PlanNodePtr>& sources() const override;
+
+    std::string_view name() const override;
+
+private:
+    void addDetails(std::stringstream& /* stream */) const override;
+    std::vector<core::PlanNodePtr> sources_;
+};
+
+class CudfHashJoinBridge : public exec::JoinBridge {
+public:
+    using hash_type = std::pair<std::unique_ptr<cudf::table>, std::shared_ptr<cudf::hash_join>>;
+
+    void setHashTable(std::optional<hash_type> hashObject);
+
+    std::optional<hash_type> HashOrFuture(ContinueFuture* future);
+
+private:
+    std::optional<hash_type> hashObject_;
+};
+
+class CudfHashJoinBuild : public exec::Operator {
+public:
+  CudfHashJoinBuild(
+    int32_t operatorId,
+    exec::DriverCtx* driverCtx,
+    std::shared_ptr<const CudfHashJoinNode> joinNode);
+
+    void addInput(RowVectorPtr input) override;
+
+    bool needsInput() const override;
+
+    RowVectorPtr getOutput() override;
+
+    void noMoreInput() override;
+
+    exec::BlockingReason isBlocked(ContinueFuture* future) override;
+
+    bool isFinished() override;
+
+private:
+    std::vector<RowVectorPtr> inputs_;
+    ContinueFuture future_{ContinueFuture::makeEmpty()};
+};
+
+class CudfHashJoinProbe : public exec::Operator {
+public:
+    using hash_type = CudfHashJoinBridge::hash_type;
+    CudfHashJoinProbe(
+        int32_t operatorId,
+        exec::DriverCtx* driverCtx,
+        std::shared_ptr<const CudfHashJoinNode> joinNode);
+
+    bool needsInput() const override;
+
+    void addInput(RowVectorPtr input) override;
+
+    RowVectorPtr getOutput() override;
+
+    exec::BlockingReason isBlocked(ContinueFuture* future) override;
+
+    bool isFinished() override;
+
+private:
+    std::optional<hash_type> hashObject_;
+    bool finished_{false};
+};
+
+class CudfHashJoinBridgeTranslator : public exec::Operator::PlanNodeTranslator {
+public:
+    std::unique_ptr<exec::Operator> toOperator(exec::DriverCtx* ctx, int32_t id, const core::PlanNodePtr& node);
+
+    std::unique_ptr<exec::JoinBridge> toJoinBridge(const core::PlanNodePtr& node);
+
+    exec::OperatorSupplier toOperatorSupplier(const core::PlanNodePtr& node);
+};
+
+} // namespace facebook::velox::cudf_velox
---
+++ velox/experimental/cudf/exec/VeloxCudfInterop.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "velox/vector/ComplexVector.h"
+#include "velox/vector/BaseVector.h"
+#include "velox/common/memory/Memory.h"
+
+#include <cudf/column/column.hpp>
+#include <cudf/table/table.hpp>
+#include <cudf/types.hpp>
+
+namespace facebook::velox::cudf_velox {
+
+std::unique_ptr<cudf::table> to_cudf_table(const facebook::velox::RowVectorPtr& leftBatch);
+facebook::velox::VectorPtr to_velox_column(const cudf::column_view& col, facebook::velox::memory::MemoryPool* pool);
+facebook::velox::RowVectorPtr to_velox_column(const cudf::table_view& table, facebook::velox::memory::MemoryPool* pool, std::string name_prefix = "c");
+
+} // namespace facebook::velox::cudf_velox
