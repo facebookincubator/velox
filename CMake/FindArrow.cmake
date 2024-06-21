056e3cd20a58101aaa5abad85342a5fe58aab7e1
PHILO-HE <feilong.he@intel.com>
2024-06-21 13:51:35 +0800

    Find from system

--- CMake/FindArrow.cmake
+++ CMake/FindArrow.cmake
@@ -12,47 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-  if(DEFINED ENV{ARROW_EP_PATH})
-    message(
-      STATUS
-        "Arrow path is set: $ENV{ARROW_EP_PATH}, trying to use existing arrow libraries."
-    )
-    #find_package(Arrow REQUIRED PATHS $ENV{ARROW_EP_PATH}/install/lib/cmake/Arrow/)
-    set(ARROW_LIB_PATH $ENV{ARROW_EP_PATH}/install/lib/)
-    find_library(ARROW_LIB libarrow.a PATHS ${ARROW_LIB_PATH})
-    find_library(PARQUET_LIB libparquet.a PATHS ${ARROW_LIB_PATH})
-    find_library(ARROW_TESTING_LIB libarrow_testing.a PATHS ${ARROW_LIB_PATH})
-    if("${ARROW_LIB}" STREQUAL "ARROW_LIB-NOTFOUND"
-       #OR "${PARQUET_LIB}" STREQUAL "PARQUET_LIB-NOTFOUND"
-       OR "${ARROW_TESTING_LIB}" STREQUAL "ARROW_TESTING_LIB-NOTFOUND")
-      message(FATAL_ERROR "Arrow libraries not found in ${ARROW_LIB_PATH}")
-    endif()
-    set(Arrow_FOUND true)
+find_library(ARROW_LIB libarrow.a)
+find_library(PARQUET_LIB libparquet.a)
+find_library(ARROW_TESTING_LIB libarrow_testing.a)
+if("${ARROW_LIB}" STREQUAL "ARROW_LIB-NOTFOUND"
+   # OR "${PARQUET_LIB}" STREQUAL "PARQUET_LIB-NOTFOUND"
+   OR "${ARROW_TESTING_LIB}" STREQUAL "ARROW_TESTING_LIB-NOTFOUND")
+  message(STATUS "Arrow libraries not found")
+  set(Arrow_FOUND false)
+  return()
+endif()
+set(Arrow_FOUND true)
 
-    add_library(thrift STATIC IMPORTED GLOBAL)
-    if(NOT Thrift_FOUND)
-      set(THRIFT_ROOT $ENV{ARROW_EP_PATH}/src/arrow_ep-build/thrift_ep-install)
-      find_library(THRIFT_LIB thrift PATHS ${THRIFT_ROOT}/lib)
-      if("${THRIFT_LIB}" STREQUAL "THRIFT_LIB-NOTFOUND")
-        message(FATAL_ERROR "Thrift library not found in ${THRIFT_ROOT}/lib")
-      endif()
-      set(THRIFT_INCLUDE_DIR ${THRIFT_ROOT}/include)
-    endif()
-    set_property(TARGET thrift PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-                                        ${THRIFT_INCLUDE_DIR})
-    set_property(TARGET thrift PROPERTY IMPORTED_LOCATION ${THRIFT_LIB})
+add_library(arrow STATIC IMPORTED GLOBAL)
+add_library(parquet STATIC IMPORTED GLOBAL)
+add_library(arrow_testing STATIC IMPORTED GLOBAL)
 
-    add_library(arrow STATIC IMPORTED GLOBAL)
-    add_library(parquet STATIC IMPORTED GLOBAL)
-    add_library(arrow_testing STATIC IMPORTED GLOBAL)
-
-    set_target_properties(
-      arrow arrow_testing parquet
-      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                 $ENV{ARROW_EP_PATH}/install/include)
-    set_target_properties(arrow PROPERTIES IMPORTED_LOCATION ${ARROW_LIB})
-    set_property(TARGET arrow PROPERTY INTERFACE_LINK_LIBRARIES ${RE2} thrift)
-    set_target_properties(parquet PROPERTIES IMPORTED_LOCATION ${PARQUET_LIB})
-    set_target_properties(arrow_testing PROPERTIES IMPORTED_LOCATION
-                                                   ${ARROW_TESTING_LIB})
-  endif()
+find_path(ARROW_INCLUDE_PATH arrow/api.h)
+set_target_properties(
+  arrow arrow_testing parquet PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                                         ${ARROW_INCLUDE_PATH})
+set_target_properties(arrow PROPERTIES IMPORTED_LOCATION ${ARROW_LIB})
+set_target_properties(parquet PROPERTIES IMPORTED_LOCATION ${PARQUET_LIB})
+set_target_properties(arrow_testing PROPERTIES IMPORTED_LOCATION
+                                               ${ARROW_TESTING_LIB})
