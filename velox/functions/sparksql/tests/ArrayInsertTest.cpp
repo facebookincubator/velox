b1db4f6b90875dc39ac235a5928f204b6d2361d9
Tengfei Huang <tengfei.h@gmail.com>
2024-07-10 10:09:00 +0800

    address comments

--- velox/functions/sparksql/tests/ArrayInsertTest.cpp
+++ velox/functions/sparksql/tests/ArrayInsertTest.cpp
@@ -51,71 +51,67 @@ TEST_F(ArrayInsertTest, nullPosition) {
   const auto expected = makeArrayVectorFromJson<int64_t>({"null"});
 
   testExpression(
-      "array_insert(c0, cast(null as integer), 1, false)", {arrays}, expected);
+      "array_insert(c0, null::INTEGER, 1, false)", {arrays}, expected);
 }
 
 TEST_F(ArrayInsertTest, basic) {
   const auto arrays = makeArrayVectorFromJson<int64_t>({"[1]", "[2, 2]"});
 
-  const auto expected =
-      makeArrayVectorFromJson<int64_t>({"[0, 1]", "[0, 2, 2]"});
+  auto expected = makeArrayVectorFromJson<int64_t>({"[0, 1]", "[0, 2, 2]"});
   testExpression("array_insert(c0, 1, 0, false)", {arrays}, expected);
 
-  const auto expected1 =
-      makeArrayVectorFromJson<int64_t>({"[null, 1]", "[null, 2, 2]"});
+  expected = makeArrayVectorFromJson<int64_t>({"[null, 1]", "[null, 2, 2]"});
   testExpression(
-      "array_insert(c0, 1, cast(null as integer), false)", {arrays}, expected1);
+      "array_insert(c0, 1, null::INTEGER, false)", {arrays}, expected);
 }
 
 TEST_F(ArrayInsertTest, posGTArraySize) {
   const auto arrays = makeArrayVectorFromJson<int64_t>({"[1]", "[2, 2]"});
 
-  const auto expected =
+  auto expected =
       makeArrayVectorFromJson<int64_t>({"[1, null, 0]", "[2, 2, 0]"});
   testExpression("array_insert(c0, 3, 0, false)", {arrays}, expected);
 
-  const auto expected1 =
+  expected =
       makeArrayVectorFromJson<int64_t>({"[1, null, null]", "[2, 2, null]"});
   testExpression(
-      "array_insert(c0, 3, cast(null as integer), false)", {arrays}, expected1);
+      "array_insert(c0, 3, null::INTEGER, false)", {arrays}, expected);
 }
 
 TEST_F(ArrayInsertTest, negativePos) {
   const auto arrays =
       makeArrayVectorFromJson<int64_t>({"[1]", "[2, 2]", "[3, 3, 3]"});
 
-  const auto expected = makeArrayVectorFromJson<int64_t>(
+  auto expected = makeArrayVectorFromJson<int64_t>(
       {"[0, null, 1]", "[0, 2, 2]", "[3, 0, 3, 3]"});
   testExpression("array_insert(c0, -3, 0, false)", {arrays}, expected);
 
-  const auto expected1 =
+  expected =
       makeArrayVectorFromJson<int64_t>({"[1, 0]", "[2, 2, 0]", "[3, 3, 3, 0]"});
-  testExpression("array_insert(c0, -1, 0, false)", {arrays}, expected1);
+  testExpression("array_insert(c0, -1, 0, false)", {arrays}, expected);
 
-  const auto expected2 = makeArrayVectorFromJson<int64_t>(
+  expected = makeArrayVectorFromJson<int64_t>(
       {"[null, null, 1]", "[null, 2, 2]", "[3, null, 3, 3]"});
   testExpression(
-      "array_insert(c0, -3, cast(null as integer), false)",
-      {arrays},
-      expected2);
+      "array_insert(c0, -3, null::INTEGER, false)", {arrays}, expected);
 }
 
 TEST_F(ArrayInsertTest, negativePosLegacy) {
   const auto arrays =
       makeArrayVectorFromJson<int64_t>({"[1]", "[2, 2]", "[3, 3, 3]"});
 
-  const auto expected = makeArrayVectorFromJson<int64_t>(
+  auto expected = makeArrayVectorFromJson<int64_t>(
       {"[0, null, null, 1]", "[0, null, 2, 2]", "[0, 3, 3, 3]"});
   testExpression("array_insert(c0, -3, 0, true)", {arrays}, expected);
 
-  const auto expected1 =
+  expected =
       makeArrayVectorFromJson<int64_t>({"[0, 1]", "[2, 0, 2]", "[3, 3, 0, 3]"});
-  testExpression("array_insert(c0, -1, 0, true)", {arrays}, expected1);
+  testExpression("array_insert(c0, -1, 0, true)", {arrays}, expected);
 
-  const auto expected2 = makeArrayVectorFromJson<int64_t>(
+  expected = makeArrayVectorFromJson<int64_t>(
       {"[null, null, null, 1]", "[null, null, 2, 2]", "[null, 3, 3, 3]"});
   testExpression(
-      "array_insert(c0, -3, cast(null as integer), true)", {arrays}, expected2);
+      "array_insert(c0, -3, null::INTEGER, true)", {arrays}, expected);
 }
 
 } // namespace
