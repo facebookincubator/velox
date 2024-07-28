13e32c423f0643b7d3f9051f9e22d71022c34a1b
joey.ljy <joey.ljy@alibaba-inc.com>
2024-07-28 23:06:47 +0800

    Support DELTA_BYTE_ARRAY encoding in native Parquet reader

--- velox/dwio/parquet/reader/DeltaBpDecoder.h
+++ velox/dwio/parquet/reader/DeltaBpDecoder.h
@@ -86,6 +86,15 @@ class DeltaBpDecoder {
     return static_cast<int64_t>(totalValuesRemaining_);
   }
 
+  template <typename T>
+  void readValues(std::vector<T>& values, int32_t numValues) {
+    VELOX_CHECK_LE(numValues, totalValuesRemaining_);
+    VELOX_CHECK_EQ(values.size(), numValues);
+    for (auto i = 0; i < numValues; i++) {
+      values[i] = T(readLong());
+    }
+  }
+
  private:
   bool getVlqInt(uint64_t& v) {
     uint64_t tmp = 0;
--- velox/dwio/parquet/reader/PageReader.cpp
+++ velox/dwio/parquet/reader/PageReader.cpp
@@ -699,6 +699,14 @@ void PageReader::makeDecoder() {
               "DELTA_BINARY_PACKED decoder only supports INT32 and INT64");
       }
       break;
+    case Encoding::DELTA_BYTE_ARRAY:
+      if (parquetType == thrift::Type::BYTE_ARRAY) {
+        deltaByteArrDecoder_ =
+            std::make_unique<DeltaByteArrayDecoder>(pageData_);
+        break;
+      } else {
+        VELOX_UNSUPPORTED("DELTA_BYTE_ARRAY decoder only supports BYTE_ARRAY");
+      }
     default:
       VELOX_UNSUPPORTED("Encoding not supported yet: {}", encoding_);
   }
@@ -739,6 +747,8 @@ void PageReader::skip(int64_t numRows) {
     booleanDecoder_->skip(toSkip);
   } else if (deltaBpDecoder_) {
     deltaBpDecoder_->skip(toSkip);
+  } else if (deltaByteArrDecoder_) {
+    deltaByteArrDecoder_->skip(toSkip);
   } else {
     VELOX_FAIL("No decoder to skip");
   }
