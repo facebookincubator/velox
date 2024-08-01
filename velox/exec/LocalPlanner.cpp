e498fd6b2b19230dc1d7b40103e38883c50da7f3
SeaRise <hhssearise@foxmail.com>
2024-08-01 11:24:48 +0800

    trigger ci

--- velox/exec/LocalPlanner.cpp
+++ velox/exec/LocalPlanner.cpp
@@ -238,7 +238,6 @@ uint32_t maxDrivers(
       if (!connectorInsertHandle->supportsMultiThreading()) {
         return 1;
       } else {
-        
         auto writerCount = tableWrite->hasPartitioningScheme()
             ? queryConfig.taskPartitionedWriterCount()
             : queryConfig.taskWriterCount();
