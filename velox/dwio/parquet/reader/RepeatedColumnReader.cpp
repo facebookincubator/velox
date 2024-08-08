959f214cfebc5101a947ab70e93041cfaf2f55f0
yan ma <yan.ma@intel.com>
2024-08-08 17:56:38 +0800

    fix internal tests

--- velox/dwio/parquet/reader/RepeatedColumnReader.cpp
+++ velox/dwio/parquet/reader/RepeatedColumnReader.cpp
@@ -211,6 +211,13 @@ void MapColumnReader::read(
   elementReader_->seekTo(childTargetReadOffset_, false);
 }
 
+void MapColumnReader::filterRowGroups(
+    uint64_t rowGroupSize,
+    const dwio::common::StatsContext& context,
+    dwio::common::FormatData::FilterRowGroupsResult& result) const {
+  // empty placeholder to avoid incorrect calling on parent's impl
+}
+
 ListColumnReader::ListColumnReader(
     const TypePtr& requestedType,
     const std::shared_ptr<const dwio::common::TypeWithId>& fileType,
@@ -304,6 +311,13 @@ void ListColumnReader::read(
   // repdefs will be scanned and new lengths provided, overwriting the
   // previous ones before the next read().
   child_->seekTo(childTargetReadOffset_, false);
+}
+
+void ListColumnReader::filterRowGroups(
+    uint64_t rowGroupSize,
+    const dwio::common::StatsContext& context,
+    dwio::common::FormatData::FilterRowGroupsResult& result) const {
+  // empty placeholder to avoid incorrect calling on parent's impl
 }
 
 } // namespace facebook::velox::parquet
