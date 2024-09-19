abb6cfaa95884db992cb05c664f15da1a0794ffd
zhaokuo03 <zhaokuo03@meituan.com>
2024-09-19 20:55:07 +0800

    fix

--- velox/functions/sparksql/RegexFunctions.cpp
+++ velox/functions/sparksql/RegexFunctions.cpp
@@ -30,94 +30,6 @@ void ensureRegexIsConstant(
   }
 }
 
-/// Spark uses java.util.regex in regexp_replacement. This function preprocesses
-/// the pattern from Spark to ensure it is compatible with RE2.
-/// java.util.regex supports named capturing groups in the format
-/// (?<name>regex), but in RE2, this is written as (?P<name>regex), so we need
-/// to convert the former format to the latter.
-FOLLY_ALWAYS_INLINE std::string prepareRegexpPattern(
-    const StringView& pattern) {
-  static const RE2 kRegex("[(][?]<([^>]*)>");
-
-  std::string newPattern = pattern.getString();
-  RE2::GlobalReplace(&newPattern, kRegex, R"((?P<\1>)");
-
-  return newPattern;
-}
-
-/// Spark uses java.util.regex in regexp_replacement. This function preprocesses
-/// the replacement to ensure it is compatible with RE2.
-/// 1. RE2 replacement only supports group index capture, so we need to convert
-/// group name captures to group index captures.
-/// 2. Group index capture in java.util.regex replacement is '$N', while in RE2
-/// replacement it is '\N'. We need to convert it.
-/// 3. Replacement in RE2 only supports '\' followed by a digit or another '\',
-/// while java.util.regex will ignore '\' in replacements, so we need to
-/// unescape it.
-FOLLY_ALWAYS_INLINE std::string prepareReplacement(
-    const RE2& re,
-    const StringView& replacement) {
-  if (replacement.empty()) {
-    return std::string{};
-  }
-
-  auto newReplacement = replacement.getString();
-
-  // If newReplacement contains a reference to a
-  // named capturing group ${name}, replace the name with its index.
-  static constexpr const char* kNamedGroup = R"(\${([^}]*)})";
-  static const RE2 kExtractRegex(kNamedGroup);
-  VELOX_DCHECK(
-      kExtractRegex.ok(),
-      "Invalid regular expression {}: {}.",
-      kNamedGroup,
-      kExtractRegex.error());
-  re2::StringPiece groupName[2];
-  while (kExtractRegex.Match(
-      newReplacement,
-      0,
-      newReplacement.size(),
-      RE2::UNANCHORED,
-      groupName,
-      2)) {
-    auto groupIter = re.NamedCapturingGroups().find(groupName[1].as_string());
-    if (groupIter == re.NamedCapturingGroups().end()) {
-      VELOX_USER_FAIL(
-          "Invalid replacement sequence: unknown group {{ {} }}.",
-          groupName[1].as_string());
-    }
-
-    RE2::GlobalReplace(
-        &newReplacement,
-        fmt::format(R"(\${{{}}})", groupName[1].as_string()),
-        fmt::format("${}", groupIter->second));
-  }
-
-  // Convert references to numbered capturing groups from $g to \g.
-  static constexpr const char* kIndexedGroup = R"(\$(\d+))";
-  static const RE2 kConvertRegex(kIndexedGroup);
-  VELOX_DCHECK(
-      kConvertRegex.ok(),
-      "Invalid regular expression {}: {}.",
-      kIndexedGroup,
-      kConvertRegex.error());
-  RE2::GlobalReplace(&newReplacement, kConvertRegex, R"(\\\1)");
-
-  // RE2 allows '\' followed by anything other than a digit or '\',
-  // while java.util.regex will ignore '\' in replacement. We should unescape
-  // this character.
-  static constexpr const char* kUnescape = R"(\\([^0-9\\]))";
-  static const RE2 kUnescapeRegex(kUnescape);
-  VELOX_DCHECK(
-      kUnescapeRegex.ok(),
-      "Invalid regular expression {}: {}.",
-      kUnescape,
-      kUnescapeRegex.error());
-  RE2::GlobalReplace(&newReplacement, kUnescapeRegex, R"(\1)");
-
-  return newReplacement;
-}
-
 // REGEXP_REPLACE(string, pattern, overwrite) → string
 // REGEXP_REPLACE(string, pattern, overwrite, position) → string
 //
@@ -151,7 +63,7 @@ struct RegexpReplaceFunction {
       const arg_type<Varchar>* replacement,
       const arg_type<int64_t>* /*position*/) {
     if (pattern) {
-      const auto processedPattern = prepareRegexpPattern(*pattern);
+      const auto processedPattern = prepareRegexpReplacePattern(*pattern);
       re_.emplace(processedPattern, RE2::Quiet);
       VELOX_USER_CHECK(
           re_->ok(),
@@ -258,7 +170,7 @@ struct RegexpReplaceFunction {
     if (re_.has_value()) {
       return re_.value();
     }
-    auto processedPattern = prepareRegexpPattern(pattern);
+    auto processedPattern = prepareRegexpReplacePattern(pattern);
     return *cache_.findOrCompile(StringView(processedPattern));
   }
 
@@ -266,7 +178,7 @@ struct RegexpReplaceFunction {
       RE2& re,
       const arg_type<Varchar>& replacement) {
     if (!constantReplacement_) {
-      processedReplacement_ = prepareReplacement(re, replacement);
+      processedReplacement_ = prepareRegexpReplaceReplacement(re, replacement);
     }
 
     return processedReplacement_;
