e82442ef280f380be575e6b597f1025d890bd8cd
joey.ljy <joey.ljy@alibaba-inc.com>
2024-09-24 08:51:42 +0800

    Fix format

--- CMake/ResolveDependency.cmake
+++ CMake/ResolveDependency.cmake
@@ -140,7 +140,7 @@ macro(resolve_dependency_url dependency_name)
     VELOX_${dependency_name}_SOURCE_URL VELOX_${dependency_name}_URL
     ${VELOX_${dependency_name}_SOURCE_URL})
   message(VERBOSE "Set VELOX_${dependency_name}_SOURCE_URL to "
-                 "${VELOX_${dependency_name}_SOURCE_URL}")
+          "${VELOX_${dependency_name}_SOURCE_URL}")
   if(DEFINED ENV{VELOX_${dependency_name}_URL})
     set_with_default(VELOX_${dependency_name}_BUILD_SHA256_CHECKSUM
                      VELOX_${dependency_name}_SHA256 "")
