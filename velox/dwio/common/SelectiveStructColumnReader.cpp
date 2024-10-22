27fbeec4dee62cd7dfb6de561b01931f4b09c98c
Christian Zentgraf <kitgocz@gmail.com>
2024-10-22 12:40:53 -0400

    Fix deprecated errors when using shared_ptr::unique()
    After the macOS CommandLineTools SDK15  (with Clang16), the new shared_ptr.h now issues the
    deprecated warnings  for C++17 when using the shared_ptr::unique function. This function is removed for C++20.
    
    The solution is to replace the function with the actual implementation using use_count.

--- velox/dwio/common/SelectiveStructColumnReader.cpp
+++ velox/dwio/common/SelectiveStructColumnReader.cpp
@@ -314,7 +314,7 @@ void fillRowVectorChildren(
 }
 
 VectorPtr tryReuseResult(const VectorPtr& result) {
-  if (!result.unique()) {
+  if (result.use_count() != 1) {
     return nullptr;
   }
   switch (result->encoding()) {
@@ -345,7 +345,7 @@ void setConstantField(
     const VectorPtr& constant,
     vector_size_t size,
     VectorPtr& field) {
-  if (field && field->isConstantEncoding() && field.unique() &&
+  if (field && field->isConstantEncoding() && field.use_count() == 1 &&
       field->size() > 0 && field->equalValueAt(constant.get(), 0, 0)) {
     field->resize(size);
   } else {
@@ -358,7 +358,7 @@ void setNullField(
     VectorPtr& field,
     const TypePtr& type,
     memory::MemoryPool* pool) {
-  if (field && field->isConstantEncoding() && field.unique() &&
+  if (field && field->isConstantEncoding() && field.use_count() == 1 &&
       field->size() > 0 && field->isNullAt(0)) {
     field->resize(size);
   } else {
@@ -442,7 +442,7 @@ void SelectiveStructColumnReaderBase::getValues(
     }
     auto lazyLoader =
         std::make_unique<ColumnLoader>(this, children_[index], numReads_);
-    if (childResult && childResult->isLazy() && childResult.unique()) {
+    if (childResult && childResult->isLazy() && childResult.use_count() == 1) {
       static_cast<LazyVector&>(*childResult)
           .reset(std::move(lazyLoader), rows.size());
     } else {
