73ef460d5c966b6e885af6fd579e2372939f36cc
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2024-11-04 12:00:11 -0600

    simplify override

--- scripts/setup-centos9.sh
+++ scripts/setup-centos9.sh
@@ -183,7 +183,6 @@ function install_arrow {
 }
 
 function install_cuda {
-  dnf install -y patch
   # See https://developer.nvidia.com/cuda-downloads
   dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
   dnf install -y cuda-nvcc-$(echo $1 | tr '.' '-') cuda-cudart-devel-$(echo $1 | tr '.' '-')
