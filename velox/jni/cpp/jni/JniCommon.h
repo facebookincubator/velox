af323043b72976058e4d538b0b35476118288b1c
yiming <100650920@qq.com>
2024-11-18 12:01:19 +0800

    velox-sdk part 1 1. remove some comment

--- velox/jni/cpp/jni/JniCommon.h
+++ velox/jni/cpp/jni/JniCommon.h
@@ -48,13 +48,11 @@ struct SharedPtrHandle {
   SharedPtrHandle(std::shared_ptr<T> node)
       : plan_node(std::static_pointer_cast<void>(node)) {
 #ifdef VELOX_ENABLE_JNI_BINDINGS_REF_DEBUG
-    // 构造函数，增加全局实例计数器
     global_instance_count.fetch_add(1, std::memory_order_relaxed);
     print_instance_count("Constructor with node");
 #endif VELOX_ENABLE_JNI_BINDINGS_REF_DEBUG
   }
 
-  // 拷贝构造函数和移动构造函数也需要增加计数器
   SharedPtrHandle(const SharedPtrHandle&) {
 #ifdef VELOX_ENABLE_JNI_BINDINGS_REF_DEBUG
     global_instance_count.fetch_add(1, std::memory_order_relaxed);
@@ -69,13 +67,11 @@ struct SharedPtrHandle {
 #endif VELOX_ENABLE_JNI_BINDINGS_REF_DEBUG
   }
 
-  // 拷贝赋值操作符和移动赋值操作符不需要修改计数器
   SharedPtrHandle& operator=(const SharedPtrHandle&) = default;
   SharedPtrHandle& operator=(SharedPtrHandle&&) noexcept = default;
 
   ~SharedPtrHandle() {
 #ifdef VELOX_ENABLE_JNI_BINDINGS_REF_DEBUG
-    // 析构函数，减少全局实例计数器
     global_instance_count.fetch_sub(1, std::memory_order_relaxed);
     print_instance_count("Destructor");
 #endif VELOX_ENABLE_JNI_BINDINGS_REF_DEBUG
@@ -90,7 +86,6 @@ struct SharedPtrHandle {
   }
 
   static void print_instance_count(const char* message) {
-    // 打印当前全局实例计数器的值
     std::cout << message << ": SharedPtrHandle instances count is "
               << global_instance_count.load(std::memory_order_relaxed)
               << std::endl;
@@ -122,21 +117,16 @@ static inline jmethodID getStaticMethodId(
 }
 
 static std::string jbyteArrayToString(JNIEnv* env, jbyteArray array) {
-  // 获取数组长度
   jsize length = env->GetArrayLength(array);
 
-  // 如果数组为空，返回空的 std::string
   if (length == 0) {
     return std::string();
   }
 
-  // 获取数组元素
   jbyte* bytes = env->GetByteArrayElements(array, nullptr);
 
-  // 创建 std::string 对象
   std::string str(reinterpret_cast<char*>(bytes), length);
 
-  // 释放数组元素
   env->ReleaseByteArrayElements(array, bytes, JNI_ABORT);
 
   return str;
@@ -146,11 +136,8 @@ static inline std::string jStringToCString(JNIEnv* env, jstring jStr) {
   if (!jStr) {
     return "";
   }
-  // 将jstring转换为UTF-8格式的C字符串
   const char* chars = env->GetStringUTFChars(jStr, nullptr);
-  // 使用C字符串创建std::string实例
   std::string ret(chars);
-  // 通知JVM不再需要这个C字符串了
   env->ReleaseStringUTFChars(jStr, chars);
   return ret;
 }
@@ -161,31 +148,22 @@ static inline std::vector<std::string> ConvertJStringArrayToVector(
   if (!jStringArray) {
     return {};
   }
-  // 获取数组长度
   jsize arrayLength = env->GetArrayLength(jStringArray);
 
-  // 创建一个std::vector<std::string>，预留足够的空间
   std::vector<std::string> result;
   result.reserve(arrayLength);
 
-  // 遍历Java字符串数组
   for (jsize i = 0; i < arrayLength; i++) {
-    // 获取Java字符串
     jstring jStr = (jstring)env->GetObjectArrayElement(jStringArray, i);
 
-    // 检查是否正常获取到Java字符串
     if (jStr) {
-      // 将Java字符串转换为C++字符串
       const char* rawStr = env->GetStringUTFChars(jStr, nullptr);
       std::string str(rawStr);
 
-      // 释放Java字符串内存
       env->ReleaseStringUTFChars(jStr, rawStr);
 
-      // 将C++字符串添加到vector中
       result.push_back(str);
 
-      // 删除局部引用，防止内存泄漏
       env->DeleteLocalRef(jStr);
     }
   }
