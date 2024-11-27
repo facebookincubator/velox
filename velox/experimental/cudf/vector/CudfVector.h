2246d019d3e55b4c1542025cf4e7a323ef5a5b15
Bradley Dice <bdice@bradleydice.com>
2024-11-27 12:14:30 -0800

    Add CudfVector.

---
+++ velox/experimental/cudf/vector/CudfVector.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "velox/buffer/Buffer.h"
+#include "velox/common/memory/MemoryPool.h"
+#include "velox/vector/BaseVector.h"
+#include "velox/vector/TypeAliases.h"
+
+#include <cudf/table/table.hpp>
+#include <cudf/utilities/default_stream.hpp>
+
+#include <memory>
+#include <utility>
+
+namespace facebook::velox::cudf_velox {
+
+// Vector class which holds GPU data from cuDF. This also owns the stream.
+class CudfVector : public BaseVector {
+ public:
+  CudfVector(
+      velox::memory::MemoryPool* pool,
+      TypePtr type,
+      vector_size_t size,
+      std::unique_ptr<cudf::table>&& table)
+      : BaseVector(
+            pool,
+            std::move(type),
+            VectorEncoding::Simple::FLAT,
+            BufferPtr(nullptr),
+            size), table_{std::move(table)} {}
+
+ private:
+  std::unique_ptr<cudf::table> table_;
+};
+
+using CudfVectorPtr = std::shared_ptr<CudfVector>;
+
+} // namespace facebook::velox::cudf_velox
