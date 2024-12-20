580876c7b89151625c448102d9eb83b72480779f
Bradley Dice <bdice@bradleydice.com>
2024-12-20 13:04:47 -0800

    Fix: Materialize lazy vectors and convert to cuDF eagerly.

--- velox/experimental/cudf/exec/CudfConversion.h
+++ velox/experimental/cudf/exec/CudfConversion.h
@@ -60,7 +60,7 @@ class CudfFromVelox : public exec::Operator {
 
  private:
   CudfVectorPtr outputTable_;
-  std::vector<RowVectorPtr> inputs_;
+  std::vector<std::unique_ptr<cudf::table>> inputs_;
   bool finished_ = false;
 };
 
