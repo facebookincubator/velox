cbb73617332922ba8c9ff77958ae4ca616d8bd98
rexan <rexan@apache.org>
2025-02-05 05:17:21 -0500

    feat: Support Spark ArraySort with lambda function

--- velox/functions/prestosql/registration/ArrayFunctionsRegistration.cpp
+++ velox/functions/prestosql/registration/ArrayFunctionsRegistration.cpp
@@ -157,12 +157,16 @@ void registerArrayFunctions(const std::string& prefix) {
       makeArrayShuffle,
       getMetadataForArrayShuffle());
 
-  VELOX_REGISTER_VECTOR_FUNCTION(udf_array_sort, prefix + "array_sort");
-  VELOX_REGISTER_VECTOR_FUNCTION(
-      udf_array_sort_desc, prefix + "array_sort_desc");
+  exec::registerStatefulVectorFunction(
+      prefix + "array_sort", arraySortSignatures(true), makeArraySortAsc);
+  exec::registerStatefulVectorFunction(
+      prefix + "array_sort_desc",
+      arraySortSignatures(false),
+      makeArraySortDesc);
 
-  exec::registerExpressionRewrite([prefix](const auto& expr) {
-    return rewriteArraySortCall(prefix, expr);
+  auto checker = std::make_shared<SimpleComparisonChecker>();
+  exec::registerExpressionRewrite([prefix, checker](const auto& expr) {
+    return rewriteArraySortCall(prefix, expr, checker);
   });
 
   VELOX_REGISTER_VECTOR_FUNCTION(udf_array_sum, prefix + "array_sum");
