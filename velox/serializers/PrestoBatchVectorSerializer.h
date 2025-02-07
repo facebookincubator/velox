610c90316b3efe8c7cd4b3482e2a6aa15811ea0b
Kevin Wilfong <kevinwilfong@meta.com>
2025-02-07 14:57:45 -0800

    feat: Optimize PrestoBatchVectorSerializer [3/7]: Serialize ArrayVectors (#12062)
    Summary:
    
    Context:
    This is a series of diffs in which I reimplement PrestoBatchVectorSerializer to write directly to the output stream, 
    rather than the indirect route it currently uses via VectorStreams. Reusing VectorStreams and much of the code
    for PrestoIterativeVectorSerializer prevented us from capturing all of the performance benefits of writing data in
    batches rather than row by row. These changes combined will speed up PrestoBatchVectorSerializer 2-3x (as
    measured in Presto queries and other use cases).
    
    In the final diff I will integrate the new serialization functions into PrestoBatchVectorSerializer's serialize 
    function which will switch it to the new optimized writing path, therefore I will land these changes as a stack.
    
    In this diff:
    I provide the implementations for serializing ArrayVectors.
    
    Reviewed By: xiaoxmeng
    
    Differential Revision: D68047629

--- velox/serializers/PrestoBatchVectorSerializer.h
+++ velox/serializers/PrestoBatchVectorSerializer.h
@@ -520,6 +520,12 @@ class PrestoBatchVectorSerializer : public BatchVectorSerializer {
       const VectorPtr& vector,
       const folly::Range<const RangeType*>& ranges);
 
+  template <typename RangeType>
+  void serializeArrayVector(
+      BufferedOutputStream* stream,
+      const VectorPtr& vector,
+      const folly::Range<const RangeType*>& ranges);
+
   const std::unique_ptr<folly::io::Codec> codec_;
   const PrestoVectorSerde::PrestoOptions opts_;
   StreamArena arena_;
