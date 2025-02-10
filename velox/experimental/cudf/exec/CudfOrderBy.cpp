56a666095aa61f00b6f56acb1082565c560c0506
Devavret Makkar <dmakkar@nvidia.com>
2025-02-10 11:41:02 +0000

    Add utility function to concatenate cuDF tables
    Move and refactor the table concatenation utility function to a central location in Utilities.cpp and Utilities.h. This change simplifies table concatenation across different cuDF operators and removes duplicate code.

--- velox/experimental/cudf/exec/CudfOrderBy.cpp
+++ velox/experimental/cudf/exec/CudfOrderBy.cpp
@@ -86,18 +86,15 @@ void CudfOrderBy::noMoreInput() {
     return;
   }
   auto cudf_tables = std::vector<std::unique_ptr<cudf::table>>(inputs_.size());
-  auto cudf_table_views = std::vector<cudf::table_view>(inputs_.size());
   for (int i = 0; i < inputs_.size(); i++) {
     VELOX_CHECK_NOT_NULL(inputs_[i]);
     cudf_tables[i] = inputs_[i]->release();
-    cudf_table_views[i] = cudf_tables[i]->view();
   }
-  auto tbl = cudf::concatenate(cudf_table_views);
+  auto tbl = concatenateTables(std::move(cudf_tables));
 
   // Release input data
-  cudf_table_views.clear();
-  cudf_tables.clear();
   inputs_.clear();
+
   VELOX_CHECK_NOT_NULL(tbl);
   if (cudfDebugEnabled()) {
     std::cout << "Sort input table number of columns: " << tbl->num_columns()
