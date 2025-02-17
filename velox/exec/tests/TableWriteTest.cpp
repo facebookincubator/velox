968516a26414897dfabd12958deec99c452b1afc
Ke Jia <ke.a.jia@intel.com>
2025-02-17 15:59:51 +0000

    Specify partition key order in TableWrite operator

--- velox/exec/tests/TableWriteTest.cpp
+++ velox/exec/tests/TableWriteTest.cpp
@@ -2126,108 +2126,122 @@ TEST_P(AllTableWriterTest, commitStrategies) {
 }
 
 TEST_P(PartitionedTableWriterTest, specialPartitionName) {
-  const int32_t numPartitions = 50;
-  const int32_t numBatches = 2;
+  std::function<void(
+      const std::vector<std::string>&, const std::vector<TypePtr>)>
+      testSpecialPartitionName = [&](const std::vector<std::string>&
+                                         partitionKeys,
+                                     const std::vector<TypePtr>
+                                         partitionTypes) {
+        const int32_t numPartitions = 50;
+        const int32_t numBatches = 2;
 
-  const auto rowType =
-      ROW({"c0", "p0", "p1", "c1", "c3", "c5"},
-          {INTEGER(), INTEGER(), VARCHAR(), BIGINT(), REAL(), VARCHAR()});
-  const std::vector<std::string> partitionKeys = {"p0", "p1"};
-  const std::vector<TypePtr> partitionTypes = {INTEGER(), VARCHAR()};
+        const auto rowType =
+            ROW({"c0", "p0", "p1", "c1", "c3", "c5"},
+                {INTEGER(), INTEGER(), VARCHAR(), BIGINT(), REAL(), VARCHAR()});
 
-  const std::vector charsToEscape = {
-      '"',
-      '#',
-      '%',
-      '\'',
-      '*',
-      '/',
-      ':',
-      '=',
-      '?',
-      '\\',
-      '\x7F',
-      '{',
-      '[',
-      ']',
-      '^'};
-  ASSERT_GE(numPartitions, charsToEscape.size());
-  std::vector<RowVectorPtr> vectors = makeBatches(numBatches, [&](auto) {
-    return makeRowVector(
-        rowType->names(),
-        {
-            makeFlatVector<int32_t>(
-                numPartitions, [&](auto row) { return row + 100; }),
-            makeFlatVector<int32_t>(
-                numPartitions, [&](auto row) { return row; }),
-            makeFlatVector<StringView>(
-                numPartitions,
-                [&](auto row) {
-                  // special character
-                  return StringView::makeInline(
-                      fmt::format("str_{}{}", row, charsToEscape.at(row % 15)));
-                }),
-            makeFlatVector<int64_t>(
-                numPartitions, [&](auto row) { return row + 1000; }),
-            makeFlatVector<float>(
-                numPartitions, [&](auto row) { return row + 33.23; }),
-            makeFlatVector<StringView>(
-                numPartitions,
-                [&](auto row) {
-                  return StringView::makeInline(
-                      fmt::format("bucket_{}", row * 3));
-                }),
+        const std::vector charsToEscape = {
+            '"',
+            '#',
+            '%',
+            '\'',
+            '*',
+            '/',
+            ':',
+            '=',
+            '?',
+            '\\',
+            '\x7F',
+            '{',
+            '[',
+            ']',
+            '^'};
+        ASSERT_GE(numPartitions, charsToEscape.size());
+        std::vector<RowVectorPtr> vectors = makeBatches(numBatches, [&](auto) {
+          return makeRowVector(
+              rowType->names(),
+              {
+                  makeFlatVector<int32_t>(
+                      numPartitions, [&](auto row) { return row + 100; }),
+                  makeFlatVector<int32_t>(
+                      numPartitions, [&](auto row) { return row; }),
+                  makeFlatVector<StringView>(
+                      numPartitions,
+                      [&](auto row) {
+                        // special character
+                        return StringView::makeInline(fmt::format(
+                            "str_{}{}", row, charsToEscape.at(row % 15)));
+                      }),
+                  makeFlatVector<int64_t>(
+                      numPartitions, [&](auto row) { return row + 1000; }),
+                  makeFlatVector<float>(
+                      numPartitions, [&](auto row) { return row + 33.23; }),
+                  makeFlatVector<StringView>(
+                      numPartitions,
+                      [&](auto row) {
+                        return StringView::makeInline(
+                            fmt::format("bucket_{}", row * 3));
+                      }),
+              });
         });
-  });
-  createDuckDbTable(vectors);
+        createDuckDbTable(vectors);
 
-  auto inputFilePaths = makeFilePaths(numBatches);
-  for (int i = 0; i < numBatches; i++) {
-    writeToFile(inputFilePaths[i]->getPath(), vectors[i]);
-  }
+        auto inputFilePaths = makeFilePaths(numBatches);
+        for (int i = 0; i < numBatches; i++) {
+          writeToFile(inputFilePaths[i]->getPath(), vectors[i]);
+        }
 
-  auto outputDirectory = TempDirectoryPath::create();
-  auto plan = createInsertPlan(
-      PlanBuilder().tableScan(rowType),
-      rowType,
-      outputDirectory->getPath(),
-      partitionKeys,
-      bucketProperty_,
-      compressionKind_,
-      getNumWriters(),
-      connector::hive::LocationHandle::TableType::kNew,
-      commitStrategy_);
+        auto outputDirectory = TempDirectoryPath::create();
+        auto plan = createInsertPlan(
+            PlanBuilder().tableScan(rowType),
+            rowType,
+            outputDirectory->getPath(),
+            partitionKeys,
+            bucketProperty_,
+            compressionKind_,
+            getNumWriters(),
+            connector::hive::LocationHandle::TableType::kNew,
+            commitStrategy_);
 
-  auto task = assertQuery(plan, inputFilePaths, "SELECT count(*) FROM tmp");
+        auto task =
+            assertQuery(plan, inputFilePaths, "SELECT count(*) FROM tmp");
 
-  std::set<std::string> actualPartitionDirectories =
-      getLeafSubdirectories(outputDirectory->getPath());
+        std::set<std::string> actualPartitionDirectories =
+            getLeafSubdirectories(outputDirectory->getPath());
 
-  std::set<std::string> expectedPartitionDirectories;
-  const std::vector<std::string> expectedCharsAfterEscape = {
-      "%22",
-      "%23",
-      "%25",
-      "%27",
-      "%2A",
-      "%2F",
-      "%3A",
-      "%3D",
-      "%3F",
-      "%5C",
-      "%7F",
-      "%7B",
-      "%5B",
-      "%5D",
-      "%5E"};
-  for (auto i = 0; i < numPartitions; ++i) {
-    // url encoded
-    auto partitionName = fmt::format(
-        "p0={}/p1=str_{}{}", i, i, expectedCharsAfterEscape.at(i % 15));
-    expectedPartitionDirectories.emplace(
-        fs::path(outputDirectory->getPath()) / partitionName);
-  }
-  EXPECT_EQ(actualPartitionDirectories, expectedPartitionDirectories);
+        std::set<std::string> expectedPartitionDirectories;
+        const std::vector<std::string> expectedCharsAfterEscape = {
+            "%22",
+            "%23",
+            "%25",
+            "%27",
+            "%2A",
+            "%2F",
+            "%3A",
+            "%3D",
+            "%3F",
+            "%5C",
+            "%7F",
+            "%7B",
+            "%5B",
+            "%5D",
+            "%5E"};
+        for (auto i = 0; i < numPartitions; ++i) {
+          // url encoded
+          std::string partitionName;
+          if (partitionKeys == std::vector<std::string>{"p0", "p1"}) {
+            partitionName = fmt::format(
+                "p0={}/p1=str_{}{}", i, i, expectedCharsAfterEscape.at(i % 15));
+          } else if (partitionKeys == std::vector<std::string>{"p1", "p0"}) {
+            partitionName = fmt::format(
+                "p1=str_{}{}/p0={}", i, expectedCharsAfterEscape.at(i % 15), i);
+          }
+          expectedPartitionDirectories.emplace(
+              fs::path(outputDirectory->getPath()) / partitionName);
+        }
+        EXPECT_EQ(actualPartitionDirectories, expectedPartitionDirectories);
+      };
+  testSpecialPartitionName({"p0", "p1"}, {VARCHAR(), INTEGER()});
+  testSpecialPartitionName({"p1", "p0"}, {INTEGER(), VARCHAR()});
 }
 
 TEST_P(PartitionedTableWriterTest, multiplePartitions) {
