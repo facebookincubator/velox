7d143a3b522e01c96d20bc0c3ce6c4cd37327c5b
Xin Zhang <desertsxin@gmail.com>
2025-02-18 21:04:00 +0000

    update metric names and comments.

--- velox/connectors/hive/storage_adapters/s3fs/S3Counters.h
+++ velox/connectors/hive/storage_adapters/s3fs/S3Counters.h
@@ -17,30 +17,40 @@
 
 namespace facebook::velox::filesystems {
 
-// The number of opened connections for S3 read operations.
+// The number of connections open for S3 read operations.
 constexpr std::string_view kMetricS3ActiveConnections{
-    "presto_cpp.hive.s3.active_connections"};
-// The number of write operations to S3 objects.
-constexpr std::string_view kMetricS3StartedUploads{
-    "presto_cpp.hive.s3.started_uploads"};
+    "velox.s3.active_connections"};
+
+// The number of S3 upload calls that started.
+constexpr std::string_view kMetricS3StartedUploads{"velox.s3.started_uploads"};
+
+// The number of S3 upload calls that were completed.
 constexpr std::string_view kMetricS3SuccessfulUploads{
-    "presto_cpp.hive.s3.successful_uploads"};
-constexpr std::string_view kMetricS3FailedUploads{
-    "presto_cpp.hive.s3.failed_uploads"};
-/// The number of metadata retrieval operations for S3 objects,
-/// which can occur during both read and write operations.
-constexpr std::string_view kMetricS3MetadataCalls{
-    "presto_cpp.hive.s3.metadata_calls"};
-// The number of read operations on S3 objects.
-constexpr std::string_view kMetricS3GetObjectCalls{
-    "presto_cpp.hive.s3.get_object_calls"};
-constexpr std::string_view kMetricS3GetObjectErrors{
-    "presto_cpp.hive.s3.get_object_errors"};
+    "velox.s3.successful_uploads"};
+
+// The number of S3 upload calls that failed.
+constexpr std::string_view kMetricS3FailedUploads{"velox.s3.failed_uploads"};
+
+// The number of S3 head (metadata) calls.
+constexpr std::string_view kMetricS3MetadataCalls{"velox.s3.metadata_calls"};
+
+// The number of S3 head (metadata) calls that failed.
 constexpr std::string_view kMetricS3GetMetadataErrors{
-    "presto_cpp.hive.s3.get_metadata_errors"};
-constexpr std::string_view kMetricS3GetObjectRetries{
-    "presto_cpp.hive.s3.get_object_retries"};
+    "velox.s3.get_metadata_errors"};
+
+// The number of retries made during S3 head (metadata) calls.
 constexpr std::string_view kMetricS3GetMetadataRetries{
-    "presto_cpp.hive.s3.get_metadata_retries"};
+    "velox.s3.get_metadata_retries"};
+
+// The number of S3 getObject calls.
+constexpr std::string_view kMetricS3GetObjectCalls{"velox.s3.get_object_calls"};
+
+// The number of S3 getObject calls that failed.
+constexpr std::string_view kMetricS3GetObjectErrors{
+    "velox.s3.get_object_errors"};
+
+// The number of retries made during S3 getObject calls.
+constexpr std::string_view kMetricS3GetObjectRetries{
+    "velox.s3.get_object_retries"};
 
 } // namespace facebook::velox::filesystems
