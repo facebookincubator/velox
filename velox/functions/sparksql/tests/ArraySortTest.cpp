ccee61b7f3d271631515a6d873c7cd6e04cba564
rexan <rexan@apache.org>
2025-02-20 07:32:15 -0500

    Fix typo

--- velox/functions/sparksql/tests/ArraySortTest.cpp
+++ velox/functions/sparksql/tests/ArraySortTest.cpp
@@ -212,7 +212,7 @@ TEST_F(ArraySortTest, lambda) {
   VELOX_ASSERT_THROW(
       evaluate(
           "array_sort(c0, (x, y) -> IF(lessthan(x, y), 1, IF(equalto(x, y), 0, null)))",
-          data),
+          makeRowVector({data})),
       "Else clause of a SWITCH statement must have the same type as 'then' clauses. Expected BIGINT, but got UNKNOWN.");
 }
 
