ccfae3e1b862d73ff075d132dce7ef56e29f787f
Kevin Wilfong <kevinwilfong@meta.com>
2025-02-28 12:49:55 -0800

    test: Add InputGenerator for TimestampWithTimeZone (#12481)
    Summary:
    
    This diff adds an implementation of AbstractInputGenerator for the TimestampWithTimeZone 
    type.  It uses a random int64_t for the number of milliseconds (there may be overflow since we
    only have 52 bits for the milliseconds but that doesn't matter since it's random anyway) and
    selects a random time zone ID from a list of valid time zone IDs provided by TimeZoneMap.
    
    Differential Revision: D70362561

---
+++ velox/functions/prestosql/types/fuzzer_utils/TimestampWithTimeZoneInputGenerator.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/functions/prestosql/types/fuzzer_utils/TimestampWithTimeZoneInputGenerator.h"
+
+#include "velox/common/fuzzer/Utils.h"
+#include "velox/functions/prestosql/types/TimestampWithTimeZoneType.h"
+#include "velox/type/Variant.h"
+#include "velox/type/tz/TimeZoneMap.h"
+
+namespace facebook::velox::fuzzer {
+TimestampWithTimeZoneInputGenerator::TimestampWithTimeZoneInputGenerator(
+    const size_t seed,
+    const double nullRatio)
+    : AbstractInputGenerator(
+          seed,
+          TIMESTAMP_WITH_TIME_ZONE(),
+          nullptr,
+          nullRatio),
+      timeZoneIds_(tz::getTimeZoneIDs()) {}
+
+variant TimestampWithTimeZoneInputGenerator::generate() {
+  if (coinToss(rng_, nullRatio_)) {
+    return variant::null(type_->kind());
+  }
+
+  int16_t timeZoneId = timeZoneIds_
+      [boost::random::uniform_int_distribution<size_t>()(rng_) %
+       timeZoneIds_.size()];
+
+  return pack(
+      boost::random::uniform_int_distribution<int64_t>()(rng_), timeZoneId);
+}
+} // namespace facebook::velox::fuzzer
