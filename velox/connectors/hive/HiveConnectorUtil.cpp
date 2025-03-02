4b5f475b6fe9600f2a6ec8dfe9c4fab0c0254f61
ccat3z <fzhang.chn@outlook.com>
2025-03-02 22:33:17 +0800

    Revert "feat(orc): Add param force_positional"
    This reverts commit 3a9938d7bae938e67ac433318e11e6a813306eec.

--- velox/connectors/hive/HiveConnectorUtil.cpp
+++ velox/connectors/hive/HiveConnectorUtil.cpp
@@ -560,13 +560,6 @@ void configureReaderOptions(
     case dwio::common::FileFormat::ORC: {
       useColumnNamesForColumnMapping =
           hiveConfig->isOrcUseColumnNames(sessionProperties);
-
-      auto forcePositionalIt = tableParameters.find("force_positional");
-      if (forcePositionalIt != tableParameters.end() &&
-          folly::to<bool>(forcePositionalIt->second)) {
-        useColumnNamesForColumnMapping = false;
-      }
-
       readerOptions.setUseNestedColumnNamesForColumnMapping(
           hiveConfig->isOrcUseNestedColumnNames());
       break;
