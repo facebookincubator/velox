ddd66e8863fef5106f4a7bd470667b65e6558433
iamorchid <will.j.zhang@outlook.com>
2025-03-07 10:39:26 +0800

    fix: Re-use output across probe rows for NestedLoopJoin

--- velox/exec/NestedLoopJoinProbe.cpp
+++ velox/exec/NestedLoopJoinProbe.cpp
@@ -263,10 +263,27 @@ RowVectorPtr NestedLoopJoinProbe::generateOutput() {
     finishProbeInput();
   }
 
-  if (output_ != nullptr && output_->size() == 0) {
-    output_ = nullptr;
+  if (!readyToProduceOutput()) {
+    return nullptr;
   }
+
+  output_->resize(numOutputRows_);
   return std::move(output_);
+}
+
+bool NestedLoopJoinProbe::readyToProduceOutput() {
+  if (!output_ || numOutputRows_ == 0) {
+    return false;
+  }
+
+  // For cross join, output is produced directly
+  if (isCrossJoin()) {
+    return true;
+  }
+
+  // if the input_ has no remaining rows or the output_ is fully filled,
+  // it's right time for output.
+  return !input_ || numOutputRows_ >= outputBatchSize_;
 }
 
 bool NestedLoopJoinProbe::advanceProbe() {
@@ -357,9 +374,6 @@ bool NestedLoopJoinProbe::addToOutput() {
   // Check if the current probed row needs to be added as a mismatch (for left
   // and full outer joins).
   checkProbeMismatchRow();
-  if (output_ != nullptr) {
-    output_->resize(numOutputRows_);
-  }
 
   // Signals that all input has been generated for the probeRow and build
   // vectors; safe to move to the next probe record.
