903957ed98253d7838bac22c56bd22ad67829473
David Reveman <reveman@rivosinc.com>
2025-03-13 10:58:36 -0400

    build: Use breeze BlockReduce instead of cub::BlockReduce for dwio
    No change in behavior. Just using breeze instead of cub to avoid
    unnecessary external dependencies.

--- velox/experimental/wave/dwio/decode/GpuDecoder-inl.cuh
+++ velox/experimental/wave/dwio/decode/GpuDecoder-inl.cuh
@@ -16,11 +16,11 @@
 
 #pragma once
 
+#include <breeze/functions/reduce.h>
 #include <breeze/functions/scan.h>
 #include <breeze/platforms/platform.h>
 #include <breeze/utils/types.h>
 #include <breeze/platforms/cuda.cuh>
-#include <cub/cub.cuh> // @manual
 #include "velox/experimental/wave/common/Bits.cuh"
 #include "velox/experimental/wave/common/Block.cuh"
 
@@ -551,11 +551,15 @@ __device__ void decodeMainlyConstant(GpuDecode& plan) {
   }
 }
 
+// First thread returns total sum.
 template <int kBlockSize, typename T, typename U>
 __device__ T sum(const U* values, int size) {
-  using Reduce = cub::BlockReduce<T, kBlockSize>;
+  using namespace breeze::functions;
+  using namespace breeze::utils;
+  CudaPlatform<kBlockSize, kWarpThreads> p;
+  using BlockReduceT = BlockReduce<decltype(p), T>;
   extern __shared__ char smem[];
-  auto* reduceStorage = reinterpret_cast<typename Reduce::TempStorage*>(smem);
+  auto* reduceStorage = reinterpret_cast<typename BlockReduceT::Scratch*>(smem);
   T total = 0;
   for (int i = 0; i < size; i += kBlockSize) {
     auto numValid = min(size - i, kBlockSize);
@@ -563,7 +567,11 @@ __device__ T sum(const U* values, int size) {
     if (threadIdx.x < numValid) {
       value = values[i + threadIdx.x];
     }
-    total += Reduce(*reduceStorage).Sum(value, numValid);
+    total += BlockReduceT::template Reduce<ReduceOpAdd, /*kItemsPerThread=*/1>(
+        p,
+        make_slice(&value),
+        make_slice(reduceStorage).template reinterpret<SHARED>(),
+        numValid);
     __syncthreads();
   }
   return total;
@@ -1285,7 +1293,9 @@ __global__ void decodeGlobal(GpuDecode* plan) {
 
 template <int32_t kBlockSize>
 int32_t sharedMemorySizeForDecode(DecodeStep step) {
-  using Reduce32 = cub::BlockReduce<int32_t, kBlockSize>;
+  using namespace breeze::functions;
+  using PlatformT = CudaPlatform<kBlockSize, kWarpThreads>;
+  using BlockReduceT = BlockReduce<PlatformT, int32_t>;
   using BlockScanStorageT = BlockScanStorage<kBlockSize>;
   switch (step) {
     case DecodeStep::kSelective32:
@@ -1299,7 +1309,7 @@ int32_t sharedMemorySizeForDecode(DecodeStep step) {
       break;
 
     case DecodeStep::kRleTotalLength:
-      return sizeof(typename Reduce32::TempStorage);
+      return sizeof(typename BlockReduceT::Scratch);
     case DecodeStep::kMainlyConstant:
     case DecodeStep::kRleBool:
     case DecodeStep::kRle:
