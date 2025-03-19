87ace46759f309ba656806392a09101d5f37e408
Qian Sun <qian.sun2020@gmail.com>
2025-03-19 16:02:09 +0800

    add test

--- velox/functions/sparksql/tests/ArrayCompactTest.cpp
+++ velox/functions/sparksql/tests/ArrayCompactTest.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "velox/functions/sparksql/tests/SparkFunctionBaseTest.h"
 
 using namespace facebook::velox::test;
@@ -31,14 +32,34 @@ class ArrayCompactTest : public SparkFunctionBaseTest {
   }
 };
 
-TEST_F(ArrayCompactTest, nestedNull) {
-  const auto array = makeNestedArrayVectorFromJson<int32_t>({
-      "[[1, null, 2, null], [1, 2, 3], [null], [[1, 2, 3, null], [null], [4, null, 6]]]",
-  });
+TEST_F(ArrayCompactTest, basicArray) {
+  const auto array = makeArrayVectorFromJson<int64_t>(
+      {"[1, 2, 3, 4, null]",
+       "[3, 4, 5, 6]",
+       "[1, null, 2, null]",
+       "[10, null, 20, 30]",
+       "[null]",
+       "[]",
+       "null"});
 
-  auto expected = makeNestedArrayVectorFromJson<int32_t>({
-      "[[1, 2], [1, 2, 3], [], [[1, 2, 3, null], [4, null, 6]]]",
-  });
+  VectorPtr expected = makeArrayVectorFromJson<int64_t>(
+      {"[1, 2, 3, 4]",
+       "[3, 4, 5, 6]",
+       "[1, 2]",
+       "[10, 20, 30]",
+       "[]",
+       "[]",
+       "null"});
+
+  testExpression("array_compact(c0)", {array}, expected);
+}
+
+TEST_F(ArrayCompactTest, complexArray) {
+  const auto array = makeNestedArrayVectorFromJson<int32_t>(
+      {"[[1, 2, null], [null], [3, null, 4, null]]"});
+
+  auto expected = makeNestedArrayVectorFromJson<int32_t>(
+      {"[[1, 2, null], [3, null, 4, null]]"});
 
   testExpression("array_compact(c0)", {array}, expected);
 }
