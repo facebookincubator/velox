2fd784e7b9060c8905c2cef1c6b8f921e2ff14c9
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2025-03-20 13:49:06 -0500

    use stream_c

--- velox/experimental/cudf/vector/CudfVector.cpp
+++ velox/experimental/cudf/vector/CudfVector.cpp
@@ -22,25 +22,29 @@
 
 #include <cmath>
 
-static int64_t estimate_size(cudf::table_view const& view) {
+namespace facebook::velox::cudf_velox {
+namespace {
+static int64_t estimate_size(
+    cudf::table_view const& view,
+    rmm::cuda_stream_view stream) {
   // Compute the size in bits for each row.
-  auto const row_sizes = cudf::row_bit_count(view);
+  auto const row_sizes = cudf::row_bit_count(view, stream);
   // Accumulate the row sizes to compute a sum.
   auto const agg = cudf::make_sum_aggregation<cudf::reduce_aggregation>();
   cudf::data_type sum_dtype{cudf::type_id::INT64};
-  auto const total_size_scalar = cudf::reduce(*row_sizes, *agg, sum_dtype);
+  auto const total_size_scalar =
+      cudf::reduce(*row_sizes, *agg, sum_dtype, stream);
   auto const total_size_in_bits =
       static_cast<cudf::numeric_scalar<int64_t>*>(total_size_scalar.get())
-          ->value();
+          ->value(stream);
   // Convert the size in bits to the size in bytes.
   return static_cast<int64_t>(
       std::ceil(static_cast<double>(total_size_in_bits) / 8));
 }
-
-namespace facebook::velox::cudf_velox {
+} // namespace
 
 uint64_t CudfVector::estimateFlatSize() const {
-  return estimate_size(table_->view());
+  return estimate_size(table_->view(), stream_);
 }
 
 } // namespace facebook::velox::cudf_velox
