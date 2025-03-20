6cb43880af77931e8278bfb998ce2233faacc98e
Qian Sun <qian.sun2020@gmail.com>
2025-03-20 14:50:12 +0800

    feat: Add Spark array_prepend function
    register and add tests
    
    fix codestyle error
    
    Update array.rst
    
    reuse string from arg
    
    add fast path for primitives
    
    fix
    
    fix
    
    try with nullopt
    
    try
    
    fix
    
    use callNullable
    
    add test for complex array

---
+++ velox/functions/sparksql/tests/ArrayPrependTest.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/functions/sparksql/tests/SparkFunctionBaseTest.h"
+
+using namespace facebook::velox::test;
+
+namespace facebook::velox::functions::sparksql::test {
+namespace {
+
+class ArrayPrependTest : public SparkFunctionBaseTest {
+ protected:
+  void testExpression(
+      const std::string& expression,
+      const std::vector<VectorPtr>& input,
+      const VectorPtr& expected) {
+    auto result = evaluate(expression, makeRowVector(input));
+    assertEqualVectors(expected, result);
+  }
+};
+
+TEST_F(ArrayPrependTest, intArrays) {
+  const auto arrayVector = makeArrayVector<int64_t>(
+      {{1, 2, 3, 4}, {3, 4, 5}, {7, 8, 9}, {10, 20, 30}});
+  const auto elementVector = makeFlatVector<int64_t>({11, 22, 33, 44});
+
+  VectorPtr expected = makeArrayVector<int64_t>({
+      {11, 1, 2, 3, 4},
+      {22, 3, 4, 5},
+      {33, 7, 8, 9},
+      {44, 10, 20, 30},
+  });
+  testExpression(
+      "array_prepend(c0, c1)", {arrayVector, elementVector}, expected);
+}
+
+TEST_F(ArrayPrependTest, nullArrays) {
+  const auto arrayVector = makeArrayVectorFromJson<int64_t>(
+      {"[1, 2, 3, null]", "[3, 4, 5]", "null", "[10, 20, null]"});
+
+  const auto elementVector = makeNullableFlatVector<int64_t>(
+      {11, std::nullopt, std::nullopt, std::nullopt});
+
+  VectorPtr expected = makeArrayVectorFromJson<int64_t>(
+      {"[11, 1, 2, 3, null]",
+       "[null, 3, 4, 5]",
+       "null",
+       "[null, 10, 20, null]"});
+  testExpression(
+      "array_prepend(c0, c1)", {arrayVector, elementVector}, expected);
+}
+
+TESTT_F(ArrayPrependTest, complexArray) {
+  const auto arrayVector = makeNestedArrayVectorFromJson<int32_t>(
+      {"[[1, 2, null], [null], [3, null, 4, null]]",
+       "[[1, null, 2], null, [3, null, 4, null]]",
+       "[[1, 2], [], [null, 3, 4]]",
+       "null",
+       "[[], [1, 2]]",
+       "[[1, 2], null]",
+       "[[1, 2], []]"});
+
+  VectorPtr elementVector = makeArrayVectorFromJson<int64_t>(
+      {"[1, 2, 3]",
+       "[5]",
+       "null",
+       "[10, 20]",
+       "[6]",
+       "[7, null, 8]",
+       "[null]"});
+
+  const auto expected = makeNestedArrayVectorFromJson<int32_t>(
+      {"[[1, 2, 3], [1, 2, null], [null], [3, null, 4, null]]",
+       "[[5], [1, null, 2], null, [3, null, 4, null]]",
+       "[null, [1, 2], [], [null, 3, 4]]",
+       "null",
+       "[[6], [], [1, 2]]",
+       "[[7, null, 8], [1, 2], null]",
+       "[[null], [1, 2], []]"});
+  testExpression(
+      "array_prepend(c0, c1)", {arrayVector, elementVector}, expected);
+}
+
+} // namespace
+} // namespace facebook::velox::functions::sparksql::test
