08b96fe81c2794fee99ec16d8fc5ef8faea0a050
Kevin Wilfong <kevinwilfong@meta.com>
2025-03-21 15:38:27 -0700

    fix: Expand the range of supported years (#12694)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/12694
    
    Presto Java supports any timestamp that can fit in the 52 bits well allocate for millis UTC in
    the TimestampWithTimeZone type.  Likewise the Timestamp type can support dates with
    years past 200 million.
    
    In Velox currently we limit the year to what can be represented with 16 bits (this is what
    std::chrono applies in C++20).
    
    This change adds support for all years where all dates in the year can be expressed as a 64
    bit signed integer millis since epoch. (Note that this is 1 year less than Presto Java on either
    end, but Presto Java silently overflows).
    
    Reviewed By: yuandagits
    
    Differential Revision: D71349259

--- velox/functions/lib/tests/DateTimeFormatterTest.cpp
+++ velox/functions/lib/tests/DateTimeFormatterTest.cpp
@@ -1331,16 +1331,16 @@ TEST_F(JodaDateTimeFormatterTest, formatResultSize) {
   auto* timezone = tz::locateZone("GMT");
 
   EXPECT_EQ(
-      getJodaDateTimeFormatter("yyyy-MM-dd")->maxResultSize(timezone), 12);
-  EXPECT_EQ(getJodaDateTimeFormatter("yyyy-MM")->maxResultSize(timezone), 9);
-  EXPECT_EQ(getJodaDateTimeFormatter("y")->maxResultSize(timezone), 6);
+      getJodaDateTimeFormatter("yyyy-MM-dd")->maxResultSize(timezone), 16);
+  EXPECT_EQ(getJodaDateTimeFormatter("yyyy-MM")->maxResultSize(timezone), 13);
+  EXPECT_EQ(getJodaDateTimeFormatter("y")->maxResultSize(timezone), 10);
   EXPECT_EQ(
       getJodaDateTimeFormatter("yyyy////MM////dd")->maxResultSize(timezone),
-      18);
+      22);
   EXPECT_EQ(
       getJodaDateTimeFormatter("yyyy-MM-dd HH:mm:ss.SSS")
           ->maxResultSize(timezone),
-      31);
+      35);
   // No padding. CENTURY_OF_ERA can be at most 3 digits.
   EXPECT_EQ(getJodaDateTimeFormatter("C")->maxResultSize(timezone), 3);
   // Needs to pad to make result contain 4 digits.
@@ -1460,11 +1460,13 @@ TEST_F(MysqlDateTimeTest, formatYear) {
       formatMysqlDateTime("%Y", fromTimestampString("-1-01-01"), timezone),
       "-0001");
   EXPECT_THROW(
-      formatMysqlDateTime("%Y", fromTimestampString("-99999-01-01"), timezone),
-      VeloxRuntimeError);
+      formatMysqlDateTime(
+          "%Y", fromTimestampString("-292275056-01-01"), timezone),
+      VeloxUserError);
   EXPECT_THROW(
-      formatMysqlDateTime("%Y", fromTimestampString("99999-01-01"), timezone),
-      VeloxRuntimeError);
+      formatMysqlDateTime(
+          "%Y", fromTimestampString("292278995-01-01"), timezone),
+      VeloxUserError);
 }
 
 TEST_F(MysqlDateTimeTest, formatMonthDay) {
