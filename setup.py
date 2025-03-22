b0b27f8d410484a984e18c2f3090eb0ce4a4601a
Jacob Wujciak-Jens <jacob@wujciak.de>
2025-03-22 03:20:51 +0100

    try pyproject.toml

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,92 +21,40 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import distutils.command.build
-import distutils.command.clean
+
 import os
 import re
 import shutil
 import subprocess
 import sys
+from distutils.command.clean import clean as clean_orig
 from pathlib import Path
 
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
+from setuptools.command.build import build as build_orig
 from setuptools.command.build_ext import build_ext
 
 ROOT_DIR = Path(__file__).parent.resolve()
 
-with open("pyvelox/README.md") as f:
-    readme = f.read()
 
-
-# Override build directory
-class BuildCommand(distutils.command.build.build):
+class BuildCommand(build_orig):
     def initialize_options(self):
-        distutils.command.build.build.initialize_options(self)
+        super().initialize_options()
         self.build_base = "_build"
 
 
-def _get_version():
-    version = open("./version.txt").read().strip()
-    version = re.sub("#.*\n?", "", version, flags=re.MULTILINE)
-    sha = "Unknown"
-    try:
-        sha = (
-            subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=str(ROOT_DIR))
-            .decode("ascii")
-            .strip()
-        )
-    except Exception:
-        pass
-
-    if os.getenv("BUILD_VERSION"):
-        version = os.getenv("BUILD_VERSION")
-    elif sha != "Unknown":
-        version += "+" + sha[:7]
-
-    return version, sha
-
-
-def _export_version(version, sha):
-    version_path = ROOT_DIR / "pyvelox" / "version.py"
-    with open(version_path, "w") as f:
-        f.write("__version__ = '{}'\n".format(version))
-        f.write("git_version = {}\n".format(repr(sha)))
-
-
-VERSION, SHA = _get_version()
-_export_version(VERSION, SHA)
-
-print("-- Building version " + VERSION)
-
-
-class clean(distutils.command.clean.clean):
-    def run(self):
-        # Run default behavior first
-        distutils.command.clean.clean.run(self)
-
-        # Remove pyvelox extension
-        for path in (ROOT_DIR / "pyvelox").glob("**/*.so"):
-            print(f"removing '{path}'")
-            path.unlink()
-        # Remove build directory
-        build_dirs = [
-            ROOT_DIR / "_build",
-        ]
-        for path in build_dirs:
-            if path.exists():
-                print(f"removing '{path}' (and everything under it)")
-                shutil.rmtree(str(path), ignore_errors=True)
-
-
-# Based off of
-# https://github.com/pytorch/audio/blob/2c8aad97fc8d7647ee8b2df2de9312cce0355ef6/build_tools/setup_helpers/extension.py#L46
 class CMakeBuild(build_ext):
     def run(self):
+        try:
+            import pyarrow  # noqa: F401
+        except ImportError:
+            raise RuntimeError("pyarrow must be installed before building pyvelox.")
+
         try:
             subprocess.check_output(["cmake", "--version"])
         except OSError:
             raise RuntimeError("CMake is not available.")
+
         super().run()
 
     def build_extension(self, ext):
@@ -113,10 +63,8 @@ class CMakeBuild(build_ext):
         # The build in question must have been built with 'VELOX_BUILD_PYTHON_PACKAGE=ON' and the same python version.
         if "VELOX_BUILD_DIR" in os.environ:
             velox_dir = os.path.abspath(os.environ["VELOX_BUILD_DIR"])
-
             if not os.path.isdir(extdir):
                 os.symlink(velox_dir, os.path.dirname(extdir), target_is_directory=True)
-
             print(f"Using pre-built Velox library from {velox_dir}")
             return
 
@@ -140,68 +88,48 @@ class CMakeBuild(build_ext):
             f"-DCMAKE_INSTALL_PREFIX={extdir}",
             "-DCMAKE_VERBOSE_MAKEFILE=ON",
             "-DVELOX_BUILD_PYTHON_PACKAGE=ON",
-            f"-DPYTHON_EXECUTABLE={exec_path} ",
+            f"-DPYTHON_EXECUTABLE={exec_path}",
         ]
+
         build_args = []
 
-        # Default to Ninja
         if "CMAKE_GENERATOR" not in os.environ:
             cmake_args += ["-GNinja"]
 
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += ["-j{}".format(self.parallel)]
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ and getattr(
+            self, "parallel", None
+        ):
+            build_args += [f"-j{self.parallel}"]
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
         subprocess.check_call(
-            ["cmake", str(ROOT_DIR)] + cmake_args,
-            cwd=self.build_temp,
+            ["cmake", str(ROOT_DIR)] + cmake_args, cwd=self.build_temp
         )
         subprocess.check_call(
             ["cmake", "--build", "."] + build_args, cwd=self.build_temp
         )
 
 
+class CleanCommand(clean_orig):
+    def run(self):
+        super().run()
+        for path in (ROOT_DIR / "pyvelox").rglob("*.so"):
+            print(f"removing '{path}'")
+            path.unlink()
+        build_dirs = [ROOT_DIR / "_build"]
+        for path in build_dirs:
+            if path.exists():
+                print(f"removing '{path}' (and everything under it)")
+                shutil.rmtree(str(path), ignore_errors=True)
+
+
 setup(
-    name="pyvelox",
-    version=VERSION,
-    description="Python bindings and extensions for Velox",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/facebookincubator/velox",
-    author="Meta",
-    author_email="velox@fb.com",
-    license="Apache License 2.0",
-    install_requires=[
-        "cffi",
-        "typing",
-        "tabulate",
-        "typing-inspect",
-        "pyarrow",
-    ],
-    extras_require={"tests": ["pyarrow"]},
-    python_requires=">=3.9",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: Implementation :: CPython",
-    ],
-    packages=find_packages() + find_packages(where="./test"),
-    zip_safe=False,
     ext_modules=[Extension(name="pyvelox.legacy", sources=[])],
-    cmdclass={"build_ext": CMakeBuild, "clean": clean, "build": BuildCommand},
+    cmdclass={
+        "build_ext": CMakeBuild,
+        "clean": CleanCommand,
+        "build": BuildCommand,
+    },
 )
