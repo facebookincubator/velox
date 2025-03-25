e1c202b6d9f92ea241583bc124be972ec1c8a915
Xin Zhang <desertsxin@gmail.com>
2025-03-25 21:29:33 +0000

    Refactor UUID cast functions

--- velox/functions/prestosql/types/UuidRegistration.cpp
+++ velox/functions/prestosql/types/UuidRegistration.cpp
@@ -25,6 +25,75 @@
 
 namespace facebook::velox {
 namespace {
+template <TypeKind>
+struct UuidWriter {
+  static void write(exec::StringWriter& writer, const uint8_t* uuidBytes) =
+      delete;
+};
+
+template <TypeKind>
+struct UuidParser {
+  static int128_t parse(const StringView& uuidValue) = delete;
+};
+
+template <>
+struct UuidWriter<TypeKind::VARCHAR> {
+  static void write(exec::StringWriter& writer, const uint8_t* uuidBytes) {
+    writer.resize(36);
+    // Do not use boost::lexical_cast. It is very slow.
+
+    // 2 hex digits per each value in [0, 127] range (1 byte).
+    static const char* const kHexTable =
+        "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+        "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
+        "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f"
+        "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f"
+        "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
+        "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
+        "c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
+        "e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+
+    size_t offset = 0;
+    for (auto i = 0; i < 16; ++i) {
+      writer.data()[offset] = kHexTable[uuidBytes[i] * 2];
+      writer.data()[offset + 1] = kHexTable[uuidBytes[i] * 2 + 1];
+
+      offset += 2;
+      if (i == 3 || i == 5 || i == 7 || i == 9) {
+        writer.data()[offset] = '-';
+        offset++;
+      }
+    }
+  }
+};
+
+template <>
+struct UuidParser<TypeKind::VARCHAR> {
+  static int128_t parse(const StringView& uuidValue) {
+    auto uuid = boost::lexical_cast<boost::uuids::uuid>(uuidValue);
+    int128_t u;
+    memcpy(&u, &uuid, 16);
+    return u;
+  }
+};
+
+template <>
+struct UuidWriter<TypeKind::VARBINARY> {
+  static void write(exec::StringWriter& writer, const uint8_t* uuidBytes) {
+    writer.resize(16);
+    memcpy(writer.data(), uuidBytes, 16);
+  }
+};
+
+template <>
+struct UuidParser<TypeKind::VARBINARY> {
+  static int128_t parse(const StringView& uuidValue) {
+    int128_t u;
+    memcpy(&u, uuidValue.data(), 16);
+    return u;
+  }
+};
+
 class UuidCastOperator : public exec::CastOperator {
  public:
   bool isSupportedFromType(const TypePtr& other) const override {
@@ -87,38 +156,7 @@ class UuidCastOperator : public exec::CastOperator {
       const auto* uuidBytes = reinterpret_cast<const uint8_t*>(&uuid);
 
       exec::StringWriter writer(flatResult, row);
-
-      if constexpr (KIND == TypeKind::VARCHAR) {
-        writer.resize(36);
-        // Do not use boost::lexical_cast. It is very slow.
-
-        // 2 hex digits per each value in [0, 127] range (1 byte).
-        static const char* const kHexTable =
-            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
-            "202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
-            "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f"
-            "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f"
-            "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
-            "a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
-            "c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
-            "e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
-
-        size_t offset = 0;
-        for (auto i = 0; i < 16; ++i) {
-          writer.data()[offset] = kHexTable[uuidBytes[i] * 2];
-          writer.data()[offset + 1] = kHexTable[uuidBytes[i] * 2 + 1];
-
-          offset += 2;
-          if (i == 3 || i == 5 || i == 7 || i == 9) {
-            writer.data()[offset] = '-';
-            offset++;
-          }
-        }
-      } else if constexpr (KIND == TypeKind::VARBINARY) {
-        writer.resize(16);
-        memcpy(writer.data(), uuidBytes, 16);
-      }
-
+      UuidWriter<KIND>::write(writer, uuidBytes);
       writer.finalize();
     });
   }
@@ -134,19 +172,9 @@ class UuidCastOperator : public exec::CastOperator {
 
     context.applyToSelectedNoThrow(rows, [&](auto row) {
       const auto uuidValue = uuidInput->valueAt(row);
-
-      int128_t u;
-
-      if (KIND == TypeKind::VARCHAR) {
-        auto uuid = boost::lexical_cast<boost::uuids::uuid>(uuidValue);
-        memcpy(&u, &uuid, 16);
-      } else if (KIND == TypeKind::VARBINARY) {
-        memcpy(&u, uuidValue.data(), 16);
-      }
-
-      // Convert a big endian value from Boost to native byte-order.
+      auto u = UuidParser<KIND>::parse(uuidValue);
+      // Convert a big endian value to native byte-order.
       u = DecimalUtil::bigEndian(u);
-
       flatResult->set(row, u);
     });
   }
