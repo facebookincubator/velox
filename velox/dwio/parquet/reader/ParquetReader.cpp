91a77f9a06c14ae7ff201fc8499a42082e7105ff
Jacob Khaliqi <jacobkhaliqi@gmail.com>
2025-03-25 10:31:11 -0700

    refactor(parquet): Use velox parquet reader in FileSerializeTest

--- velox/dwio/parquet/reader/ParquetReader.cpp
+++ velox/dwio/parquet/reader/ParquetReader.cpp
@@ -1298,4 +1298,7 @@ FileMetaDataPtr ParquetReader::fileMetaData() const {
   return readerBase_->fileMetaData();
 }
 
+const thrift::FileMetaData& ParquetReader::thriftFileMetaData() const {
+  return readerBase_->thriftFileMetaData();
+}
 } // namespace facebook::velox::parquet
