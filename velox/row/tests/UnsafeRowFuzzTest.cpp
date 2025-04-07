1aec31f0eae4ba6d46004770e89ecffd429ed44d
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-04-07 14:21:53 +0100

    Fix code style

--- velox/row/tests/UnsafeRowFuzzTest.cpp
+++ velox/row/tests/UnsafeRowFuzzTest.cpp
@@ -142,8 +142,7 @@ class UnsafeRowFuzzTests : public ::testing::Test, public VectorTestBase {
       serialized.push_back(rawBuffer + offset);
       offset += size;
 
-      VELOX_CHECK_EQ(
-          size, row.rowSize(i), "Row {}: {}", i, data->toString(i));
+      VELOX_CHECK_EQ(size, row.rowSize(i), "Row {}: {}", i, data->toString(i));
     }
 
     VELOX_CHECK_EQ(offset, totalSize);
@@ -305,7 +304,7 @@ TEST_F(UnsafeRowFuzzTests, nestedMaps) {
            {{6}},
        }),
        makeNullableFlatVector<int32_t>({1, 2, 3, std::nullopt, 5, 6})},
-       [](auto row) { return row == 3; });
+      [](auto row) { return row == 3; });
   auto keys = makeFlatVector<int32_t>({1, 2, 3, 4, 5, 6});
   auto values = makeMapVector({0, 2, 2, 3, 4, 5}, keys, innerMaps, {1});
   auto data = makeRowVector({values});
