b1e14a64d118d03c3522b1a2ff3428f1836ce28e
Ke Jia <ke.a.jia@intel.com>
2025-04-23 11:20:09 +0000

    Fix anti join result mismatch

--- velox/exec/MergeJoin.cpp
+++ velox/exec/MergeJoin.cpp
@@ -818,7 +818,7 @@ RowVectorPtr MergeJoin::getOutput() {
         continue;
       } else if (isAntiJoin(joinType_)) {
         output = filterOutputForAntiJoin(output);
-        if (output) {
+        if (output != nullptr && output->size() > 0) {
           return output;
         }
 
@@ -926,6 +926,8 @@ RowVectorPtr MergeJoin::doGetOutput() {
     // results from the current match.
     if (addToOutput()) {
       return std::move(output_);
+    } else {
+      previousLeftMatch_ = leftMatch_;
     }
   }
 
@@ -990,6 +992,8 @@ RowVectorPtr MergeJoin::doGetOutput() {
 
     if (addToOutput()) {
       return std::move(output_);
+    } else {
+      previousLeftMatch_ = leftMatch_;
     }
   }
 
@@ -1239,6 +1243,8 @@ RowVectorPtr MergeJoin::doGetOutput() {
 
       if (addToOutput()) {
         return std::move(output_);
+      } else {
+        previousLeftMatch_ = leftMatch_;
       }
 
       if (!rightInput_) {
@@ -1274,9 +1280,6 @@ RowVectorPtr MergeJoin::applyFilter(const RowVectorPtr& output) {
     // If all matches for a given left-side row fail the filter, add a row to
     // the output with nulls for the right-side columns.
     const auto onMiss = [&](auto row) {
-      if (isAntiJoin(joinType_)) {
-        return;
-      }
       rawIndices[numPassed++] = row;
 
       if (isFullJoin(joinType_)) {
@@ -1353,11 +1356,7 @@ RowVectorPtr MergeJoin::applyFilter(const RowVectorPtr& output) {
 
         joinTracker_->processFilterResult(i, passed, onMiss);
 
-        if (isAntiJoin(joinType_)) {
-          if (!passed) {
-            rawIndices[numPassed++] = i;
-          }
-        } else {
+        if (!isAntiJoin(joinType_)) {
           if (passed) {
             rawIndices[numPassed++] = i;
           }
@@ -1371,24 +1370,28 @@ RowVectorPtr MergeJoin::applyFilter(const RowVectorPtr& output) {
 
     // Every time we start a new left key match, `processFilterResult()` will
     // check if at least one row from the previous match passed the filter. If
-    // none did, it calls onMiss to add a record with null right projections to
-    // the output.
+    // none did, it calls onMiss to add a record with null right projections
+    // to the output.
     //
     // Before we leave the current buffer, since we may not have seen the next
-    // left key match yet, the last key match may still be pending to produce a
-    // row (because `processFilterResult()` was not called yet).
+    // left key match yet, the last key match may still be pending to produce
+    // a row (because `processFilterResult()` was not called yet).
     //
     // To handle this, we need to call `noMoreFilterResults()` unless the
-    // same current left key match may continue in the next buffer. So there are
-    // two cases to check:
+    // same current left key match may continue in the next buffer. So there
+    // are two cases to check:
     //
-    // 1. If leftMatch_ is nullopt, there for sure the next buffer will contain
-    // a different key match.
+    // 1. If leftMatch_ is nullopt, there for sure the next buffer will
+    // contain a different key match.
     //
     // 2. leftMatch_ may not be nullopt, but may be related to a different
     // (subsequent) left key. So we check if the last row in the batch has the
     // same left row number as the last key match.
     if (!leftMatch_ || !joinTracker_->isCurrentLeftMatch(numRows - 1)) {
+      joinTracker_->noMoreFilterResults(onMiss);
+    }
+
+    if (isAntiJoin(joinType_) && leftMatch_ && !previousLeftMatch_) {
       joinTracker_->noMoreFilterResults(onMiss);
     }
   } else {
