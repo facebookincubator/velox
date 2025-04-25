0584b92142ec179366bed665c88487790e4a38a9
Deepak Majeti <deepak.majeti@ibm.com>
2025-04-25 13:28:45 -0400

    fix remaining namespaces

--- velox/benchmarks/basic/FeatureNormalization.cpp
+++ velox/benchmarks/basic/FeatureNormalization.cpp
@@ -111,7 +111,7 @@ BENCHMARK(normalizeConstant) {
 
 int main(int argc, char* argv[]) {
   folly::Init init{&argc, &argv};
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  folly::gflags::ParseCommandLineFlags(&argc, &argv, true);
   memory::MemoryManager::initialize({});
   benchmark = std::make_unique<FeatureNormailzationBenchmark>();
   folly::runBenchmarks();
