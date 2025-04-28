af7fc8e59dfd3f8a5caf27a3679f9ba63272d835
arnavb <arnavb@uber.com>
2025-04-28 08:30:32 +0000

    update

--- velox/functions/sparksql/tests/FactorialTest.cpp
+++ velox/functions/sparksql/tests/FactorialTest.cpp
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <cstdint>
-#include <limits>
 
 #include "velox/functions/sparksql/tests/SparkFunctionBaseTest.h"
 
