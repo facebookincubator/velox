ea8a946e64cef875de2526e6480aefc4be98e102
zhli1142015 <zhli@microsoft.com>
2025-04-29 17:18:20 +0800

    feat: Add Spark unbase64 function

--- velox/functions/sparksql/registration/RegisterString.cpp
+++ velox/functions/sparksql/registration/RegisterString.cpp
@@ -22,6 +22,7 @@
 #include "velox/functions/sparksql/Split.h"
 #include "velox/functions/sparksql/String.h"
 #include "velox/functions/sparksql/StringToMap.h"
+#include "velox/functions/sparksql/UnBase64Function.h"
 
 namespace facebook::velox::functions {
 void registerSparkStringFunctions(const std::string& prefix) {
@@ -147,6 +148,8 @@ void registerStringFunctions(const std::string& prefix) {
   registerFunctionCallToSpecialForm(
       ConcatWsCallToSpecialForm::kConcatWs,
       std::make_unique<ConcatWsCallToSpecialForm>());
+
+  registerFunction<UnBase64Function, Varbinary, Varchar>({prefix + "unbase64"});
 }
 } // namespace sparksql
 } // namespace facebook::velox::functions
