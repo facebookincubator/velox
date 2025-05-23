7efe4adc064e6b12d700d3d97d3f8c6ffcda9e64
Oliver Xu <oliverxu@meta.com>
2025-05-22 17:09:13 -0700

    feat(type): Add support for BIGINT noise scale
    Summary:
    ### Summary
    Add another function signature to support BIGINT as a valid variable type for noiseScale in NOISY_COUNT_IF_GAUSSIAN(col, noise_scale) in Velox. Also provide unit test for this feature.
    
    #### Changes
    *   Modified `NoisyCountIfGaussianAggregate.cpp` to include BIGINT as a valid noise scale argument type.
    *   Added test cases to `NoisyCountIfGaussianAggregationTest.cpp` for BIGINT noise scale support.
    
    Differential Revision: D75258579

--- velox/functions/prestosql/aggregates/tests/NoisyCountIfGaussianAggregationTest.cpp
+++ velox/functions/prestosql/aggregates/tests/NoisyCountIfGaussianAggregationTest.cpp
@@ -188,4 +188,17 @@ TEST_F(NoisyCountIfGaussianAggregationTest, zeroRowsMultipleGroupsNoise) {
       "SELECT c0, sum(if(c1, 1, 0)), sum(if(c2, 1, 0)) FROM tmp group by c0, c1");
 }
 
+// Test that the aggregation supports BIGINT as a valid
+// noise scale.
+TEST_F(NoisyCountIfGaussianAggregationTest, bigIntNoiseScale) {
+  auto vectors = makeVectors(rowType_, 10, 4);
+  createDuckDbTable(vectors);
+
+  testAggregations(
+      vectors,
+      {},
+      {"noisy_count_if_gaussian(c1, 2)", "noisy_count_if_gaussian(c2, 2)"},
+      "SELECT sum(if(c1, 1, 0)), sum(if(c2, 1, 0)) FROM tmp");
+}
+
 } // namespace facebook::velox::aggregate::test
