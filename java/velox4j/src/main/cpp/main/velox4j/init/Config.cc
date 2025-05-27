2dceef8ef0f2f852c5c3898122994f31536d901d
Hongze Zhang <hongze.zzz123@gmail.com>
2025-05-27 20:15:33 +0200

    fixup

--- java/velox4j/src/main/cpp/main/velox4j/init/Config.cc
+++
@@ -1,8 +0,0 @@
-#include "Config.h"
-
-namespace velox4j {
-using namespace facebook::velox;
-config::ConfigBase::Entry<Preset> VELOX4J_INIT_PRESET(
-    "velox4j.init.preset",
-    Preset::SPARK);
-} // namespace velox4j
