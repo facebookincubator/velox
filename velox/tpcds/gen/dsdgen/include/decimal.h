03ffe8a81c242a1af3f1d33399ed44b446d90461
Pramod Satya <pramod.satya@ibm.com>
2025-05-28 14:34:17 -0700

    Address review comments

--- velox/tpcds/gen/dsdgen/include/decimal.h
+++ velox/tpcds/gen/dsdgen/include/decimal.h
@@ -37,15 +37,11 @@
 decimal_t* mk_decimal(int s, int p);
 
 int itodec(decimal_t* dest, int i);
-int ftodec(decimal_t* d, double f);
 int strtodec(decimal_t* d, const char* src);
 
-int dectostr(std::vector<char>& dest, decimal_t* d);
-int dectof(double* dest, decimal_t*);
 #define dectoi(d) atoi(d->number)
 
 int decimal_t_op(decimal_t* dest, int o, decimal_t* d1, decimal_t* d2);
-void print_decimal(int nColumn, decimal_t* d, int s);
 void set_precision(decimal_t* d, int sie, int precision);
 #define NegateDecimal(d) (d)->number *= -1
 #endif /* R_DECIMAL_H */
