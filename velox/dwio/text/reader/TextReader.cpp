248dd239e12eb5d0e517c32946b3cf96e7b87415
Yi Cheng Lee <yclee03@fb.com>
2025-05-29 14:52:41 -0700

    feat: Add skeleton code for RowReaderImpl (#13499)
    Summary: Pull Request resolved: https://github.com/facebookincubator/velox/pull/13499
    
    Differential Revision: D75468153

--- velox/dwio/text/reader/TextReader.cpp
+++ velox/dwio/text/reader/TextReader.cpp
@@ -31,4 +31,248 @@ FileContents::FileContents(
   return;
 }
 
+/// TODO: Add implementation
+RowReaderImpl::RowReaderImpl(
+    std::shared_ptr<FileContents> fileContents,
+    const RowReaderOptions& opts,
+    bool prestoTextReader)
+    : columnSelector_{ColumnSelector::apply(
+          opts.selector(),
+          fileContents->schema)},
+      prestoTextReader_{prestoTextReader} {}
+
+/// TODO: Add implementation
+void RowReaderImpl::updateSelected() {
+  return;
+}
+
+/// TODO: Add implementation
+const ColumnSelector& RowReaderImpl::getColumnSelector() const {
+  return columnSelector_;
+}
+
+/// TODO: Add implementation
+std::shared_ptr<const TypeWithId> RowReaderImpl::getSelectedType() const {
+  return nullptr;
+}
+
+/**
+ * Create a response buffer matching the selected schema.
+ */
+/// TODO: Add implementation
+std::unique_ptr<BaseVector> RowReaderImpl::createRowBatch(
+    uint64_t /*capacity*/) const {
+  return nullptr;
+}
+
+/**
+ * Read the next batch of rows from the file.
+ * Returns true if there is more data to be read
+ * beyond what has been returned by this call, which
+ * might be none.
+ */
+/// TODO: Add implementation
+bool RowReaderImpl::next(BaseVector& /*data*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+uint64_t RowReaderImpl::getRowNumber() const {
+  return 0;
+}
+
+/// TODO: Add implementation
+uint64_t RowReaderImpl::seekToRow(uint64_t /*rowNumber*/) {
+  return 0;
+}
+
+/// TODO: Add implementation
+uint64_t RowReaderImpl::skipRows(uint64_t /*numberOfRowsToSkip*/) {
+  return 0;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::isSelectedField(
+    const std::shared_ptr<const velox::dwio::common::TypeWithId>& /*t*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+std::string RowReaderImpl::getString(
+    RowReaderImpl& /*th*/,
+    bool& /*isNull*/,
+    DelimType& /*delim*/) {
+  return "";
+}
+
+/// TODO: Add implementation
+template <typename T>
+T RowReaderImpl::getInteger(
+    RowReaderImpl& /*th*/,
+    bool& /*isNull*/,
+    DelimType& /*delim*/) {
+  return T();
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::getBoolean(
+    RowReaderImpl& /*th*/,
+    bool& /*isNull*/,
+    DelimType& /*delim*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+float RowReaderImpl::getFloat(
+    RowReaderImpl& /*th*/,
+    bool& /*isNull*/,
+    DelimType& /*delim*/) {
+  return 0.0;
+}
+
+/// TODO: Add implementation
+double RowReaderImpl::getDouble(
+    RowReaderImpl& /*th*/,
+    bool& /*isNull*/,
+    DelimType& /*delim*/) {
+  return 0;
+}
+
+/// TODO: Add implementation
+template <class T, class reqT>
+void RowReaderImpl::putValue(
+    std::function<
+        T(RowReaderImpl& /*th*/, bool& /*isNull*/, DelimType& delim)> /*f*/,
+    BaseVector* FOLLY_NULLABLE /*data*/,
+    DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::readElement(
+    const std::shared_ptr<const Type>& /*t*/,
+    const std::shared_ptr<const Type>& /*reqT*/,
+    BaseVector* FOLLY_NULLABLE /*data*/,
+    DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+const std::shared_ptr<const RowType>& RowReaderImpl::getType() const {
+  static const std::shared_ptr<const RowType> dummy = nullptr;
+  return dummy;
+}
+
+/// TODO: Add implementation
+const char* RowReaderImpl::getStreamNameData() const {
+  return "";
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::read(
+    void* /*buf*/,
+    uint64_t /*length*/,
+    uint64_t /*offset*/,
+    velox::dwio::common::LogType /*logType*/) {
+  return;
+}
+
+/// TODO: Add implementation
+uint64_t RowReaderImpl::getLength() {
+  return 0;
+}
+
+/// TODO: Add implementation
+uint64_t RowReaderImpl::getStreamLength() {
+  return 0;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::setEOF() {
+  return;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::incrementDepth() {
+  return;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::decrementDepth(DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::setEOE(DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::resetEOE(DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::isEOE(DelimType /*delim*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::setEOR(DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::isEOR(DelimType /*delim*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::isOuterEOR(DelimType /*delim*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::isEOEorEOR(DelimType /*delim*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+void RowReaderImpl::setNone(DelimType& /*delim*/) {
+  return;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::isNone(DelimType /*delim*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+DelimType RowReaderImpl::getDelimType(uint8_t /*v*/) {
+  return DelimType();
+}
+
+/// TODO: Add implementation
+uint8_t RowReaderImpl::getByte(DelimType& /*delim*/) {
+  return 0;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::getEOR(DelimType& /*delim*/, bool& /*isNull*/) {
+  return false;
+}
+
+/// TODO: Add implementation
+bool RowReaderImpl::skipLine() {
+  return false;
+}
+
+void RowReaderImpl::resetLine() {
+  return;
+}
+
+uint64_t RowReaderImpl::incrementNumElements(BaseVector& /*data*/) {
+  return 0;
+}
+
 } // namespace facebook::velox::text
--- velox/dwio/text/reader/TextReader.h
+++ velox/dwio/text/reader/TextReader.h
@@ -28,7 +28,9 @@ using velox::dwio::text::compression::ReaderDecompressor;
 
 namespace facebook::velox::text {
 
-using namespace velox::dwio::common;
+using dwio::common::ColumnSelector;
+using dwio::common::RowReaderOptions;
+using dwio::common::TypeWithId;
 
 struct FileContents {
   const size_t COLUMN_POSITION_INVALID = std::numeric_limits<size_t>::max();
@@ -43,7 +45,7 @@ struct FileContents {
   common::CompressionKind compression;
 
   std::unique_ptr<ReaderDecompressor> decompressedStream;
-  SerDeOptions serDeOptions;
+  dwio::common::SerDeOptions serDeOptions;
   std::array<bool, 128> needsEscape;
 
   FileContents(
@@ -56,5 +58,147 @@ using DelimType = uint8_t;
 constexpr DelimType DelimTypeNone = 0;
 constexpr DelimType DelimTypeEOR = 1;
 constexpr DelimType DelimTypeEOE = 2;
+
+class RowReaderImpl : public dwio::common::RowReader {
+ private:
+  const RowReaderOptions& getDefaultOpts();
+
+  const std::shared_ptr<FileContents> contents_;
+  RowReaderOptions options_;
+
+  ColumnSelector columnSelector_;
+
+  const std::shared_ptr<const TypeWithId> schemaWithId_;
+  mutable std::shared_ptr<const TypeWithId> selectedSchema_;
+
+  uint64_t currentRow_;
+
+  uint64_t pos_;
+  bool atEOL_;
+  bool atEOF_;
+  bool atSOL_;
+  uint8_t depth_;
+  std::string unreadData_;
+  uint64_t limit_;
+  uint64_t fileLength_;
+  const bool prestoTextReader_;
+
+  const std::shared_ptr<const RowType>& getType() const;
+
+  bool isSelectedField(const std::shared_ptr<const TypeWithId>& t);
+
+  const char* getStreamNameData() const;
+
+  uint64_t getLength();
+
+  uint64_t getStreamLength();
+
+  void setEOF();
+
+  void incrementDepth();
+
+  void decrementDepth(DelimType& delim);
+
+  void setEOE(DelimType& delim);
+
+  void resetEOE(DelimType& delim);
+
+  bool isEOE(DelimType delim);
+
+  void setEOR(DelimType& delim);
+
+  bool isEOR(DelimType delim);
+
+  bool isOuterEOR(DelimType delim);
+
+  bool isEOEorEOR(DelimType delim);
+
+  void setNone(DelimType& delim);
+
+  bool isNone(DelimType delim);
+
+  DelimType getDelimType(uint8_t v);
+
+  void read(
+      void* buf,
+      uint64_t length,
+      uint64_t offset,
+      velox::dwio::common::LogType);
+
+  /// TODO: Add implementation
+  template <bool skipLF = false>
+  uint8_t getByteUnchecked(DelimType& delim) {
+    return '\n';
+  }
+
+  uint8_t getByte(DelimType& delim);
+
+  bool getEOR(DelimType& delim, bool& isNull);
+
+  bool skipLine();
+
+  void resetLine();
+
+  static std::string
+  getString(RowReaderImpl& th, bool& isNull, DelimType& delim);
+
+  template <typename T>
+  static T getInteger(RowReaderImpl& th, bool& isNull, DelimType& delim);
+
+  static bool getBoolean(RowReaderImpl& th, bool& isNull, DelimType& delim);
+
+  static float getFloat(RowReaderImpl& th, bool& isNull, DelimType& delim);
+
+  static double getDouble(RowReaderImpl& th, bool& isNull, DelimType& delim);
+
+  /// TODO: Add implementation
+  static void trim(std::string& s) {}
+
+  void readElement(
+      const std::shared_ptr<const Type>& t,
+      const std::shared_ptr<const Type>& reqT,
+      BaseVector* FOLLY_NULLABLE data,
+      DelimType& delim);
+
+  uint64_t incrementNumElements(BaseVector& data);
+
+  /// TODO: Add implementation
+  template <class T>
+  T checkCast(BaseVector* FOLLY_NULLABLE data) {
+    return nullptr;
+  }
+
+  template <class T, class reqT>
+  void putValue(
+      std::function<T(RowReaderImpl& th, bool& isNull, DelimType& delim)> f,
+      BaseVector* FOLLY_NULLABLE data,
+      DelimType& delim);
+
+ public:
+  RowReaderImpl(
+      std::shared_ptr<FileContents> fileContents,
+      const RowReaderOptions& options,
+      bool prestoTextReader = false);
+
+  void updateSelected();
+  const ColumnSelector& getColumnSelector() const;
+
+  std::shared_ptr<const TypeWithId> getSelectedType() const;
+
+  std::unique_ptr<BaseVector> createRowBatch(uint64_t size) const override;
+
+  bool next(BaseVector& data) override;
+
+  uint64_t getRowNumber() const;
+
+  uint64_t seekToRow(uint64_t rowNumber);
+
+  uint64_t skipRows(uint64_t numberOfRowsToSkip);
+
+  /// TODO: Add implementation
+  bool isPrestoTextReader() const {
+    return false;
+  }
+};
 
 } // namespace facebook::velox::text
