50a768d68ece2ae4c5cd97d7d3ef8b7076eefd76
lingbin <lingbinlb@gmail.com>
2025-05-30 19:18:38 +0800

    refactor(Task): add '#undef' after using 'CLEAR' marco
    As a best practice, we should explicitly '#undef' a macro after we
    no longer use them.
    
    No functional changes.

--- velox/exec/Task.cpp
+++ velox/exec/Task.cpp
@@ -432,6 +432,7 @@ Task::~Task() {
 #define CLEAR(_action_)   \
   clearStage = #_action_; \
   _action_;
+
   CLEAR(threadFinishPromises_.clear());
   CLEAR(splitGroupStates_.clear());
   CLEAR(taskStats_ = TaskStats());
@@ -449,6 +450,9 @@ Task::~Task() {
   CLEAR(pool_.reset());
   CLEAR(planFragment_ = core::PlanFragment());
   CLEAR(queryCtx_.reset());
+
+#undef CLEAR
+
   clearStage = "exiting ~Task()";
 
   // Ful-fill the task deletion promises at the end.
