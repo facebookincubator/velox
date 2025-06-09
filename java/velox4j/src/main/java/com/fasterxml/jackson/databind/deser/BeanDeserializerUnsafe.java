a9a79764907dec68859b68be02806ae36a8b1be1
Hongze Zhang <hongze.zzz123@gmail.com>
2025-06-10 00:25:30 +0200

    java comments

--- java/velox4j/src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerUnsafe.java
+++ java/velox4j/src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerUnsafe.java
@@ -17,6 +17,7 @@ package com.fasterxml.jackson.databind.deser;
 
 import java.util.Set;
 
+/** Package-hacking the protected APIs of BeanDeserializer for Velox4J's use. */
 public final class BeanDeserializerUnsafe {
   public static Set<String> getIgnorableProps(BeanDeserializer beanDeserializer) {
     return beanDeserializer._ignorableProps;
