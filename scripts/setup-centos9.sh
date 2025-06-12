c07661a39c866f7447eeec3bfbcdd7f0ed24e440
Jacob Wujciak-Jens <jacob@wujciak.de>
2025-06-12 11:36:00 +0200

    remove flags only used by lzo

--- scripts/setup-centos9.sh
+++ scripts/setup-centos9.sh
@@ -33,7 +33,6 @@ SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 source "$SCRIPTDIR"/setup-common.sh
 CXXFLAGS=$(get_cxx_flags) # Used by boost.
 export CXXFLAGS
-export CFLAGS=${CXXFLAGS//"-std=c++17"/} # Used by LZO.
 export COMPILER_FLAGS=${CXXFLAGS}
 SUDO="${SUDO:-""}"
 USE_CLANG="${USE_CLANG:-false}"
