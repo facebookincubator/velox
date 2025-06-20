336be64abd65abb1690ada842c30a86c1135eb33
Jalpreet Singh Nanda <jalpreet.nanda@ibm.com>
2025-06-20 08:46:15 +0800

    Support insert data into iceberg table. Co-authored-by lpingbj@cn.ibm.com

---
+++ velox/connectors/hive/iceberg/IcebergDataSink.cpp
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/connectors/hive/iceberg/IcebergDataSink.h"
+
+#include "velox/common/base/Fs.h"
+
+namespace facebook::velox::connector::hive::iceberg {
+
+namespace {
+
+std::string toJson(const std::vector<std::string>& partitionValues) {
+  folly::dynamic jsonObject = folly::dynamic::object();
+  folly::dynamic valuesArray = folly::dynamic::array();
+  for (const auto& value : partitionValues) {
+    valuesArray.push_back(value);
+  }
+  jsonObject["partitionValues"] = valuesArray;
+  return folly::toJson(jsonObject);
+}
+
+} // namespace
+
+IcebergDataSink::IcebergDataSink(
+    facebook::velox::RowTypePtr inputType,
+    std::shared_ptr<const IcebergInsertTableHandle> insertTableHandle,
+    const facebook::velox::connector::ConnectorQueryCtx* connectorQueryCtx,
+    facebook::velox::connector::CommitStrategy commitStrategy,
+    const std::shared_ptr<const HiveConfig>& hiveConfig)
+    : IcebergDataSink(
+          std::move(inputType),
+          insertTableHandle,
+          connectorQueryCtx,
+          commitStrategy,
+          hiveConfig,
+          [&insertTableHandle]() {
+            std::vector<column_index_t> channels(
+                insertTableHandle->inputColumns().size());
+            std::iota(channels.begin(), channels.end(), 0);
+            return channels;
+          }()) {}
+
+IcebergDataSink::IcebergDataSink(
+    RowTypePtr inputType,
+    std::shared_ptr<const HiveInsertTableHandle> insertTableHandle,
+    const ConnectorQueryCtx* connectorQueryCtx,
+    CommitStrategy commitStrategy,
+    const std::shared_ptr<const HiveConfig>& hiveConfig,
+    const std::vector<column_index_t>& dataChannels)
+    : HiveDataSink(
+          inputType,
+          insertTableHandle,
+          connectorQueryCtx,
+          commitStrategy,
+          hiveConfig,
+          0,
+          nullptr,
+          dataChannels) {
+  if (isPartitioned()) {
+    partitionData_.resize(maxOpenWriters_);
+  }
+}
+
+std::vector<std::string> IcebergDataSink::generateMetadata() const {
+  auto icebergInsertTableHandle =
+      std::dynamic_pointer_cast<const IcebergInsertTableHandle>(
+          insertTableHandle_);
+
+  std::vector<std::string> commitTasks;
+  commitTasks.reserve(writerInfo_.size());
+  std::string fileFormat(toString(insertTableHandle_->storageFormat()));
+  std::transform(
+      fileFormat.begin(), fileFormat.end(), fileFormat.begin(), ::toupper);
+
+  for (int i = 0; i < writerInfo_.size(); ++i) {
+    const auto& info = writerInfo_.at(i);
+    VELOX_CHECK_NOT_NULL(info);
+    // Following metadata (json format) is consumed by Presto CommitTaskData.
+    // It contains the minimal subset of metadata.
+    // Complete metrics is missing now and this could lead to suboptimal query
+    // plan, will collect full iceberg metrics in following PR.
+    // clang-format off
+    auto commitDataJson = folly::toJson(
+        folly::dynamic::object(
+            "path", info->writerParameters.writeDirectory() + "/" +
+            info->writerParameters.writeFileName()
+        )
+        ("fileSizeInBytes", ioStats_.at(i)->rawBytesWritten())
+        ("metrics", folly::dynamic::object("recordCount", info->numWrittenRows))
+        ("partitionSpecId", icebergInsertTableHandle->partitionSpec()->specId)
+        ("partitionDataJson", partitionData_.empty() || partitionData_[i].empty() ? "" : toJson(partitionData_[i]))
+        ("fileFormat", fileFormat)
+        ("content", "DATA")
+    );
+    // clang-format on
+    commitTasks.push_back(commitDataJson);
+  }
+  return commitTasks;
+}
+
+void IcebergDataSink::splitInputRowsAndEnsureWriters(RowVectorPtr input) {
+  VELOX_CHECK(isPartitioned());
+
+  std::fill(partitionSizes_.begin(), partitionSizes_.end(), 0);
+
+  const auto numRows = partitionIds_.size();
+  for (auto row = 0; row < numRows; ++row) {
+    auto id = getIcebergWriterId(row);
+    uint32_t index = ensureWriter(id);
+
+    updatePartitionRows(index, numRows, row);
+
+    VELOX_CHECK_LT(
+        index,
+        partitionData_.size(),
+        "Writer index {} out of partition bounds:{}.",
+        index,
+        partitionData_.size());
+
+    if (!partitionData_[index].empty()) {
+      continue;
+    }
+
+    std::vector<std::string> partitionValues(partitionChannels_.size());
+
+    for (auto i = 0; i < partitionChannels_.size(); ++i) {
+      auto block = input->childAt(partitionChannels_[i]);
+      partitionValues[i] = block->toString(row);
+    }
+
+    partitionData_[index] = partitionValues;
+  }
+
+  for (auto i = 0; i < partitionSizes_.size(); ++i) {
+    if (partitionSizes_[i] != 0) {
+      VELOX_CHECK_NOT_NULL(partitionRows_[i]);
+      partitionRows_[i]->setSize(partitionSizes_[i] * sizeof(vector_size_t));
+    }
+  }
+}
+
+std::string IcebergDataSink::makePartitionDirectory(
+    const std::string& tableDirectory,
+    const std::optional<std::string>& partitionSubdirectory) const {
+  std::string tableLocation = fmt::format("{}/data", tableDirectory);
+  if (partitionSubdirectory.has_value()) {
+    return fs::path(tableLocation) / partitionSubdirectory.value();
+  }
+  return tableLocation;
+}
+
+HiveWriterId IcebergDataSink::getIcebergWriterId(size_t row) const {
+  std::optional<uint32_t> partitionId;
+  if (isPartitioned()) {
+    VELOX_CHECK_LT(partitionIds_[row], std::numeric_limits<uint32_t>::max());
+    partitionId = static_cast<uint32_t>(partitionIds_[row]);
+  }
+
+  return HiveWriterId{partitionId, std::nullopt};
+}
+
+std::optional<std::string> IcebergDataSink::getPartitionName(
+    const HiveWriterId& id) const {
+  std::optional<std::string> partitionName;
+  if (isPartitioned()) {
+    partitionName =
+        partitionIdGenerator_->partitionName(id.partitionId.value(), true);
+  }
+  return partitionName;
+}
+
+} // namespace facebook::velox::connector::hive::iceberg
