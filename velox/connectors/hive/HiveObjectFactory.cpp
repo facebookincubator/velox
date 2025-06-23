99ecbd31cdc293e1e912bc3b360419ef889661a8
yingsu00 <yingsu00@outlook.com>
2025-06-23 23:47:41 +0800

    refactor: Introduce ConnectorObjectFactory and HiveObjectFactory
    This commit is the first part of the effort to decouple Hive from exec
    tests, which aims to make VELOX_ENABLE_HIVE_CONNECTOR=OFF build without
    errors. The content of this commit include:
    
    - Add a new ConnectorObjectFactory interface in velox/connectors,
      defining abstract methods for creating ConnectoSplits, TableHandles,
      InsertTableHandles,etc.
    - Create HiveObjectFactory in velox/connectors/hive that implements the
      common interface.
    - Add a new HiveObjectFactoryTest suite to verify that dynamic options
      yield correct Hive-specific objects without leaking connector internals
      into core or exec tests.

--- velox/connectors/hive/HiveObjectFactory.cpp
+++ velox/connectors/hive/HiveObjectFactory.cpp
@@ -13,95 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "velox/connectors/hive/HiveObjectFactory.h"
 
+#include <string>
+
+#include <folly/dynamic.h>
+
+#include "velox/connectors/ConnectorNames.h"
 #include "velox/connectors/hive/HiveConnector.h"
-
-#include "velox/connectors/hive/HiveConfig.h"
+#include "velox/connectors/hive/HiveConnectorSplit.h"
 #include "velox/connectors/hive/HiveDataSink.h"
-#include "velox/connectors/hive/HiveDataSource.h"
-#include "velox/connectors/hive/HivePartitionFunction.h"
-
-#include <boost/lexical_cast.hpp>
-#include <memory>
-
-using namespace facebook::velox::exec;
-using namespace facebook::velox::common;
 
 namespace facebook::velox::connector::hive {
 
-namespace {
-std::vector<std::unique_ptr<HiveConnectorMetadataFactory>>&
-hiveConnectorMetadataFactories() {
-  static std::vector<std::unique_ptr<HiveConnectorMetadataFactory>> factories;
-  return factories;
-}
-} // namespace
+using namespace velox::common;
+using namespace facebook::velox::connector;
 
-HiveConnector::HiveConnector(
-    const std::string& id,
-    std::shared_ptr<const config::ConfigBase> config,
-    folly::Executor* executor)
-    : Connector(id, std::move(config)),
-      hiveConfig_(std::make_shared<HiveConfig>(connectorConfig())),
-      fileHandleFactory_(
-          hiveConfig_->isFileHandleCacheEnabled()
-              ? std::make_unique<SimpleLRUCache<FileHandleKey, FileHandle>>(
-                    hiveConfig_->numCacheFileHandles())
-              : nullptr,
-          std::make_unique<FileHandleGenerator>(config)),
-      executor_(executor) {
-  if (hiveConfig_->isFileHandleCacheEnabled()) {
-    LOG(INFO) << "Hive connector " << connectorId()
-              << " created with maximum of "
-              << hiveConfig_->numCacheFileHandles()
-              << " cached file handles with expiration of "
-              << hiveConfig_->fileHandleExpirationDurationMs() << "ms.";
-  } else {
-    LOG(INFO) << "Hive connector " << connectorId()
-              << " created with file handle cache disabled";
-  }
-  for (auto& factory : hiveConnectorMetadataFactories()) {
-    metadata_ = factory->create(this);
-    if (metadata_ != nullptr) {
-      break;
-    }
-  }
-}
-
-std::unique_ptr<DataSource> HiveConnector::createDataSource(
-    const RowTypePtr& outputType,
-    const ConnectorTableHandlePtr& tableHandle,
-    const std::unordered_map<std::string, ColumnHandlePtr>& columnHandles,
-    ConnectorQueryCtx* connectorQueryCtx) {
-  return std::make_unique<HiveDataSource>(
-      outputType,
-      tableHandle,
-      columnHandles,
-      &fileHandleFactory_,
-      executor_,
-      connectorQueryCtx,
-      hiveConfig_);
-}
-
-std::unique_ptr<DataSink> HiveConnector::createDataSink(
-    RowTypePtr inputType,
-    ConnectorInsertTableHandlePtr connectorInsertTableHandle,
-    ConnectorQueryCtx* connectorQueryCtx,
-    CommitStrategy commitStrategy) {
-  auto hiveInsertHandle =
-      std::dynamic_pointer_cast<const HiveInsertTableHandle>(
-          connectorInsertTableHandle);
-  VELOX_CHECK_NOT_NULL(
-      hiveInsertHandle, "Hive connector expecting hive write handle!");
-  return std::make_unique<HiveDataSink>(
-      inputType,
-      hiveInsertHandle,
-      connectorQueryCtx,
-      commitStrategy,
-      hiveConfig_);
-}
-
-std::shared_ptr<ConnectorSplit> HiveConnectorFactory::makeConnectorSplit(
+std::shared_ptr<ConnectorSplit> HiveObjectFactory::makeConnectorSplit(
     const std::string& connectorId,
     const std::string& filePath,
     uint64_t start,
@@ -215,7 +143,7 @@ std::shared_ptr<ConnectorSplit> HiveConnectorFactory::makeConnectorSplit(
   return builder.build();
 }
 
-std::shared_ptr<connector::ColumnHandle> HiveConnectorFactory::makeColumnHandle(
+std::shared_ptr<connector::ColumnHandle> HiveObjectFactory::makeColumnHandle(
     const std::string& connectorId,
     const std::string& name,
     const TypePtr& dataType,
@@ -288,7 +216,7 @@ std::shared_ptr<connector::ColumnHandle> HiveConnectorFactory::makeColumnHandle(
       parseParams);
 }
 
-std::shared_ptr<ConnectorTableHandle> HiveConnectorFactory::makeTableHandle(
+std::shared_ptr<ConnectorTableHandle> HiveObjectFactory::makeTableHandle(
     const std::string& connectorId,
     const std::string& tableName,
     std::vector<std::shared_ptr<const connector::ColumnHandle>> columnHandles,
@@ -352,7 +280,7 @@ std::shared_ptr<ConnectorTableHandle> HiveConnectorFactory::makeTableHandle(
 }
 
 std::shared_ptr<ConnectorInsertTableHandle>
-HiveConnectorFactory::makeInsertTableHandle(
+HiveObjectFactory::makeInsertTableHandle(
     const std::string& connectorId,
     std::vector<std::shared_ptr<const connector::ColumnHandle>> inputColumns,
     std::shared_ptr<const ConnectorLocationHandle> locationHandle,
@@ -369,7 +297,7 @@ HiveConnectorFactory::makeInsertTableHandle(
       std::dynamic_pointer_cast<const hive::LocationHandle>(locationHandle);
   VELOX_CHECK(
       hiveLoc,
-      "HiveConnectorFactory::makeInsertTableHandle: "
+      "HiveObjectFactory::makeInsertTableHandle: "
       "expected HiveLocationHandle");
 
   auto fmt =
@@ -418,8 +346,7 @@ HiveConnectorFactory::makeInsertTableHandle(
       std::move(fileNameGen));
 }
 
-std::shared_ptr<ConnectorLocationHandle>
-HiveConnectorFactory::makeLocationHandle(
+std::shared_ptr<ConnectorLocationHandle> HiveObjectFactory::makeLocationHandle(
     const std::string& connectorId,
     LocationHandle::TableType tableType,
     const folly::dynamic& options) const {
@@ -444,7 +371,7 @@ HiveConnectorFactory::makeLocationHandle(
       connectorId);
 }
 
-core::PartitionFunctionSpecPtr HiveConnectorFactory::makePartitionFunctionSpec(
+core::PartitionFunctionSpecPtr HiveObjectFactory::makePartitionFunctionSpec(
     const std::string& /*connectorId*/,
     const folly::dynamic& options) const {
   VELOX_CHECK(options.isObject(), "Expected options to be a dynamic object");
@@ -494,104 +421,6 @@ core::PartitionFunctionSpecPtr HiveConnectorFactory::makePartitionFunctionSpec(
       std::move(bucketToPartition),
       std::move(channels),
       std::move(constValues));
-}
-
-std::unique_ptr<core::PartitionFunction> HivePartitionFunctionSpec::create(
-    int numPartitions,
-    bool localExchange) const {
-  std::vector<int> bucketToPartitions;
-  if (bucketToPartition_.empty()) {
-    // NOTE: if hive partition function spec doesn't specify bucket to partition
-    // mapping, then we do round-robin mapping based on the actual number of
-    // partitions.
-    bucketToPartitions.resize(numBuckets_);
-    for (int bucket = 0; bucket < numBuckets_; ++bucket) {
-      bucketToPartitions[bucket] = bucket % numPartitions;
-    }
-    if (localExchange) {
-      // Shuffle the map from bucket to partition for local exchange so we don't
-      // use the same map for remote shuffle.
-      std::shuffle(
-          bucketToPartitions.begin(),
-          bucketToPartitions.end(),
-          std::mt19937{0});
-    }
-  }
-  return std::make_unique<velox::connector::hive::HivePartitionFunction>(
-      numBuckets_,
-      bucketToPartition_.empty() ? std::move(bucketToPartitions)
-                                 : bucketToPartition_,
-      channels_,
-      constValues_);
-}
-
-std::string HivePartitionFunctionSpec::toString() const {
-  std::ostringstream keys;
-  size_t constIndex = 0;
-  for (auto i = 0; i < channels_.size(); ++i) {
-    if (i > 0) {
-      keys << ", ";
-    }
-    auto channel = channels_[i];
-    if (channel == kConstantChannel) {
-      keys << "\"" << constValues_[constIndex++]->toString(0) << "\"";
-    } else {
-      keys << channel;
-    }
-  }
-
-  return fmt::format("HIVE(({}) buckets: {})", keys.str(), numBuckets_);
-}
-
-folly::dynamic HivePartitionFunctionSpec::serialize() const {
-  folly::dynamic obj = folly::dynamic::object;
-  obj["name"] = "HivePartitionFunctionSpec";
-  obj["numBuckets"] = ISerializable::serialize(numBuckets_);
-  obj["bucketToPartition"] = ISerializable::serialize(bucketToPartition_);
-  obj["keys"] = ISerializable::serialize(channels_);
-  std::vector<velox::core::ConstantTypedExpr> constValueExprs;
-  constValueExprs.reserve(constValues_.size());
-  for (const auto& value : constValues_) {
-    constValueExprs.emplace_back(value);
-  }
-  obj["constants"] = ISerializable::serialize(constValueExprs);
-  return obj;
-}
-
-// static
-core::PartitionFunctionSpecPtr HivePartitionFunctionSpec::deserialize(
-    const folly::dynamic& obj,
-    void* context) {
-  std::vector<column_index_t> channels =
-      ISerializable::deserialize<std::vector<column_index_t>>(
-          obj["keys"], context);
-  const auto constTypedValues =
-      ISerializable::deserialize<std::vector<velox::core::ConstantTypedExpr>>(
-          obj["constants"], context);
-  std::vector<VectorPtr> constValues;
-  constValues.reserve(constTypedValues.size());
-  auto* pool = static_cast<memory::MemoryPool*>(context);
-  for (const auto& value : constTypedValues) {
-    constValues.emplace_back(value->toConstantVector(pool));
-  }
-  return std::make_shared<HivePartitionFunctionSpec>(
-      ISerializable::deserialize<int>(obj["numBuckets"], context),
-      ISerializable::deserialize<std::vector<int>>(
-          obj["bucketToPartition"], context),
-      std::move(channels),
-      std::move(constValues));
-}
-
-void registerHivePartitionFunctionSerDe() {
-  auto& registry = DeserializationWithContextRegistryForSharedPtr();
-  registry.Register(
-      "HivePartitionFunctionSpec", HivePartitionFunctionSpec::deserialize);
-}
-
-bool registerHiveConnectorMetadataFactory(
-    std::unique_ptr<HiveConnectorMetadataFactory> factory) {
-  hiveConnectorMetadataFactories().push_back(std::move(factory));
-  return true;
 }
 
 } // namespace facebook::velox::connector::hive
