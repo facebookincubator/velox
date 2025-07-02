b462f957fb5c6ebef8c00afedfe5d9fb3d179c66
zhli1142015 <zhli@pczhlicht.localdomain>
2025-07-02 16:59:33 +0800

    row_ranges change

---
+++ velox/dwio/parquet/reader/RowRanges.h
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "velox/common/base/Exceptions.h"
+
+/// Represents a closed interval of row indices [from_, to_].
+///
+/// RowRange is used to describe a contiguous range of row indices, inclusive of
+/// both endpoints. It provides utility methods for counting the number of rows,
+/// checking relative positions between ranges, and computing unions and
+/// intersections.
+///
+/// Invariants:
+/// - from_ <= to_
+///
+/// Example:
+///   RowRange r(10, 20); // Represents rows 10 through 20, inclusive.
+namespace facebook::velox::parquet {
+
+struct RowRange {
+  uint64_t from_;
+  uint64_t to_;
+
+  RowRange(uint64_t from, uint64_t to) : from_(from), to_(to) {
+    VELOX_CHECK_LE(from, to);
+  }
+
+  uint64_t count() const {
+    return to_ - from_ + 1;
+  }
+
+  bool isBefore(const RowRange& other) const {
+    return to_ < other.from_;
+  }
+
+  bool isAfter(const RowRange& other) const {
+    return from_ > other.to_;
+  }
+
+  static std::optional<RowRange> TryUnion(
+      const RowRange& left,
+      const RowRange& right) {
+    if (left.from_ <= right.from_) {
+      if (left.to_ + 1 >= right.from_) {
+        return RowRange(left.from_, std::max(left.to_, right.to_));
+      }
+    } else {
+      if (right.to_ + 1 >= left.from_) {
+        return RowRange(right.from_, std::max(left.to_, right.to_));
+      }
+    }
+    return std::nullopt;
+  }
+
+  static std::optional<RowRange> Intersection(
+      const RowRange& left,
+      const RowRange& right) {
+    if (left.from_ <= right.from_) {
+      if (left.to_ >= right.from_) {
+        return RowRange(right.from_, std::min(left.to_, right.to_));
+      }
+    } else {
+      if (right.to_ >= left.from_) {
+        return RowRange(left.from_, std::min(left.to_, right.to_));
+      }
+    }
+    return std::nullopt;
+  }
+
+  std::string toString() const {
+    std::ostringstream os;
+    os << "[" << from_ << ", " << to_ << "]";
+    return os.str();
+  }
+};
+
+/// Represents a collection of row intervals (ranges) for efficient row
+/// selection and manipulation.
+class RowRanges {
+ public:
+  static const RowRanges EMPTY;
+  RowRanges() = default;
+
+  explicit RowRanges(const RowRange& range) {
+    ranges_.push_back(range);
+  }
+
+  /// Creates a RowRanges object representing a single continuous range of rows
+  /// starting from 0 up to (rowCount - 1).
+  static RowRanges createSingle(int64_t rowCount) {
+    VELOX_CHECK_GE(rowCount, 0);
+    if (rowCount == 0) {
+      return EMPTY;
+    }
+    return RowRanges(RowRange(0, rowCount - 1));
+  }
+
+  /// Computes the union of two RowRanges objects.
+  static RowRanges Union(const RowRanges& left, const RowRanges& right) {
+    std::vector<RowRange> all = left.ranges_;
+    all.insert(all.end(), right.ranges_.begin(), right.ranges_.end());
+    std::sort(all.begin(), all.end(), [](const RowRange& a, const RowRange& b) {
+      return a.from_ < b.from_;
+    });
+    RowRanges result;
+    for (const auto& r : all) {
+      result.add(r);
+    }
+    return result;
+  }
+
+  /// Computes the intersection of two RowRanges objects.
+  static RowRanges Intersection(const RowRanges& left, const RowRanges& right) {
+    RowRanges result;
+    size_t i = 0, j = 0;
+    const auto& A = left.ranges_;
+    const auto& B = right.ranges_;
+    while (i < A.size() && j < B.size()) {
+      const RowRange& a = A[i];
+      const RowRange& b = B[j];
+      if (a.isAfter(b)) {
+        j++;
+      } else if (b.isAfter(a)) {
+        i++;
+      } else {
+        auto inter = RowRange::Intersection(a, b);
+        if (inter.has_value()) {
+          result.add(*inter);
+        }
+        if (a.to_ < b.to_) {
+          i++;
+        } else {
+          j++;
+        }
+      }
+    }
+    return result;
+  }
+
+  /// Add an interval to the end, and merge with the previous one if possible.
+  void add(const RowRange& range) {
+    if (ranges_.empty()) {
+      ranges_.push_back(range);
+      return;
+    }
+    auto& last = ranges_.back();
+    // Try to merge: if overlapping or adjacent, extend the last interval;
+    // otherwise, append directly.
+    if (auto m = RowRange::TryUnion(last, range)) {
+      last = *m;
+    } else {
+      VELOX_CHECK(last.isBefore(range));
+      ranges_.push_back(range);
+    }
+  }
+
+  /// Return the total number of elements in all intervals.
+  int64_t rowCount() const {
+    int64_t cnt = 0;
+    for (const auto& r : ranges_) {
+      cnt += r.count();
+    }
+    return cnt;
+  }
+
+  /// Check if the given interval [from_, to_] overlaps with any existing
+  /// interval.
+  bool isOverlapping(int64_t from_, int64_t to_) const {
+    RowRange query(from_, to_);
+    for (const auto& r : ranges_) {
+      if (query.isBefore(r)) {
+        break;
+      }
+      if (!query.isAfter(r)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /// Return string representation, e.g. "[[0, 9], [15, 20]]".
+  std::string toString() const {
+    std::ostringstream os;
+    os << "[";
+    for (size_t i = 0; i < ranges_.size(); ++i) {
+      if (i)
+        os << ", ";
+      os << ranges_[i].toString();
+    }
+    os << "]";
+    return os.str();
+  }
+
+  const std::vector<RowRange>& getRanges() const {
+    return ranges_;
+  }
+
+ private:
+  std::vector<RowRange> ranges_;
+};
+
+const RowRanges RowRanges::EMPTY = RowRanges();
+} // namespace facebook::velox::parquet
---
+++ velox/dwio/parquet/tests/reader/RowRangesTest.cpp
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "velox/dwio/parquet/reader/RowRanges.h"
+#include "velox/dwio/parquet/tests/ParquetTestBase.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::parquet;
+
+class RowRangesTest : public ParquetTestBase {};
+
+TEST(RowRangesTest, rowRange) {
+  RowRange r(5, 10);
+  EXPECT_EQ(r.from_, 5);
+  EXPECT_EQ(r.to_, 10);
+  EXPECT_EQ(r.count(), 6); // 10 - 5 + 1
+  EXPECT_EQ(r.toString(), "[5, 10]");
+  auto ru1 = RowRange::TryUnion(RowRange(1, 5), RowRange(6, 8));
+  ASSERT_TRUE(ru1.has_value());
+  EXPECT_EQ(ru1->toString(), "[1, 8]");
+
+  auto ru2 = RowRange::TryUnion(RowRange(1, 5), RowRange(7, 8));
+  EXPECT_FALSE(ru2.has_value());
+
+  auto ri = RowRange::Intersection(RowRange(2, 6), RowRange(4, 10));
+  ASSERT_TRUE(ri.has_value());
+  EXPECT_EQ(ri->toString(), "[4, 6]");
+
+  auto ri2 = RowRange::Intersection(RowRange(1, 3), RowRange(4, 5));
+  EXPECT_FALSE(ri2.has_value());
+}
+
+TEST(RowRangesTest, createSingle) {
+  RowRanges rr = RowRanges::createSingle(10);
+  EXPECT_EQ(rr.rowCount(), 10);
+  EXPECT_EQ(rr.toString(), "[[0, 9]]");
+
+  RowRanges rr0 = RowRanges::createSingle(0);
+  EXPECT_EQ(rr0.rowCount(), 0);
+  EXPECT_EQ(rr0.toString(), "[]");
+}
+
+TEST(RowRangesTest, union) {
+  RowRanges a(RowRange(113, 241));
+  RowRanges b(RowRange(221, 340));
+  EXPECT_EQ(RowRanges::Union(a, b).toString(), "[[113, 340]]");
+
+  RowRanges c(RowRange(113, 230));
+  RowRanges d(RowRange(231, 340));
+  EXPECT_EQ(RowRanges::Union(c, d).toString(), "[[113, 340]]");
+
+  RowRanges e(RowRange(113, 230));
+  RowRanges f(RowRange(232, 340));
+  EXPECT_EQ(RowRanges::Union(e, f).toString(), "[[113, 230], [232, 340]]");
+
+  RowRanges x;
+  x.add(RowRange(5, 8));
+  x.add(RowRange(10, 20));
+  x.add(RowRange(25, 30));
+  EXPECT_EQ(x.toString(), "[[5, 8], [10, 20], [25, 30]]");
+
+  RowRanges y;
+  y.add(RowRange(9, 18));
+
+  EXPECT_EQ(RowRanges::Union(x, y).toString(), "[[5, 20], [25, 30]]");
+}
+
+TEST(RowRangesTest, intersection) {
+  RowRanges a(RowRange(113, 241));
+  RowRanges b(RowRange(221, 340));
+  EXPECT_EQ(RowRanges::Intersection(a, b).toString(), "[[221, 241]]");
+
+  RowRanges c(RowRange(113, 230));
+  RowRanges d(RowRange(231, 340));
+  EXPECT_EQ(RowRanges::Intersection(c, d).toString(), "[]");
+
+  RowRanges x;
+  x.add(RowRange(0, 100));
+  x.add(RowRange(200, 300));
+  RowRanges y;
+  y.add(RowRange(50, 250));
+  EXPECT_EQ(
+      RowRanges::Intersection(x, y).toString(), "[[50, 100], [200, 250]]");
+}
+
+TEST(RowRangesTest, overlapAndCount) {
+  RowRanges r;
+  r.add(RowRange(0, 4));
+  r.add(RowRange(10, 12));
+  EXPECT_TRUE(r.isOverlapping(2, 3));
+  EXPECT_TRUE(r.isOverlapping(3, 10));
+  EXPECT_FALSE(r.isOverlapping(5, 9));
+
+  EXPECT_EQ(r.rowCount(), 8);
+}
+TEST(RowRangesTest, addAdjacentChains) {
+  RowRanges r;
+  r.add(RowRange(0, 0));
+  EXPECT_EQ(r.toString(), "[[0, 0]]");
+  r.add(RowRange(1, 1));
+  EXPECT_EQ(r.toString(), "[[0, 1]]");
+  r.add(RowRange(2, 2));
+  EXPECT_EQ(r.toString(), "[[0, 2]]");
+  EXPECT_EQ(r.rowCount(), 3);
+}
+
+TEST(RowRangesTest, addNonAdjacent) {
+  RowRanges r;
+  r.add(RowRange(5, 10));
+  r.add(RowRange(12, 15));
+  EXPECT_EQ(r.toString(), "[[5, 10], [12, 15]]");
+  EXPECT_EQ(r.rowCount(), (10 - 5 + 1) + (15 - 12 + 1));
+}
+
+TEST(RowRangesTest, unionChainedAdjacent) {
+  RowRanges a(RowRange(0, 3));
+  RowRanges b(RowRange(4, 7));
+  RowRanges c(RowRange(8, 10));
+  auto u = RowRanges::Union(RowRanges::Union(a, b), c);
+  EXPECT_EQ(u.toString(), "[[0, 10]]");
+}
+
+TEST(RowRangesTest, unionInterleaved) {
+  RowRanges a;
+  a.add(RowRange(0, 2));
+  a.add(RowRange(10, 12));
+  RowRanges b;
+  b.add(RowRange(1, 11));
+  auto u = RowRanges::Union(a, b);
+  EXPECT_EQ(u.toString(), "[[0, 12]]");
+}
+
+TEST(RowRangesTest, intersectionTouchingEdge) {
+  RowRanges a(RowRange(100, 200));
+  RowRanges b(RowRange(200, 300));
+  auto inter = RowRanges::Intersection(a, b);
+  EXPECT_EQ(inter.toString(), "[[200, 200]]");
+  EXPECT_EQ(inter.rowCount(), 1);
+}
+
+TEST(RowRangesTest, intersectionSinglePoint) {
+  RowRanges a(RowRange(50, 50));
+  RowRanges b(RowRange(0, 100));
+  auto inter = RowRanges::Intersection(a, b);
+  EXPECT_EQ(inter.toString(), "[[50, 50]]");
+  EXPECT_EQ(inter.rowCount(), 1);
+}
+
+TEST(RowRangesTest, intersectionContainment) {
+  RowRanges a(RowRange(0, 100));
+  RowRanges b(RowRange(20, 30));
+  EXPECT_EQ(RowRanges::Intersection(a, b).toString(), "[[20, 30]]");
+  RowRanges c(RowRange(101, 110));
+  EXPECT_EQ(RowRanges::Intersection(a, c).toString(), "[]");
+}
+
+TEST(RowRangesTest, overlapExactAndGap) {
+  RowRanges r;
+  r.add(RowRange(100, 110));
+  EXPECT_TRUE(r.isOverlapping(100, 110));
+  EXPECT_TRUE(r.isOverlapping(102, 108));
+  EXPECT_FALSE(r.isOverlapping(90, 99));
+  EXPECT_FALSE(r.isOverlapping(111, 120));
+}
+
+TEST(RowRangesTest, overlapAcrossSegments) {
+  RowRanges r;
+  r.add(RowRange(0, 10));
+  r.add(RowRange(20, 30));
+  EXPECT_FALSE(r.isOverlapping(11, 19));
+  EXPECT_TRUE(r.isOverlapping(5, 15));
+  EXPECT_TRUE(r.isOverlapping(15, 25));
+}
