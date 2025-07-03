6f63c93b56e6a8fce66c0ad42ef5f50094d5137a
Oliver Xu <oliverxu@meta.com>
2025-07-02 21:15:09 -0700

    feat(sketch): Add core cardinality functions for SfmSketch (#13924)
    Summary:
    
    ### Changes in sketch
    
    * Edited `SfmSketch.h` to add more functions for `SfmSketch` and removed some unnecessary code.
    * Edited `SfmSketchTest.cpp` to add more test cases and remove some unnecessary code.
    * Edited `SfmSketch.cpp` to add a new function `cardinality()` that estimates the cardinality of the sketch using maximum pseudolikelihood (Newton's method).
    
    ### Impact
    
    This diff adds new functionality to the `SfmSketch` class and improves the testing of the class. The new function `cardinality()` provides an estimate of the cardinality of the sketch, which is useful for private distinct counting.
    
    Differential Revision: D77310142

--- velox/functions/lib/aggregates/noisy_aggregation/sketch/SfmSketch.h
+++ velox/functions/lib/aggregates/noisy_aggregation/sketch/SfmSketch.h
@@ -19,6 +19,7 @@
 #include <cstdint>
 #include "folly/hash/MurmurHash.h"
 #include "velox/functions/lib/aggregates/noisy_aggregation/sketch/BitMap.h"
+#include "velox/functions/lib/aggregates/noisy_aggregation/sketch/SecureRandomizationStrategy.h"
 
 namespace facebook::velox::functions::aggregate {
 
@@ -26,6 +27,7 @@ namespace facebook::velox::functions::aggregate {
 /// <a href="https://arxiv.org/pdf/2302.02056.pdf">Sketch-Flip-Merge: Mergeable
 /// Sketches for Private Distinct Counting</a>.
 class SfmSketch {
+ public:
   static constexpr double NON_PRIVATE_EPSILON =
       std::numeric_limits<double>::infinity();
   static constexpr int32_t MAX_ESTIMATION_ITERATIONS = 1000;
@@ -51,6 +53,76 @@ class SfmSketch {
 
   void addHash(uint64_t hash);
   void addIndexAndZeros(uint32_t bucketIndex, uint32_t zeros);
+  void mergeWith(SfmSketch& other);
+  uint64_t cardinality() const;
+
+  template <typename RandomizationStrategy>
+  void mergeWith(
+      SfmSketch& other,
+      RandomizationStrategy randomizationStrategy) {
+    VELOX_CHECK_EQ(
+        precision_,
+        other.precision_,
+        "Cannot merge two SFM sketches with different precision: {} vs. {}",
+        precision_,
+        other.precision_);
+    VELOX_CHECK_EQ(
+        indexBitLength_,
+        other.indexBitLength_,
+        "Cannot merge two SFM sketches with different indexBitLength: {} vs. {}",
+        indexBitLength_,
+        other.indexBitLength_);
+
+    if (!privacyEnabled() && !other.privacyEnabled()) {
+      // If neither sketch is private, we just take the OR of the sketches
+      bitMap_.bitwiseOr(other.getBitMap());
+    } else {
+      // If either sketch is private, we combine using a randomized merge
+      double p1 = randomizedResponseProbability_;
+      double p2 = other.randomizedResponseProbability_;
+      double p = mergeRandomizedResponseProbabilities(p1, p2);
+      double normalizer = (1 - 2 * p) / ((1 - 2 * p1) * (1 - 2 * p2));
+
+      for (uint32_t i = 0; i < bitMap_.length(); i++) {
+        double bit1 = bitMap_.bitAt(i) ? 1.0 : 0.0;
+        double bit2 = other.bitMap_.bitAt(i) ? 1.0 : 0.0;
+        double x = 1 - 2 * p - normalizer * (1 - p1 - bit1) * (1 - p2 - bit2);
+        double probability = p + normalizer * x;
+        probability = std::min(1.0, std::max(0.0, probability));
+        bitMap_.setBit(i, randomizationStrategy.nextBoolean(probability));
+      }
+    }
+
+    randomizedResponseProbability_ = mergeRandomizedResponseProbabilities(
+        randomizedResponseProbability_, other.randomizedResponseProbability_);
+  }
+
+  void enablePrivacy(double epsilon) {
+    enablePrivacy(epsilon, getDefaultRandomizationStrategy());
+  }
+
+  static SecureRandomizationStrategy getDefaultRandomizationStrategy() {
+    return SecureRandomizationStrategy();
+  }
+
+  template <typename RandomizationStrategy>
+  void enablePrivacy(
+      double epsilon,
+      RandomizationStrategy randomizationStrategy) {
+    VELOX_CHECK_NOT_NULL(
+        &randomizationStrategy, "randomizationStrategy can't be null.");
+    VELOX_CHECK(!privacyEnabled(), "privacy is already enabled.");
+    validateEpsilon(epsilon);
+    randomizedResponseProbability_ =
+        calculateRandomizedResponseProbability(epsilon);
+    bitMap_.flipAll(randomizedResponseProbability_, randomizationStrategy);
+  }
+
+  // For math details, see Theorem 4.8, <a
+  // href="https://arxiv.org/pdf/2302.02056.pdf">arXiv:2302.02056</a>.
+  static double mergeRandomizedResponseProbabilities(double p1, double p2) {
+    return (p1 + p2 - 3 * p1 * p2) / (1 - 2 * p1 * p2);
+  }
 
   static uint32_t computeIndex(uint64_t hash, uint32_t indexBitLength) {
     VELOX_CHECK(
@@ -123,6 +195,12 @@ class SfmSketch {
   static void validateRandomizedResponseProbability(double p);
   double observationProbability(uint32_t level) const;
   void flipBitOn(uint32_t bucketIndex, uint32_t zeros);
+  double logLikelihoodFirstDerivative(double n) const;
+  double logLikelihoodTermFirstDerivative(uint32_t level, bool on, double n)
+      const;
+  double logLikelihoodSecondDerivative(double n) const;
+  double logLikelihoodTermSecondDerivative(uint32_t level, bool on, double n)
+      const;
 
   uint32_t indexBitLength_;
   uint32_t precision_;
