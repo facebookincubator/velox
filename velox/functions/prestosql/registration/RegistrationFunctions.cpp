176347671d603bc1ebe1aba3767d8209a49fcee4
yingsu00 <yingsu00@outlook.com>
2025-07-03 20:24:32 +0800

    fix: Fix build error when VELOX_ENABLE_GEO is disabled
    When VELOX_ENABLE_GEO is set to OFF, build would fail with error "use
    of undeclared identifier 'registerGeometryFunctions'". This commit
    fixes this problem by checking VELOX_ENABLE_GEO when
    registerGeometryFunctions is called.
    
    co-authored-by: wraymo <raywangv@gmail.com>

--- velox/functions/prestosql/registration/RegistrationFunctions.cpp
+++ velox/functions/prestosql/registration/RegistrationFunctions.cpp
@@ -43,7 +43,9 @@ extern void registerMapAllowingDuplicates(
     const std::string& name,
     const std::string& prefix);
 extern void registerBingTileFunctions(const std::string& prefix);
+#ifdef VELOX_ENABLE_GEO
 extern void registerGeometryFunctions(const std::string& prefix);
+#endif
 extern void registerInternalArrayFunctions();
 
 namespace prestosql {
@@ -97,9 +99,11 @@ void registerBingTileFunctions(const std::string& prefix) {
   functions::registerBingTileFunctions(prefix);
 }
 
+#ifdef VELOX_ENABLE_GEO
 void registerGeometryFunctions(const std::string& prefix) {
   functions::registerGeometryFunctions(prefix);
 }
+#endif
 
 void registerGeneralFunctions(const std::string& prefix) {
   functions::registerGeneralFunctions(prefix);
@@ -138,7 +142,9 @@ void registerAllScalarFunctions(const std::string& prefix) {
   registerIntegerFunctions(prefix);
   registerFloatingPointFunctions(prefix);
   registerBingTileFunctions(prefix);
+#ifdef VELOX_ENABLE_GEO
   registerGeometryFunctions(prefix);
+#endif
   registerGeneralFunctions(prefix);
   registerDateTimeFunctions(prefix);
   registerURLFunctions(prefix);
