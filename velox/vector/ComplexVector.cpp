50d5331596ef6c0fd9045c1d74f7899c34c4a5cd
Pedro Eugenio Rocha Pedreira <pedroerp@meta.com>
2025-07-07 17:11:31 -0700

    fix(vector): EnsureWritable should copy buffer views
    Summary:
    Ensure that ensureWritable() copies buffer views in complex types used
    for offsets and lengths. BaseVector already does the right thing for null
    buffers, but complex types previouslyu only checked whether offsets and
    lengths were unique, but not if they were not views (buffer->unique() vs.
    buffer->isMutable()). Adding the fix plus unit tests.
    
    Differential Revision: D77904499

--- velox/vector/ComplexVector.cpp
+++ velox/vector/ComplexVector.cpp
@@ -1165,7 +1165,7 @@ std::string ArrayVector::toString(vector_size_t index) const {
 
 void ArrayVector::ensureWritable(const SelectivityVector& rows) {
   auto newSize = std::max<vector_size_t>(rows.end(), BaseVector::length_);
-  if (offsets_ && !offsets_->unique()) {
+  if (offsets_ && !offsets_->isMutable()) {
     BufferPtr newOffsets =
         AlignedBuffer::allocate<vector_size_t>(newSize, BaseVector::pool_);
     auto rawNewOffsets = newOffsets->asMutable<vector_size_t>();
@@ -1184,7 +1184,7 @@ void ArrayVector::ensureWritable(const SelectivityVector& rows) {
     rawOffsets_ = offsets_->as<vector_size_t>();
   }
 
-  if (sizes_ && !sizes_->unique()) {
+  if (sizes_ && !sizes_->isMutable()) {
     BufferPtr newSizes =
         AlignedBuffer::allocate<vector_size_t>(newSize, BaseVector::pool_);
     auto rawNewSizes = newSizes->asMutable<vector_size_t>();
@@ -1473,7 +1473,7 @@ std::string MapVector::toString(vector_size_t index) const {
 
 void MapVector::ensureWritable(const SelectivityVector& rows) {
   auto newSize = std::max<vector_size_t>(rows.end(), BaseVector::length_);
-  if (offsets_ && !offsets_->unique()) {
+  if (offsets_ && !offsets_->isMutable()) {
     BufferPtr newOffsets =
         AlignedBuffer::allocate<vector_size_t>(newSize, BaseVector::pool_);
     auto rawNewOffsets = newOffsets->asMutable<vector_size_t>();
@@ -1492,7 +1492,7 @@ void MapVector::ensureWritable(const SelectivityVector& rows) {
     rawOffsets_ = offsets_->as<vector_size_t>();
   }
 
-  if (sizes_ && !sizes_->unique()) {
+  if (sizes_ && !sizes_->isMutable()) {
     BufferPtr newSizes =
         AlignedBuffer::allocate<vector_size_t>(newSize, BaseVector::pool_);
     auto rawNewSizes = newSizes->asMutable<vector_size_t>();
