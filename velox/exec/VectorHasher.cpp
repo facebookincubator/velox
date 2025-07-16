1310a99341a8c0de741a07bce728307624e2db77
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-07-16 09:41:23 +0800

    support decimal as partition key

--- velox/exec/VectorHasher.cpp
+++ velox/exec/VectorHasher.cpp
@@ -41,6 +41,9 @@ namespace facebook::velox::exec {
       case TypeKind::BIGINT: {                                           \
         return TEMPLATE_FUNC<TypeKind::BIGINT>(__VA_ARGS__);             \
       }                                                                  \
+      case TypeKind::HUGEINT: {                                          \
+        return TEMPLATE_FUNC<TypeKind::HUGEINT>(__VA_ARGS__);            \
+      }                                                                  \
       case TypeKind::VARCHAR:                                            \
       case TypeKind::VARBINARY: {                                        \
         return TEMPLATE_FUNC<TypeKind::VARCHAR>(__VA_ARGS__);            \
