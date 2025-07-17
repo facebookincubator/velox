a70efb21b9a1551acc82992b2f4146b37c764dd4
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2025-07-17 03:34:02 +0000

    Include required header

--- velox/experimental/cudf/connectors/parquet/ParquetDataSource.cpp
+++ velox/experimental/cudf/connectors/parquet/ParquetDataSource.cpp
@@ -24,6 +24,7 @@
 #include "velox/experimental/cudf/vector/CudfVector.h"
 
 #include "velox/common/time/Timer.h"
+#include "velox/connectors/hive/HiveConnectorSplit.h"
 #include "velox/expression/FieldReference.h"
 
 #include <cudf/io/parquet.hpp>
