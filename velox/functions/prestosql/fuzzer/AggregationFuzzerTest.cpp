4285697a3ba65c9c44790f651a416a01a2f12f8f
Oliver Xu <oliverxu@meta.com>
2025-07-19 15:28:05 -0700

    feat: Add noisy_approx_set_sfm_from_index_and_zeros function (#14153)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/14153
    
    Add a new function to Velox. The function is used to return a SfmSketch set by adding items from bucket_index and trailing zeros.
    
    Differential Revision: D78391518

--- velox/functions/prestosql/fuzzer/AggregationFuzzerTest.cpp
+++ velox/functions/prestosql/fuzzer/AggregationFuzzerTest.cpp
@@ -153,6 +153,7 @@ int main(int argc, char** argv) {
       "noisy_approx_distinct_sfm",
       "noisy_empty_approx_set_sfm",
       "merge_sfm",
+      "noisy_approx_set_sfm_from_index_and_zeros",
       // https://github.com/facebookincubator/velox/issues/13547
       "merge",
   };
