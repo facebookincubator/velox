62cc6049be95c0ed47a22c3efb1d59c7dddb2b8c
Zhen Li <zhli@microsoft.com>
2025-07-22 13:37:54 +0800

    config and metrics

--- velox/dwio/parquet/reader/StructColumnReader.cpp
+++ velox/dwio/parquet/reader/StructColumnReader.cpp
@@ -116,7 +116,7 @@ void StructColumnReader::read(
 std::shared_ptr<dwio::common::BufferedInput> StructColumnReader::loadRowGroup(
     uint32_t index,
     const std::shared_ptr<dwio::common::BufferedInput>& input,
-    const dwio::common::RowRanges& rowRanges) {
+    dwio::common::RowRanges& rowRanges) {
   if (isRowGroupBuffered(index, *input)) {
     enqueueRowGroup(index, *input, rowRanges);
     return input;
@@ -138,7 +138,7 @@ bool StructColumnReader::isRowGroupBuffered(
 void StructColumnReader::enqueueRowGroup(
     uint32_t index,
     dwio::common::BufferedInput& input,
-    const dwio::common::RowRanges& rowRanges) {
+    dwio::common::RowRanges& rowRanges) {
   for (auto& child : children_) {
     if (auto structChild = dynamic_cast<StructColumnReader*>(child)) {
       structChild->enqueueRowGroup(index, input, rowRanges);
