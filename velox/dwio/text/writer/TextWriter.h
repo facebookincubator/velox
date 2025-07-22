d58b397c8c0c7950ada80712647f6cbb001f365a
Yi Cheng Lee <yclee03@meta.com>
2025-07-22 11:51:13 -0700

    feat: Support Compression (#14118)
    Summary:
    
    Support ZSTD, Gzip and raw deflate compression
    
    Differential Revision: D77546411

--- velox/dwio/text/writer/TextWriter.h
+++ velox/dwio/text/writer/TextWriter.h
@@ -44,7 +44,7 @@ class TextWriter : public dwio::common::Writer {
   TextWriter(
       RowTypePtr schema,
       std::unique_ptr<dwio::common::FileSink> sink,
-      const std::shared_ptr<text::WriterOptions>& options,
+      const std::shared_ptr<WriterOptions>& options,
       const SerDeOptions& serDeOptions = SerDeOptions());
 
   ~TextWriter() override = default;
@@ -74,6 +74,7 @@ class TextWriter : public dwio::common::Writer {
 
   const RowTypePtr schema_;
   const std::unique_ptr<BufferedWriterSink> bufferedWriterSink_;
+  const std::shared_ptr<WriterOptions> options_;
 
   uint8_t headerLineCount_;
   SerDeOptions serDeOptions_;
