515ab0c3d179677009ffe60fa374224d7bd77afa
Ping Liu <lpingbj@cn.ibm.com>
2025-07-28 15:40:33 +0100

    Collect Iceberg data file statistics during insertion.

---
+++ velox/dwio/common/DataFileStatistics.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <folly/json/dynamic.h>
+
+namespace facebook::velox::dwio::common {
+
+// Iceberg data_file struct fields.
+struct DataFileStatistics {
+  int64_t numRecords;
+  std::unordered_map<int32_t, int64_t> columnsSizes;
+  std::unordered_map<int32_t, int64_t> valueCounts;
+  std::unordered_map<int32_t, int64_t> nullValueCounts;
+  std::unordered_map<int32_t, int64_t> nanValueCounts;
+  std::unordered_map<int32_t, std::string> lowerBounds;
+  std::unordered_map<int32_t, std::string> upperBounds;
+
+  // Split offsets for the data file. For example, all row
+  // group offsets in a Parquet file. Must be sorted ascending.
+  std::vector<uint64_t> splitOffsets;
+
+  DataFileStatistics() : numRecords(0) {}
+
+  folly::dynamic toJson() const;
+
+  folly::dynamic splitOffsetsAsJson() const;
+};
+
+} // namespace facebook::velox::dwio::common
