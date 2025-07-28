fbae075f661a3859744a94bff597a385f67078f0
mbkkt <mbkkt@users.noreply.github.com>
2025-07-28 13:22:38 +0000

    Fix fmt

--- velox/functions/sparksql/DecimalCeil.cpp
+++ velox/functions/sparksql/DecimalCeil.cpp
@@ -51,9 +51,7 @@ void registerDecimalCeil(const std::string& prefix) {
       exec::SignatureVariable(
           P2::name(),
           fmt::format(
-              "min(38, {p} - {s} + min({s}, 1))",
-              fmt::arg("p", P1::name()),
-              fmt::arg("s", S1::name())),
+              "min(38, {0} - {1} + min({1}, 1))", P1::name(), S1::name()),
           exec::ParameterType::kIntegerParameter),
       exec::SignatureVariable(
           S2::name(), "0", exec::ParameterType::kIntegerParameter),
