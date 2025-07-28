463c1a61339bd139fc3d3d908ed313a4c4d68d54
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-07-28 13:23:57 +0800

    address comments

--- velox/functions/lib/Murmur3Hash32Base.cpp
+++ velox/functions/lib/Murmur3Hash32Base.cpp
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#include "common/base/BitUtil.h"
-#include "functions/lib/Hash.h"
+#include "velox/common/base/BitUtil.h"
+#include "velox/functions/lib/Murmur3hash32Base.h"
 
 namespace facebook::velox::functions {
 
