ffc67862b5272cf6cda2e7cc2d5f4c8422e57dca
Patrick Sullivan <patricksullivan@meta.com>
2025-07-31 08:15:44 -0700

    feat: Implement geometry_as_geojson, geometry_from_geojson (#14252)
    Summary:
    
    Adds GeoJson UDFs to velox.
    
    Differential Revision: D78950059

--- velox/functions/prestosql/registration/GeometryFunctionsRegistration.cpp
+++ velox/functions/prestosql/registration/GeometryFunctionsRegistration.cpp
@@ -145,6 +145,10 @@ void registerAccessors(const std::string& prefix) {
       {{prefix + "ST_InteriorRings"}});
   registerFunction<StGeometriesFunction, Array<Geometry>, Geometry>(
       {{prefix + "ST_Geometries"}});
+  registerFunction<GeometryAsGeoJsonFunction, Varchar, Geometry>(
+      {{prefix + "geometry_as_geojson"}});
+  registerFunction<GeometryFromGeoJsonFunction, Geometry, Varchar>(
+      {{prefix + "geometry_from_geojson"}});
   registerFunction<
       GeometryNearestPointsFunction,
       Array<Geometry>,
