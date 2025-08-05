2e07aa508480e3443dd37f2198d48280655ddb62
beliefer <beliefer@163.com>
2025-08-05 15:58:12 +0800

    Add sudo while moving ccache

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -92,7 +92,7 @@ function install_build_prerequisites {
   # Install ccache
   curl -L https://github.com/ccache/ccache/releases/download/v"${CCACHE_VERSION}"/ccache-"${CCACHE_VERSION}"-darwin.tar.gz -o ccache.tar.gz
   tar -xf ccache.tar.gz
-  mv ccache-"${CCACHE_VERSION}"-darwin/ccache /usr/local/bin/
+  sudo mv ccache-"${CCACHE_VERSION}"-darwin/ccache /usr/local/bin/
   rm -rf ccache-"${CCACHE_VERSION}"-darwin ccache.tar.gz
 }
 
