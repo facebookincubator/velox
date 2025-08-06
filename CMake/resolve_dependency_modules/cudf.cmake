3ef27e25b636e17d9104cf7bf3d7f6262c91217b
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2025-08-06 13:32:12 -0500

    remove VELOX_CUDF_CXX_STANDARD changes

--- CMake/resolve_dependency_modules/cudf.cmake
+++ CMake/resolve_dependency_modules/cudf.cmake
@@ -45,23 +45,6 @@ set(VELOX_cudf_VERSION
     25.08
     CACHE STRING "cudf version")
 
-# cudf 25.08+ requires C++20
-if(VELOX_cudf_VERSION VERSION_GREATER_EQUAL "25.08")
-  set(VELOX_CUDF_CXX_STANDARD
-      20
-      CACHE STRING "C++ standard for cudf modules")
-  set(VELOX_CUDF_CXX_STANDARD_REQUIRED
-      ON
-      CACHE BOOL "C++ standard required for cudf modules")
-else()
-  set(VELOX_CUDF_CXX_STANDARD
-      ${CMAKE_CXX_STANDARD}
-      CACHE STRING "C++ standard for cudf modules")
-  set(VELOX_CUDF_CXX_STANDARD_REQUIRED
-      ON
-      CACHE BOOL "C++ standard required for cudf modules")
-endif()
-
 set(VELOX_cudf_BUILD_SHA256_CHECKSUM
     01d8bd30e8b953b97c71adb2bfc9d83be440b3df6f822f92af95a471ebf001da)
 set(VELOX_cudf_SOURCE_URL
@@ -110,11 +93,6 @@ FetchContent_MakeAvailable(cudf)
 target_compile_options(
   cudf PRIVATE -Wno-non-virtual-dtor -Wno-missing-field-initializers
                -Wno-deprecated-copy -Wno-restrict)
-
-set_target_properties(
-  cudf
-  PROPERTIES CXX_STANDARD ${VELOX_CUDF_CXX_STANDARD}
-             CXX_STANDARD_REQUIRED ${VELOX_CUDF_CXX_STANDARD_REQUIRED})
 
 unset(BUILD_SHARED_LIBS)
 endblock()
