efe2227796760b7868f88f1f9f1b13ed7198ad11
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-09-05 15:20:15 +0100

    align with LocalFileSystem extractPath

--- velox/experimental/cudf/connectors/parquet/ParquetConnectorSplit.cpp
+++ velox/experimental/cudf/connectors/parquet/ParquetConnectorSplit.cpp
@@ -22,7 +22,7 @@ namespace facebook::velox::cudf_velox::connector::parquet {
 
 namespace {
 std::string stripFilePrefix(const std::string& targetPath) {
-  const std::string prefix = "file://";
+  const std::string prefix = "file:";
   if (targetPath.rfind(prefix, 0) == 0) {
     return targetPath.substr(prefix.length());
   }
