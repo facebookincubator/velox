27304cc7134e5ff1040505802cb901e8af874635
Xiaoxuan Meng <xiaoxmeng@meta.com>
2025-09-23 23:36:24 -0700

    fix: Support clustered raw input n EbfAggregator to optimize the memory allocations (#14932)
    Summary:
    
    Add clustered raw input support and this can avoid a copy of encoded event. This change also optimize the memory allocations of result vector by allocating string buffer once and the memory allocation counters drop from 13317 to 2191. But we don't see much improvement in cpu time.
    
    Reviewed By: kostasxmeta
    
    Differential Revision: D83032862

--- velox/connectors/hive/HiveDataSink.cpp
+++ velox/connectors/hive/HiveDataSink.cpp
@@ -846,8 +846,9 @@ uint32_t HiveDataSink::appendWriter(const HiveWriterId& id) {
   writer = maybeCreateBucketSortWriter(std::move(writer));
   writers_.emplace_back(std::move(writer));
   addThreadLocalRuntimeStat(
-      std::string(dwio::common::toString(insertTableHandle_->storageFormat())) +
-          "WriterCount",
+      fmt::format(
+          "{}WriterCount",
+          dwio::common::toString(insertTableHandle_->storageFormat())),
       RuntimeCounter(1));
   // Extends the buffer used for partition rows calculations.
   partitionSizes_.emplace_back(0);
