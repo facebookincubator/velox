db238470bc4a8b1a884c28c24895f3618fac26ec
Sutou Kouhei <kou@clear-code.com>
2025-09-25 10:15:04 +0900

    Use newer FBThrift

--- scripts/setup-versions.sh
+++ scripts/setup-versions.sh
@@ -19,7 +19,7 @@
 # The versions should match the declared versions in this file.
 
 # Build dependencies versions.
-FB_OS_VERSION="v2025.04.28.00"
+FB_OS_VERSION="v2025.09.22.00"
 FMT_VERSION="10.1.1"
 BOOST_VERSION="boost-1.84.0"
 ARROW_VERSION="15.0.0"
