ca61346cddac68e3a0367d5f17ef6401c134965b
Deepak Majeti <deepak.majeti@ibm.com>
2025-09-26 15:41:33 -0400

    Add initialize from map support

--- velox/experimental/cudf/CudfConfig.h
+++ velox/experimental/cudf/CudfConfig.h
@@ -17,26 +17,44 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 namespace facebook::velox::cudf_velox {
 
 struct CudfConfig {
+  /// Keys used by the initialize() method.
+  static constexpr const char* kCudfEnabled{"cudf.enabled"};
+  static constexpr const char* kCudfDebugEnabled{"cudf.debug_enabled"};
+  static constexpr const char* kCudfMemoryResource{"cudf.memory_resource"};
+  static constexpr const char* kCudfMemoryPercent{"cudf.memory_percent"};
+  static constexpr const char* kCudfFunctionNamePrefix{
+      "cudf.function_name_prefix"};
+  static constexpr const char* kCudfForceReplace{"cudf.force_replace"};
+
   /// Singleton CudfConfig instance.
   /// Clients must set the configs below before invoking registerCudf().
   static CudfConfig& getInstance();
 
+  /// Initialize from a map with the above keys.
+  void initialize(std::unordered_map<std::string, std::string>&&);
+
   /// Enable cudf by default.
   /// Clients can disable here and enable it via the QueryConfig as well.
   bool enabled{true};
+
   /// Enable debug printing.
   bool debugEnabled{false};
+
   /// Memory resource for cuDF.
   /// Possible values are (cuda, pool, async, arena, managed, managed_pool).
   std::string memoryResource{"async"};
+
   /// The initial percent of GPU memory to allocate for memory resource.
-  int memoryPercent{50};
+  int32_t memoryPercent{50};
+
   /// Register all the functions with the functionNamePrefix.
   std::string functionNamePrefix;
+
   /// Force replacement of operators. Throws an error if a replacement fails.
   bool forceReplace{false};
 };
