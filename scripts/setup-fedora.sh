2e4f127d79f7be74ebf2d537230245a316402ade
Sutou Kouhei <kou@clear-code.com>
2025-10-15 16:19:11 +0900

    Use system fmt

--- scripts/setup-fedora.sh
+++ scripts/setup-fedora.sh
@@ -58,7 +58,7 @@ function install_build_prerequisites {
 function install_velox_deps_from_dnf {
   dnf_install \
     bison boost-devel c-ares-devel curl-devel double-conversion-devel \
-    elfutils-libelf-devel flex gflags-devel glog-devel gmock-devel \
+    elfutils-libelf-devel flex fmt-devel gflags-devel glog-devel gmock-devel \
     gtest-devel libdwarf-devel libevent-devel libicu-devel \
     libsodium-devel libzstd-devel lz4-devel openssl-devel-engine \
     re2-devel snappy-devel xxhash-devel zlib-devel grpc-devel grpc-plugins
