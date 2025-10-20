facebookincubator/velox

commit d93affa03c2a1d503a046ca6a3f23c08391a534c

build: Add 'abseil' as a build dependency for re2 (#14849)
Summary:
This script failed for me.  It seems that re2 has trouble building on Mac with Homebrew.  This fixes the problem.  Is there a better solution for this issue?

See below for the error message I encountered:

```
+ install_re2
+ wget_and_untar https://github.com/google/re2/archive/refs/tags/2024-07-02.tar.gz re2
+ local URL=https://github.com/google/re2/archive/refs/tags/2024-07-02.tar.gz
+ local DIR=re2
+ mkdir -p /Users/tdcmeehan/git/clones/dyncon/presto-native-execution/velox/deps-download
+ pushd /Users/tdcmeehan/git/clones/dyncon/presto-native-execution/velox/deps-download
~/git/clones/dyncon/presto-native-execution/velox/deps-download ~/git/clones/dyncon/presto-native-execution/velox
+ SUDO=
+ '[' -d re2 ']'
+ mkdir -p re2
+ pushd re2
~/git/clones/dyncon/presto-native-execution/velox/deps-download/re2 ~/git/clones/dyncon/presto-native-execution/velox/deps-download ~/git/clones/dyncon/presto-native-execution/velox
+ curl -L https://github.com/google/re2/archive/refs/tags/2024-07-02.tar.gz -o re2.tar.gz
  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                 Dload  Upload   Total   Spent    Left  Speed
  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
100  381k    0  381k    0     0   869k      0 --:--:-- --:--:-- --:--:--  869k
+ tar -xz --strip-components=1 --no-same-owner -f re2.tar.gz
+ popd
~/git/clones/dyncon/presto-native-execution/velox/deps-download ~/git/clones/dyncon/presto-native-execution/velox
+ popd
~/git/clones/dyncon/presto-native-execution/velox
+ cmake_install_dir re2 -DRE2_BUILD_TESTING=OFF
+ pushd /Users/tdcmeehan/git/clones/dyncon/presto-native-execution/velox/deps-download/re2
~/git/clones/dyncon/presto-native-execution/velox/deps-download/re2 ~/git/clones/dyncon/presto-native-execution/velox
+ shift
+ cmake_install -DRE2_BUILD_TESTING=OFF
+ local NAME
+++ pwd

scripts/setup-common.sh
@@ -125,6 +125,8 @@ function install_boost {
 }
 
 function install_protobuf {
+  install_abseil
+
   wget_and_untar https://github.com/protocolbuffers/protobuf/releases/download/v"${PROTOBUF_VERSION}"/protobuf-all-"${PROTOBUF_VERSION}".tar.gz protobuf
   cmake_install_dir protobuf -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_ABSL_PROVIDER=package
 }
@@ -139,9 +141,20 @@ function install_ranges_v3 {
   cmake_install_dir ranges_v3 -DRANGES_ENABLE_WERROR=OFF -DRANGE_V3_TESTS=OFF -DRANGE_V3_EXAMPLES=OFF
 }
 
+function install_abseil {
+  github_checkout abseil/abseil-cpp "${ABSEIL_VERSION}" --depth 1
+  cmake_install \
+    -DABSL_BUILD_TESTING=OFF \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DABSL_PROPAGATE_CXX_STD=ON \
+    -DABSL_ENABLE_INSTALL=ON
+}
+
 function install_re2 {
+  install_abseil
+
   wget_and_untar https://github.com/google/re2/archive/refs/tags/"${RE2_VERSION}".tar.gz re2
-  cmake_install_dir re2 -DRE2_BUILD_TESTING=OFF
+  cmake_install_dir re2 -DRE2_BUILD_TESTING=OFF -Dabsl_DIR="${INSTALL_PREFIX}/lib/cmake/absl"
 }
 
 function install_glog {
@@ -303,12 +316,7 @@ function install_gcs-sdk-cpp {
   # https://github.com/googleapis/google-cloud-cpp/blob/main/doc/packaging.md#required-libraries
 
   # abseil-cpp
-  github_checkout abseil/abseil-cpp "${ABSEIL_VERSION}" --depth 1
-  cmake_install \
-    -DABSL_BUILD_TESTING=OFF \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DABSL_PROPAGATE_CXX_STD=ON \
-    -DABSL_ENABLE_INSTALL=ON
+  install_abseil
 
   # protobuf
   github_checkout protocolbuffers/protobuf v"${PROTOBUF_VERSION}" --depth 1

commit a52307a549406b808aa80d74b929ef8b13eb64f1

build: Avoid staying in subdir of dependency after it has been installed (#15113)
Summary:
In https://github.com/apache/incubator-gluten/pull/10836, we encountered a Velox compilation issue caused by the changes introduced in https://github.com/facebookincubator/velox/pull/14849. The error occurred because ABSOLUTE_SCRIPTDIR was modified.

```
2025-10-09T03:42:02.6189401Z ++ realpath scripts
2025-10-09T03:42:02.6207867Z + ABSOLUTE_SCRIPTDIR=/__w/incubator-gluten/incubator-gluten/ep/build-velox/build/velox_ep/deps-download/abseil-cpp/scripts
2025-10-09T03:42:02.6210432Z + VELOX_ARROW_CMAKE_PATCH=/__w/incubator-gluten/incubator-gluten/ep/build-velox/build/velox_ep/deps-download/abseil-cpp/scripts/../CMake/resolve_dependency_modules/arrow/cmake-compatibility.patch
2025-10-09T03:42:02.6212114Z + cd /__w/incubator-gluten/incubator-gluten/ep/build-velox/build/velox_ep/deps-download/arrow
2025-10-09T03:42:02.6213956Z + git apply /__w/incubator-gluten/incubator-gluten/ep/build-velox/build/velox_ep/deps-download/abseil-cpp/scripts/../CMake/resolve_dependency_modules/arrow/cmake-compatibility.patch
2025-10-09T03:42:02.6230732Z error: can't open patch '/__w/incubator-gluten/incubator-gluten/ep/build-velox/build/velox_ep/deps-download/abseil-cpp/scripts/../CMake/resolve_dependency_modules/arrow/cmake-compatibility.patch': No such file or directory
2025-10-09T03:42:02.6235097Z + exit 1
```

Pull Request resolved: https://github.com/facebookincubator/velox/pull/15113

Reviewed By: bikramSingh91

Differential Revision: D85060996

Pulled By: pedroerp

fbshipit-source-id: 255eb7a82bc3d9ffb6c47e2a4c96066b4974823e

scripts/setup-common.sh
@@ -299,7 +299,7 @@ function install_aws_deps {
   local AWS_REPO_NAME="aws/aws-sdk-cpp"
 
   github_checkout $AWS_REPO_NAME "$AWS_SDK_VERSION" --depth 1 --recurse-submodules
-  cmake_install -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" -DBUILD_SHARED_LIBS:BOOL=OFF -DMINIMIZE_SIZE:BOOL=ON -DENABLE_TESTING:BOOL=OFF -DBUILD_ONLY:STRING="s3;identity-management"
+  cmake_install_dir aws-sdk-cpp -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" -DBUILD_SHARED_LIBS:BOOL=OFF -DMINIMIZE_SIZE:BOOL=ON -DENABLE_TESTING:BOOL=OFF -DBUILD_ONLY:STRING="s3;identity-management"
 }
 
 function install_minio {
@@ -329,13 +329,13 @@ function install_gcs-sdk-cpp {
 
   # protobuf
   github_checkout protocolbuffers/protobuf v"${PROTOBUF_VERSION}" --depth 1
-  cmake_install \
+  cmake_install_dir protobuf \
     -Dprotobuf_BUILD_TESTS=OFF \
     -Dprotobuf_ABSL_PROVIDER=package
 
   # grpc
   github_checkout grpc/grpc "${GRPC_VERSION}" --depth 1
-  cmake_install \
+  cmake_install_dir grpc \
     -DgRPC_BUILD_TESTS=OFF \
     -DgRPC_ABSL_PROVIDER=package \
     -DgRPC_ZLIB_PROVIDER=package \
@@ -347,19 +347,19 @@ function install_gcs-sdk-cpp {
 
   # crc32
   github_checkout google/crc32c "${CRC32_VERSION}" --depth 1
-  cmake_install \
+  cmake_install_dir crc32c \
     -DCRC32C_BUILD_TESTS=OFF \
     -DCRC32C_BUILD_BENCHMARKS=OFF \
     -DCRC32C_USE_GLOG=OFF
 
   # nlohmann json
   github_checkout nlohmann/json "${NLOHMAN_JSON_VERSION}" --depth 1
-  cmake_install \
+  cmake_install_dir json \
     -DJSON_BuildTests=OFF
 
   # google-cloud-cpp
   github_checkout googleapis/google-cloud-cpp "${GOOGLE_CLOUD_CPP_VERSION}" --depth 1
-  cmake_install \
+  cmake_install_dir google-cloud-cpp \
     -DGOOGLE_CLOUD_CPP_ENABLE_EXAMPLES=OFF \
     -DGOOGLE_CLOUD_CPP_ENABLE=storage
 }
@@ -370,6 +370,7 @@ function install_azure-storage-sdk-cpp {
   export AZURE_SDK_DISABLE_AUTO_VCPKG=ON
   vcpkg_commit_id=7a6f366cefd27210f6a8309aed10c31104436509
   github_checkout azure/azure-sdk-for-cpp azure-storage-files-datalake_"${AZURE_SDK_VERSION}"
+  pushd azure-sdk-for-cpp || exit
   sed -i='' "s/set(VCPKG_COMMIT_STRING .*)/set(VCPKG_COMMIT_STRING $vcpkg_commit_id)/" cmake-modules/AzureVcpkg.cmake
 
   azure_core_dir="sdk/core/azure-core"
@@ -406,6 +407,7 @@ function install_azure-storage-sdk-cpp {
     cd sdk/storage/azure-storage-files-datalake || exit
     cmake_install -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" -DBUILD_SHARED_LIBS=OFF
   )
+  popd || exit
 }
 
 function install_hdfs_deps {

scripts/setup-helper-functions.sh
@@ -49,6 +49,7 @@ function prompt {
     done
   ) 2>/dev/null
 }
+
 function github_checkout {
   local REPO=$1
   shift
@@ -69,7 +70,6 @@ function github_checkout {
   if [ ! -d "${DIRNAME}" ]; then
     git clone -q -b "$VERSION" "${GIT_CLONE_PARAMS[@]}" "https://github.com/${REPO}.git"
   fi
-  cd "${DIRNAME}" || exit
 }
 
 # get_cxx_flags [$CPU_ARCH]