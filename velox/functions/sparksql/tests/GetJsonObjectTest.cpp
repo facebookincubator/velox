52b3004b68257060c2ffa6e12c4634218d96a5b4
wecharyu <yuwq1996@gmail.com>
2025-10-22 16:21:36 +0800

    feat: Improve json path in Spark get_json_object

--- velox/functions/sparksql/tests/GetJsonObjectTest.cpp
+++ velox/functions/sparksql/tests/GetJsonObjectTest.cpp
@@ -69,6 +69,7 @@ TEST_F(GetJsonObjectTest, basic) {
   EXPECT_EQ(getJsonObject(R"({"a b": "1"})", "$. a b"), "1");
   EXPECT_EQ(getJsonObject(R"({"two spaces": "1"})", "$.  two spaces"), "1");
   EXPECT_EQ(getJsonObject(R"({"a": "1"})", "$.a "), std::nullopt);
+  EXPECT_EQ(getJsonObject(R"({"a ": "1"})", "$.a "), "1");
   EXPECT_EQ(getJsonObject(R"({"a b": "1"})", "$.a b "), std::nullopt);
   EXPECT_EQ(
       getJsonObject(R"({"two spaces": "1"})", "$.  two spaces "), std::nullopt);
@@ -77,6 +78,8 @@ TEST_F(GetJsonObjectTest, basic) {
       getJsonObject(R"({"my": {"hello": true}})", "$.  my.  hello"), "true");
   EXPECT_EQ(
       getJsonObject(R"({"my": {"hello": true}})", "$.my.  hello"), "true");
+  EXPECT_EQ(getJsonObject(R"({"a$ ": {"b": 10}})", "$.a$ .b"), "10");
+  EXPECT_EQ(getJsonObject(R"({"a$. b": {"c": 10}})", "$.a$. b"), std::nullopt);
   // Json object as result.
   EXPECT_EQ(
       getJsonObject(
