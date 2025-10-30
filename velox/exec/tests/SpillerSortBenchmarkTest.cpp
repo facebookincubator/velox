e4bb2d62830eddf7b328566f1324ca9dbe38a628
NEUpanning <emmning@163.com>
2025-10-30 11:04:54 +0800

    benchmark changes

--- velox/exec/tests/SpillerSortBenchmarkTest.cpp
+++ velox/exec/tests/SpillerSortBenchmarkTest.cpp
@@ -34,67 +34,67 @@ void runTest(RowTypePtr rowType, bool serializeRowContainer, int strLength) {
 }
 
 BENCHMARK(integer_type) {
-  runTest(ROW({"c0"}, {INTEGER()}), false, 0);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), INTEGER()}), false, 0);
 }
 
 BENCHMARK_RELATIVE(integer_type_serialize_rows) {
-  runTest(ROW({"c0"}, {INTEGER()}), true, 0);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), INTEGER()}), true, 0);
 }
 
 BENCHMARK(bigint_type) {
-  runTest(ROW({"c0"}, {BIGINT()}), false, 0);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), BIGINT()}), false, 0);
 }
 
 BENCHMARK_RELATIVE(bigint_type_serialize_rows) {
-  runTest(ROW({"c0"}, {BIGINT()}), true, 0);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), BIGINT()}), true, 0);
 }
 
 BENCHMARK(string_type_10bytes) {
-  runTest(ROW({"c0"}, {VARBINARY()}), false, 10);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), VARBINARY()}), false, 10);
 }
 
 BENCHMARK_RELATIVE(string_type_10bytes_serialize_rows) {
-  runTest(ROW({"c0"}, {VARBINARY()}), true, 10);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), VARBINARY()}), true, 10);
 }
 
 BENCHMARK(string_type_50bytes) {
-  runTest(ROW({"c0"}, {VARBINARY()}), false, 50);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), VARBINARY()}), false, 50);
 }
 
 BENCHMARK_RELATIVE(string_type_50bytes_long_serialize_rows) {
-  runTest(ROW({"c0"}, {VARBINARY()}), true, 50);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), VARBINARY()}), true, 50);
 }
 
 BENCHMARK(array_of_integer_type) {
-  runTest(ROW({"c0"}, {ARRAY(INTEGER())}), false, 10);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(INTEGER())}), false, 10);
 }
 
 BENCHMARK_RELATIVE(array_of_integer_type_serialize_rows) {
-  runTest(ROW({"c0"}, {ARRAY(INTEGER())}), true, 10);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(INTEGER())}), true, 10);
 }
 
 BENCHMARK(array_of_10bytes_varbinary_type) {
-  runTest(ROW({"c0"}, {ARRAY(VARBINARY())}), false, 10);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(VARBINARY())}), false, 10);
 }
 
 BENCHMARK_RELATIVE(array_of_10bytes_varbinary_type_serialize_rows) {
-  runTest(ROW({"c0"}, {ARRAY(VARBINARY())}), true, 10);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(VARBINARY())}), true, 10);
 }
 
 BENCHMARK(array_of_50bytes_varbinary_type) {
-  runTest(ROW({"c0"}, {ARRAY(VARBINARY())}), false, 50);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(VARBINARY())}), false, 50);
 }
 
 BENCHMARK_RELATIVE(array_of_50bytes_varbinary_type_serialize_rows) {
-  runTest(ROW({"c0"}, {ARRAY(VARBINARY())}), true, 50);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(VARBINARY())}), true, 50);
 }
 
 BENCHMARK(array_of_100bytes_varbinary_type) {
-  runTest(ROW({"c0"}, {ARRAY(VARBINARY())}), false, 100);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(VARBINARY())}), false, 100);
 }
 
 BENCHMARK_RELATIVE(array_of_100bytes_varbinary_type_serialize_rows) {
-  runTest(ROW({"c0"}, {ARRAY(VARBINARY())}), true, 100);
+  runTest(ROW({"c0", "c1"}, {INTEGER(), ARRAY(VARBINARY())}), true, 100);
 }
 
 int main(int argc, char* argv[]) {
