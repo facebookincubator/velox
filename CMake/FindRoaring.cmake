830d7ba93d040a76c88a599aab75859b47505aa0
Hongze Zhang <hongze.zzz123@gmail.com>
2025-11-11 19:08:46 +0100

    Use pkg_check_modules for system roaring

--- CMake/FindRoaring.cmake
+++ CMake/FindRoaring.cmake
@@ -14,36 +14,17 @@
 
 find_package(PkgConfig REQUIRED)
 
-pkg_search_module(Roaring_PC QUIET roaring)
+pkg_check_modules(Roaring QUIET IMPORTED_TARGET roaring)
 
-find_library(Roaring_LIBRARY NAMES roaring HINTS ${Roaring_PC_LIBDIR} ${Roaring_PC_LIBRARY_DIRS})
+if(Roaring_FOUND)
+  add_library(Roaring::roaring INTERFACE IMPORTED)
+  set_target_properties(Roaring::roaring PROPERTIES INTERFACE_LINK_LIBRARIES PkgConfig::Roaring)
+  message(STATUS "Found Roaring via pkg-config.")
+  return()
+endif()
 
-find_path(
-  Roaring_INCLUDE_DIR
-  NAMES "roaring/roaring.h"
-  HINTS ${Roaring_PC_INCLUDEDIR}/include ${Roaring_PC_INCLUDE_DIRS}
-)
-
-mark_as_advanced(Roaring_LIBRARY Roaring_INCLUDE_DIR)
-
-include(FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args(
-  Roaring
-  REQUIRED_VARS Roaring_LIBRARY Roaring_INCLUDE_DIR
-  VERSION_VAR Roaring_PC_VERSION
-)
-
-set(Roaring_LIBRARIES ${Roaring_LIBRARY})
-set(Roaring_INCLUDE_DIRS ${Roaring_INCLUDE_DIR})
-
-if(Roaring_FOUND AND NOT (TARGET Roaring::roaring))
-  add_library(Roaring::roaring UNKNOWN IMPORTED)
-
-  set_target_properties(
-    Roaring::roaring
-    PROPERTIES
-      IMPORTED_LOCATION ${Roaring_LIBRARY}
-      INTERFACE_INCLUDE_DIRECTORIES ${Roaring_INCLUDE_DIRS}
-  )
+if(Roaring_FIND_REQUIRED)
+  message(FATAL_ERROR "Failed to find Roaring.")
+elseif(NOT re2_FIND_QUIETLY)
+  message(WARNING "Failed to find Roaring.")
 endif()
