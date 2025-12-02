a58e69cad716fd8065c823fe82e04103e224014a
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2025-12-02 18:06:58 +0000

    Add unit tests

--- velox/experimental/cudf/tests/TableScanTest.cpp
+++ velox/experimental/cudf/tests/TableScanTest.cpp
@@ -283,6 +283,67 @@ TEST_F(TableScanTest, allColumnsUsingFileDataSource) {
   // ASSERT_LT(0, it->second.customStats.at("ioWaitWallNanos").sum);
 }
 
+TEST_F(TableScanTest, allColumnsUsingExperimentalReader) {
+  auto vectors = makeVectors(10, 1'000);
+  auto filePath = TempFilePath::create();
+  writeToFile(filePath->getPath(), vectors, "c");
+
+  createDuckDbTable(vectors);
+  const std::string duckDbSql = "SELECT * FROM tmp";
+
+  auto splits = makeCudfHiveConnectorSplits({filePath});
+
+  // Helper to test scan all columns for the given splits
+  auto testScanAllColumnsUsingExperimentalReader =
+      [&](const core::PlanNodePtr& plan) {
+        auto task = AssertQueryBuilder(duckDbQueryRunner_)
+                        .plan(plan)
+                        .splits(splits)
+                        .assertResults(duckDbSql);
+
+        // A quick sanity check for memory usage reporting. Check that peak
+        // total memory usage for the project node is > 0.
+        auto planStats = toPlanStats(task->taskStats());
+        auto scanNodeId = plan->id();
+        auto it = planStats.find(scanNodeId);
+        ASSERT_TRUE(it != planStats.end());
+        // TODO (dm): enable this test once we start to track gpu memory
+        // ASSERT_TRUE(it->second.peakMemoryBytes > 0);
+
+        //  Verifies there is no dynamic filter stats.
+        ASSERT_TRUE(it->second.dynamicFilterStats.empty());
+
+        // TODO: We are not writing any customStats yet so disable this check
+        // ASSERT_LT(0, it->second.customStats.at("ioWaitWallNanos").sum);
+      };
+
+  // Reset the CudfHiveConnector config to use the experimental reader
+  auto config = std::unordered_map<std::string, std::string>{
+      {facebook::velox::cudf_velox::connector::hive::CudfHiveConfig::
+           kUseExperimentalCudfReader,
+       "true"}};
+  resetCudfHiveConnector(
+      std::make_shared<config::ConfigBase>(std::move(config)));
+
+  // Test scan all columns with CudfHiveConnectorSplits
+  {
+    auto plan = tableScanNode();
+    testScanAllColumnsUsingExperimentalReader(plan);
+  }
+
+  // Test scan all columns with CudfHiveConnectorSplits
+  {
+    config.insert(
+        {facebook::velox::cudf_velox::connector::hive::CudfHiveConfig::
+             kUseBufferedInput,
+         "false"});
+    resetCudfHiveConnector(
+        std::make_shared<config::ConfigBase>(std::move(config)));
+    auto plan = tableScanNode();
+    testScanAllColumnsUsingExperimentalReader(plan);
+  }
+}
+
 TEST_F(TableScanTest, directBufferInputRawInputBytes) {
   constexpr int kSize = 10;
   auto vector = makeRowVector({
