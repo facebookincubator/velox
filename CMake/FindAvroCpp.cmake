b27e1623a73f9eb6f7f3cc7d2433adc6c32382a1
Chen Zhang <izchen@126.com>
2025-12-18 14:45:21 +0800

    fix for CI

--- CMake/FindAvroCpp.cmake
+++ CMake/FindAvroCpp.cmake
@@ -19,31 +19,26 @@ find_path(AVROCPP_INCLUDE_DIRS NAMES avro/DataFile.hh)
 find_library(AVROCPP_SHARED_LIB NAMES avrocpp)
 find_library(AVROCPP_STATIC_LIB NAMES avrocpp_s)
 
-if (AVROCPP_SHARED_LIB)
+if(AVROCPP_SHARED_LIB)
   set(AVROCPP_LIBRARIES "${AVROCPP_SHARED_LIB}")
   set(_AVROCPP_LIBTYPE SHARED)
-elseif (AVROCPP_STATIC_LIB)
+elseif(AVROCPP_STATIC_LIB)
   set(AVROCPP_LIBRARIES "${AVROCPP_STATIC_LIB}")
   set(_AVROCPP_LIBTYPE STATIC)
-endif ()
+endif()
 
-find_package_handle_standard_args(AvroCpp
-    REQUIRED_VARS AVROCPP_INCLUDE_DIRS AVROCPP_LIBRARIES
-)
+find_package_handle_standard_args(AvroCpp REQUIRED_VARS AVROCPP_INCLUDE_DIRS AVROCPP_LIBRARIES)
 
-if (AvroCpp_FOUND)
-  if (NOT TARGET AvroCpp::avrocpp)
+if(AvroCpp_FOUND)
+  if(NOT TARGET AvroCpp::avrocpp)
     add_library(AvroCpp::avrocpp ${_AVROCPP_LIBTYPE} IMPORTED)
-    set_target_properties(AvroCpp::avrocpp PROPERTIES
+    set_target_properties(
+      AvroCpp::avrocpp
+      PROPERTIES
         IMPORTED_LOCATION "${AVROCPP_LIBRARIES}"
         INTERFACE_INCLUDE_DIRECTORIES "${AVROCPP_INCLUDE_DIRS}"
     )
-  endif ()
-endif ()
+  endif()
+endif()
 
-mark_as_advanced(
-    AVROCPP_INCLUDE_DIRS
-    AVROCPP_SHARED_LIB
-    AVROCPP_STATIC_LIB
-    AVROCPP_LIBRARIES
-)
+mark_as_advanced(AVROCPP_INCLUDE_DIRS AVROCPP_SHARED_LIB AVROCPP_STATIC_LIB AVROCPP_LIBRARIES)
