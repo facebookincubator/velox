25c334f8ae295791b1bbeba80ceef9b78713e238
Xiaoxuan Meng <xiaoxmeng@meta.com>
2025-12-28 17:38:35 -0800

    refactor: Move KeyEncoder from velox/exec to velox/serializers (#15867)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/15867
    
    Move KeyEncoder library and tests to velox/serializers to enable access from nimble OSS build, which uses VELOX_BUILD_MINIMAL_WITH_DWIO and doesn't include velox_exec dependencies.
    
    Reviewed By: gggrace14
    
    Differential Revision: D89872199

--- velox/serializers/tests/KeyEncoderTest.cpp
+++ velox/serializers/tests/KeyEncoderTest.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "velox/exec/KeyEncoder.h"
+#include "velox/serializers/KeyEncoder.h"
 
 #include <fmt/ranges.h>
 #include <folly/init/Init.h>
