73b738e33410d8c4bfa6e08d2f3361565a161de3
James Gill <jagill@meta.com>
2025-12-30 12:17:45 -0800

    refactor(geo): Reduce memcpy in GeometryCollection serialization (#15873)
    Summary:
    
    Writing a GeometryCollection requires writing the length of the child geometry
    before writing the child geometry. Previously we did this by writing to a
    temporary std::string, then writing the length of that string to the output and
    then copying the std::string to the output. Recursive GeometryCollections made
    this worse because the "output" was actually just the parent std::string.
    
    This commit writes a placeholder size (0), recording the offset. It then writes
    the geometry, then writes the resulting length at the offset. This prevents all
    the memcpys.
    
    Removing this greatly simplifies our code an allows us to remove
    VarbinaryWriter and almost all templating, which futher allows us to move most
    implementations into the cpp file.
    
    Differential Revision: D89928986

--- velox/common/geospatial/GeometrySerde.h
+++ velox/common/geospatial/GeometrySerde.h
@@ -24,375 +24,103 @@
 #include "velox/common/base/IOUtils.h"
 #include "velox/common/geospatial/GeometryConstants.h"
 #include "velox/expression/ComplexViewTypes.h"
+#include "velox/expression/StringWriter.h"
 #include "velox/type/StringView.h"
 
 namespace facebook::velox::common::geospatial {
-
-/**
- * VarbinaryWriter is a utility for serializing raw binary data to a
- * generic writer interface. It supports writing either raw byte arrays
- * or trivially copyable types.
- *
- * @tparam StringWriter A type that provides an `append(std::string_view)`
- * method, used to consume the binary output. Examples include `std::string` or
- *         `core::StringWriter`.
- */
-template <typename StringWriter>
-class VarbinaryWriter {
- public:
-  /* implicit */ VarbinaryWriter(StringWriter& stringWriter)
-      : stringWriter_(stringWriter) {}
-  VarbinaryWriter() = delete;
-
-  void write(const char* data, size_t size) {
-    stringWriter_.append(std::string_view(data, size));
-  }
-
-  template <typename T>
-  void write(const T& value) {
-    static_assert(
-        std::is_trivially_copyable_v<T>, "T must be trivially copyable");
-    stringWriter_.append(
-        std::string_view(reinterpret_cast<const char*>(&value), sizeof(T)));
-  }
-
- private:
-  StringWriter& stringWriter_;
-};
 
 class GeometrySerializer {
  public:
   /// Serialize geometry into Velox's internal format.  Do not call this within
   /// GEOS_TRY macro: it will catch the exceptions that need to bubble up.
-  template <typename StringWriter>
   static void serialize(
       const geos::geom::Geometry& geometry,
-      StringWriter& stringWriter) {
-    VarbinaryWriter writer(stringWriter);
+      exec::StringWriter& writer) {
     writeGeometry(geometry, writer);
   }
 
-  template <typename StringWriter>
   static void serializeEnvelope(
       double xMin,
       double yMin,
       double xMax,
       double yMax,
-      StringWriter& stringWriter) {
-    VarbinaryWriter writer(stringWriter);
-    writer.write(static_cast<uint8_t>(GeometrySerializationType::ENVELOPE));
-    writer.write(xMin);
-    writer.write(yMin);
-    writer.write(xMax);
-    writer.write(yMax);
-  }
+      exec::StringWriter& writer);
 
-  template <typename StringWriter>
   static void serializeEnvelope(
       geos::geom::Envelope& envelope,
-      StringWriter& stringWriter) {
-    if (FOLLY_UNLIKELY(envelope.isNull())) {
-      serializeEnvelope(
-          std::numeric_limits<double>::quiet_NaN(),
-          std::numeric_limits<double>::quiet_NaN(),
-          std::numeric_limits<double>::quiet_NaN(),
-          std::numeric_limits<double>::quiet_NaN(),
-          stringWriter);
-    } else {
-      serializeEnvelope(
-          envelope.getMinX(),
-          envelope.getMinY(),
-          envelope.getMaxX(),
-          envelope.getMaxY(),
-          stringWriter);
-    }
-  }
+      exec::StringWriter& writer);
 
   /// Determines if a ring of coordinates (from `start` to `end`) is oriented
   /// clockwise.
-  FOLLY_ALWAYS_INLINE static bool isClockwise(
+  static bool isClockwise(
       const std::unique_ptr<geos::geom::CoordinateSequence>& coordinates,
       size_t start,
-      size_t end) {
-    double sum = 0.0;
-    for (size_t i = start; i < end - 1; i++) {
-      const auto& p1 = coordinates->getAt(i);
-      const auto& p2 = coordinates->getAt(i + 1);
-      sum += (p2.x - p1.x) * (p2.y + p1.y);
-    }
-    return sum > 0.0;
+      size_t end);
+
+ private:
+  /// Append a trivially copyable value to the writer as a binary string.
+  template <typename T>
+  static void appendBytes(exec::StringWriter& writer, const T& value) {
+    static_assert(
+        std::is_trivially_copyable_v<T>, "T must be trivially copyable");
+    writer.append(
+        std::string_view(reinterpret_cast<const char*>(&value), sizeof(T)));
   }
 
   /// Reverses the order of coordinates in the sequence between `start` and
   /// `end`
-  FOLLY_ALWAYS_INLINE static void reverse(
+  static void reverse(
       const std::unique_ptr<geos::geom::CoordinateSequence>& coordinates,
       size_t start,
-      size_t end) {
-    for (size_t i = 0; i < (end - start) / 2; ++i) {
-      auto temp = coordinates->getAt(start + i);
-      coordinates->setAt(coordinates->getAt(end - 1 - i), start + i);
-      coordinates->setAt(temp, end - 1 - i);
-    }
-  }
+      size_t end);
 
   /// Ensures that a polygon ring has the canonical orientation:
   /// - Exterior rings (shells) must be clockwise.
   /// - Interior rings (holes) must be counter-clockwise.
-  FOLLY_ALWAYS_INLINE static void canonicalizePolygonCoordinates(
+  static void canonicalizePolygonCoordinates(
       const std::unique_ptr<geos::geom::CoordinateSequence>& coordinates,
       size_t start,
       size_t end,
-      bool isShell) {
-    bool isClockwiseFlag = isClockwise(coordinates, start, end);
-
-    if ((isShell && !isClockwiseFlag) || (!isShell && isClockwiseFlag)) {
-      reverse(coordinates, start, end);
-    }
-  }
+      bool isShell);
 
   /// Applies `canonicalizePolygonCoordinates` to all rings in a polygon.
-  FOLLY_ALWAYS_INLINE static void canonicalizePolygonCoordinates(
+  static void canonicalizePolygonCoordinates(
       const std::unique_ptr<geos::geom::CoordinateSequence>& coordinates,
       const std::vector<size_t>& partIndexes,
-      const std::vector<bool>& shellPart) {
-    for (size_t part = 0; part < partIndexes.size() - 1; part++) {
-      canonicalizePolygonCoordinates(
-          coordinates,
-          partIndexes[part],
-          partIndexes[part + 1],
-          shellPart[part]);
-    }
-    if (!partIndexes.empty()) {
-      canonicalizePolygonCoordinates(
-          coordinates,
-          partIndexes.back(),
-          coordinates->size(),
-          shellPart.back());
-    }
-  }
-
- private:
-  template <typename T>
+      const std::vector<bool>& shellPart);
   static void writeGeometry(
       const geos::geom::Geometry& geometry,
-      VarbinaryWriter<T>& writer) {
-    auto geometryType = geometry.getGeometryTypeId();
-    switch (geometryType) {
-      case geos::geom::GEOS_POINT:
-        writePoint(geometry, writer);
-        break;
-      case geos::geom::GEOS_MULTIPOINT:
-        writeMultiPoint(geometry, writer);
-        break;
-      case geos::geom::GEOS_LINESTRING:
-      case geos::geom::GEOS_LINEARRING:
-        writePolyline(geometry, writer, false);
-        break;
-      case geos::geom::GEOS_MULTILINESTRING:
-        writePolyline(geometry, writer, true);
-        break;
-      case geos::geom::GEOS_POLYGON:
-        writePolygon(geometry, writer, false);
-        break;
-      case geos::geom::GEOS_MULTIPOLYGON:
-        writePolygon(geometry, writer, true);
-        break;
-      case geos::geom::GEOS_GEOMETRYCOLLECTION:
-        writeGeometryCollection(geometry, writer);
-        break;
-      default:
-        VELOX_FAIL(
-            "Unrecognized geometry type: {}",
-            static_cast<uint32_t>(geometryType));
-        break;
-    }
-  }
+      exec::StringWriter& writer);
 
-  template <typename T>
   static void writeEnvelope(
       const geos::geom::Geometry& geometry,
-      VarbinaryWriter<T>& writer) {
-    if (geometry.isEmpty()) {
-      writer.write(std::numeric_limits<double>::quiet_NaN());
-      writer.write(std::numeric_limits<double>::quiet_NaN());
-      writer.write(std::numeric_limits<double>::quiet_NaN());
-      writer.write(std::numeric_limits<double>::quiet_NaN());
-      return;
-    }
+      exec::StringWriter& writer);
 
-    auto envelope = geometry.getEnvelopeInternal();
-    writer.write(envelope->getMinX());
-    writer.write(envelope->getMinY());
-    writer.write(envelope->getMaxX());
-    writer.write(envelope->getMaxY());
-  }
-
-  template <typename T>
   static void writeCoordinates(
       const std::unique_ptr<geos::geom::CoordinateSequence>& coords,
-      VarbinaryWriter<T>& writer) {
-    for (size_t i = 0; i < coords->size(); ++i) {
-      writer.write(coords->getX(i));
-      writer.write(coords->getY(i));
-    }
-  }
+      exec::StringWriter& writer);
 
-  template <typename T>
   static void writePoint(
       const geos::geom::Geometry& point,
-      VarbinaryWriter<T>& writer) {
-    writer.write(static_cast<uint8_t>(GeometrySerializationType::POINT));
-    if (!point.isEmpty()) {
-      writeCoordinates(point.getCoordinates(), writer);
-    } else {
-      writer.write(std::numeric_limits<double>::quiet_NaN());
-      writer.write(std::numeric_limits<double>::quiet_NaN());
-    }
-  }
+      exec::StringWriter& writer);
 
-  template <typename T>
   static void writeMultiPoint(
       const geos::geom::Geometry& geometry,
-      VarbinaryWriter<T>& writer) {
-    writer.write(static_cast<uint8_t>(GeometrySerializationType::MULTI_POINT));
-    writer.write(static_cast<int32_t>(EsriShapeType::MULTI_POINT));
-    writeEnvelope(geometry, writer);
-    writer.write(static_cast<int32_t>(geometry.getNumPoints()));
-    writeCoordinates(geometry.getCoordinates(), writer);
-  }
+      exec::StringWriter& writer);
 
-  template <typename T>
   static void writePolyline(
       const geos::geom::Geometry& geometry,
-      VarbinaryWriter<T>& writer,
-      bool multiType) {
-    size_t numParts;
-    size_t numPoints = geometry.getNumPoints();
+      exec::StringWriter& writer,
+      bool multiType);
 
-    if (multiType) {
-      numParts = geometry.getNumGeometries();
-      writer.write(
-          static_cast<uint8_t>(GeometrySerializationType::MULTI_LINE_STRING));
-    } else {
-      numParts = (numPoints > 0) ? 1 : 0;
-      writer.write(
-          static_cast<uint8_t>(GeometrySerializationType::LINE_STRING));
-    }
-
-    writer.write(static_cast<int32_t>(EsriShapeType::POLYLINE));
-
-    writeEnvelope(geometry, writer);
-
-    writer.write(static_cast<int32_t>(numParts));
-    writer.write(static_cast<int32_t>(numPoints));
-
-    size_t partIndex = 0;
-    for (size_t geomIdx = 0; geomIdx < numParts; ++geomIdx) {
-      writer.write(static_cast<int32_t>(partIndex));
-      partIndex += geometry.getGeometryN(geomIdx)->getNumPoints();
-    }
-
-    if (multiType) {
-      for (size_t partIdx = 0; partIdx < numParts; ++partIdx) {
-        const auto* part = geometry.getGeometryN(partIdx);
-        writeCoordinates(part->getCoordinates(), writer);
-      }
-    } else {
-      writeCoordinates(geometry.getCoordinates(), writer);
-    }
-  }
-
-  template <typename T>
   static void writePolygon(
       const geos::geom::Geometry& geometry,
-      VarbinaryWriter<T>& writer,
-      bool multiType) {
-    size_t numGeometries = geometry.getNumGeometries();
-    size_t numParts = 0;
-    size_t numPoints = geometry.getNumPoints();
+      exec::StringWriter& writer,
+      bool multiType);
 
-    for (size_t geomIdx = 0; geomIdx < numGeometries; geomIdx++) {
-      auto polygon = dynamic_cast<const geos::geom::Polygon*>(
-          geometry.getGeometryN(geomIdx));
-      if (polygon && polygon->getNumPoints() > 0) {
-        numParts += polygon->getNumInteriorRing() + 1;
-      }
-    }
-
-    if (multiType) {
-      writer.write(
-          static_cast<uint8_t>(GeometrySerializationType::MULTI_POLYGON));
-    } else {
-      writer.write(static_cast<uint8_t>(GeometrySerializationType::POLYGON));
-    }
-
-    writer.write(static_cast<int32_t>(EsriShapeType::POLYGON));
-    writeEnvelope(geometry, writer);
-
-    writer.write(static_cast<int32_t>(numParts));
-    writer.write(static_cast<int32_t>(numPoints));
-
-    if (numParts == 0) {
-      return;
-    }
-
-    std::vector<size_t> partIndexes(numParts);
-    std::vector<bool> shellPart(numParts);
-
-    size_t currentPart = 0;
-    size_t currentPoint = 0;
-    for (size_t geomIdx = 0; geomIdx < numGeometries; geomIdx++) {
-      const geos::geom::Polygon* polygon =
-          dynamic_cast<const geos::geom::Polygon*>(
-              geometry.getGeometryN(geomIdx));
-
-      partIndexes[currentPart] = currentPoint;
-      shellPart[currentPart] = true;
-      currentPart++;
-      currentPoint += polygon->getExteriorRing()->getNumPoints();
-
-      size_t holesCount = polygon->getNumInteriorRing();
-      for (size_t holeIndex = 0; holeIndex < holesCount; holeIndex++) {
-        partIndexes[currentPart] = currentPoint;
-        shellPart[currentPart] = false;
-        currentPart++;
-        currentPoint += polygon->getInteriorRingN(holeIndex)->getNumPoints();
-      }
-    }
-
-    for (size_t partIndex : partIndexes) {
-      writer.write(static_cast<int32_t>(partIndex));
-    }
-
-    auto coordinates = geometry.getCoordinates();
-    canonicalizePolygonCoordinates(coordinates, partIndexes, shellPart);
-    writeCoordinates(coordinates, writer);
-  }
-
-  template <typename T>
   static void writeGeometryCollection(
       const geos::geom::Geometry& collection,
-      VarbinaryWriter<T>& writer) {
-    writer.write(
-        static_cast<uint8_t>(GeometrySerializationType::GEOMETRY_COLLECTION));
-
-    for (size_t geometryIndex = 0;
-         geometryIndex < collection.getNumGeometries();
-         ++geometryIndex) {
-      auto* geometry = collection.getGeometryN(geometryIndex);
-      // Use a temporary buffer to serialize the geometry and calculate its
-      // length
-      std::string tempBuffer;
-      VarbinaryWriter tempOutput(tempBuffer);
-
-      writeGeometry(*geometry, tempOutput);
-
-      int32_t length = static_cast<int32_t>(tempBuffer.size());
-      writer.write(length);
-      writer.write(tempBuffer.data(), tempBuffer.size());
-    }
-  }
+      exec::StringWriter& writer);
 };
 
 class GeometryDeserializer {
