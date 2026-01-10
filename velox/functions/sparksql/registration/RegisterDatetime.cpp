61428f97ce0b44f610506f6c08f9a530d794c2bf
Kent Yao <kentyao@microsoft.com>
2026-01-10 19:53:26 +0800

    update order

--- velox/functions/sparksql/registration/RegisterDatetime.cpp
+++ velox/functions/sparksql/registration/RegisterDatetime.cpp
@@ -65,10 +65,10 @@ void registerDatetimeFunctions(const std::string& prefix) {
   registerFunction<DateSubFunction, Date, Date, int32_t>({prefix + "date_sub"});
   registerFunction<DayFunction, int32_t, Date>(
       {prefix + "day", prefix + "dayofmonth"});
+  registerFunction<DayNameFunction, Varchar, Date>({prefix + "dayname"});
   registerFunction<DayOfYearFunction, int32_t, Date>({prefix + "dayofyear"});
   registerFunction<DayOfWeekFunction, int32_t, Date>({prefix + "dayofweek"});
   registerFunction<WeekdayFunction, int32_t, Date>({prefix + "weekday"});
-  registerFunction<DayNameFunction, Varchar, Date>({prefix + "dayname"});
   registerFunction<QuarterFunction, int32_t, Date>({prefix + "quarter"});
   registerFunction<MonthFunction, int32_t, Date>({prefix + "month"});
   registerFunction<NextDayFunction, Date, Date, Varchar>({prefix + "next_day"});
