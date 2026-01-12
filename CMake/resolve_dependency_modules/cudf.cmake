8e55649c25796e9ee28d2862dd4defc2dadca845
Bradley Dice <bdice@bradleydice.com>
2026-01-12 13:21:51 -0600

    Update and pin cuDF dependencies

--- CMake/resolve_dependency_modules/cudf.cmake
+++ CMake/resolve_dependency_modules/cudf.cmake
@@ -17,18 +17,50 @@ include_guard(GLOBAL)
 # 3.30.4 is the minimum version required by cudf
 cmake_minimum_required(VERSION 3.30.4)
 
-# Add velox_resolve_dependency_url here for rapids-cmake, rmm, and kvikio if a specific version or commit is needed.
+# rapids_cmake commit a9d2afb from 2026-01-05
+set(VELOX_rapids_cmake_VERSION 26.02)
+set(VELOX_rapids_cmake_COMMIT a9d2afb645aaed0ae9bff0b074613a1f09081416)
+set(
+  VELOX_rapids_cmake_BUILD_SHA256_CHECKSUM
+  955784e2c752fbf1d7110734ccef79c1949dbee664be70e2fad275b11d881318
+)
+set(
+  VELOX_rapids_cmake_SOURCE_URL
+  "https://github.com/rapidsai/rapids-cmake/archive/${VELOX_rapids_cmake_COMMIT}.tar.gz"
+)
+velox_resolve_dependency_url(rapids_cmake)
 
-set(VELOX_cudf_VERSION 25.12 CACHE STRING "cudf version")
+# rmm commit aac8e28 from 2026-01-12
+set(VELOX_rmm_VERSION 26.02)
+set(VELOX_rmm_COMMIT aac8e28364d8bd0d5d37b0b1d3a9b4c9167795f2)
+set(
+  VELOX_rmm_BUILD_SHA256_CHECKSUM
+  7d0b164699b733ba15e96e8923a096a4ea8b48ae0bf7d1cad99d2d059e932b41
+)
+set(VELOX_rmm_SOURCE_URL "https://github.com/rapidsai/rmm/archive/${VELOX_rmm_COMMIT}.tar.gz")
+velox_resolve_dependency_url(rmm)
 
+# kvikio commit 1141512 from 2026-01-05
+set(VELOX_kvikio_VERSION 26.02)
+set(VELOX_kvikio_COMMIT 11415120c7e019c3fcf2ec2201d6e03502cbc645)
+set(
+  VELOX_kvikio_BUILD_SHA256_CHECKSUM
+  5c333da29eceda6ed056f8dd8f198234cc1717dd6548d05fbd5aae1dcd8678e0
+)
+set(
+  VELOX_kvikio_SOURCE_URL
+  "https://github.com/rapidsai/kvikio/archive/${VELOX_kvikio_COMMIT}.tar.gz"
+)
+velox_resolve_dependency_url(kvikio)
+
+# cudf commit 6b9810a from 2026-01-12
+set(VELOX_cudf_VERSION 26.02 CACHE STRING "cudf version")
+set(VELOX_cudf_COMMIT 6b9810ae9dae172363ab185b70a2ed678ee15309)
 set(
   VELOX_cudf_BUILD_SHA256_CHECKSUM
-  4ec101a368e1423a1a3831a121f0d24e2d91ed044aed68faee0ba18fda38b450
+  5aee7f74d13f26424f7a4a1d6cbf0f805b1f911038e92e459dfc6373ad34bdcc
 )
-set(
-  VELOX_cudf_SOURCE_URL
-  "https://github.com/rapidsai/cudf/archive/181bd7b85c614e9c8f755a62f07f4b9c9334b615.tar.gz"
-)
+set(VELOX_cudf_SOURCE_URL "https://github.com/rapidsai/cudf/archive/${VELOX_cudf_COMMIT}.tar.gz")
 velox_resolve_dependency_url(cudf)
 
 # Use block so we don't leak variables
@@ -38,7 +70,30 @@ block(SCOPE_FOR VARIABLES)
   set(CUDF_BUILD_TESTUTIL OFF)
   set(BUILD_SHARED_LIBS ON)
 
-  # Add FetchContent_Declare here for rapids-cmake, rmm, and kvikio if a specific version or commit is needed.
+  FetchContent_Declare(
+    rapids-cmake
+    URL ${VELOX_rapids_cmake_SOURCE_URL}
+    URL_HASH ${VELOX_rapids_cmake_BUILD_SHA256_CHECKSUM}
+    UPDATE_DISCONNECTED 1
+  )
+
+  FetchContent_Declare(
+    rmm
+    URL ${VELOX_rmm_SOURCE_URL}
+    URL_HASH ${VELOX_rmm_BUILD_SHA256_CHECKSUM}
+    SOURCE_SUBDIR
+    cpp
+    UPDATE_DISCONNECTED 1
+  )
+
+  FetchContent_Declare(
+    kvikio
+    URL ${VELOX_kvikio_SOURCE_URL}
+    URL_HASH ${VELOX_kvikio_BUILD_SHA256_CHECKSUM}
+    SOURCE_SUBDIR
+    cpp
+    UPDATE_DISCONNECTED 1
+  )
 
   FetchContent_Declare(
     cudf
