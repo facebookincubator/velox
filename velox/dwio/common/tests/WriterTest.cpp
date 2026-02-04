5ca6091864ad95acb778698b2db52f78f94de16c
Ping Liu <ping.liu.ping@gmail.com>
2026-02-04 17:08:42 +0000

    Collect iceberg parquet data file stats
    Fix

--- velox/dwio/common/tests/WriterTest.cpp
+++ velox/dwio/common/tests/WriterTest.cpp
@@ -45,7 +45,9 @@ class MockWriter : public Writer {
 
   void abort() override {}
 
-  void close() override {}
+  std::unique_ptr<FileMetadata> close() override {
+    return nullptr;
+  }
 };
 
 TEST(WriterTest, stateString) {
