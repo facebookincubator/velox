a0da28f877487fa1819c3f0587a9640c4db886cc
Christian Zentgraf <czentgr@us.ibm.com>
2026-02-04 17:47:46 -0500

    feat(ci): Add ASAN and UBSAN build and CI job

--- velox/exec/Cursor.h
+++ velox/exec/Cursor.h
@@ -41,7 +41,7 @@ struct CursorParameters {
   int32_t numConcurrentSplitGroups{1};
 
   /// Optional, created if not present.
-  std::shared_ptr<core::QueryCtx> queryCtx;
+  std::shared_ptr<core::QueryCtx> queryCtx{nullptr};
 
   uint64_t bufferedBytes{512 * 1024};
 
@@ -49,14 +49,14 @@ struct CursorParameters {
   /// MultiThreadedTaskCursor. A new pool is created if not specified.
   ///
   /// Only used if serialExecution is false.
-  std::shared_ptr<memory::MemoryPool> outputPool;
+  std::shared_ptr<memory::MemoryPool> outputPool{nullptr};
 
   /// Ungrouped (by default) or grouped (bucketed) execution.
   core::ExecutionStrategy executionStrategy{
       core::ExecutionStrategy::kUngrouped};
 
   /// Contains leaf plan nodes that need to be executed in the grouped mode.
-  std::unordered_set<core::PlanNodeId> groupedExecutionLeafNodeIds;
+  std::unordered_set<core::PlanNodeId> groupedExecutionLeafNodeIds = {};
 
   /// Number of splits groups the task will be processing. Must be 1 for
   /// ungrouped execution.
