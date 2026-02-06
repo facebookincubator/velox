c47912774432a6961ed7da4e3409cb1020e4a8a8
Kevin Wilfong <kevinwilfong@meta.com>
2026-02-06 13:12:34 -0800

    refactor: CastKernel [2/11] Port cast to/from Date to CastKernel (#16274)
    Summary:
    
    This change is part of a stack to address the performance and usability issues 
    with CastExpr/CastHooks/CastPolicy and different engine semantics. Please 
    see https://github.com/facebookincubator/velox/issues/16087 for a fuller 
    description of the motivation. The stack will be landed as a unit, I've broken it 
    up to try to make it easier to review.
    
    This change introduces the castToDate and castFromDate functions to 
    CastKernel and implements them in PrestoCastKernel and SparkCastKernel.
    
    I have removed what I can of the relevant code in CastExpr and the CastHooks 
    to help with seeing mapping to where the new code came from, it was just 
    moved and slightly refactored. Note that I couldn't remove the 
    removeWhiteSpaces functions from the Hooks, but it is copied from there. 
    They will deleted from the Hooks once it's possible later in the stack.
    
    Differential Revision: D91834120

--- velox/expression/PrestoCastKernel.cpp
+++ velox/expression/PrestoCastKernel.cpp
@@ -16,8 +16,115 @@
 
 #include "velox/expression/PrestoCastKernel.h"
 
+#include "velox/expression/StringWriter.h"
+
 namespace facebook::velox::exec {
 
 PrestoCastKernel::PrestoCastKernel(const core::QueryConfig& config)
     : legacyCast_(config.isLegacyCast()) {}
+
+VectorPtr PrestoCastKernel::castFromDate(
+    const SelectivityVector& rows,
+    const BaseVector& input,
+    exec::EvalCtx& context,
+    const TypePtr& toType,
+    bool setNullInResultAtError) const {
+  const auto* inputFlatVector = input.as<SimpleVector<int32_t>>();
+
+  VectorPtr result;
+  initializeResultVector(rows, toType, context, result);
+
+  switch (toType->kind()) {
+    case TypeKind::VARCHAR: {
+      auto* resultFlatVector = result->as<FlatVector<StringView>>();
+      applyToSelectedNoThrowLocal(
+          rows, context, result, setNullInResultAtError, [&](int row) {
+            // TODO Optimize to avoid creating an intermediate string.
+            auto output = DATE()->toString(inputFlatVector->valueAt(row));
+            auto writer = exec::StringWriter(resultFlatVector, row);
+            writer.resize(output.size());
+            ::memcpy(writer.data(), output.data(), output.size());
+            writer.finalize();
+          });
+
+      return result;
+    }
+    case TypeKind::TIMESTAMP: {
+      static const int64_t kMillisPerDay{86'400'000};
+      const auto* timeZone =
+          getTimeZoneFromConfig(context.execCtx()->queryCtx()->queryConfig());
+      auto* resultFlatVector = result->as<FlatVector<Timestamp>>();
+      applyToSelectedNoThrowLocal(
+          rows, context, result, setNullInResultAtError, [&](int row) {
+            auto timestamp = Timestamp::fromMillis(
+                inputFlatVector->valueAt(row) * kMillisPerDay);
+            if (timeZone) {
+              timestamp.toGMT(*timeZone);
+            }
+            resultFlatVector->set(row, timestamp);
+          });
+
+      return result;
+    }
+    default:
+      VELOX_UNSUPPORTED(
+          "Cast from DATE to {} is not supported", toType->toString());
+  }
+}
+
+VectorPtr PrestoCastKernel::castToDate(
+    const SelectivityVector& rows,
+    const BaseVector& input,
+    exec::EvalCtx& context,
+    bool setNullInResultAtError) const {
+  const auto& fromType = input.type();
+
+  VectorPtr result;
+  initializeResultVector(rows, DATE(), context, result);
+  auto* resultFlatVector = result->as<FlatVector<int32_t>>();
+
+  switch (fromType->kind()) {
+    case TypeKind::VARCHAR: {
+      auto* inputVector = input.as<SimpleVector<StringView>>();
+      applyToSelectedNoThrowLocal(
+          rows, context, result, setNullInResultAtError, [&](int row) {
+            // Cast from string to date allows only complete ISO 8601
+            // formatted strings :
+            // [+-](YYYY-MM-DD).
+            const auto resultValue = util::fromDateString(
+                inputVector->valueAt(row), util::ParseMode::kPrestoCast);
+
+            if (resultValue.hasValue()) {
+              resultFlatVector->set(row, resultValue.value());
+            } else {
+              setError(
+                  input,
+                  context,
+                  *result,
+                  row,
+                  resultValue.error().message(),
+                  setNullInResultAtError);
+            }
+          });
+
+      return result;
+    }
+    case TypeKind::TIMESTAMP: {
+      auto* inputVector = input.as<SimpleVector<Timestamp>>();
+      const auto* timeZone =
+          getTimeZoneFromConfig(context.execCtx()->queryCtx()->queryConfig());
+      applyToSelectedNoThrowLocal(
+          rows, context, result, setNullInResultAtError, [&](int row) {
+            const auto days = util::toDate(inputVector->valueAt(row), timeZone);
+            resultFlatVector->set(row, days);
+          });
+
+      return result;
+    }
+    default:
+      VELOX_UNSUPPORTED(
+          "Cast from {} to DATE is not supported", fromType->toString());
+  }
+}
+
 } // namespace facebook::velox::exec
