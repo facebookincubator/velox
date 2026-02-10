d36084e18cbb0efe2af187b7c088cd8b9c0a902a
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2026-02-10 03:36:46 -0600

    clang format fix

--- velox/experimental/cudf/exec/CudfLocalPartition.cpp
+++ velox/experimental/cudf/exec/CudfLocalPartition.cpp
@@ -247,9 +247,7 @@ void CudfLocalPartition::addInput(RowVectorPtr input) {
           outputType_,
           partitionData.num_rows(),
           std::make_unique<cudf::table>(
-              partitionData,
-              stream,
-              cudf::get_current_device_resource_ref()),
+              partitionData, stream, cudf::get_current_device_resource_ref()),
           stream);
       enqueuePartition(i, partitionCudfVector);
     }
