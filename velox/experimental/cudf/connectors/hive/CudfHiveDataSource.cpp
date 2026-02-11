87503e571cb4fd403d04c414fb5736fb64fbe464
Muhammad Haseeb <mhaseeb@nvidia.com>
2026-02-11 03:47:57 +0000

    Temporarily disable split fields until Presto investigation

--- velox/experimental/cudf/connectors/hive/CudfHiveDataSource.cpp
+++ velox/experimental/cudf/connectors/hive/CudfHiveDataSource.cpp
@@ -573,7 +573,6 @@ void CudfHiveDataSource::setupCudfDataSourceAndOptions() {
   // Reader options
   readerOptions_ =
       cudf::io::parquet_reader_options::builder(std::move(sourceInfo))
-          .skip_bytes(split_->start)
           .use_pandas_metadata(cudfHiveConfig_->isUsePandasMetadata())
           .use_arrow_schema(cudfHiveConfig_->isUseArrowSchema())
           .allow_mismatched_pq_schemas(
@@ -581,10 +580,26 @@ void CudfHiveDataSource::setupCudfDataSourceAndOptions() {
           .timestamp_type(cudfHiveConfig_->timestampType())
           .build();
 
-  // Set num_bytes only if available
+  // TODO(mh): Temporarily disable these until we figure out why Presto-GPU
+  // passes incorrect values for split `start` and `length` fields
+#if 0
+  // Set skip_bytes and num_bytes if available
+  if (split_->start != 0) {
+    readerOptions_.set_skip_bytes(split_->start);
+  }
   if (split_->size() != std::numeric_limits<uint64_t>::max()) {
     readerOptions_.set_num_bytes(split_->size());
   }
+#else
+  VELOX_CHECK_EQ(
+      split_->start,
+      0,
+      "CudfHiveDataSource cannot process splits with non-zero offset");
+  VELOX_CHECK_EQ(
+      split_->size(),
+      std::numeric_limits<uint64_t>::max(),
+      "CudfHiveDataSource cannot process splits with finite length");
+#endif
 
   if (subfieldFilterExpr_ != nullptr) {
     readerOptions_.set_filter(*subfieldFilterExpr_);
