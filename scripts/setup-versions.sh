7df150ee23d815da612ee2851fd7514b0a7342ba
Suryadev Sahadevan Rajesh <suryadev@meta.com>
2026-02-12 12:59:50 -0800

    Fix license header: remaining directories (#16379)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/16379
    
    Update copyright header from "Facebook, Inc. and its affiliates" to "Meta Platforms, Inc. and affiliates" across remaining velox directories: public_tld, tpcds, tool, serializers, python, benchmarks, tpch, parse, duckdb, docs, examples, external, flag_definitions, and the top-level CMakeLists.txt.
    
    Differential Revision: D93129924

--- scripts/setup-versions.sh
+++ scripts/setup-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2034
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
