0db21f2f834cb829c365b1ae068e80c73447fa8d
Rui Mo <rui@apache.org>
2026-02-20 12:10:33 +0000

    Extend time type

--- velox/type/SimpleFunctionApi.h
+++ velox/type/SimpleFunctionApi.h
@@ -374,6 +374,11 @@ struct SimpleTypeTrait<Time> : public TypeTraits<TypeKind::BIGINT> {
   static constexpr const char* name = "TIME";
 };
 
+template <>
+struct SimpleTypeTrait<TimeMicroUtc> : public TypeTraits<TypeKind::BIGINT> {
+  static constexpr const char* name = "TIME MICRO UTC";
+};
+
 template <typename T, bool comparable, bool orderable>
 struct SimpleTypeTrait<Generic<T, comparable, orderable>> {
   static constexpr TypeKind typeKind = TypeKind::INVALID;
