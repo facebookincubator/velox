15b3b9115807f5062dec49fc400759e800415af3
Peter Enescu <enescu@meta.com>
2026-03-06 10:51:42 -0800

    build: Use FBThrift instead of Apache Thrift (#16019)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/16019
    
    Removes external Thrift dependency primarily used in Parquet as noted by [#13175](https://github.com/facebookincubator/velox/issues/13175).
    
    *Issues noted below for which we'd like to move to FBThrift from Apache Thrift.*
    
    FBThrift and Apache Thrift has many incompatible API changes:
    
    * `XXX.__isset.YYY` -> `XXX.YYY().has_value()` (or `bool(XXX.YYY())`): The latter implicit `bool(...)` syntax is used in this PR
    * `XXX.__isset.YYY = true` -> `XXX.YYY().ensure()`
    * `XXX.YYY` -> `XXX.YYY().value()` (or `*XXX.YYY()`): The latter `*...` syntax is used in this PR
    * `XXX.__set_YYY(value)` -> `XXX.YYY() = value`
    * `thrift::ThriftTransport`/`TCompactProcotolT` -> `folly::IOBuf`/`CompactProtocolReader`
    * FBThrift doesn't support `optional` and default value that is used by `DataPageHeaderV2.is_compressed`: We need to use the default value manually in our code
    * FBThrift raises an exception when we access to an optional field that isn't set (Apache Thrift doesn't have the check)
    
    There are many changes in this PR but there is no logic change and no refactoring. This just converts API usage for FBThrift.
    
    (Copy of [#14942](https://github.com/facebookincubator/velox/pull/14942) from kou due to some push permission issues.)
    
    Differential Revision: D90723225

--- velox/dwio/parquet/writer/arrow/tests/MetadataTest.cpp
+++ velox/dwio/parquet/writer/arrow/tests/MetadataTest.cpp
@@ -290,8 +290,8 @@ TEST(Metadata, TestBuildAccess) {
     ASSERT_EQ(2, rg2Column1->encodingStats().size());
     ASSERT_EQ(3, rg2Column2->encodingStats().size());
 
-    // Test FileMetaData::set_file_path.
-    ASSERT_TRUE(rg2Column1->filePath().empty());
+    // Test FileMetaData::set_file_path
+    ASSERT_FALSE(rg2Column1->has_file_path());
     fAccessors[loopIndex]->setFilePath("/foo/bar/bar.parquet");
     ASSERT_EQ("/foo/bar/bar.parquet", rg2Column1->filePath());
   }
