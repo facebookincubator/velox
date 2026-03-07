2dff55a1b6477317bd90a92053d6d0d850f1e773
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2026-03-06 16:52:14 -0800

    feat(cudf): Add varchar support to cuDF min/max aggregation registry (#16651)
    Summary:
    The cuDF aggregation registry was missing varchar in minMaxSignatures, causing MAX(varchar) to fall back to Velox CPU.
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/16651
    
    Reviewed By: jainxrohit
    
    Differential Revision: D95591905
    
    Pulled By: peterenescu
    
    fbshipit-source-id: 6531218da66d390b42a93eee781eb57b18d35dad

--- velox/experimental/cudf/exec/CudfHashAggregation.cpp
+++ velox/experimental/cudf/exec/CudfHashAggregation.cpp
@@ -1435,6 +1435,10 @@ bool registerStepAwareBuiltinAggregationFunctions(const std::string& prefix) {
       FunctionSignatureBuilder()
           .returnType("double")
           .argumentType("double")
+          .build(),
+      FunctionSignatureBuilder()
+          .returnType("varchar")
+          .argumentType("varchar")
           .build()};
 
   registerAggregationFunctionForStep(
