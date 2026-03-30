57bccd844eb638e7a3807df524ec099ac3faeacb
Chengcheng Jin <Chengcheng.jin@ibm.com>
2026-03-30 12:30:03 +0100

    fix compile

--- velox/exec/tests/utils/TestIndexStorageConnector.cpp
+++ velox/exec/tests/utils/TestIndexStorageConnector.cpp
@@ -215,7 +215,7 @@ void extractColumns(
       child = BaseVector::create(resultTypes[resultChannel], rows.size(), pool);
     }
     child->resize(rows.size());
-    table->extractColumn(rows, projection.inputChannel, child, false);
+    table->extractColumn(rows, projection.inputChannel, child);
   }
 }
 } // namespace
