d46810108f71ee8a9d7bdeb057d09dd3953dba8c
Simon Eves <seves@nvidia.com>
2026-04-07 14:30:46 -0700

    Ignore config flag when configuring join filters, per @shrshi and @mattgara

--- velox/experimental/cudf/exec/CudfHashJoin.cpp
+++ velox/experimental/cudf/exec/CudfHashJoin.cpp
@@ -425,8 +425,13 @@ CudfHashJoinProbe::CudfHashJoinProbe(
     // simplify expression
     exec::ExprSet exprs({joinNode_->filter()}, operatorCtx_->execCtx());
     VELOX_CHECK_EQ(exprs.exprs().size(), 1);
-    useAstFilter_ = CudfConfig::getInstance().astExpressionEnabled &&
-        !containsDecimalType(exprs.exprs()[0], false);
+
+    // For now we disable AST-based filtering (and force precomputation)
+    // if the filter expression contains decimal types, using the same
+    // shallow search as used for regular expression evaluation.
+    if (containsDecimalType(exprs.exprs()[0], false)) {
+      useAstFilter_ = false;
+    }
 
     // Validate AST filtering for this join type now to avoid run-time error.
     if (joinNode_->isRightSemiFilterJoin() ||
