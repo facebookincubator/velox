d84e8062e773c1551b228017a5621ec92d51012d
Yuan <yuanzhou@apache.org>
2026-04-10 17:28:22 +0100

    fix iceberg reader
    Signed-off-by: Yuan <yuanzhou@apache.org>
    Signed-off-by: Mohammad Linjawi <linjawi.mohammad@gmail.com>
    (cherry picked from commit 4cd49ab99ea8ea55301a68172832906f0eff416e)
    (cherry picked from commit 4fc58bec6fbe05a26a7700e9bc3d24948874b47c)

--- velox/connectors/hive/HiveSplitReader.cpp
+++ velox/connectors/hive/HiveSplitReader.cpp
@@ -19,8 +19,68 @@
 #include "velox/connectors/hive/FileConfig.h"
 #include "velox/connectors/hive/HiveConnectorSplit.h"
 #include "velox/connectors/hive/HiveConnectorUtil.h"
+#include "velox/connectors/hive/iceberg/IcebergSplit.h"
+#include "velox/connectors/hive/iceberg/IcebergSplitReader.h"
 
 namespace facebook::velox::connector::hive {
+
+std::unique_ptr<FileSplitReader> HiveSplitReader::create(
+    const std::shared_ptr<const HiveConnectorSplit>& hiveSplit,
+    const FileTableHandlePtr& tableHandle,
+    const std::unordered_map<std::string, FileColumnHandlePtr>* partitionKeys,
+    const ConnectorQueryCtx* connectorQueryCtx,
+    const std::shared_ptr<const FileConfig>& fileConfig,
+    const RowTypePtr& readerOutputType,
+    const std::shared_ptr<io::IoStatistics>& dataIoStats,
+    const std::shared_ptr<io::IoStatistics>& metadataIoStats,
+    const std::shared_ptr<IoStats>& ioStats,
+    FileHandleFactory* fileHandleFactory,
+    folly::Executor* ioExecutor,
+    const std::shared_ptr<common::ScanSpec>& scanSpec,
+    const std::unordered_map<std::string, FileColumnHandlePtr>* infoColumns,
+    std::vector<column_index_t> bucketChannels,
+    const common::SubfieldFilters* subfieldFiltersForValidation) {
+  // Create the SplitReader based on hiveSplit->customSplitInfo["table_format"]
+  if (hiveSplit->customSplitInfo.count("table_format") > 0 &&
+      hiveSplit->customSplitInfo.at("table_format") == "hive-iceberg") {
+    auto icebergSplit =
+        std::dynamic_pointer_cast<const iceberg::HiveIcebergSplit>(hiveSplit);
+    VELOX_CHECK_NOT_NULL(
+        icebergSplit,
+        "Expected HiveIcebergSplit for table_format=hive-iceberg");
+    return std::make_unique<iceberg::IcebergSplitReader>(
+        icebergSplit,
+        tableHandle,
+        partitionKeys,
+        connectorQueryCtx,
+        fileConfig,
+        readerOutputType,
+        dataIoStats,
+        metadataIoStats,
+        ioStats,
+        fileHandleFactory,
+        ioExecutor,
+        scanSpec,
+        std::make_shared<ColumnHandleMap>());
+  } else {
+    return std::make_unique<HiveSplitReader>(
+        hiveSplit,
+        tableHandle,
+        partitionKeys,
+        connectorQueryCtx,
+        fileConfig,
+        readerOutputType,
+        dataIoStats,
+        metadataIoStats,
+        ioStats,
+        fileHandleFactory,
+        ioExecutor,
+        scanSpec,
+        infoColumns,
+        std::move(bucketChannels),
+        subfieldFiltersForValidation);
+  }
+}
 
 HiveSplitReader::HiveSplitReader(
     const std::shared_ptr<const HiveConnectorSplit>& hiveSplit,
