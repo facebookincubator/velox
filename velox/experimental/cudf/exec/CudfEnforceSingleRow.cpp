f469b7e6a95ae5b593ef3029bb3cad6ea03fdc1a
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2026-04-13 11:54:10 -0700

    Update velox/experimental/cudf/exec/CudfEnforceSingleRow.cpp
    Co-authored-by: Devavret Makkar <devavret@users.noreply.github.com>

--- velox/experimental/cudf/exec/CudfEnforceSingleRow.cpp
+++ velox/experimental/cudf/exec/CudfEnforceSingleRow.cpp
@@ -95,7 +95,7 @@ RowVectorPtr CudfEnforceSingleRow::getOutput() {
     return nullptr;
   }
 
-  return std::move(input_);
+  return std::exchange(input_, nullptr);
 }
 
 bool CudfEnforceSingleRow::isFinished() {
