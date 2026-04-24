f98a3d18501ca12e3637a6728fa4271e76d7b9dc
Simon Eves <seves@nvidia.com>
2026-04-24 13:17:57 -0700

    Reinstate agg registration changes

--- velox/experimental/cudf/exec/AggregationRegistry.cpp
+++ velox/experimental/cudf/exec/AggregationRegistry.cpp
@@ -82,6 +82,38 @@ void registerCommonAggregationFunctions(
           .argumentType("double")
           .build()};
 
+  // Decimal sum signatures.
+  auto decimalSumSingle = std::vector<exec::FunctionSignaturePtr>{
+      FunctionSignatureBuilder()
+          .integerVariable("a_precision")
+          .integerVariable("a_scale")
+          .returnType("decimal(38, a_scale)")
+          .argumentType("decimal(a_precision, a_scale)")
+          .build()};
+  auto decimalSumPartial = std::vector<exec::FunctionSignaturePtr>{
+      FunctionSignatureBuilder()
+          .integerVariable("a_precision")
+          .integerVariable("a_scale")
+          .returnType("varbinary")
+          .argumentType("decimal(a_precision, a_scale)")
+          .build()};
+  auto decimalSumFinal = std::vector<exec::FunctionSignaturePtr>{
+      FunctionSignatureBuilder()
+          .integerVariable("a_scale")
+          .returnType("decimal(38, a_scale)")
+          .argumentType("varbinary")
+          .build()};
+  auto decimalSumIntermediate =
+      std::vector<exec::FunctionSignaturePtr>{FunctionSignatureBuilder()
+                                                  .returnType("varbinary")
+                                                  .argumentType("varbinary")
+                                                  .build()};
+
+  sumSingleSignatures.insert(
+      sumSingleSignatures.end(),
+      decimalSumSingle.begin(),
+      decimalSumSingle.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "sum",
@@ -109,6 +141,12 @@ void registerCommonAggregationFunctions(
           .returnType("double")
           .argumentType("double")
           .build()};
+
+  sumPartialSignatures.insert(
+      sumPartialSignatures.end(),
+      decimalSumPartial.begin(),
+      decimalSumPartial.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "sum",
@@ -125,16 +163,27 @@ void registerCommonAggregationFunctions(
           .argumentType("double")
           .build()};
 
+  auto sumFinalSignatures = sumFinalIntermediateSignatures;
+  sumFinalSignatures.insert(
+      sumFinalSignatures.end(), decimalSumFinal.begin(), decimalSumFinal.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "sum",
       core::AggregationNode::Step::kFinal,
-      sumFinalIntermediateSignatures);
+      sumFinalSignatures);
+
+  auto sumIntermediateSignatures = sumFinalIntermediateSignatures;
+  sumIntermediateSignatures.insert(
+      sumIntermediateSignatures.end(),
+      decimalSumIntermediate.begin(),
+      decimalSumIntermediate.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "sum",
       core::AggregationNode::Step::kIntermediate,
-      sumFinalIntermediateSignatures);
+      sumIntermediateSignatures);
 
   auto countSinglePartialSignatures = std::vector<exec::FunctionSignaturePtr>{
       FunctionSignatureBuilder()
@@ -226,6 +275,12 @@ void registerCommonAggregationFunctions(
       FunctionSignatureBuilder()
           .returnType("varchar")
           .argumentType("varchar")
+          .build(),
+      FunctionSignatureBuilder()
+          .integerVariable("p")
+          .integerVariable("s")
+          .returnType("decimal(p,s)")
+          .argumentType("decimal(p,s)")
           .build()};
 
   registerAggregationFunctionForStep(
@@ -288,6 +343,39 @@ void registerCommonAggregationFunctions(
           .argumentType("double")
           .build()};
 
+  // Decimal avg signatures.
+  auto decimalAvgSingle = std::vector<exec::FunctionSignaturePtr>{
+      FunctionSignatureBuilder()
+          .integerVariable("a_precision")
+          .integerVariable("a_scale")
+          .returnType("decimal(a_precision, a_scale)")
+          .argumentType("decimal(a_precision, a_scale)")
+          .build()};
+  auto decimalAvgPartial = std::vector<exec::FunctionSignaturePtr>{
+      FunctionSignatureBuilder()
+          .integerVariable("a_precision")
+          .integerVariable("a_scale")
+          .returnType("varbinary")
+          .argumentType("decimal(a_precision, a_scale)")
+          .build()};
+  auto decimalAvgFinal = std::vector<exec::FunctionSignaturePtr>{
+      FunctionSignatureBuilder()
+          .integerVariable("a_precision")
+          .integerVariable("a_scale")
+          .returnType("decimal(a_precision, a_scale)")
+          .argumentType("varbinary")
+          .build()};
+  auto decimalAvgIntermediate =
+      std::vector<exec::FunctionSignaturePtr>{FunctionSignatureBuilder()
+                                                  .returnType("varbinary")
+                                                  .argumentType("varbinary")
+                                                  .build()};
+
+  avgSingleSignatures.insert(
+      avgSingleSignatures.end(),
+      decimalAvgSingle.begin(),
+      decimalAvgSingle.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "avg",
@@ -315,17 +403,28 @@ void registerCommonAggregationFunctions(
           .returnType("row(double,bigint)")
           .argumentType("double")
           .build()};
+
+  avgPartialSignatures.insert(
+      avgPartialSignatures.end(),
+      decimalAvgPartial.begin(),
+      decimalAvgPartial.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "avg",
       core::AggregationNode::Step::kPartial,
       avgPartialSignatures);
 
-  auto avgFinalSignatures = std::vector<exec::FunctionSignaturePtr>{
+  auto avgFinalIntermediateSignatures = std::vector<exec::FunctionSignaturePtr>{
       FunctionSignatureBuilder()
           .returnType("double")
           .argumentType("row(double,bigint)")
           .build()};
+
+  auto avgFinalSignatures = avgFinalIntermediateSignatures;
+  avgFinalSignatures.insert(
+      avgFinalSignatures.end(), decimalAvgFinal.begin(), decimalAvgFinal.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "avg",
@@ -337,6 +436,16 @@ void registerCommonAggregationFunctions(
           .returnType("row(double,bigint)")
           .argumentType("row(double,bigint)")
           .build()};
+
+  // WHY DOES SUM NOT HAVE THE EQUIVALENT OF THE ABOVE?
+  // THE ABOVE THEN CLASHES WITH BELOW
+  // @mattgara HELP! :)
+  // auto avgIntermediateSignatures = avgFinalIntermediateSignatures;
+  avgIntermediateSignatures.insert(
+      avgIntermediateSignatures.end(),
+      decimalAvgIntermediate.begin(),
+      decimalAvgIntermediate.end());
+
   registerAggregationFunctionForStep(
       registry,
       prefix + "avg",
