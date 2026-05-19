9af20692bab2c0e64827bc319a8bb753eb1121e7
Muhammad Haseeb <mhaseeb@nvidia.com>
2026-05-19 22:15:21 +0000

    Style fix

--- velox/experimental/cudf/connectors/hive/CudfSplitReader.cpp
+++ velox/experimental/cudf/connectors/hive/CudfSplitReader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
