5508f4376f49b6f2bf92530024d14a8244023821
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-06-03 18:21:11 -0700

    revert

--- velox/experimental/cudf/exec/CudfHashJoin.cpp
+++ velox/experimental/cudf/exec/CudfHashJoin.cpp
@@ -418,7 +418,9 @@ void CudfHashJoinProbe::initialize() {
   exec::ExprSet exprs({joinNode_->filter()}, operatorCtx_->execCtx());
   VELOX_CHECK_EQ(exprs.exprs().size(), 1);
 
-  // if the filter expression contains decimal types anywhere in its subtree.
+  // For now we disable AST-based filtering (and force precomputation)
+  // if the filter expression contains decimal types, using the same
+  // shallow search as used for regular expression evaluation.
   if (containsDecimalType(exprs.exprs()[0], false)) {
     useAstFilter_ = false;
   }
