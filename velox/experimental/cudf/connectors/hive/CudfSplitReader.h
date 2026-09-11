9e8bcd94c77cc6483800ecf7f4be14ed2244f4d2
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-09-11 09:26:52 -0700

    Update velox/experimental/cudf/connectors/hive/CudfSplitReader.h
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- velox/experimental/cudf/connectors/hive/CudfSplitReader.h
+++ velox/experimental/cudf/connectors/hive/CudfSplitReader.h
@@ -133,7 +133,7 @@ class CudfSplitReader : public NvtxHelper {
   std::shared_ptr<io::IoStatistics> ioStatistics_;
   std::shared_ptr<IoStats> ioStats_;
 
-  cuda::stream_ref stream_{cudaStream_t{nullptr}};
+  cuda::stream_ref stream_{cudaStream_t{cudaStreamDefault}};
 
   // Parquet metadata(s) for the current split(s).
   std::vector<cudf::io::parquet::FileMetaData> fileMetaData_;
