bf3ccfee05b2110318084cb09aac72c15a900689
Maria Basmanova <mbasmanova@meta.com>
2026-09-19 18:26:06 -0400

    fix(hive): Register Nimble direct input session property
    Summary: `SET SESSION hive.nimble_direct_buffered_input_enabled = true` failed with `Unknown session property` even though the Hive connector supported the setting. Register the property so session overrides reach the connector.
    
    Differential Revision: D120843873
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/19105

--- velox/connectors/hive/FileConfig.cpp
+++ velox/connectors/hive/FileConfig.cpp
@@ -46,6 +46,7 @@ const std::vector<config::ConfigProperty>& FileConfig::registeredProperties() {
     VELOX_HIVE_CONFIG_REGISTER(kCacheIndexSession);
     VELOX_HIVE_CONFIG_REGISTER(kPinIndexSession);
     VELOX_HIVE_CONFIG_REGISTER(kSelectiveNimbleReaderEnabledSession);
+    VELOX_HIVE_CONFIG_REGISTER(kNimbleDirectBufferedInputEnabledSession);
     VELOX_HIVE_CONFIG_REGISTER(kDirectBufferedInputSharedAllocationSession);
     VELOX_HIVE_CONFIG_REGISTER(kMaxCoalescedDistanceSession);
     VELOX_HIVE_CONFIG_REGISTER(kParallelUnitLoadCountSession);
